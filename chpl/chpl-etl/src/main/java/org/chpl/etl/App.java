913b3daa6d25e42894fbf87a2ed3d95e887817ce
Andrew Larned <alarned@ainq.com>
2015-04-29 12:43:20 -0400

    Programmatically execute provided graph
     * Added two new libraries not previously needed
     * Starting point graph in resources
     * Removed OBE parameter file

--- chpl/chpl-etl/src/main/java/org/chpl/etl/App.java
+++ chpl/chpl-etl/src/main/java/org/chpl/etl/App.java
@@ -1,11 +1,17 @@
 package org.chpl.etl;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.Future;
 
+import org.jetel.graph.Result;
 import org.jetel.graph.TransformationGraph;
 import org.jetel.graph.TransformationGraphXMLReaderWriter;
 import org.jetel.graph.runtime.EngineInitializer;
 import org.jetel.graph.runtime.GraphRuntimeContext;
+import org.jetel.main.runGraph;
 
 /**
  * Hello world!
@@ -29,6 +35,40 @@ public class App
         //create transformation graph from xml file
         TransformationGraph graph= new TransformationGraph();
         TransformationGraphXMLReaderWriter graphReader=new TransformationGraphXMLReaderWriter(runtimeContext);
+        
+        FileInputStream inGraph = null;
+        try{
+        	inGraph=new FileInputStream(App.class.getResource("/chpl.grf").toURI().getPath());
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+            return;
+        } catch (URISyntaxException e) {
+			e.printStackTrace();
+			return;
+		}
+
+        try{
+            graph = graphReader.read(inGraph);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return;
+        }
+
+        graph.dumpGraphConfiguration();
+        
+        //execute graph
+        Future<Result> result;
+        try{
+            result = runGraph.executeGraph(graph, runtimeContext);
+            if (!result.get().equals(Result.FINISHED_OK)){
+                System.out.println("Failed graph execution!\n");
+                return;
+            }
+        }catch (Exception e) {
+            System.out.println("Failed graph execution!\n" + e.getMessage());
+            return;
+        }
 
 	}
 }
