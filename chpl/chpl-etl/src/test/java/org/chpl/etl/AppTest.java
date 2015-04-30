3e20cdda723ab1ecf32865b2eae5d213a8eeae02
Andrew Larned <alarned@ainq.com>
2015-04-30 08:50:43 -0400

    Changed to JUnit 4

--- chpl/chpl-etl/src/test/java/org/chpl/etl/AppTest.java
+++ chpl/chpl-etl/src/test/java/org/chpl/etl/AppTest.java
@@ -1,39 +1,18 @@
 package org.chpl.etl;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+	
+    @Test
+    @Ignore
+    public void test() {
+    	fail("Not yet implemented");
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigorous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    
 }
