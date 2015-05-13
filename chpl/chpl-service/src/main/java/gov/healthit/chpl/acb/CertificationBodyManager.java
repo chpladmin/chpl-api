d134d09e5b52604122ee35cd7f992a051079a1f7
chris-watson <cxwatson@gmail.com>
2015-05-13 12:26:10 -0400

    Work on JPA, Moved config to rest-servlet.xml

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/acb/CertificationBodyManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/acb/CertificationBodyManager.java
@@ -10,23 +10,22 @@ import org.springframework.security.acls.model.Sid;
 
 public interface CertificationBodyManager {
 	
-	@PreAuthorize("hasPermission(#acb, admin)")
+	
 	public void addPermission(CertificationBody acb, Sid recipient, Permission permission);
 	
-	@PreAuthorize("hasPermission(#acb, admin)")
+	
 	public void deletePermission(CertificationBody acb, Sid recipient, Permission permission);
 	
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	
 	public void create(CertificationBody acb);
 	
-	@PreAuthorize("hasPermission(#acb, 'delete') or hasPermission(#acb, admin)")
+	
 	public void delete(CertificationBody acb);
 	
-	@PostFilter("hasPermission(filterObject, 'read') or hasPermission(filterObject, admin)")
+	
 	public List<CertificationBody> getAll();
 	
-	@PreAuthorize("hasPermission(#id, 'gov.healthit.chpl.CertificationBody', read) or "
-			+ "hasPermission(#id, 'gov.healthit.chpl.CertificationBody', admin)")
+
 	public CertificationBody getById(Long id);
 	
 }
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/acb/CertificationBodyManagerImpl.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/acb/CertificationBodyManagerImpl.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ApplicationObjectSupport;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.acls.domain.BasePermission;
 import org.springframework.security.acls.domain.ObjectIdentityImpl;
 import org.springframework.security.acls.domain.PrincipalSid;
@@ -19,9 +21,10 @@ import org.springframework.security.acls.model.Sid;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Service
 public class CertificationBodyManagerImpl extends ApplicationObjectSupport implements CertificationBodyManager {
 
 	@Autowired
@@ -31,6 +34,7 @@ public class CertificationBodyManagerImpl extends ApplicationObjectSupport imple
 	
 	
 	@Transactional
+	@PreAuthorize("hasPermission(#acb, admin)")
 	public void addPermission(CertificationBody acb, Sid recipient, Permission permission) {
 		MutableAcl acl;
 		ObjectIdentity oid = new ObjectIdentityImpl(CertificationBody.class, acb.getId());
@@ -54,6 +58,7 @@ public class CertificationBodyManagerImpl extends ApplicationObjectSupport imple
 	}
 
 	@Transactional
+	//@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public void create(CertificationBody acb) {
 		// Create the ACB itself
 		certificationBodyDAO.create(acb);
@@ -69,6 +74,7 @@ public class CertificationBodyManagerImpl extends ApplicationObjectSupport imple
 	}
 
 	@Transactional
+	@PreAuthorize("hasPermission(#acb, 'delete') or hasPermission(#acb, admin)")
 	public void delete(CertificationBody acb) {
 		
 		certificationBodyDAO.delete(acb.getId());
@@ -83,6 +89,7 @@ public class CertificationBodyManagerImpl extends ApplicationObjectSupport imple
 	}
 
 	@Transactional
+	@PreAuthorize("hasPermission(#acb, admin)")
 	public void deletePermission(CertificationBody acb, Sid recipient, Permission permission) {
 		ObjectIdentity oid = new ObjectIdentityImpl(CertificationBody.class, acb.getId());
 		MutableAcl acl = (MutableAcl) mutableAclService.readAclById(oid);
@@ -105,6 +112,7 @@ public class CertificationBodyManagerImpl extends ApplicationObjectSupport imple
 	}
 
 	@Transactional(readOnly = true)
+	@PostFilter("hasPermission(filterObject, 'read') or hasPermission(filterObject, admin)")
 	public List<CertificationBody> getAll() {
 		logger.debug("Returning all acbs");
 
@@ -112,6 +120,8 @@ public class CertificationBodyManagerImpl extends ApplicationObjectSupport imple
 	}
 
 	@Transactional(readOnly = true)
+	@PreAuthorize("hasPermission(#id, 'gov.healthit.chpl.CertificationBody', read) or "
+			+ "hasPermission(#id, 'gov.healthit.chpl.CertificationBody', admin)")
 	public CertificationBody getById(Long id) {
 		if (logger.isDebugEnabled()) {
 			logger.debug("Returning acb with id: " + id);
