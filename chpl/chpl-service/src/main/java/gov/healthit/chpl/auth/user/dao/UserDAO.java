0592a378578b83146b9337cac09c58f9b39e7ad6
chris-watson <cxwatson@gmail.com>
2015-05-18 17:15:37 -0400

    work on User DAO

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/auth/user/dao/UserDAO.java
@@ -0,0 +1,22 @@
+package gov.healthit.chpl.auth.user.dao;
+import gov.healthit.chpl.auth.user.User;
+
+import java.util.List;
+
+public interface UserDAO {
+	
+	public void create(User user);
+
+	public void deactivate(Long userId);
+	
+	public void deactivate(String uname);
+	
+	public List<User> findAll();
+
+	public User getById(Long userId);
+	
+	public User getByName(String uname);
+
+	public void update(User user);
+}
+
---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/auth/user/dao/impl/UserDAOImpl.java
@@ -0,0 +1,82 @@
+package gov.healthit.chpl.auth.user.dao.impl;
+
+
+import gov.healthit.chpl.auth.user.User;
+import gov.healthit.chpl.auth.user.dao.UserDAO;
+
+import java.util.List;
+
+import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository(value="userDAO")
+public class UserDAOImpl extends BaseDAOImpl implements UserDAO {
+	
+	@Transactional
+	@Override
+	public void create(User user) {
+		
+		entityManager.persist(user);
+		
+	}
+	
+	@Transactional
+	@Override
+	public void update(User user) {
+		
+		entityManager.merge(user);
+		
+	}
+	
+	@Transactional
+	@Override
+	public void deactivate(Long userId) {
+		/*TODO: De-activate rather than deleting
+		Query query = entityManager.createNativeQuery("delete FROM users WHERE id = :acbid");
+		query.setParameter("acbid", userId);
+		query.executeUpdate();
+		*/
+	}
+	
+	@Override
+	public void deactivate(String uname) {
+		// TODO Auto-generated method stub
+	}
+	
+	
+	
+	@Override
+	public List<User> findAll() {
+		
+		List<User> result = entityManager.createQuery( "from User", User.class ).getResultList();
+		
+		return result;
+	}
+
+	@Override
+	public User getById(Long userId) {
+		
+		User user = null;
+		
+		Query query = entityManager.createQuery( "from User where id = :userid", User.class );
+		query.setParameter("userid", userId);
+		List<User> result = query.getResultList();
+		user = result.get(0);
+		return user;
+	}
+
+	@Override
+	public User getByName(String uname) {
+		
+		User user = null;
+		
+		Query query = entityManager.createQuery( "from User where uname = :uname", User.class );
+		query.setParameter("uname", uname);
+		List<User> result = query.getResultList();
+		user = result.get(0);
+		return user;
+	}
+	
+}
