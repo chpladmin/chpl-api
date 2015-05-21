124714865664764a6a9615705ba9e4747c036e58
Andrew Larned <andrew.larned@gmail.com>
2015-05-21 08:59:42 -0400

    Combined graphs into single graph

--- chpl/chpl-etl/src/main/java/org/chpl/etl/App.java
+++ chpl/chpl-etl/src/main/java/org/chpl/etl/App.java
@@ -36,11 +36,7 @@ public class App
 			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, e);
 		}
 		
-		etlGraph.setGraph("/graphs/openchpl_checksum_analysis.grf");
-        etlGraph.execute();
-		etlGraph.setGraph("/graphs/openchpl_insert_vendor_and_product.grf");
-        etlGraph.execute();
-		etlGraph.setGraph("/graphs/openchpl_insert_certified_product.grf");
-        etlGraph.execute();
+		etlGraph.setGraph("/graphs/openchpl_etl.grf");
+		etlGraph.execute();
 	}
 }
