26116c6a60e4c7d9626e781b9f823eb07a9cd8c7
chris-watson <cxwatson@gmail.com>
2015-06-02 15:18:43 -0400

    Added CHPLAuthenticator, a CHPL-specific authenticator, which will return a list of permissions beyond a user's roles

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/security/CHPLAuthenticator.java
@@ -0,0 +1,54 @@
+package gov.healthit.chpl.security;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import gov.healthit.chpl.auth.authentication.Claim;
+import gov.healthit.chpl.auth.authentication.UserAuthenticator;
+import gov.healthit.chpl.auth.jwt.JWTAuthor;
+import gov.healthit.chpl.auth.jwt.JWTCreationException;
+import gov.healthit.chpl.auth.user.User;
+import gov.healthit.chpl.auth.user.UserManager;
+
+
+
+public class CHPLAuthenticator extends UserAuthenticator {
+
+	@Autowired
+	private JWTAuthor jwtAuthor;
+	
+	@Autowired
+	private UserManager userManager;
+	
+	@Override
+	public String getJWT(User user) throws JWTCreationException {
+		
+		String jwt = null;
+		
+		Map<String, List<String>> claims = new HashMap<String, List<String>>();
+		List<String> roleStrings = new ArrayList<String>();
+		
+		for (Claim role : user.getClaims()){
+			roleStrings.add(role.getAuthority());
+		}
+		claims.put("Authorities", roleStrings);
+		
+		
+		
+		jwt = jwtAuthor.createJWT(user.getSubjectName(), claims);
+		return jwt;
+		
+	}
+	
+	/*
+	private List<String> getPermissions(User user){
+		
+		//userManager
+		
+	}
+	*/
+}
