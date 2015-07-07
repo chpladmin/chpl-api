95277b9141b4cadc21932599cbbb15b8189be4d0
Andrew Larned <andrew.larned@gmail.com>
2015-07-07 09:40:51 -0400

    ETL processes CHPL file as of July 2 version

--- chpl/chpl-etl/src/main/java/org/chpl/etl/App.java
+++ chpl/chpl-etl/src/main/java/org/chpl/etl/App.java
@@ -45,14 +45,14 @@ public class App
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, e);
         }
 
-//        etlGraph.setGraph("/graphs/openchpl_etl.grf");
-//        etlGraph.execute();
+        etlGraph.setGraph("/graphs/openchpl_etl.grf");
+        etlGraph.execute();
 
-        etlGraph.setGraph("/graphs/openchpl_checksum_analysis.grf");
-        etlGraph.execute();
-        etlGraph.setGraph("/graphs/openchpl_insert_vendor_and_product.grf");
-        etlGraph.execute();
-        etlGraph.setGraph("/graphs/openchpl_insert_certified_product.grf");
-        etlGraph.execute();
+//        etlGraph.setGraph("/graphs/openchpl_checksum_analysis.grf");
+//        etlGraph.execute();
+//        etlGraph.setGraph("/graphs/openchpl_insert_vendor_and_product.grf");
+//        etlGraph.execute();
+//        etlGraph.setGraph("/graphs/openchpl_insert_certified_product.grf");
+//        etlGraph.execute();
     }
 }
