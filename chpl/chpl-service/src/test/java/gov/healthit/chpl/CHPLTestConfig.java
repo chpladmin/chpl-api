e7ffceeb2e7155f594012c75b3ae045afad9911b
chris-watson <cxwatson@gmail.com>
2015-08-13 17:19:32 -0400

    Work on embedded database for test

--- chpl/chpl-service/src/test/java/gov/healthit/chpl/CHPLTestConfig.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/CHPLTestConfig.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
@@ -37,8 +42,19 @@ public class CHPLTestConfig  {
 		}
 	}
 	
+	
 	@Bean
-	public org.springframework.orm.jpa.LocalEntityManagerFactoryBean entityManagerFactory(){
+	public DataSource dataSource() {
+
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).addScript("db/sql/create-db.sql").addScript("db/sql/insert-data.sql").build();
+		return db;
+
+	}
+	
+	
+	@Bean
+	public org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean entityManagerFactory(){
 		
 		if (props == null){
 			try {
