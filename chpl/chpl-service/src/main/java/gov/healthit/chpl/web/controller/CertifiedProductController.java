b356502fa672003697e811a5414ef5f7ceca7dd3
katy.ekey <kekey@ainq.com>
2015-09-09 11:54:03 -0400

    start update certified product work

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/web/controller/CertifiedProductController.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/web/controller/CertifiedProductController.java
@@ -8,12 +8,14 @@ import gov.healthit.chpl.dao.EntityRetrievalException;
 import gov.healthit.chpl.domain.CertifiedProduct;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.domain.ProductVersion;
+import gov.healthit.chpl.domain.UpdateCertifiedProductRequest;
 import gov.healthit.chpl.dto.CertifiedProductDTO;
 import gov.healthit.chpl.dto.ProductVersionDTO;
 import gov.healthit.chpl.manager.CertifiedProductDetailsManager;
 import gov.healthit.chpl.manager.CertifiedProductManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,5 +53,31 @@ public class CertifiedProductController {
 			}
 		}
 		return products;
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.POST,
+			produces="application/json; charset=utf-8")
+	public @ResponseBody CertifiedProduct updateCertifiedProduct(@RequestBody(required=true) UpdateCertifiedProductRequest updateRequest) 
+		throws EntityRetrievalException {
+		
+		CertifiedProductDTO toUpdate = new CertifiedProductDTO();
+		toUpdate.setId(updateRequest.getId());
+		toUpdate.setTestingLabId(updateRequest.getTestingLabId());
+		toUpdate.setCertificationBodyId(updateRequest.getCertificationBodyId());
+		toUpdate.setPracticeTypeId(updateRequest.getPracticeTypeId());
+		toUpdate.setProductClassificationTypeId(updateRequest.getProductClassificationTypeId());
+		toUpdate.setCertificationStatusId(updateRequest.getCertificationStatusId());
+		toUpdate.setChplProductNumber(updateRequest.getChplProductNumber());
+		toUpdate.setReportFileLocation(updateRequest.getReportFileLocation());
+		toUpdate.setQualityManagementSystemAtt(updateRequest.getQualityManagementSystemAtt());
+		toUpdate.setAcbCertificationId(updateRequest.getAcbCertificationId());
+		toUpdate.setOtherAcb(updateRequest.getOtherAcb());
+		
+		//TODO
+		//toUpdate.setIsChplVisible(updateRequest.getIsChplVisible());
+		
+		toUpdate = cpManager.update(toUpdate);
+		
+		return new CertifiedProduct(toUpdate);
 	}
 }
