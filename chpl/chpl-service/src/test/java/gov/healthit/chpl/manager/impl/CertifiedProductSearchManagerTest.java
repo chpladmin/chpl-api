09c00b8fd483c16d96ed0686eb3c9c0f5c2944a1
chris-watson <cxwatson@gmail.com>
2015-09-18 15:37:52 -0400

    Added unit tests for CertifiedProductSearchManager.

--- chpl/chpl-service/src/test/java/gov/healthit/chpl/manager/impl/CertifiedProductSearchManagerTest.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/manager/impl/CertifiedProductSearchManagerTest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import gov.healthit.chpl.dao.EntityRetrievalException;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.domain.CertifiedProductSearchResult;
 import gov.healthit.chpl.domain.SearchRequest;
 import gov.healthit.chpl.domain.SearchResponse;
 import gov.healthit.chpl.dto.CertifiedProductDetailsDTO;
@@ -19,6 +20,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
@@ -37,19 +39,146 @@ public class CertifiedProductSearchManagerTest extends TestCase {
 	@Autowired
 	private CertifiedProductSearchManager certifiedProductSearchManager;
 	
+	
 	@Test
+	@Transactional
+	public void testSearchVendor(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setVendor("Test Vendor 1");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(2, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getVendor().get("name").startsWith("Test Vendor 1"));
+		}
+	}
+	
+	@Test
+	@Transactional
+	public void testSearchProduct(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setProduct("Test Product 1");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(2, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getProduct().get("name").startsWith("Test Product 1"));
+		}
+	}
+	
+	@Test
+	@Transactional
+	public void testSearchVersion(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setVersion("1.0.1");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(1, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getProduct().get("version").startsWith("1.0.1"));
+		}
+	}
+	
+	@Test
+	@Transactional
+	public void testSearchCertificationEdition(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setCertificationEdition("2014");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(2, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getCertificationEdition().get("name").startsWith("2014"));
+		}
+	}
+	
+	@Test
+	@Transactional
+	public void testSearchCertificationBody(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setCertificationBody("InfoGard");
+		searchRequest.setVisibleOnCHPL("BOTH");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(3, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getCertifyingBody().get("name").startsWith("InfoGard"));
+		}
+	}
+	
+	@Test
+	@Transactional
+	public void testSearchProductClassificationType(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setProductClassification("Complete EHR");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(1, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getClassificationType().get("name").startsWith("Complete EHR"));
+		}
+		
+	}
+	
+	@Test
+	@Transactional
+	public void testSearchPracticeType(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setPracticeType("Ambulatory");
+		searchRequest.setVisibleOnCHPL("BOTH");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(3, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getPracticeType().get("name").startsWith("Ambulatory"));
+		}
+	}
+	
+	
+	@Test
+	@Transactional
+	public void testSearchVisibleOnCHPL(){
+		
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setVisibleOnCHPL("YES");
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(2, response.getResults().size());
+		
+		for (CertifiedProductSearchResult result : response.getResults() ){
+			assertTrue(result.getVisibleOnChpl());
+		}
+		
+	}
+	
+	@Test
+	@Transactional
 	public void testSearch(){
 		
 		SearchRequest searchRequest = new SearchRequest();
-		searchRequest.setVendor("Test");
-		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
-		assertEquals((int)response.getRecordCount(), response.getResults().size());
-		assertEquals((int)response.getResults().size(), 2);
 		
-		searchRequest.setVersion("1.0.0");
-		SearchResponse responseWithVersion = certifiedProductSearchManager.search(searchRequest);
-		assertEquals((int) responseWithVersion.getRecordCount(),  responseWithVersion.getResults().size());
-		assertEquals((int) responseWithVersion.getResults().size(), 1);
+		searchRequest.setSearchTerm("Test");
+		searchRequest.setVendor("Test Vendor");
+		searchRequest.setProduct("Test");
+		searchRequest.setVersion("1.0.1");
+		searchRequest.setCertificationEdition("2014");
+		searchRequest.setCertificationBody("InfoGard");
+		searchRequest.setProductClassification("Complete EHR");
+		searchRequest.setPracticeType("Ambulatory");
+		searchRequest.setVisibleOnCHPL("YES");
+		searchRequest.setOrderBy("product");
+		searchRequest.setSortDescending(true);
+		searchRequest.setPageNumber(0);
+		
+		SearchResponse response = certifiedProductSearchManager.search(searchRequest);
+		assertEquals(1, response.getResults().size());
+		
 	}
 	
 }
