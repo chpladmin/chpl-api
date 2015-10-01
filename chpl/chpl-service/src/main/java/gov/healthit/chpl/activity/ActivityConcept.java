740d88cd0bf720106e46245ce2e9d65f1df63bb5
chris-watson <cxwatson@gmail.com>
2015-10-01 13:57:07 -0400

    (OCD-194) Added ActivityConcept Enum

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/activity/ActivityConcept.java
@@ -0,0 +1,31 @@
+package gov.healthit.chpl.activity;
+
+public enum ActivityConcept {
+	
+	ACTIVITY_CONCEPT_CERTIFIED_PRODUCT(1L, "CERTIFIED_PRODUCT"),
+	ACTIVITY_CONCEPT_PRODUCT(2L, "PRODUCT"),
+	ACTIVITY_CONCEPT_VENDOR(3L, "VENDOR"),
+	ACTIVITY_CONCEPT_CERTIFICATION(4L, "CERTIFICATION"),
+	ACTIVITY_CONCEPT_CQM(5L, "CQM"),
+	ACTIVITY_CONCEPT_CERTIFICATION_BODY(6L, "CERTIFICATION_BODY"),
+	ACTIVITY_CONCEPT_VERSION(7L, "VERSION"),
+	ACTIVITY_CONCEPT_USER(8L, "USER"),
+	ACTIVITY_CONCEPT_ATL(9L, "ATL");
+	
+	private final Long id;
+	private final String name;
+	
+	private ActivityConcept(Long id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
