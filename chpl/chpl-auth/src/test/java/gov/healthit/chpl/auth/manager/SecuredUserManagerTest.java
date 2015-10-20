5da4edad62d19924bd257d3fb97b0c8cd0b80e64
chris-watson <cxwatson@gmail.com>
2015-10-20 17:03:10 -0400

    (OCD-127) Work on unit testing Auth codebase

---
+++ chpl/chpl-auth/src/test/java/gov/healthit/chpl/auth/manager/SecuredUserManagerTest.java
@@ -0,0 +1,24 @@
+package gov.healthit.chpl.auth.manager;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { gov.healthit.chpl.auth.CHPLAuthenticationSecurityTestConfig.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    TransactionalTestExecutionListener.class,
+    DbUnitTestExecutionListener.class })
+@DatabaseSetup("classpath:data/testData.xml")
+public class SecuredUserManagerTest {
+
+}
