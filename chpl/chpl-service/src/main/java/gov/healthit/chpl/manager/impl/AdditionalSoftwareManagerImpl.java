a8754aff383a775e255311fd988dd262d2f6bf25
chris-watson <cxwatson@gmail.com>
2015-12-28 15:50:50 -0500

    (OCD-440) Added controller for Additional Software.

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/impl/AdditionalSoftwareManagerImpl.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/impl/AdditionalSoftwareManagerImpl.java
@@ -1,8 +1,11 @@
 package gov.healthit.chpl.manager.impl;
 
+import gov.healthit.chpl.dao.AdditionalSoftwareDAO;
 import gov.healthit.chpl.dao.CQMResultDAO;
 import gov.healthit.chpl.dao.CertificationResultDAO;
 import gov.healthit.chpl.dao.EntityCreationException;
+import gov.healthit.chpl.dao.EntityRetrievalException;
+import gov.healthit.chpl.dto.AdditionalSoftwareDTO;
 import gov.healthit.chpl.dto.CQMResultAdditionalSoftwareMapDTO;
 import gov.healthit.chpl.dto.CertificationResultAdditionalSoftwareMapDTO;
 
@@ -19,12 +22,16 @@ public class AdditionalSoftwareManagerImpl {
 	@Autowired
 	private CQMResultDAO cqmResultDAO;
 	
+	@Autowired
+	private AdditionalSoftwareDAO additionalSoftwareDAO;
+	
 	public CertificationResultAdditionalSoftwareMapDTO addAdditionalSoftwareCertificationResultMapping(Long additionalSoftwareId, Long certificationResultId) throws EntityCreationException{
 		
 		CertificationResultAdditionalSoftwareMapDTO dto = new CertificationResultAdditionalSoftwareMapDTO();
 		dto.setAdditionalSoftwareId(additionalSoftwareId);
 		dto.setCertificationResultId(certificationResultId);
 		
+		// "CERTIFICATION"),
 		CertificationResultAdditionalSoftwareMapDTO created = certificationResultDAO.createAdditionalSoftwareMapping(dto);
 		return created;
 	}
@@ -35,12 +42,18 @@ public class AdditionalSoftwareManagerImpl {
 		dto.setAdditionalSoftwareId(additionalSoftwareId);
 		dto.setCqmResultId(cqmResultId);
 		
+		// "CQM"),
 		CQMResultAdditionalSoftwareMapDTO created = cqmResultDAO.createAdditionalSoftwareMapping(dto);
 		return created;
 		
 	}
-	public void associateAdditionalSoftwareCerifiedProductSelf(Long additionalSoftwareId, Long certifiedProductId){
+	public void associateAdditionalSoftwareCerifiedProductSelf(Long additionalSoftwareId, Long certifiedProductId) throws EntityRetrievalException{
+		
+		AdditionalSoftwareDTO dto = additionalSoftwareDAO.getById(additionalSoftwareId);
+		dto.setCertifiedProductSelfId(certifiedProductId);
+		additionalSoftwareDAO.update(dto);
 		
 	}
+	
 	
 }
---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/web/controller/AdditionalSoftwareController.java
@@ -0,0 +1,51 @@
+package gov.healthit.chpl.web.controller;
+
+import gov.healthit.chpl.dao.EntityCreationException;
+import gov.healthit.chpl.dao.EntityRetrievalException;
+import gov.healthit.chpl.manager.AdditionalSoftwareManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/additional_software")
+public class AdditionalSoftwareController {
+	
+	@Autowired
+	AdditionalSoftwareManager additionalSoftwareManager;
+	
+	@RequestMapping(value="/add_certified_product_self", method=RequestMethod.POST,
+			produces="application/json; charset=utf-8")
+	public @ResponseBody String associateAdditionalSoftwareCerifiedProductSelf(
+			@RequestParam Long additionalSoftwareId,
+			@RequestParam Long certifiedProductId)
+	{
+		additionalSoftwareManager.associateAdditionalSoftwareCerifiedProductSelf(additionalSoftwareId, certifiedProductId);
+		return "{\"success\" : true }";
+	}
+	
+	
+	@RequestMapping(value="/add_additional_software_certification_result_mapping", method=RequestMethod.POST,
+			produces="application/json; charset=utf-8")
+	public @ResponseBody String addAdditionalSoftwareCertificationResultMapping(@RequestParam Long additionalSoftwareId, Long certificationResultId)
+	{
+		additionalSoftwareManager.addAdditionalSoftwareCertificationResultMapping(additionalSoftwareId, certificationResultId);
+		return "{\"success\" : true }";
+	}
+	
+	@RequestMapping(value="/add_additional_software_cqm_result_mapping", method=RequestMethod.POST,
+			produces="application/json; charset=utf-8")
+	public @ResponseBody String addAdditionalSoftwareCQMResultMapping(@RequestParam Long additionalSoftwareId, Long cqmResultId)
+	{
+		additionalSoftwareManager.addAdditionalSoftwareCQMResultMapping(additionalSoftwareId, cqmResultId);
+		return "{\"success\" : true }";
+	}	
+	
+	
+	
+}
