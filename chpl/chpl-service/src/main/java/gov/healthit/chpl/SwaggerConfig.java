bae5c74b1e1ceca8a12f9b737b60cae6a83b8912
katy.ekey <kekey@ainq.com>
2016-01-08 11:42:35 -0500

    development: OCD-466: change basepath for swagger

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/SwaggerConfig.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/SwaggerConfig.java
@@ -3,33 +3,56 @@ package gov.healthit.chpl;
 import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import com.google.common.base.Predicate;
 
+import springfox.documentation.PathProvider;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiListingReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@PropertySource("classpath:environment.properties")
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements EnvironmentAware {
+
+	@Autowired ServletContext context;
+	@Autowired private Environment env;
+	
+	@Override
+    public void setEnvironment(final Environment environment) {
+        this.env = environment;
+    }
+	
     @Bean
     public Docket customDocket(){
        return new Docket(DocumentationType.SWAGGER_2)
     		   .apiInfo(apiInfo())
+    		   .pathProvider(pathProvider())
                .select()
                .paths(this.paths())
                .build(); 
     }
     
     private ApiInfo apiInfo() {
-    	return new ApiInfo("CHPL", "Certified Health IT Product Listing", "1.0", 
+    	return new ApiInfo("CHPL", "Certified Health IT Product Listing", "0.1.0", 
     			"http://terms/of/service.url", "CHPL@ainq.com", 
     			"License Text", "http://license.url");
+    }
+    
+    private PathProvider pathProvider() {
+    	return new AbsolutePathProvider(context);
     }
     
     @SuppressWarnings("unchecked")
@@ -52,5 +75,16 @@ public class SwaggerConfig {
     		  regex("/versions.*")
     		  
     		 );
+    }
+    
+    private class AbsolutePathProvider extends RelativePathProvider {
+    	public AbsolutePathProvider(ServletContext context) {
+    		super(context);
+    	}
+    	
+    	@Override
+    	public String getApplicationBasePath() {
+    		return env.getProperty("basePath");
+    	}
     }
 }
