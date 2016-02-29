0cd7fb62f38f05f6c6bacce74868116252531601
katy.ekey <kekey@ainq.com>
2016-02-29 14:39:58 -0500

    OCD-163: add validation to 2014 products

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/certifiedProduct/validation/AmbulatoryComplete2014Validator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/certifiedProduct/validation/AmbulatoryComplete2014Validator.java
@@ -1,32 +1,27 @@
 package gov.healthit.chpl.certifiedProduct.validation;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
-import gov.healthit.chpl.domain.CQMResultDetails;
 import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.dto.PendingCertificationResultDTO;
 import gov.healthit.chpl.dto.PendingCertifiedProductDTO;
-import gov.healthit.chpl.dto.PendingCqmCriterionDTO;
 
 @Component("ambulatoryComplete2014Validator")
 public class AmbulatoryComplete2014Validator extends AmbulatoryModular2014Validator {
 
-	private static final String[] requiredCriteria = {"170.314 (a)(2)", "170.314 (a)(4)", "170.314 (a)(9)",
-			"170.314 (a)(10)", "170.314 (a)(11)", "170.314 (a)(12)", "170.314 (a)(13)", "170.314 (a)(14)",
-			"170.314 (a)(15)", "170.314 (b)(3)", "170.314 (b)(4)", "170.314 (b)(5)(A)", "170.314 (e)(1)", "170.314 (e)(2)",
-			"170.314 (e)(3)", "170.314 (f)(1)", "170.314 (f)(2)", "170.314 (f)(3)", "170.314 (g)(2)", 
-			"170.314 (g)(3)", "170.314 (g)(4)"};
-	private static final String[] coreCqms = {"CMS146v1", "CMS155v1", "CMS153v1", "CMS126v1", "CMS117v1",
-				"CMS154v1", "CMS136v1", "CMS2v1", "CMS75v1", "CMS165v1", "CMS156v1", "CMS138v1",
-				"CMS166v1", "CMS68v1", "CMS69v1", "CMS50v1", "CMS90v1"};
-	
-	//TODO: only check g1-g4 validation and make sure we have a test report url that is a fully qualified URL
+	private static final String[] requiredCriteria = {"170.314 (a)(2)", "170.314 (a)(3)", "170.314 (a)(4)", 
+			"170.314 (a)(5)", "170.314 (a)(6)", "170.314 (a)(7)", "170.314 (a)(8)", "170.314 (a)(9)", 
+			"170.314 (a)(10)", "170.314 (a)(11)", "170.314 (a)(12)", "170.314 (a)(13)", "170.314 (a)(14)", 
+			"170.314 (a)(15)", "170.314 (b)(3)", "170.314 (b)(4)", 
+			"170.314 (b)(5)(A)", "170.314 (b)(7)", "170.314 (c)(1)", "170.314 (c)(2)", 
+			"170.314 (c)(3)", "170.314 (d)(1)", "170.314 (d)(2)", "170.314 (d)(3)", "170.314 (d)(4)",
+			"170.314 (d)(5)", "170.314 (d)(6)", "170.314 (d)(7)", "170.314 (d)(8)",
+			"170.314 (e)(1)", "170.314 (e)(2)", "170.314 (e)(3)", "170.314 (f)(1)", "170.314 (f)(2)", 
+			"170.314 (f)(3)", "170.314 (g)(2)", "170.314 (g)(3)", "170.314 (g)(4)"};
+
 	@Override
 	public void validate(PendingCertifiedProductDTO product) {
 		super.validate(product);
@@ -42,58 +37,64 @@ public class AmbulatoryComplete2014Validator extends AmbulatoryModular2014Valida
 			if(!hasCert) {
 				product.getWarningMessages().add("Required certification criteria " + requiredCriteria[i] + " was not found.");
 			}
-		}		
+		}	
 		
-		//has at least 9 cqms
-		int ambulatoryCqmCount = 0;
-		for(PendingCqmCriterionDTO cqmCriterion : product.getCqmCriterion()) {
-			if(cqmCriterion.getTypeId() == AMBULATORY_CQM_TYPE_ID && cqmCriterion.isMeetsCriteria()) {
-				ambulatoryCqmCount++;
+		//(a)(1) (OR) ((a)(18), (a)(19), (a)(20))"
+		boolean hasA1 = false;
+		boolean hasA18 = false;
+		boolean hasA19 = false;
+		boolean hasA20 = false;
+		for(PendingCertificationResultDTO certCriteria : product.getCertificationCriterion()) {
+			if(certCriteria.getNumber().equals("170.314 (a)(1)") && certCriteria.getMeetsCriteria()) {
+				hasA1 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (a)(18)") && certCriteria.getMeetsCriteria()) {
+				hasA18 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (a)(19)") && certCriteria.getMeetsCriteria()) {
+				hasA19 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (a)(20)") && certCriteria.getMeetsCriteria()) {
+				hasA20 = true;
+			}
+		}
+		if(!hasA1) {
+			if(!hasA18 || !hasA19 || !hasA20) {
+				product.getErrorMessages().add("Neither (a)(1) nor the combination of (a)(18), (a)(19), and (a)(20) were found.");
 			}
 		}
 		
-		if(ambulatoryCqmCount < 9) {
-			product.getWarningMessages().add(ambulatoryCqmCount + " Ambulatory CQM(s) were found but at least 9 are required.");
+		//(b)(1), (b)(2)** 
+		//(in replacement for (b)(1) and (b)(2) - 
+		//(b)(1) and (b)(8), OR 
+		//(b)(8) and (h)(1), OR 
+		//(b)(1) and (b)(2) and (b)(8), OR 
+		//(b)(1) and (b)(2) and (h)(1), OR 
+		//(b)(1) and (b)(2) and (b)(8) and (h)(1)"
+		boolean hasB1 = false;
+		boolean hasB2 = false;
+		boolean hasB8 = false;
+		boolean hasH1 = false;
+		for(PendingCertificationResultDTO certCriteria : product.getCertificationCriterion()) {
+			if(certCriteria.getNumber().equals("170.314 (b)(1)") && certCriteria.getMeetsCriteria()) {
+				hasB1 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (b)(2)") && certCriteria.getMeetsCriteria()) {
+				hasB2 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (b)(8)") && certCriteria.getMeetsCriteria()) {
+				hasB8 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (h)(1)") && certCriteria.getMeetsCriteria()) {
+				hasH1 = true;
+			}
 		}
-		
-		//has at least 6 cqms from the core set
-		int coreAmbulatoryCount = 0;
-		for(int i = 0; i < coreCqms.length; i++) {
-			String[] coreCqmParts = coreCqms[i].split("v");
-			String cqmNumber = coreCqmParts[0];
-			String cqmVersion = "v" + coreCqmParts[1];
-			int cqmVersionMinimunm = new Integer(coreCqmParts[1]).intValue();
-			boolean hasCurr = false;
-			for(PendingCqmCriterionDTO cqmCriterion : product.getCqmCriterion()) {
-				if(cqmCriterion.getTypeId() == AMBULATORY_CQM_TYPE_ID && cqmCriterion.isMeetsCriteria() && 
-					cqmCriterion.getCmsId().equals(cqmNumber)) {
-					
-					String currCriterionVersion = cqmCriterion.getVersion();
-					String currCriterionVersionNumber = currCriterionVersion.substring(currCriterionVersion.indexOf("v")+1);
-					int versionNumber = new Integer(currCriterionVersionNumber).intValue();
-					if(versionNumber >= cqmVersionMinimunm) {
-						hasCurr = true;
-					} 
+		if(!hasB1 && !hasB2) {
+			if(!hasB1 && !hasB8) {
+				if(!hasB8 && !hasH1) {
+					product.getErrorMessages().add("An allowed combination of (b)(1), (b)(2), (b)(8), and (h)(1) was not found.");
 				}
 			}
-			if(hasCurr) {
-				coreAmbulatoryCount++;
-			}
-		}
-		if(coreAmbulatoryCount < 6) {
-			product.getWarningMessages().add(coreAmbulatoryCount + " Ambulatory CQM(s) were met from CMS's recommended core set were found but at least 6 are required.");
-		}
-		
-		//checks 3 domains
-		Set<String> checkedDomains = new HashSet<String>();
-		for(PendingCqmCriterionDTO cqmCriterion : product.getCqmCriterion()) {
-			if(!StringUtils.isEmpty(cqmCriterion.getDomain()) &&
-					cqmCriterion.getTypeId() == AMBULATORY_CQM_TYPE_ID && cqmCriterion.isMeetsCriteria()) {
-				checkedDomains.add(cqmCriterion.getDomain());
-			}
-		}
-		if(checkedDomains.size() < 3) {
-			product.getWarningMessages().add(checkedDomains.size() + " domains were found but at least 3 are required.");
 		}
 	}
 	
@@ -112,60 +113,64 @@ public class AmbulatoryComplete2014Validator extends AmbulatoryModular2014Valida
 			if(!hasCert) {
 				product.getWarningMessages().add("Required certification criteria " + requiredCriteria[i] + " was not found.");
 			}
-		}		
+		}	
 		
-		//has at least 9 cqms
-		int ambulatoryCqmCount = 0;
-		for(CQMResultDetails cqmCriterion : product.getCqmResults()) {
-			if(cqmCriterion.getTypeId() == AMBULATORY_CQM_TYPE_ID && cqmCriterion.isSuccess()) {
-				ambulatoryCqmCount++;
+		//(a)(1) (OR) ((a)(18), (a)(19), (a)(20))"
+		boolean hasA1 = false;
+		boolean hasA18 = false;
+		boolean hasA19 = false;
+		boolean hasA20 = false;
+		for(CertificationResult certCriteria : product.getCertificationResults()) {
+			if(certCriteria.getNumber().equals("170.314 (a)(1)") && certCriteria.isSuccess()) {
+				hasA1 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (a)(18)") && certCriteria.isSuccess()) {
+				hasA18 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (a)(19)") && certCriteria.isSuccess()) {
+				hasA19 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (a)(20)") && certCriteria.isSuccess()) {
+				hasA20 = true;
+			}
+		}
+		if(!hasA1) {
+			if(!hasA18 || !hasA19 || !hasA20) {
+				product.getErrorMessages().add("Neither (a)(1) nor the combination of (a)(18), (a)(19), and (a)(20) were found.");
 			}
 		}
 		
-		if(ambulatoryCqmCount < 9) {
-			product.getWarningMessages().add(ambulatoryCqmCount + " Ambulatory CQM(s) were found but at least 9 are required.");
+		//(b)(1), (b)(2)** 
+		//(in replacement for (b)(1) and (b)(2) - 
+		//(b)(1) and (b)(8), OR 
+		//(b)(8) and (h)(1), OR 
+		//(b)(1) and (b)(2) and (b)(8), OR 
+		//(b)(1) and (b)(2) and (h)(1), OR 
+		//(b)(1) and (b)(2) and (b)(8) and (h)(1)"
+		boolean hasB1 = false;
+		boolean hasB2 = false;
+		boolean hasB8 = false;
+		boolean hasH1 = false;
+		for(CertificationResult certCriteria : product.getCertificationResults()) {
+			if(certCriteria.getNumber().equals("170.314 (b)(1)") && certCriteria.isSuccess()) {
+				hasB1 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (b)(2)") && certCriteria.isSuccess()) {
+				hasB2 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (b)(8)") && certCriteria.isSuccess()) {
+				hasB8 = true;
+			}
+			if(certCriteria.getNumber().equals("170.314 (h)(1)") && certCriteria.isSuccess()) {
+				hasH1 = true;
+			}
 		}
-		
-		//has at least 6 cqms from the core set
-		int coreAmbulatoryCount = 0;
-		for(int i = 0; i < coreCqms.length; i++) {
-			String[] coreCqmParts = coreCqms[i].split("v");
-			String cqmNumber = coreCqmParts[0];
-			int cqmVersionMinimunm = new Integer(coreCqmParts[1]).intValue();
-			boolean hasCurr = false;
-			for(CQMResultDetails cqmCriterion : product.getCqmResults()) {
-				if(cqmCriterion.getTypeId() == AMBULATORY_CQM_TYPE_ID && cqmCriterion.isSuccess() && 
-					cqmCriterion.getCmsId().equals(cqmNumber)) {
-					
-					for(String currCriterionVersion : cqmCriterion.getSuccessVersions()) {
-						String currCriterionVersionNumber = currCriterionVersion.substring(currCriterionVersion.indexOf("v")+1);
-						int versionNumber = new Integer(currCriterionVersionNumber).intValue();
-						if(versionNumber >= cqmVersionMinimunm) {
-							hasCurr = true;
-							break;
-						} 
-					}
-					
-					if(hasCurr) {
-						coreAmbulatoryCount++;
-					}
+		if(!hasB1 && !hasB2) {
+			if(!hasB1 && !hasB8) {
+				if(!hasB8 && !hasH1) {
+					product.getErrorMessages().add("An allowed combination of (b)(1), (b)(2), (b)(8), and (h)(1) was not found.");
 				}
 			}
-		}
-		if(coreAmbulatoryCount < 6) {
-			product.getWarningMessages().add(coreAmbulatoryCount + " Ambulatory CQM(s) were met from CMS's recommended core set were found but at least 6 are required.");
-		}
-		
-		//checks 3 domains
-		Set<String> checkedDomains = new HashSet<String>();
-		for(CQMResultDetails cqmCriterion : product.getCqmResults()) {
-			if(!StringUtils.isEmpty(cqmCriterion.getDomain()) &&
-					cqmCriterion.getTypeId() == AMBULATORY_CQM_TYPE_ID && cqmCriterion.isSuccess()) {
-				checkedDomains.add(cqmCriterion.getDomain());
-			}
-		}
-		if(checkedDomains.size() < 3) {
-			product.getWarningMessages().add(checkedDomains.size() + " domains were found but at least 3 are required.");
 		}
 	}
 
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/certifiedProduct/validation/CertifiedProductValidator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/certifiedProduct/validation/CertifiedProductValidator.java
@@ -6,6 +6,9 @@ import gov.healthit.chpl.dto.PendingCertifiedProductDTO;
 public interface CertifiedProductValidator {
 	static final long AMBULATORY_CQM_TYPE_ID = 1;
 	static final long INPATIENT_CQM_TYPE_ID = 2;
+	static final String URL_PATTERN = "^https?://([\\da-z\\.-]+)\\.([a-z\\.]{2,6})(:[0-9]+)?([\\/\\w \\.\\-\\,=&%#]*)*(\\?([\\/\\w \\.\\-\\,=&%#]*)*)?";
+
+		
 	
 	public void validate(PendingCertifiedProductDTO product);
 	public void validate(CertifiedProductSearchDetails product);
