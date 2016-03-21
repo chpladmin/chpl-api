9e05cca96e83990c4e314a51427d93e06dc87e39
Noby Nobriga <nnobriga@deloitte.com>
2016-03-21 15:37:14 -0500

    Refactored product collection lookup. Added more data in results. Changed product id delimiter from semi-colon ( ; ) to pipe ( | )

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/web/controller/CertificationIdController.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/web/controller/CertificationIdController.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-
+import java.util.regex.Pattern;
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,30 +41,33 @@ public class CertificationIdController {
 			notes="Calculates the details of a collection of products in order to retrieve an EHR Certification ID.")
 	@RequestMapping(value="/getCertificationId", method=RequestMethod.GET,
 			produces="application/json; charset=utf-8")
-	public @ResponseBody CertificationIdResults getCertificationId(@RequestParam(required=true) String products) 
+	public @ResponseBody CertificationIdResults getCertificationId(@RequestParam(required=false) String products) 
 		throws InvalidArgumentsException {
-	
-		// Make sure a value is provided
-		if (null == products || 0 == products.length) {
-			throw new InvalidArgumentsException("At least one product id must be provided in the request.");
+
+		// Make sure the value is formatted correctly (Ex: "123|234|345")
+		if ((null != products) && (0 != products.trim().length())) {
+			if (!Pattern.matches("[0-9]+(\\|[0-9]+)*", products)) {
+				throw new InvalidArgumentsException("Product ids are integers and must be separated by pipes.");
+			}
+		} else {
+			products = null;
 		}
 
-		// Make sure the value is formatted correctly (Ex: "123;234;345")
-		if (!Pattern.matches("[0-9]+(\\;[0-9]+)*", products)) {
-			throw new InvalidArgumentsException("Product ids are integers and must be separated by semi-colons.");
+		// Lookup specified products
+		List<ProductDTO> productDtos = new ArrayList<ProductDTO>();
+		if (null != products) {
+			String[] productArray = products.trim().split("\\|");
+			List<Long> productIdList = new ArrayList<Long>();
+			if (productArray.length > 0) {
+				for (String id : productArray) {
+					if (null != id)
+						productIdList.add(new Long(id));
+				}
+				productDtos = productManager.getByIds(productIdList);
+			}
 		}
-
-		// Split into individual product ids
-		String[] productArray = products.split(";").trim();
-		if (null == productArray || 0 == productArray.length) {
-			throw new InvalidArgumentsException("At least one product id must be provided in the request.");
-		}
-
-		// Lookup the certification id
-		// TODO
 		
 		CertificationIdResults results = new CertificationIdResults();
-		
 		Map<String, Integer> percents = new HashMap<String, Integer>();
 		Map<String, Integer> counts = new HashMap<String, Integer>();
 		
@@ -72,8 +76,20 @@ public class CertificationIdController {
 		
 		percents.put("criteria", 100);
 		counts.put("criteria", 1);
+		percents.put("cqmDomains", 100);
+		counts.put("cqmDomains", 1);
+		percents.put("cqmsAmbulatory", 100);
+		counts.put("cqmsAmbulatory", 1);
+		percents.put("cqmsInpatient", 100);
+		counts.put("cqmsInpatient", 1);
 		
-		results.setEhrCertificationId("TESTCERTID" + productArray);
+		List<Product> resultProducts = new ArrayList<Product>();
+		for (ProductDTO dto : productDtos) {
+			resultProducts.add(new Product(dto));
+		}
+		results.setProducts(resultProducts);
+
+		results.setEhrCertificationId("TESTCERTID");
 
 		return results;
 	}
