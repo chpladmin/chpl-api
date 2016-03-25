755343d285eba54ad67a26fb61257655b31d7756
katy.ekey <kekey@ainq.com>
2016-03-25 13:37:42 -0400

    development: OCD-597: fix validation

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/certifiedProduct/validation/CertifiedProduct2014Validator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/certifiedProduct/validation/CertifiedProduct2014Validator.java
@@ -122,6 +122,11 @@ public class CertifiedProduct2014Validator extends CertifiedProductValidatorImpl
 		if(product.getClassificationType() == null || product.getClassificationType().get("id") == null) {
 			product.getErrorMessages().add("Product classification is required but was not found.");
 		}
+		if(StringUtils.isEmpty(product.getReportFileLocation())) {
+			product.getErrorMessages().add("Test Report URL is required but was not found.");
+		} else if(urlRegex.matcher(product.getReportFileLocation()).matches() == false) {
+			product.getErrorMessages().add("Test Report URL provided is not a valid URL format.");
+		}
 		
 		//check sed/ucd/tasks
 		for(CertificationResult cert : product.getCertificationResults()) {
