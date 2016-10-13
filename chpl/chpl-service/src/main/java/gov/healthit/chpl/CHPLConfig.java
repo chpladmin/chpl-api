cbf437dc6144479ea6b948890b02b96f921e20f4
Andrew Larned <andrew.larned@gmail.com>
2016-10-13 11:09:43 -0400

    OCD-924: Temporarily disable caching

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/CHPLConfig.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/CHPLConfig.java
@@ -29,7 +29,7 @@ import gov.healthit.chpl.registration.APIKeyAuthenticationFilter;
 @EnableWebMvc
 @EnableTransactionManagement(proxyTargetClass=true)
 @EnableWebSecurity
-@EnableCaching
+//@EnableCaching
 @PropertySource("classpath:/environment.properties")
 @ComponentScan(basePackages = {"gov.healthit.chpl.**"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class)})
 public class CHPLConfig implements EnvironmentAware {
