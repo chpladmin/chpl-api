508003f5bf37d3fb8674a3a759cdf1958b16726b
Katy Ekey <kekey@ainq.com>
2016-12-20 14:40:18 -0500

    OCD-1088: change to use certification_status_event for event history
    	* remove most references to certification event
    	* remove most references to event type

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/CertificationStatusEventDAO.java
@@ -0,0 +1,18 @@
+package gov.healthit.chpl.dao;
+
+import java.util.List;
+
+import gov.healthit.chpl.dto.CertificationStatusEventDTO;
+
+
+public interface CertificationStatusEventDAO {
+
+	public CertificationStatusEventDTO create(CertificationStatusEventDTO dto) throws EntityCreationException, EntityRetrievalException;
+	public CertificationStatusEventDTO update(CertificationStatusEventDTO dto) throws EntityRetrievalException;
+	public void delete(Long id) throws EntityRetrievalException;
+	public CertificationStatusEventDTO getById(Long id) throws EntityRetrievalException;
+	public List<CertificationStatusEventDTO> findAll();	
+	public List<CertificationStatusEventDTO> findByCertifiedProductId(Long certifiedProductId);	
+	public CertificationStatusEventDTO findInitialCertificationEventForCertifiedProduct(Long certifiedProductId);
+	
+}
