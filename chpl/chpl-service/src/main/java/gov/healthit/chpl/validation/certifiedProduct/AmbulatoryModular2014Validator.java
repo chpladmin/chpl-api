b81ebf0a438e2f342d881342923a7c20572266d8
Daniel Lucas <daniel.r.lucas@gmail.com>
2017-01-19 17:13:53 -0500

    OCD-1096: do not throw error when removing TT; only throw error when adding TT if CP ICS=0

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/certifiedProduct/AmbulatoryModular2014Validator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/certifiedProduct/AmbulatoryModular2014Validator.java
@@ -170,36 +170,6 @@ public class AmbulatoryModular2014Validator extends CertifiedProduct2014Validato
 			logger.error("Could not find certified product details with id " + product.getId(), ex);
 		}
 		
-		//compare old with current - they cannot remove test tools
-		for(CertificationResult oldCert : oldProduct.getCertificationResults()) {
-			if(oldCert.isSuccess() != null && oldCert.isSuccess() == Boolean.TRUE && oldCert.getTestToolsUsed() != null) {
-				for(CertificationResultTestTool oldTestTool : oldCert.getTestToolsUsed()) {
-					TestToolDTO testTool = ttDao.getById(oldTestTool.getTestToolId());
-					if(testTool.isRetired()) {
-						boolean certStillExists = false;
-						boolean certHasRetiredTool = false;
-						//make sure this test tool still exists in the passed in product/cert
-						//because users are not allowed to remove exisitng test tools if they are retired
-						for(CertificationResult cert : product.getCertificationResults()) {
-							if(cert.isSuccess() != null && cert.isSuccess() == Boolean.TRUE) {
-								if(cert.getNumber().equals(oldCert.getNumber())) {
-									certStillExists = true;
-									for(CertificationResultTestTool newTestTool : cert.getTestToolsUsed()) {
-										if(newTestTool.getTestToolId().equals(oldTestTool.getTestToolId())) {
-											certHasRetiredTool = true;
-										}
-									}
-								}
-							}
-						}
-						if(certStillExists && !certHasRetiredTool) {
-							product.getErrorMessages().add("Certification " + oldCert.getNumber() + " exists but is missing the required test tool '" + testTool.getName() + "'. This tool was present before and cannt be removed since it is retired.");
-						}
-					}
-				}
-			}
-		}
-		
 		//compare current test tools with old ones - they cannot add any additional retired tools or change versions
 		//now check all the new certs for whatever is required
 		for(CertificationResult cert : product.getCertificationResults()) {
