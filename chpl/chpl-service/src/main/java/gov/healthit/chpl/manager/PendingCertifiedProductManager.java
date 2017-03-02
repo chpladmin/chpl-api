b0c31a1492d4b906998bbd62be68fdcb21498291
Katy Ekey <kekey@ainq.com>
2017-03-02 12:49:33 -0500

    OCD-1281: do not use Spring ACLs for pending CPs

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/PendingCertifiedProductManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/PendingCertifiedProductManager.java
@@ -3,16 +3,14 @@ package gov.healthit.chpl.manager;
 
 import java.util.List;
 
-import org.springframework.security.acls.model.Permission;
-import org.springframework.security.acls.model.Sid;
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.security.access.AccessDeniedException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import gov.healthit.chpl.auth.dto.UserDTO;
-import gov.healthit.chpl.auth.user.UserRetrievalException;
 import gov.healthit.chpl.dao.EntityCreationException;
 import gov.healthit.chpl.dao.EntityRetrievalException;
-import gov.healthit.chpl.domain.CQMCriterion;
 import gov.healthit.chpl.domain.PendingCertifiedProductDetails;
 import gov.healthit.chpl.dto.CertificationBodyDTO;
 import gov.healthit.chpl.dto.PendingCertifiedProductDTO;
@@ -20,24 +18,19 @@ import gov.healthit.chpl.entity.PendingCertifiedProductEntity;
 
 
 public interface PendingCertifiedProductManager {
-	public PendingCertifiedProductDetails getById(Long id) throws EntityRetrievalException;
-	public List<PendingCertifiedProductDTO> getByAcb(CertificationBodyDTO acb);
-	public List<PendingCertifiedProductDetails> getDetailsByAcb(CertificationBodyDTO acb);
-	public List<PendingCertifiedProductDTO> getPending();
-	public List<CQMCriterion> getApplicableCriteria(PendingCertifiedProductDTO pendingCpDto);
-	
+	public PendingCertifiedProductDetails getById(List<CertificationBodyDTO> userAcbs, Long id) 
+			throws EntityNotFoundException, EntityRetrievalException, AccessDeniedException;
+	public PendingCertifiedProductDetails getById(Long acbId, Long id) throws EntityRetrievalException;
+	public List<PendingCertifiedProductDTO> getPendingCertifiedProductsByAcb(Long acbId);	
 	public PendingCertifiedProductDTO createOrReplace(Long acbId, PendingCertifiedProductEntity toCreate) 
 			throws EntityRetrievalException, EntityCreationException, JsonProcessingException;
-			
+	public void deletePendingCertifiedProduct(List<CertificationBodyDTO> userAcbs, Long pendingProductId) 
+		throws EntityRetrievalException, EntityNotFoundException, EntityCreationException, 
+			AccessDeniedException, JsonProcessingException;
+	public void deletePendingCertifiedProduct(Long acbId, Long pendingProductId) throws EntityRetrievalException, JsonProcessingException, EntityCreationException;
+	public void confirm(Long acbId, Long pendingProductId) throws EntityRetrievalException, JsonProcessingException, EntityCreationException;
+	
 	public void addAllVersionsToCmsCriterion(PendingCertifiedProductDetails pcpDetails);
 	public void addAllMeasuresToCertificationCriteria(PendingCertifiedProductDetails pcpDetails);
-	public void reject(Long pendingProductId) throws EntityRetrievalException, JsonProcessingException, EntityCreationException;
-	public void confirm(Long pendingProductId) throws EntityRetrievalException, JsonProcessingException, EntityCreationException;
-	public void addPermission(CertificationBodyDTO acb, PendingCertifiedProductDTO pcpDto, Long userId, Permission permission) throws UserRetrievalException;
-	public void addPermission(CertificationBodyDTO acb, PendingCertifiedProductDTO pcpDto, UserDTO user, Permission permission);
-	public void addPermissionToAllPendingCertifiedProductsOnAcb(CertificationBodyDTO acb, UserDTO user, Permission permission);
-	public void deleteUserPermissionFromAllPendingCertifiedProductsOnAcb(CertificationBodyDTO acb, Sid user);
-	public void deletePermission(PendingCertifiedProductDTO pcpDto, Sid recipient, Permission permission);
-	public void deleteUserPermissionsOnPendingCertifiedProduct(PendingCertifiedProductDTO pcpDto, Sid recipient);
-	public void deletePermissionsForUser(UserDTO userDto) throws UserRetrievalException;
+	
 }
