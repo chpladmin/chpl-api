3e2fe4d7dfd6a1bc3f20a088116ac727f4e00a66
Daniel Lucas <daniel.r.lucas@gmail.com>
2017-03-30 18:11:54 -0400

    Refactor unit test that could fail

--- chpl/chpl-service/src/test/java/gov/healthit/chpl/web/controller/MeaningfulUseControllerTest.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/web/controller/MeaningfulUseControllerTest.java
@@ -168,44 +168,49 @@ public class MeaningfulUseControllerTest extends TestCase {
 		input.close();
 		file.delete();
 		
-		assertTrue("MeaningfulUseUserResults should return 4 but returned " + apiResult.getMeaningfulUseUsers().size(), apiResult.getMeaningfulUseUsers().size() == 4);
-		assertTrue("Errors should return 4 but returned " + apiResult.getErrors().size(), apiResult.getErrors().size() == 4);
-		assertTrue("MeaningfulUseUserResults returned " + apiResult.getMeaningfulUseUsers().get(0).getProductNumber() + " but should return CHP-024050", 
-				apiResult.getMeaningfulUseUsers().get(0).getProductNumber().equals("CHP-024050"));
-		assertTrue("MeaningfulUseUserResults returned " + apiResult.getMeaningfulUseUsers().get(0).getNumberOfUsers() + " but should return " + 10L, 
-				apiResult.getMeaningfulUseUsers().get(0).getNumberOfUsers().equals(10L));
-		assertTrue("MeaningfulUseUserResults returned " + apiResult.getMeaningfulUseUsers().get(0).getCertifiedProductId() + " but should return certifiedProductId 1", 
-				apiResult.getMeaningfulUseUsers().get(0).getCertifiedProductId() == 1);
-		assertTrue("MeaningfulUseUserResults should return correct product number", apiResult.getMeaningfulUseUsers().get(1).getProductNumber().equals("CHP-024051"));
-		assertTrue("MeaningfulUseUserResults should return correct numMeaningfulUseUsers", apiResult.getMeaningfulUseUsers().get(1).getNumberOfUsers().equals(20L));
-		assertTrue("MeaningfulUseUserResults should return correct product number", apiResult.getMeaningfulUseUsers().get(2).getProductNumber().equals("CHP-024052"));
-		assertTrue("MeaningfulUseUserResults should return correct numMeaningfulUseUsers", apiResult.getMeaningfulUseUsers().get(2).getNumberOfUsers().equals(30L));
-		assertTrue("MeaningfulUseUserResults should return correct product number", apiResult.getMeaningfulUseUsers().get(3).getProductNumber().equals("15.01.01.1009.EIC08.36.1.1.160402"));
-		assertTrue("MeaningfulUseUserResults should return correct numMeaningfulUseUsers", apiResult.getMeaningfulUseUsers().get(3).getNumberOfUsers().equals(40L));
-		assertTrue("MeaningfulUseUserResults returned " + apiResult.getMeaningfulUseUsers().get(3).getProductNumber() + " but should return 15.01.01.1009.EIC08.36.1.1.160402", 
-				apiResult.getMeaningfulUseUsers().get(3).getProductNumber().equals("15.01.01.1009.EIC08.36.1.1.160402"));
-		assertTrue("MeaningfulUseUserResults returned " + apiResult.getMeaningfulUseUsers().get(3).getNumberOfUsers() + " but should return " + 40L, 
-				apiResult.getMeaningfulUseUsers().get(3).getNumberOfUsers().equals(40L));
-		assertTrue("MeaningfulUseUserResults errors array should return incorrect CHPL Product Number for row with {wrongChplProductNumber, 50L} but returned " 
-		+ apiResult.getErrors().get(0).getError(), apiResult.getErrors().get(0).getError() != null);
-		assertTrue("MeaningfulUseUserResults errors array should return incorrect CHPL Product Number for row with {CHPL-024053, 60L} but returned " 
-				+ apiResult.getErrors().get(1).getError(), apiResult.getErrors().get(1).getError() != null);
-		assertTrue("MeaningfulUseUserResults errors array for row {CHPL-024053, 60L} should have Product Number CHPL-024053 but has " 
-				+ apiResult.getErrors().get(1).getProductNumber(), apiResult.getErrors().get(1).getProductNumber().equals("CHPL-024053"));
-		assertTrue("MeaningfulUseUserResults errors array for row {CHPL-024053, 60L} should have num_meaningful_use 60L but has " 
-				+ apiResult.getErrors().get(1).getNumberOfUsers(), apiResult.getErrors().get(1).getNumberOfUsers() == 60L);
-		assertTrue("MeaningfulUseUserResults errors array should return incorrect CHPL Product Number for row with {15.02.03.9876.AB01.01.0.1.123456, 70L} but returned " 
-				+ apiResult.getErrors().get(2).getError(), apiResult.getErrors().get(2).getError() != null);
-		assertTrue("MeaningfulUseUserResults errors array for row {15.02.03.9876.AB01.01.0.1.123456, 70L} should have Product Number 15.02.03.9876.AB01.01.0.1.123456 but has " 
-				+ apiResult.getErrors().get(2).getProductNumber(), apiResult.getErrors().get(2).getProductNumber().equals("15.02.03.9876.AB01.01.0.1.123456"));
-		assertTrue("MeaningfulUseUserResults errors array for row {15.02.03.9876.AB01.01.0.1.123456, 70L} should have num_meaningful_use 70L but has " 
-				+ apiResult.getErrors().get(2).getNumberOfUsers(), apiResult.getErrors().get(2).getNumberOfUsers() == 70L);
-		assertTrue("MeaningfulUseUserResults errors array should return incorrect CHPL Product Number for row with {15.01.01.1009.EIC08.36.1.1.160402, 70L} but returned " 
-				+ apiResult.getErrors().get(3).getError(), apiResult.getErrors().get(3).getError() != null);
-		assertTrue("MeaningfulUseUserResults errors array for row {12.01.01.1234.AB01.01.0.1.123456, 70L} should have Product Number 15.01.01.1009.EIC08.36.1.1.160402 but has " 
-				+ apiResult.getErrors().get(3).getProductNumber(), apiResult.getErrors().get(3).getProductNumber().equals("15.01.01.1009.EIC08.36.1.1.160402"));
-		assertTrue("MeaningfulUseUserResults errors array for row {15.01.01.1009.EIC08.36.1.1.160402, 70L} should have num_meaningful_use 70L but has " 
-				+ apiResult.getErrors().get(3).getNumberOfUsers(), apiResult.getErrors().get(3).getNumberOfUsers() == 70L);
+		for(MeaningfulUseUser muu : apiResult.getMeaningfulUseUsers()){
+			if(muu.getProductNumber().equals("CHP-024050")){
+				assertTrue(muu.getNumberOfUsers() == 10L);
+				assertTrue(muu.getCertifiedProductId() == 1L);
+			}
+			else if(muu.getProductNumber().equals("CHP-024051")){
+				assertTrue(muu.getNumberOfUsers() == 20L);
+				assertTrue(muu.getCertifiedProductId() == 2L);
+			}
+			else if(muu.getProductNumber().equals("CHP-024052")){
+				assertTrue(muu.getNumberOfUsers() == 30L);
+				assertTrue(muu.getCertifiedProductId() == 3L);
+			}
+			else if(muu.getProductNumber().equals("15.01.01.1009.EIC08.36.1.1.160402")){
+				assertTrue(muu.getNumberOfUsers() == 40L);
+				assertTrue(muu.getCertifiedProductId() == 5L);
+			}
+			else if(muu.getProductNumber().equals("14.01.01.1009.EIC08.36.1.1.160402")){
+				assertTrue(muu.getNumberOfUsers() == 50L);
+				assertTrue(muu.getCertifiedProductId() == 1L);
+			}
+		}
+		
+		for(MeaningfulUseUser muu : apiResult.getErrors()){
+			Boolean hasError = false;
+			if(muu.getProductNumber().equals("wrongChplProductNumber")){
+				assertTrue(muu.getNumberOfUsers() == 50L);
+				hasError=true;
+			}
+			else if(muu.getProductNumber().equals("CHPL-024053")){
+				assertTrue(muu.getNumberOfUsers() == 60L);
+				hasError=true;
+			}
+			else if(muu.getProductNumber().equals("15.02.03.9876.AB01.01.0.1.123456")){
+				assertTrue(muu.getNumberOfUsers() == 70L);
+				hasError=true;
+			}
+			else if(muu.getProductNumber().equals("15.01.01.1009.EIC08.36.1.1.160402")){
+				assertTrue(muu.getNumberOfUsers() == 70L);
+				hasError=true;
+			}
+			assertTrue(hasError);
+		}
 	}
 	
 	/** 
