89a12d16ae246beed866cc734d5d73d74d55059c
Katy Ekey <kekey@ainq.com>
2017-08-30 12:26:25 -0400

    OCD-1736: in progress job controller

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/web/controller/JobController.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/web/controller/JobController.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.csv.CSVParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
