030856614b7bef09ac867ed1133a8aedf64c869f
Katy Ekey <kekey@ainq.com>
2017-09-25 13:05:02 -0400

    OCD-1700: return 404 with error message for listing not found

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/impl/BaseDAOImpl.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/impl/BaseDAOImpl.java
@@ -4,12 +4,16 @@ package gov.healthit.chpl.dao.impl;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
 
 public class BaseDAOImpl {
 
 	@PersistenceContext
 	protected EntityManager entityManager;
-	
+	@Autowired MessageSource messageSource;
+
 	public EntityManager getEntityManager() {
 		return entityManager;
 	}
