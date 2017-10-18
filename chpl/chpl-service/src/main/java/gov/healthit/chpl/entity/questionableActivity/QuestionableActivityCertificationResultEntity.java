c67fb76a0d062d3c95daa5e1a6601d09058e8e62
Katy Ekey <kekey@ainq.com>
2017-10-18 16:26:41 -0400

    OCD-1828: add questact entities

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/entity/questionableActivity/QuestionableActivityCertificationResultEntity.java
@@ -0,0 +1,170 @@
+package gov.healthit.chpl.entity.questionableActivity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import gov.healthit.chpl.auth.entity.UserEntity;
+import gov.healthit.chpl.entity.listing.CertificationResultEntity;
+
+@Entity
+@Table(name = "questionable_activity_certification_result")
+public class QuestionableActivityCertificationResultEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "questionable_activity_trigger_id")
+    private Long triggerId;
+    
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "questionable_activity_trigger_id", insertable = false, updatable = false)
+    private QuestionableActivityTriggerEntity trigger;
+
+    @Column(name = "certification_result_id")
+    private Long certResultId;
+
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "certification_result_id", insertable = false, updatable = false)
+    private CertificationResultEntity certResult;
+    
+    @Column(name = "message")
+    private String message;
+    
+    @Column(name = "activity_date")
+    private Date activityDate;
+    
+    @Column(name = "activity_user_id")
+    private Long userId;
+    
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private UserEntity user;
+    
+    @Column(name = "deleted")
+    private Boolean deleted;
+
+    @Column(name = "last_modified_user")
+    private Long lastModifiedUser;
+
+    @Column(name = "creation_date", insertable = false, updatable = false)
+    private Date creationDate;
+
+    @Column(name = "last_modified_date", insertable = false, updatable = false)
+    private Date lastModifiedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public Long getTriggerId() {
+        return triggerId;
+    }
+
+    public void setTriggerId(Long triggerId) {
+        this.triggerId = triggerId;
+    }
+
+    public QuestionableActivityTriggerEntity getTrigger() {
+        return trigger;
+    }
+
+    public void setTrigger(QuestionableActivityTriggerEntity trigger) {
+        this.trigger = trigger;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getActivityDate() {
+        return activityDate;
+    }
+
+    public void setActivityDate(Date activityDate) {
+        this.activityDate = activityDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Long getLastModifiedUser() {
+        return lastModifiedUser;
+    }
+
+    public void setLastModifiedUser(Long lastModifiedUser) {
+        this.lastModifiedUser = lastModifiedUser;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public Long getCertResultId() {
+        return certResultId;
+    }
+
+    public void setCertResultId(Long certResultId) {
+        this.certResultId = certResultId;
+    }
+
+    public CertificationResultEntity getCertResult() {
+        return certResult;
+    }
+
+    public void setCertResult(CertificationResultEntity certResult) {
+        this.certResult = certResult;
+    }
+}
+
---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/entity/questionableActivity/QuestionableActivityListingEntity.java
@@ -0,0 +1,170 @@
+package gov.healthit.chpl.entity.questionableActivity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import gov.healthit.chpl.auth.entity.UserEntity;
+import gov.healthit.chpl.entity.search.CertifiedProductBasicSearchResultEntity;
+
+@Entity
+@Table(name = "questionable_activity_listing")
+public class QuestionableActivityListingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "questionable_activity_trigger_id")
+    private Long triggerId;
+    
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "questionable_activity_trigger_id", insertable = false, updatable = false)
+    private QuestionableActivityTriggerEntity trigger;
+
+    @Column(name = "listing_id")
+    private Long listingId;
+
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "listing_id", insertable = false, updatable = false)
+    private CertifiedProductBasicSearchResultEntity listing;
+    
+    @Column(name = "message")
+    private String message;
+    
+    @Column(name = "activity_date")
+    private Date activityDate;
+    
+    @Column(name = "activity_user_id")
+    private Long userId;
+    
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private UserEntity user;
+    
+    @Column(name = "deleted")
+    private Boolean deleted;
+
+    @Column(name = "last_modified_user")
+    private Long lastModifiedUser;
+
+    @Column(name = "creation_date", insertable = false, updatable = false)
+    private Date creationDate;
+
+    @Column(name = "last_modified_date", insertable = false, updatable = false)
+    private Date lastModifiedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public Long getTriggerId() {
+        return triggerId;
+    }
+
+    public void setTriggerId(Long triggerId) {
+        this.triggerId = triggerId;
+    }
+
+    public QuestionableActivityTriggerEntity getTrigger() {
+        return trigger;
+    }
+
+    public void setTrigger(QuestionableActivityTriggerEntity trigger) {
+        this.trigger = trigger;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getActivityDate() {
+        return activityDate;
+    }
+
+    public void setActivityDate(Date activityDate) {
+        this.activityDate = activityDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Long getLastModifiedUser() {
+        return lastModifiedUser;
+    }
+
+    public void setLastModifiedUser(Long lastModifiedUser) {
+        this.lastModifiedUser = lastModifiedUser;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public Long getListingId() {
+        return listingId;
+    }
+
+    public void setListingId(Long listingId) {
+        this.listingId = listingId;
+    }
+
+    public CertifiedProductBasicSearchResultEntity getListing() {
+        return listing;
+    }
+
+    public void setListing(CertifiedProductBasicSearchResultEntity listing) {
+        this.listing = listing;
+    }
+}
+
