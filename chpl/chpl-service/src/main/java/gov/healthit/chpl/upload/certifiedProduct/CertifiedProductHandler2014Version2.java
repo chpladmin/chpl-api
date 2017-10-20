8dadb5982b21799a27fec0dba74d0f16a87d84ee
Brian Lindsey <brianl9864@gmail.com>
2017-10-20 17:25:43 -0400

    OCD-1804 - add new 2014 template version

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/certifiedProduct/CertifiedProductHandler2014Version2.java
@@ -0,0 +1,28 @@
+package gov.healthit.chpl.upload.certifiedProduct;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import gov.healthit.chpl.upload.certifiedProduct.template.TemplateColumnIndexMap;
+import gov.healthit.chpl.upload.certifiedProduct.template.TemplateColumnIndexMap2015Version2;
+
+/**
+ * Add GAP column 170.314 (b)(5)(B)
+ * @author blindsey
+ *
+ */
+@Component("certifiedProductHandler2014Version2")
+public class CertifiedProductHandler2014Version2 extends CertifiedProductHandler2014Version1 {
+
+    private static final Logger LOGGER = LogManager.getLogger(CertifiedProductHandler2014Version2.class);
+    private TemplateColumnIndexMap templateColumnIndexMap;
+    
+    public CertifiedProductHandler2014Version2() {
+        templateColumnIndexMap = new TemplateColumnIndexMap2015Version2();
+    }
+    
+    @Override
+    public TemplateColumnIndexMap getColumnIndexMap() {
+        return templateColumnIndexMap;
+    }
+}
