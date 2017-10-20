8b1b8ac1197ee87ce639a93420093a46c9e7797c
Brian Lindsey <brianl9864@gmail.com>
2017-10-20 10:44:32 -0400

    OCD-1798 - take out code dealing with spaces

--- chpl/chpl-service/generateDailySEDDetailsDownload.sh
+++ chpl/chpl-service/generateDailySEDDetailsDownload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
 
-# to generate the XML file regularly, add a line to a crontab on the machine hosting the application that looks something like:
+# to generate the SED detail file regularly, add a line to a crontab on the machine hosting the application that looks something like:
 # 15 5 * * * cd /some/directory/chpl-api/chpl/chpl-service && ./generateDailySEDDetailsDownload.sh
 # This will run it at 0515 UTC, which (depending on DST) is 0015 EST
 
@@ -9,16 +9,8 @@
 TIMESTAMP=$(date "+%Y.%m.%d-%H.%M.%S")
 log=logs/log.generateSEDDetailsDownload.$TIMESTAMP.txt
 
-# deal with spaces in filenames by saving off the default file separator (including spaces)
-# and using a different one for this application
-SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
-
 # put header info into log, then output application info into log file
 echo "SED Details generation at: " $TIMESTAMP >> $log
 echo "####################################" >> $log
 java -cp target/chpl-service-jar-with-dependencies.jar gov.healthit.chpl.app.resource.G3Sed2015ResourceCreatorApp 2>&1 >> $log
 echo "####################################" >> $log
-
-# restore filename delimiters
-IFS=$SAVEIFS
