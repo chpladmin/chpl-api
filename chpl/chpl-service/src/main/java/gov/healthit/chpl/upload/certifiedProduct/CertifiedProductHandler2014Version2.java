816a605a688ffd41d22302e1b889f58e223fa53d
Brian Lindsey <brianl9864@gmail.com>
2017-10-20 17:30:26 -0400

    OCD-1804 - add super.handle() to new v2 handle()

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/certifiedProduct/CertifiedProductHandler2014Version2.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/certifiedProduct/CertifiedProductHandler2014Version2.java
@@ -1,10 +1,15 @@
 package gov.healthit.chpl.upload.certifiedProduct;
 
+import org.apache.commons.csv.CSVRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import gov.healthit.chpl.entity.listing.pending.PendingCertifiedProductEntity;
 import gov.healthit.chpl.upload.certifiedProduct.template.TemplateColumnIndexMap;
 import gov.healthit.chpl.upload.certifiedProduct.template.TemplateColumnIndexMap2015Version2;
+import gov.healthit.chpl.web.controller.InvalidArgumentsException;
 
 /**
  * Add GAP column 170.314 (b)(5)(B)
@@ -16,6 +21,11 @@ public class CertifiedProductHandler2014Version2 extends CertifiedProductHandler
 
     private static final Logger LOGGER = LogManager.getLogger(CertifiedProductHandler2014Version2.class);
     private TemplateColumnIndexMap templateColumnIndexMap;
+    
+    public PendingCertifiedProductEntity handle() {
+        PendingCertifiedProductEntity pendingCertifiedProduct = super.handle();
+        return pendingCertifiedProduct;
+    }
     
     public CertifiedProductHandler2014Version2() {
         templateColumnIndexMap = new TemplateColumnIndexMap2015Version2();
