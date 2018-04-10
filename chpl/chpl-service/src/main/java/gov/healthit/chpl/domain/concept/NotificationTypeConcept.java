17a4fdaa881c3db2e8a58f0682724ecd78fb19e9
Andrew Larned <alarned@ainq.com>
2018-04-10 15:04:03 -0400

    OCD-2031: Add cache_status age Notification type

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/concept/NotificationTypeConcept.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/concept/NotificationTypeConcept.java
@@ -2,20 +2,34 @@ package gov.healthit.chpl.domain.concept;
 
 import java.io.Serializable;
 
+/**
+ * Domain objects for types of notifications.
+ * @author alarned
+ *
+ */
 public enum NotificationTypeConcept implements Serializable {
-    ONC_ACB_DAILY_SURVEILLANCE_BROKEN_RULES(
-            "ONC-ACB Daily Surveillance Broken Rules"), ONC_ACB_WEEKLY_SURVEILLANCE_BROKEN_RULES(
-                    "ONC-ACB Weekly Surveillance Broken Rules"), ONC_ACB_WEEKLY_ICS_FAMILY_ERRORS(
-                            "ONC-ACB Weekly ICS Family Errors"), ONC_DAILY_SURVEILLANCE_BROKEN_RULES(
-                                    "ONC Daily Surveillance Broken Rules"), ONC_WEEKLY_SURVEILLANCE_BROKEN_RULES(
-                                            "ONC Weekly Surveillance Broken Rules"), ONC_WEEKLY_ICS_FAMILY_ERRORS(
-                                                    "ONC Weekly ICS Family Errors"), SUMMARY_STATISTICS(
-                                                            "Summary Statistics"), QUESTIONABLE_ACTIVITY(
-                                                                    "Questionable Activity");
+    /** Notification for when the cache is too old. */
+    CACHE_STATUS_AGE_NOTIFICATION("Cache Status Age Notification"),
+    /** ACB specific daily surveillance broken rules. */
+    ONC_ACB_DAILY_SURVEILLANCE_BROKEN_RULES("ONC-ACB Daily Surveillance Broken Rules"),
+    /** ACB specific weekly surveillance broken rules. */
+    ONC_ACB_WEEKLY_SURVEILLANCE_BROKEN_RULES("ONC-ACB Weekly Surveillance Broken Rules"),
+    /** ACB specific ICS family errors. */
+    ONC_ACB_WEEKLY_ICS_FAMILY_ERRORS("ONC-ACB Weekly ICS Family Errors"),
+    /** ONC-wide daily surveillance broken rules. */
+    ONC_DAILY_SURVEILLANCE_BROKEN_RULES("ONC Daily Surveillance Broken Rules"),
+    /** ONC-wide weekly surveillance broken rules. */
+    ONC_WEEKLY_SURVEILLANCE_BROKEN_RULES("ONC Weekly Surveillance Broken Rules"),
+    /** ONC0-wide ICS family errors. */
+    ONC_WEEKLY_ICS_FAMILY_ERRORS("ONC Weekly ICS Family Errors"),
+    /** Questionable Activity. */
+    QUESTIONABLE_ACTIVITY("Questionable Activity"),
+    /** Summary statistics. */
+    SUMMARY_STATISTICS("Summary Statistics");
 
     private String name;
 
-    private NotificationTypeConcept(String name) {
+    NotificationTypeConcept(final String name) {
         this.name = name;
     }
 
