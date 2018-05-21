1b61c0b44f6abcdbe856598cebc93498a0c5d46c
Katy Ekey <kekey@ainq.com>
2018-05-21 11:49:27 -0400

    OCD-2232: include cert result changes in grace period window

--- chpl/chpl-service/src/test/java/gov/healthit/chpl/questionableActivity/CertificationResultTest.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/questionableActivity/CertificationResultTest.java
@@ -3,7 +3,9 @@ package gov.healthit.chpl.questionableActivity;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -30,11 +32,15 @@ import gov.healthit.chpl.caching.UnitTestRules;
 import gov.healthit.chpl.dao.EntityCreationException;
 import gov.healthit.chpl.dao.EntityRetrievalException;
 import gov.healthit.chpl.dao.QuestionableActivityDAO;
+import gov.healthit.chpl.domain.CQMResultDetails;
 import gov.healthit.chpl.domain.CertificationResult;
+import gov.healthit.chpl.domain.CertificationStatus;
+import gov.healthit.chpl.domain.CertificationStatusEvent;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.domain.ListingUpdateRequest;
 import gov.healthit.chpl.domain.concept.QuestionableActivityTriggerConcept;
 import gov.healthit.chpl.dto.questionableActivity.QuestionableActivityCertificationResultDTO;
+import gov.healthit.chpl.dto.questionableActivity.QuestionableActivityListingDTO;
 import gov.healthit.chpl.manager.CertifiedProductDetailsManager;
 import gov.healthit.chpl.web.controller.CertifiedProductController;
 import gov.healthit.chpl.web.controller.InvalidArgumentsException;
@@ -110,6 +116,60 @@ public class CertificationResultTest extends TestCase {
 		
 		SecurityContextHolder.getContext().setAuthentication(null);
 	}
+	
+	@Test
+    @Transactional
+    @Rollback
+    public void testUpdateGapInsideActivityThreshold_DoesNotRecordActivity() throws
+        EntityCreationException, EntityRetrievalException,
+        ValidationException, InvalidArgumentsException, JsonProcessingException,
+        MissingReasonException, IOException {
+        SecurityContextHolder.getContext().setAuthentication(adminUser);
+
+        //make the certification date be now
+        //and update the listing so the last updated date minus certification date is
+        //within the questionable activity threshold
+        Date now = new Date();
+        CertifiedProductSearchDetails existingListing = cpdManager.getCertifiedProductDetails(1L);
+        existingListing.setCertificationDate(now.getTime());
+        existingListing.getCertificationEvents().clear();
+        CertificationStatusEvent statusEvent = new CertificationStatusEvent();
+        CertificationStatus status = new CertificationStatus();
+        status.setId(1L);
+        status.setName("Active");
+        statusEvent.setStatus(status);
+        statusEvent.setEventDate(now.getTime());
+        existingListing.getCertificationEvents().add(statusEvent);
+
+        ListingUpdateRequest updateRequest = new ListingUpdateRequest();
+        updateRequest.setBanDeveloper(false);
+        updateRequest.setListing(existingListing);
+        cpController.updateCertifiedProduct(updateRequest);
+        
+        //confirm the certification date was changed properly
+        CertifiedProductSearchDetails updatedListing = cpdManager.getCertifiedProductDetails(1L);
+        assertEquals(now.getTime(), updatedListing.getCertificationDate().longValue());
+        
+        //perform an update that would generate questionable activity outside
+        //of the threshold but make sure that no questionable activity was entered.
+        Date beforeActivity = new Date();
+        CertifiedProductSearchDetails listing = cpdManager.getCertifiedProductDetails(1L);
+        for(CertificationResult certResult : listing.getCertificationResults()) {
+            if(certResult.getId().longValue() == 1) {
+                certResult.setGap(Boolean.FALSE);
+            }
+        }
+        updateRequest = new ListingUpdateRequest();
+        updateRequest.setBanDeveloper(false);
+        updateRequest.setListing(listing);
+        cpController.updateCertifiedProduct(updateRequest);
+        Date afterActivity = new Date();
+
+        List<QuestionableActivityCertificationResultDTO> activities =
+                qaDao.findCertificationResultActivityBetweenDates(beforeActivity, afterActivity);
+        assertTrue(activities == null || activities.size() == 0);
+        SecurityContextHolder.getContext().setAuthentication(null);
+    }
 	
 	@Test
     @Transactional
