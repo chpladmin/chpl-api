795f276079ce9a2e95c534d5bdfa473f4e06cdce
Andrew Larned <andrew.larned@gmail.com>
2018-06-08 08:32:50 -0400

    OCD-2271: Move Scheduler back to service
    Scheduler requires access to Job Class, but so does service,
    in order to add/edit/remve Triggers. Both packages also need
    access to the properties files and the xml job definitions,
    so it doesn't make sense to have them separate right now.
    
    Also added initialization flag to schedule running thing
    to be used on STG to manage database updates.

--- chpl/chpl-scheduler/src/main/java/gov/healthit/chpl/scheduler/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- *
- */
-package gov.healthit.chpl.scheduler;
--- chpl/chpl-service/runScheduler.sh
+++ chpl/chpl-service/runScheduler.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
 
+# parse command line inputs
+#   default to not initializing the triggers
+init=''
+
+while getopts 'ih?' flag; do
+    case "${flag}" in
+        i) init='init' ;;
+        *) printf 'Usage: %s: [-i]
+   -i: initialize the triggers
+   -h, -?: print this message\n' $0; exit 0 ;;
+    esac
+done
+
 # create timestamp and filename
 TIMESTAMP=$(date "+%Y.%m.%d-%H.%M.%S")
 log=logs/log.runScheduler.$TIMESTAMP.txt
@@ -8,5 +21,5 @@ log=logs/log.runScheduler.$TIMESTAMP.txt
 # put header info into log, then output application info into log file
 echo "Starting Scheduler at: " $TIMESTAMP >> $log
 echo "####################################" >> $log
-java -Xmx800m -cp target/chpl-scheduler-jar-with-dependencies.jar gov.healthit.chpl.scheduler.ChplScheduler >&1 >> $log
+java -Xmx800m -cp target/chpl-service-jar-with-dependencies.jar gov.healthit.chpl.scheduler.ChplScheduler $init >&1 >> $log
 echo "####################################" >> $log
