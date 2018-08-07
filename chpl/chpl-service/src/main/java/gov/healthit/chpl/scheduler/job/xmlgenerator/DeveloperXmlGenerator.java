821dd97da266349bfacdefd14c76aeb869d0af71
tmy1313 <tyoung@ainq.com>
2018-08-07 15:54:12 -0400

    Save my existing files

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/xmlgenerator/DeveloperXmlGenerator.java
@@ -0,0 +1,22 @@
+package gov.healthit.chpl.scheduler.job.xmlgenerator;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import gov.healthit.chpl.domain.Developer;
+
+public class DeveloperXmlGenerator extends XmlGenerator{
+    public static void addDeveloper(Developer dev, String rootNodeName, XMLStreamWriter sw) throws XMLStreamException {
+        sw.writeStartElement(rootNodeName);
+        AddressXmlGenerator.addAddress(dev.getAddress(), "address", sw);
+        ContactXmlGenerator.addContact(dev.getContact(),"contact", sw);
+        createSimpleElement(dev.getDeveloperCode(), "developerCode", sw);
+        createSimpleElement(dev.getDeveloperId(), "developerId", sw);
+        createSimpleElement(dev.getName(), "name", sw);
+        DeveloperStatusXmlGenerator.add(dev.getStatus(), "status", sw);
+        DeveloperStatusEventXmlGenerator.add(dev.getStatusEvents(), "statusEvents", sw);
+        TransparencyAttestationMapXmlGenerator.add(dev.getTransparencyAttestations(), "transparencyAttestations", sw);
+        createSimpleElement(dev.getWebsite(), "website", sw);
+        sw.writeEndElement();
+    }
+}
