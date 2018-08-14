10a5a5a752acb1dad339bda4c8e579001814a1da
Katy Ekey <kekey@ainq.com>
2018-08-14 08:41:38 -0400

    OCD-2364: move existing reviewer tests to new pkg structure

--- chpl/chpl-service/src/test/java/gov/healthit/chpl/validation/pendingListing/reviewer/PendingListingTestFunctionalityReviewerTest.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/validation/pendingListing/reviewer/PendingListingTestFunctionalityReviewerTest.java
@@ -1,4 +1,4 @@
-package gov.heatlhit.chpl.validation.certifiedProduct;
+package gov.healthit.chpl.validation.pendingListing.reviewer;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -32,10 +32,6 @@ import org.springframework.transaction.annotation.Transactional;
 import gov.healthit.chpl.dao.CertificationCriterionDAO;
 import gov.healthit.chpl.dao.PracticeTypeDAO;
 import gov.healthit.chpl.dao.TestFunctionalityDAO;
-import gov.healthit.chpl.domain.CertificationResult;
-import gov.healthit.chpl.domain.CertificationResultTestFunctionality;
-import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
-import gov.healthit.chpl.domain.InheritedCertificationStatus;
 import gov.healthit.chpl.dto.CertificationCriterionDTO;
 import gov.healthit.chpl.dto.PendingCertificationResultDTO;
 import gov.healthit.chpl.dto.PendingCertificationResultTestFunctionalityDTO;
@@ -43,11 +39,11 @@ import gov.healthit.chpl.dto.PendingCertifiedProductDTO;
 import gov.healthit.chpl.dto.PracticeTypeDTO;
 import gov.healthit.chpl.dto.TestFunctionalityDTO;
 import gov.healthit.chpl.util.ErrorMessageUtil;
-import gov.healthit.chpl.validation.listing.reviewer.TestFunctionalityReviewer;
+import gov.healthit.chpl.validation.pendingListing.reviewer.TestFunctionalityReviewer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { gov.healthit.chpl.CHPLTestConfig.class })
-public class CertifiedProductTestFunctionalityValidatorTest {
+public class PendingListingTestFunctionalityReviewerTest {
     private static final Long EDITION_2015_ID = 3L;
     private static final Long EDITION_2014_ID = 2L;
 
@@ -67,10 +63,7 @@ public class CertifiedProductTestFunctionalityValidatorTest {
     private ErrorMessageUtil msgUtil;
     
     @InjectMocks
-    private TestFunctionalityReviewer tfReviewer;
-    
-    @InjectMocks
-    private gov.healthit.chpl.validation.pendingListing.reviewer.TestFunctionalityReviewer pendingTfReviewer;
+    private TestFunctionalityReviewer pendingTfReviewer;
     
     @Before
     public void setup() {
@@ -85,38 +78,8 @@ public class CertifiedProductTestFunctionalityValidatorTest {
                 ArgumentMatchers.eq(LocaleContextHolder.getLocale())))
             .thenReturn("In Criteria %s, Test Functionality %s is for Criteria %s and is not valid for Criteria Type %s.");
     }
-    
+
     //Case 1: A valid test functionality
-    @Transactional
-    @Rollback(true)
-    @Test
-    public void validateCertifiedProductTestFunctionality() {
-        Mockito.when(testFunctionalityDAO.getByNumberAndEdition(ArgumentMatchers.anyString(), ArgumentMatchers.anyLong()))
-                .thenReturn(getTestFunctionalityId_7());
-        Mockito.when(certificationCriterionDAO.getByNameAndYear(ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
-                .thenReturn(getCertificationCriterion_a6());
-        
-        
-        CertifiedProductSearchDetails listing = createListing("2014");
-        List<CertificationResult> certResults = new ArrayList<CertificationResult>();
-        CertificationResult certResult = createCertResult("170.314 (a)(6)");
-        CertificationResultTestFunctionality crtf = new CertificationResultTestFunctionality();
-        crtf.setDescription("No description required");
-        crtf.setId(1l);
-        crtf.setName("(a)(6)(i)");
-        crtf.setTestFunctionalityId(7l);
-        crtf.setYear("2014");
-        certResult.setTestFunctionality(new ArrayList<CertificationResultTestFunctionality>());
-        certResult.getTestFunctionality().add(crtf);
-        certResults.add(certResult);
-        listing.getCertificationResults().add(certResult);
-        
-        tfReviewer.review(listing);
-        
-        assertFalse(doesTestFunctionalityPracticeTypeErrorMessageExist(listing.getErrorMessages()));
-        assertFalse(doesTestFunctionalityCriterionErrorMessageExist(listing.getErrorMessages()));
-    }
-    
     @Transactional
     @Rollback(true)
     @Test
@@ -145,47 +108,8 @@ public class CertifiedProductTestFunctionalityValidatorTest {
         assertFalse(doesTestFunctionalityPracticeTypeErrorMessageExist(listing.getErrorMessages()));
         assertFalse(doesTestFunctionalityCriterionErrorMessageExist(listing.getErrorMessages()));
     }
-    
-    
+
     //Case 2: An invalid test functionality based on practice type
-    @Transactional
-    @Rollback(true)
-    @Test
-    public void validateCertifiedProductTestFunctionalityPracticeTypeMismatch() throws Exception {
-        Mockito.when(testFunctionalityDAO.getByNumberAndEdition(ArgumentMatchers.anyString(), ArgumentMatchers.anyLong()))
-                .thenReturn(getTestFunctionalityId_18());
-        Mockito.when(certificationCriterionDAO.getByNameAndYear(ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
-                .thenReturn(getCertificationCriterion_a6());
-        Mockito.when(messageSource.getMessage(ArgumentMatchers.any(DefaultMessageSourceResolvable.class), ArgumentMatchers.any(Locale.class)))
-                .thenReturn("In Criteria %s, Test Functionality %s is for %s Settings and is not valid for Practice Type %s.");
-        Mockito.doReturn("In Criteria 170.314 (a)(6), Test Functionality (a)(6)(11) is for "
-                + "other Settings and is not valid for Practice Type Ambulatory.")
-        .when(msgUtil).getMessage(
-                ArgumentMatchers.eq("listing.criteria.testFunctionalityPracticeTypeMismatch"),
-                ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), 
-                ArgumentMatchers.anyString(), ArgumentMatchers.anyString());
-        
-        CertifiedProductSearchDetails listing = createListing("2014");
-        List<CertificationResult> certResults = new ArrayList<CertificationResult>();
-        CertificationResult certResult = createCertResult("170.314 (a)(6)");
-        CertificationResultTestFunctionality crtf = new CertificationResultTestFunctionality();
-        crtf.setDescription("No description required");
-        crtf.setId(1l);
-        crtf.setName("(a)(6)(ii)");
-        crtf.setTestFunctionalityId(18l);
-        crtf.setYear("2104");
-        certResult.setTestFunctionality(new ArrayList<CertificationResultTestFunctionality>());
-        certResult.getTestFunctionality().add(crtf);
-        certResults.add(certResult);
-        listing.getCertificationResults().add(certResult);
-        
-        tfReviewer.review(listing);
-        
-        assertTrue(doesTestFunctionalityPracticeTypeErrorMessageExist(listing.getErrorMessages()));
-    }
-    
-       
-    
     @Transactional
     @Rollback(true)
     @Test
@@ -214,41 +138,8 @@ public class CertifiedProductTestFunctionalityValidatorTest {
         assertTrue(doesTestFunctionalityPracticeTypeErrorMessageExist(listing.getErrorMessages()));
     }
 
-    //Case 3: An invalid test functionality based on certifcation criterion
-    @Transactional
-    @Rollback(true)
-    @Test
-    public void validateCertifiedProductTestFunctionalityCertificationCriterionMismatch() {
-        Mockito.when(testFunctionalityDAO.getByNumberAndEdition(ArgumentMatchers.anyString(), ArgumentMatchers.anyLong()))
-                .thenReturn(getTestFunctionalityId_7());
-        Mockito.when(certificationCriterionDAO.getByNameAndYear(ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
-                .thenReturn(getCertificationCriterion_a7());
-        Mockito.doReturn("In Criteria 170.314 (a)(6), Test Functionality (a)(6)(11) is for "
-                + "Criteria other and is not valid for Criteria 170.314 (a)(6).")
-        .when(msgUtil).getMessage(
-                ArgumentMatchers.eq("listing.criteria.testFunctionalityCriterionMismatch"),
-                ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), 
-                ArgumentMatchers.anyString(), ArgumentMatchers.anyString());
 
-        CertifiedProductSearchDetails listing = createListing("2014");
-        List<CertificationResult> certResults = new ArrayList<CertificationResult>();
-        CertificationResult certResult = createCertResult("170.314 (a)(6)");
-        CertificationResultTestFunctionality crtf = new CertificationResultTestFunctionality();
-        crtf.setDescription("No description required");
-        crtf.setId(1l);
-        crtf.setName("(a)(7)(i)");
-        crtf.setTestFunctionalityId(18l);
-        crtf.setYear("2104");
-        certResult.setTestFunctionality(new ArrayList<CertificationResultTestFunctionality>());
-        certResult.getTestFunctionality().add(crtf);
-        certResults.add(certResult);
-        listing.getCertificationResults().add(certResult);
-        
-        tfReviewer.review(listing);
-        
-        assertTrue(doesTestFunctionalityCriterionErrorMessageExist(listing.getErrorMessages()));
-    }
-    
+    //Case 3: An invalid test functionality based on certifcation criterion
     @Transactional
     @Rollback(true)
     @Test
@@ -347,57 +238,6 @@ public class CertifiedProductTestFunctionalityValidatorTest {
         pendingCertResult.setTestTasks(null);
         pendingCertResult.setMeetsCriteria(true);
         return pendingCertResult;
-    }
-
-    private CertifiedProductSearchDetails createListing(final String year) {
-        CertifiedProductSearchDetails listing = new CertifiedProductSearchDetails();
-        String certDateString = "11-09-2016";
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-        try {
-            Date inputDate = dateFormat.parse(certDateString);
-            listing.setCertificationDate(inputDate.getTime());
-        } catch (ParseException ex) {
-            fail(ex.getMessage());
-        }
-        listing.setId(1L);
-        if (year.equals("2015")) {
-            listing.getCertificationEdition().put("name", "2015");
-            listing.getCertificationEdition().put("id", "3");
-            listing.setChplProductNumber("15.07.07.2642.IC04.36.00.1.160402");
-            listing.setPracticeType(null);
-        } else if (year.equals("2014")) {
-            listing.getCertificationEdition().put("name", "2014");
-            listing.getCertificationEdition().put("id", "2");
-            listing.setChplProductNumber("14.07.07.2642.IC04.36.00.1.160402");
-            listing.getPracticeType().put("name", "Ambulatory");
-            listing.getPracticeType().put("id", "1");
-            listing.getClassificationType().put("name", "Modular EHR");
-        }
-        InheritedCertificationStatus ics = new InheritedCertificationStatus();
-        ics.setInherits(Boolean.FALSE);
-        listing.setIcs(ics);
-        return listing;
-    }
-
-    private CertificationResult createCertResult(final String number) {
-        CertificationResult certResult = new CertificationResult();
-        certResult.setId(1L);
-        certResult.setAdditionalSoftware(null);
-        certResult.setApiDocumentation(null);
-        certResult.setG1Success(false);
-        certResult.setG2Success(false);
-        certResult.setGap(null);
-        certResult.setNumber(number);
-        certResult.setPrivacySecurityFramework("Approach 1 Approach 2");
-        certResult.setSed(null);
-        certResult.setG1Success(false);
-        certResult.setG2Success(false);
-        certResult.setTestDataUsed(null);
-        certResult.setTestFunctionality(null);
-        certResult.setTestProcedures(null);
-        certResult.setTestStandards(null);
-        certResult.setSuccess(true);
-        return certResult;
     }
 
     private CertificationCriterionDTO getCertificationCriterion_a6() {
