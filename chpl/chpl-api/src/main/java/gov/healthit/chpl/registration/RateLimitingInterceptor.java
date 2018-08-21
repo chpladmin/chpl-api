b91ce8fa085730fe8a5a1e8d310d70d78cdba84f
Brian Lindsey <brianl9864@gmail.com>
2018-08-21 09:24:21 -0400

    throttle - add info to 529 message and implement api key whitelisting

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/registration/RateLimitingInterceptor.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/registration/RateLimitingInterceptor.java
@@ -1,22 +1,27 @@
 package gov.healthit.chpl.registration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.EnvironmentAware;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class RateLimitingInterceptor extends HandlerInterceptorAdapter implements EnvironmentAware{
@@ -26,13 +31,24 @@ public class RateLimitingInterceptor extends HandlerInterceptorAdapter implement
     @Autowired
     private Environment env;
     
+    @Autowired
+    private MessageSource messageSource;
+    
     private String timeUnit;
     
     private int limit;
  
     private Map<String, SimpleRateLimiter> limiters = new ConcurrentHashMap<>();
     
+    private List<String> whitelist = new ArrayList<String>();
+    
     public RateLimitingInterceptor(){}
+    
+    public String getMessage(final String messageCode, final String input, final String input2) {
+        return String.format(messageSource.getMessage(
+                new DefaultMessageSourceResolvable(messageCode),
+                LocaleContextHolder.getLocale()), input, input2);
+    }
     
     @Override
     public void setEnvironment(Environment env) {
@@ -40,23 +56,38 @@ public class RateLimitingInterceptor extends HandlerInterceptorAdapter implement
         this.env = env;
         this.timeUnit = env.getProperty("rateLimitTimeUnit");
         this.limit = Integer.valueOf(env.getProperty("rateTokenLimit"));
+        this.whitelist = Arrays.asList(env.getProperty("whitelist").split("\\s*,\\s*"));
     }
      
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        String clientId = request.getParameter("api_key");
+        String key = null;
+        String clientIdParam = request.getParameter("api_key");
+        String clientIdHeader = request.getHeader("API-Key");
+        
+        if (clientIdHeader == clientIdParam) {
+            key = clientIdHeader;
+        } else {
+            if (clientIdHeader == null) {
+                key = clientIdParam;
+            } else if (clientIdParam == null) {
+                key = clientIdHeader;
+            } else {
+                return false;
+            }
+        }
         
         // let non-API requests pass
-        if (clientId == null) {
+        if (whitelist.contains(key)) {
             return true;
         }
-        SimpleRateLimiter rateLimiter = getRateLimiter(clientId);
+        SimpleRateLimiter rateLimiter = getRateLimiter(key);
         boolean allowRequest = rateLimiter.tryAcquire();
      
         if (!allowRequest) {
-            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-            logger.info("Client with API KEY: " + clientId +  " went over API KEY limit of " + limit + ".");
+            response.sendError(HttpStatus.TOO_MANY_REQUESTS.value(), getMessage("apikey.limit", String.valueOf(limit), timeUnit));
+            logger.info("Client with API KEY: " + key +  " went over API KEY limit of " + limit + ".");
         }
         response.addHeader("X-RateLimit-Limit", String.valueOf(limit));
         return allowRequest;
