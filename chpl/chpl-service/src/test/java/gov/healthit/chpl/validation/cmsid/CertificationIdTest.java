0c456d8021f27e3e7b9ea4ab78f4c1f23b27e0d6
Brian Lindsey <brianl9864@gmail.com>
2018-09-12 11:38:25 -0400

    OCD-1595 - fix formatting

--- chpl/chpl-service/src/test/java/gov/healthit/chpl/validation/cmsid/CertificationIdTest.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/validation/cmsid/CertificationIdTest.java
@@ -26,14 +26,16 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 import junit.framework.TestCase;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { gov.healthit.chpl.CHPLTestConfig.class })
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-    DirtiesContextTestExecutionListener.class,
-    TransactionalTestExecutionListener.class,
-    DbUnitTestExecutionListener.class })
+@ContextConfiguration(classes = {
+    gov.healthit.chpl.CHPLTestConfig.class
+})
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class
+})
 @DatabaseSetup("classpath:data/testData.xml")
 public class CertificationIdTest extends TestCase {
-    
+
     @Autowired
     private CertifiedProductManager certifiedProductManager;
 
@@ -43,63 +45,63 @@ public class CertificationIdTest extends TestCase {
     @Test
     @Transactional
     public void certificationId2014InfoMessagesTest() {
-        
+
         List<Long> productIdList = new ArrayList<Long>();
         productIdList.add(294L);
-        
+
         Validator validator = ValidatorFactory.getValidator("2014");
-        
+
         // Lookup Criteria for Validating
         List<String> criteriaDtos = certificationIdManager.getCriteriaNumbersMetByCertifiedProductIds(productIdList);
 
         // Lookup CQMs for Validating
         List<CQMMetDTO> cqmDtos = certificationIdManager.getCqmsMetByCertifiedProductIds(productIdList);
-        
+
         validator.validate(criteriaDtos, cqmDtos, new ArrayList<Integer>(2014));
         assertNotNull(validator.getMissingX());
         assertNotNull(validator.getMissingCombo());
         assertNotNull(validator.getMissingOr());
         assertNotNull(validator.getMissingAnd());
     }
-    
+
     @Test
     @Transactional
     public void certificationId2015InfoMessagesTest() {
-        
+
         List<Long> productIdList = new ArrayList<Long>();
         productIdList.add(9261L);
-        
+
         Validator validator = ValidatorFactory.getValidator("2015");
-        
+
         // Lookup Criteria for Validating
         List<String> criteriaDtos = certificationIdManager.getCriteriaNumbersMetByCertifiedProductIds(productIdList);
 
         // Lookup CQMs for Validating
         List<CQMMetDTO> cqmDtos = certificationIdManager.getCqmsMetByCertifiedProductIds(productIdList);
-        
+
         validator.validate(criteriaDtos, cqmDtos, new ArrayList<Integer>(2015));
         assertTrue(validator.getMissingX().isEmpty());
         assertTrue(validator.getMissingCombo().isEmpty());
         assertNotNull(validator.getMissingOr());
         assertNotNull(validator.getMissingAnd());
     }
-    
+
     @Test
     @Transactional
     public void certificationId20142015InfoMessagesTest() {
-        
+
         List<Long> productIdList = new ArrayList<Long>();
         productIdList.add(294L);
         productIdList.add(9261L);
-        
+
         Validator validator = ValidatorFactory.getValidator("2014/2015");
-        
+
         // Lookup Criteria for Validating
         List<String> criteriaDtos = certificationIdManager.getCriteriaNumbersMetByCertifiedProductIds(productIdList);
 
         // Lookup CQMs for Validating
         List<CQMMetDTO> cqmDtos = certificationIdManager.getCqmsMetByCertifiedProductIds(productIdList);
-        
+
         validator.validate(criteriaDtos, cqmDtos, new ArrayList<Integer>(2014));
         assertTrue(validator.getMissingX().isEmpty());
         assertNotNull(validator.getMissingCombo());
