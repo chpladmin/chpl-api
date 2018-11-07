bd1c1a8e9d64e4eef00cacf4a198f7a59762e0ff
Andrew Larned <andrew.larned@gmail.com>
2018-11-07 11:12:09 -0500

    Update trigger script to support un/pw when required for job
    [#OCD-2530]

--- chpl/chpl-service/triggerJob.sh
+++ chpl/chpl-service/triggerJob.sh
@@ -4,19 +4,38 @@
 action=''
 job=''
 group=''
+username=''
+password=''
 
-while getopts 'a:j:g:h?' flag; do
+while getopts 'a:j:g:u:h?' flag; do
     case "${flag}" in
         a) action="${OPTARG}" ;;
         j) job="${OPTARG}" ;;
         g) group="${OPTARG}" ;;
-        *) printf 'Usage: %s: [-a action] [-j jobName] [-g groupName ]
+        u) username="${OPTARG}" ;;
+        *) printf 'Usage: %s: [-a action] [-j jobName] [-g groupName] [-u username]
    -a: action (one of "list" "start" "help" "interrupt")
    -j: job name (required if -g flag is used)
-   -g: group name (optional)
+   -g: group name (optional, required if username/password is needed)
+   -u: username (required for certain jobs)
    -h, -?: print this message\n' $0; exit 0 ;;
     esac
 done
+
+# group missing
+if [ "$group" = "" ] && [ ! "$username" = "" ]; then
+    printf "\nError: group must be specified if username is\n\n"
+fi
+
+# prompt for password if username was provided
+if [ ! "$username" = "" ]; then
+    stty -echo
+    printf 'If your password has spaces or weird characters I have no idea what will happen next. Oh, and if you Ctrl-C before entering your password your terminal will stop echoing whatever you type. Good luck!\n'
+    printf 'Enter password: '
+    read password
+    stty echo
+    printf '\n'
+fi
 
 # job / group don't mismatch
 if [ "$job" = "" ] && [ ! "$group" = "" ]; then
@@ -24,4 +43,4 @@ if [ "$job" = "" ] && [ ! "$group" = "" ]; then
     exit 1
 fi
 
-java -Xmx800m -Dchpl.home=/opt/chpl -Dchpl.appName=TriggerJob -Dlog4j.configurationFile=log4j2-app.xml -cp target/chpl-service-jar-with-dependencies.jar gov.healthit.chpl.app.TriggerJob $action $job $group
+java -Xmx800m -Dchpl.home=/opt/chpl -Dchpl.appName=TriggerJob -Dlog4j.configurationFile=log4j2-app.xml -cp target/chpl-service-jar-with-dependencies.jar gov.healthit.chpl.app.TriggerJob $action $job $group $username $password
