5fa2fa54eff7a02bd19a6bf14fcb5380ea994f99
tmy1313 <tyoung@ainq.com>
2019-01-15 14:13:48 -0500

    feat: add ROLE_ONC and ROLE_ADMIN to upload pending permissions
    OCD-2655

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/pendingsurveillance/UploadActionPermissions.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/pendingsurveillance/UploadActionPermissions.java
@@ -7,33 +7,45 @@ import gov.healthit.chpl.auth.Util;
 import gov.healthit.chpl.dao.CertifiedProductDAO;
 import gov.healthit.chpl.domain.Surveillance;
 import gov.healthit.chpl.dto.CertifiedProductDTO;
-import gov.healthit.chpl.exception.EntityRetrievalException;
 import gov.healthit.chpl.permissions.domains.ActionPermissions;
 
 @Component
 public class UploadActionPermissions extends ActionPermissions{
     @Autowired
-    private CertifiedProductDAO cpDAO;
+    private CertifiedProductDAO certifiedProductDAO;
 
+    @Autowired
+    public UploadActionPermissions(CertifiedProductDAO certifiedProductDAO) {
+        this.certifiedProductDAO = certifiedProductDAO;
+    }
     @Override
     public boolean hasAccess() {
-        return Util.isUserRoleAcbAdmin();
+        return Util.isUserRoleAcbAdmin() || Util.isUserRoleAdmin() || Util.isUserRoleOnc();
     }
 
     @Override
     public boolean hasAccess(Object obj) {
-        if (!(obj instanceof Surveillance)) {
-            return false;
-        } else if (Util.isUserRoleAcbAdmin()) {
-            Surveillance surveillance = (Surveillance) obj;
-            CertifiedProductDTO dto = null;
-            try {
-                dto = cpDAO.getById(surveillance.getCertifiedProduct().getId());
-            } catch (EntityRetrievalException e) {
+        try {
+            if (!(obj instanceof Surveillance)) {
+                return false;
+            } else if (Util.isUserRoleAcbAdmin()) {
+                Surveillance surv = (Surveillance) obj;
+                //Make sure the pending surveillance belongs to the correct authority
+                if (!surv.getAuthority().equals(Util.ROLE_ACB_AUTHORITY)) {
+                    return false;
+                } else {
+                    //Make sure the user has access to the pending surv acb
+                    CertifiedProductDTO dto = certifiedProductDAO.getById(surv.getCertifiedProduct().getId());
+                    return isAcbValidForCurrentUser(dto.getCertificationBodyId());
+                }
+            } else if (Util.isUserRoleOnc() || Util.isUserRoleAdmin()) {
+                Surveillance surv = (Surveillance) obj;
+                //Make sure the pending surveillance belongs to the correct authority
+                return surv.getAuthority().equals(Util.ROLE_ONC_AUTHORITY);
+            } else {
                 return false;
             }
-            return isAcbValidForCurrentUser(dto.getCertificationBodyId());
-        } else {
+        } catch (Exception e) {
             return false;
         }
     }
