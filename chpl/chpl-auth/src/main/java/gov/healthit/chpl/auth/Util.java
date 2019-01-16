a7061995ae294d2fa4db1d2e3958496968b5b1b3
Andrew Larned <andrew.larned@gmail.com>
2019-01-16 09:58:32 -0500

    feat: add util for user's audit id
    [#OCD-2653]

--- chpl/chpl-auth/src/main/java/gov/healthit/chpl/auth/Util.java
+++ chpl/chpl-auth/src/main/java/gov/healthit/chpl/auth/Util.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import gov.healthit.chpl.auth.domain.Authority;
 import gov.healthit.chpl.auth.permission.GrantedPermission;
+import gov.healthit.chpl.auth.user.JWTAuthenticatedUser;
 import gov.healthit.chpl.auth.user.User;
 
 public class Util {
@@ -54,6 +55,24 @@ public class Util {
             user = (User) auth;
         }
         return user;
+    }
+
+    /**
+     * Get the ID of the active user. If the active user is being impersonated, get the id of the impersonating user instead.
+     * @return the user's audit-ready id
+     */
+    public static long getAuditId() {
+        JWTAuthenticatedUser user = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth instanceof JWTAuthenticatedUser) {
+            user = (JWTAuthenticatedUser) auth;
+        }
+        if (user.getImpersonatingUser() != null) {
+            return user.getImpersonatingUser().getId();
+        } else {
+            return user.getId();
+        }
     }
 
     public static String fromInt(final Integer toStr) {
