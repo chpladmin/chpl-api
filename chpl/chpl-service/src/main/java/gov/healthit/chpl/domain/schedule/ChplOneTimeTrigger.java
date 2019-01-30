dd52bf223b047fbaf744188a9ee689d99cfa2a48
tmy1313 <tyoung@ainq.com>
2019-01-30 11:17:46 -0500

    style: remove commented out code
    OCD-2634

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/schedule/ChplOneTimeTrigger.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/schedule/ChplOneTimeTrigger.java
@@ -2,35 +2,24 @@ package gov.healthit.chpl.domain.schedule;
 
 import java.io.Serializable;
 
-public class ChplOneTimeTrigger implements Serializable{
+public class ChplOneTimeTrigger implements Serializable {
     private static final long serialVersionUID = -3583084568526672515L;
 
-    //private String name;
-    //private String group;
     private ChplJob job;
     private Long runDateMillis;
 
-    //    public String getName() {
-    //        return name;
-    //    }
-    //    public void setName(String name) {
-    //        this.name = name;
-    //    }
-    //    public String getGroup() {
-    //        return group;
-    //    }
-    //    public void setGroup(String group) {
-    //        this.group = group;
-    //    }
     public ChplJob getJob() {
         return job;
     }
+
     public void setJob(ChplJob job) {
         this.job = job;
     }
+
     public Long getRunDateMillis() {
         return runDateMillis;
     }
+
     public void setRunDateMillis(Long runDateMillis) {
         this.runDateMillis = runDateMillis;
     }
