ab4f7247e97dd624ecab42480ecda45f3a2a59f9
tmy1313 <tyoung@ainq.com>
2019-02-01 09:30:02 -0500

    Revert "feat: switch CertificationResultManagerImpl to use new security"
    This reverts commit bad65d019b9d872fb1456aee616d4ab7d67f6d0a.

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/Permissions.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/Permissions.java
@@ -6,22 +6,18 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import gov.healthit.chpl.permissions.domains.CertificationResultsDomainPermissions;
 import gov.healthit.chpl.permissions.domains.DomainPermissions;
 import gov.healthit.chpl.permissions.domains.PendingSurveillanceDomainPermissions;
 
 @Component
 public class Permissions {
     public static final String PENDING_SURVEILLANCE = "PENDING_SURVEILLANCE";
-    public static final String CERTIFICATION_RESULTS = "CERTIFICATION_RESULTS";
 
     private Map<String, DomainPermissions> domainPermissions = new HashMap<String, DomainPermissions>();
 
     @Autowired
-    public Permissions(final PendingSurveillanceDomainPermissions pendingSurveillanceDomainPermissions,
-            final CertificationResultsDomainPermissions certificationResultsDomainPermissions) {
+    public Permissions(final PendingSurveillanceDomainPermissions pendingSurveillanceDomainPermissions) {
         domainPermissions.put(PENDING_SURVEILLANCE, pendingSurveillanceDomainPermissions);
-        domainPermissions.put(CERTIFICATION_RESULTS, certificationResultsDomainPermissions);
     }
 
     public boolean hasAccess(final String domain, final String action) {
