3a8ad5b94d28c1e5b4324016e4861f1d444ccb5f
tmy1313 <tyoung@ainq.com>
2019-02-11 10:31:21 -0500

    feat: add ability to find user's roles to ResourcePermissions
    OCD-2679

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/ResourcePermissions.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/ResourcePermissions.java
@@ -9,13 +9,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import gov.healthit.chpl.auth.Util;
+import gov.healthit.chpl.auth.domain.Authority;
 import gov.healthit.chpl.auth.dto.UserDTO;
 import gov.healthit.chpl.auth.user.User;
 import gov.healthit.chpl.dao.CertificationBodyDAO;
 import gov.healthit.chpl.dao.UserCertificationBodyMapDAO;
 import gov.healthit.chpl.dao.UserRoleMapDAO;
 import gov.healthit.chpl.dto.CertificationBodyDTO;
+import gov.healthit.chpl.dto.RoleDTO;
 import gov.healthit.chpl.dto.UserCertificationBodyMapDTO;
+import gov.healthit.chpl.dto.UserRoleMapDTO;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 
 @Component
@@ -31,6 +34,7 @@ public class ResourcePermissions {
             final ErrorMessageUtil errorMessageUtil) {
 
         this.userCertificationBodyMapDAO = userCertificationBodyMapDAO;
+        this.userRoleMapDAO = userRoleMapDAO;
         this.acbDAO = acbDAO;
         this.errorMessageUtil = errorMessageUtil;
     }
@@ -64,13 +68,64 @@ public class ResourcePermissions {
     }
 
     @Transactional(readOnly = true)
-    public CertificationBodyDTO getIfPermissionById(final Long id) {
+    public CertificationBodyDTO getAcbIfPermissionById(final Long id) {
         List<CertificationBodyDTO> dtos = getAllAcbsForCurrentUser();
         dtos.stream().filter(dto -> dto.getId().equals(id));
         if (dtos.size() == 0) {
             throw new AccessDeniedException(errorMessageUtil.getMessage("access.denied"));
         }
         return dtos.get(0);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RoleDTO> getRolesByUserId(final Long userId) {
+        List<RoleDTO> roles = new ArrayList<RoleDTO>();
+        List<UserRoleMapDTO> userRoleMaps = userRoleMapDAO.getByUserId(userId);
+        if (userRoleMaps != null) {
+            for (UserRoleMapDTO dto : userRoleMaps) {
+                roles.add(dto.getRole());
+            }
+        }
+        return roles;
+    }
+
+    public boolean isUserRoleAdmin() {
+        return doesUserHaveRole(Authority.ROLE_ADMIN);
+    }
+
+    public boolean isUserRoleOnc() {
+        return doesUserHaveRole(Authority.ROLE_ONC);
+    }
+
+    public boolean isUserRoleCmsStaff() {
+        return doesUserHaveRole(Authority.ROLE_CMS_STAFF);
+    }
+
+    public boolean isUserRoleAcbAdmin() {
+        return doesUserHaveRole(Authority.ROLE_ACB);
+    }
+
+    public boolean isUserRoleAtlAdmin() {
+        return doesUserHaveRole(Authority.ROLE_ATL);
+    }
+
+    public boolean isUserRoleUserCreator() {
+        return doesUserHaveRole(Authority.ROLE_USER_CREATOR);
+    }
+
+    private boolean doesUserHaveRole(final String authority) {
+        User user = Util.getCurrentUser();
+        if (user == null) {
+            return false;
+        }
+
+        List<RoleDTO> roles = getRolesByUserId(user.getId());
+        for (RoleDTO role : roles) {
+            if (role.getAuthority().equals(authority)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
