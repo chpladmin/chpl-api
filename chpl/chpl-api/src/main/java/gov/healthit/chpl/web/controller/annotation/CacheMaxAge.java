0133b5d0ec6272fa4838f86266306dd99ede7ff9
tmy1313 <tyoung@ainq.com>
2019-02-18 09:48:49 -0500

    feat: add annotation that allows for endpoint caching
    OCD-2636

---
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/annotation/CacheMaxAge.java
@@ -0,0 +1,11 @@
+package gov.healthit.chpl.web.controller.annotation;
+
+public class CacheMaxAge {
+    public static final int ONE_MINUTE = 60;
+    public static final int THREE_MINUTES = 180;
+    public static final int FIVE_MINUTES = 300;
+    public static final int TEN_MINUTES = 600;
+    public static final int FIFTEEN_MINUTES = 900;
+    public static final int THIRTY_MINUTES = 1800;
+    public static final int SIXTY_MINUTES = 3600;
+}
