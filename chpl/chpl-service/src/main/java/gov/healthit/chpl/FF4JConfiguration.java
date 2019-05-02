b1c6721226b200724409ce954e5ceb9d0498de9c
tmy1313 <tyoung@ainq.com>
2019-05-02 10:10:49 -0400

    feat: wip - implement FF4j
    OCD-2885

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/FF4JConfiguration.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/FF4JConfiguration.java
@@ -11,6 +11,7 @@ import org.ff4j.FF4j;
 import org.ff4j.audit.repository.JdbcEventRepository;
 import org.ff4j.property.store.JdbcPropertyStore;
 import org.ff4j.store.JdbcFeatureStore;
+import org.ff4j.store.JdbcQueryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,8 +22,17 @@ public class FF4JConfiguration {
     public FF4j getFF4j() {
         try {
             FF4j ff4j = new FF4j();
-            ff4j.setFeatureStore(new JdbcFeatureStore(ff4jDataSource()));
-            ff4j.setPropertiesStore(new JdbcPropertyStore(ff4jDataSource()));
+
+            JdbcFeatureStore featureStore = new JdbcFeatureStore(ff4jDataSource());
+            featureStore.setQueryBuilder(new JdbcQueryBuilder("ff4j_", "", "openchpl"));
+            ff4j.setFeatureStore(featureStore);
+
+            JdbcPropertyStore propertyStore = new JdbcPropertyStore(ff4jDataSource());
+            propertyStore.setQueryBuilder(new JdbcQueryBuilder("ff4j_", "", "openchpl"));
+            ff4j.setPropertiesStore(propertyStore);
+
+            JdbcEventRepository eventRepository = new JdbcEventRepository(ff4jDataSource());
+            eventRepository.setQueryBuilder(new JdbcQueryBuilder("ff4j_", "", "openchpl"));
             ff4j.setEventRepository(new JdbcEventRepository(ff4jDataSource()));
             return ff4j;
         } catch (NamingException | SQLException e) {
@@ -33,7 +43,6 @@ public class FF4JConfiguration {
     private DataSource ff4jDataSource() throws NamingException, SQLException {
         Context ctx = new InitialContext();
         DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/openchpl");
-        ds.getConnection().setSchema("openchpl");
         return (DataSource) ds;
     }
 
