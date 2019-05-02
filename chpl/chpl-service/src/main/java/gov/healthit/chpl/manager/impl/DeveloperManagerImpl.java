bd0a35944044bcc0dc9c609d35f844bb62617208
tmy1313 <tyoung@ainq.com>
2019-05-01 21:45:20 -0400

    Revert "fix: remove code preventing ADMIN/ONC from updating nonactive dev"
    This reverts commit 16df3ac4b33044bf1935684aad261866be8917ff.

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/impl/DeveloperManagerImpl.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/impl/DeveloperManagerImpl.java
@@ -246,24 +246,48 @@ public class DeveloperManagerImpl extends SecuredManager implements DeveloperMan
             throw new EntityCreationException(msg);
         }
 
-        /*
-         * Check to see that the Developer's website is valid.
-         */
-        if (!StringUtils.isEmpty(updatedDev.getWebsite())) {
-            if (!ValidationUtils.isWellFormedUrl(updatedDev.getWebsite())) {
-                String msg = msgUtil.getMessage("developer.websiteIsInvalid");
-                throw new EntityCreationException(msg);
+        // determine if the status has been changed in most cases only allowed by ROLE_ADMIN but ROLE_ACB
+        // can change it to UnderCertificationBanByOnc
+        boolean currentStatusChanged = !currDevStatus.getStatus().getStatusName()
+                .equals(newDevStatus.getStatus().getStatusName());
+        if (currentStatusChanged
+                && !newDevStatus.getStatus().getStatusName()
+                        .equals(DeveloperStatusType.UnderCertificationBanByOnc.toString())
+                && !resourcePermissions.isUserRoleAdmin() && !resourcePermissions.isUserRoleOnc()) {
+            String msg = msgUtil.getMessage("developer.statusChangeNotAllowedWithoutAdmin");
+            throw new EntityCreationException(msg);
+        } else if (!currDevStatus.getStatus().getStatusName().equals(DeveloperStatusType.Active.toString())
+                && !newDevStatus.getStatus().getStatusName().equals(DeveloperStatusType.Active.toString())) {
+            // if the developer is not active and not going to be active only its current status can be updated
+            updateStatusHistory(beforeDev, updatedDev);
+        } else {
+            /*
+             * Check to see that the Developer's website is valid.
+             */
+            if (!StringUtils.isEmpty(updatedDev.getWebsite())) {
+                if (!ValidationUtils.isWellFormedUrl(updatedDev.getWebsite())) {
+                    String msg = msgUtil.getMessage("developer.websiteIsInvalid");
+                    throw new EntityCreationException(msg);
+                }
+            }
+
+            if (beforeDev.getContact() != null && beforeDev.getContact().getId() != null) {
+                updatedDev.getContact().setId(beforeDev.getContact().getId());
+            }
+            // if either the before or updated statuses are active and the user is
+            // ROLE_ADMIN or ROLE_ONC
+            // OR if before status is active and user is not ROLE_ADMIN - proceed
+            if (((currDevStatus.getStatus().getStatusName().equals(DeveloperStatusType.Active.toString())
+                    || newDevStatus.getStatus().getStatusName().equals(DeveloperStatusType.Active.toString()))
+                    && (resourcePermissions.isUserRoleAdmin() || resourcePermissions.isUserRoleOnc()))
+                    || (currDevStatus.getStatus().getStatusName().equals(DeveloperStatusType.Active.toString())
+                            && !resourcePermissions.isUserRoleAdmin() && !resourcePermissions.isUserRoleOnc())) {
+
+                developerDao.update(updatedDev);
+                updateStatusHistory(beforeDev, updatedDev);
+                createOrUpdateTransparencyMappings(updatedDev);
             }
         }
-
-        if (beforeDev.getContact() != null && beforeDev.getContact().getId() != null) {
-            updatedDev.getContact().setId(beforeDev.getContact().getId());
-        }
-
-        developerDao.update(updatedDev);
-        updateStatusHistory(beforeDev, updatedDev);
-        createOrUpdateTransparencyMappings(updatedDev);
-
         DeveloperDTO after = getById(updatedDev.getId());
         activityManager.addActivity(ActivityConcept.DEVELOPER, after.getId(),
                 "Developer " + updatedDev.getName() + " was updated.", beforeDev, after);
