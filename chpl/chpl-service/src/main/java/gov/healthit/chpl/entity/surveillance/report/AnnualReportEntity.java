c6beb11dd436207d5373181157bea6c3f9477d6b
Katy Ekey <katy.ekey@gmail.com>
2019-05-22 14:35:21 -0400

    feat: wip: add entities for quarterly report

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/entity/surveillance/report/AnnualReportEntity.java
@@ -0,0 +1,129 @@
+package gov.healthit.chpl.entity.surveillance.report;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import gov.healthit.chpl.entity.CertificationBodyEntity;
+
+@Entity
+@Table(name = "annual_report")
+public class AnnualReportEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "certification_body_id")
+    private Long certificationBodyId;
+
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "certification_body_id", insertable = false, updatable = false)
+    private CertificationBodyEntity acb;
+
+    @Column(name = "year")
+    private Integer year;
+
+    @Column(name = "obstacle_summary")
+    private String obstacleSummary;
+
+    @Column(name = "findings_summary")
+    private String findingsSummary;
+
+    @Column(name = "deleted")
+    private Boolean deleted;
+
+    @Column(name = "last_modified_user")
+    private Long lastModifiedUser;
+
+    @Column(name = "creation_date", insertable = false, updatable = false)
+    private Date creationDate;
+
+    @Column(name = "last_modified_date", insertable = false, updatable = false)
+    private Date lastModifiedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public Long getCertificationBodyId() {
+        return certificationBodyId;
+    }
+
+    public void setCertificationBodyId(final Long certificationBodyId) {
+        this.certificationBodyId = certificationBodyId;
+    }
+
+    public CertificationBodyEntity getAcb() {
+        return acb;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(final Integer year) {
+        this.year = year;
+    }
+
+    public String getObstacleSummary() {
+        return obstacleSummary;
+    }
+
+    public void setObstacleSummary(final String obstacleSummary) {
+        this.obstacleSummary = obstacleSummary;
+    }
+
+    public String getFindingsSummary() {
+        return findingsSummary;
+    }
+
+    public void setFindingsSummary(final String findingsSummary) {
+        this.findingsSummary = findingsSummary;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(final Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Long getLastModifiedUser() {
+        return lastModifiedUser;
+    }
+
+    public void setLastModifiedUser(final Long lastModifiedUser) {
+        this.lastModifiedUser = lastModifiedUser;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(final Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(final Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
