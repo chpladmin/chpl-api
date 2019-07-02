ee69bb7cadb2e3a4d77f18d47d97a3cbbce5e57e
tmy1313 <tyoung@ainq.com>
2019-07-01 22:09:34 -0400

    feat: wip - add correct permissions to announcement activity metadata
    OCD-2754

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/activity/GetAnnouncementMetadataActionPermissions.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/activity/GetAnnouncementMetadataActionPermissions.java
@@ -1,11 +1,24 @@
 package gov.healthit.chpl.permissions.domains.activity;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import gov.healthit.chpl.dao.AnnouncementDAO;
+import gov.healthit.chpl.domain.activity.ActivityMetadata;
+import gov.healthit.chpl.dto.AnnouncementDTO;
 import gov.healthit.chpl.permissions.domains.ActionPermissions;
 
 @Component("activityGetAnnouncementMetadataActionPermissions")
 public class GetAnnouncementMetadataActionPermissions extends ActionPermissions {
+    private static final Logger LOGGER = LogManager.getLogger(GetAnnouncementMetadataActionPermissions.class);
+    private AnnouncementDAO announcementDAO;
+
+    @Autowired
+    public GetAnnouncementMetadataActionPermissions(final AnnouncementDAO announcementDAO) {
+        this.announcementDAO = announcementDAO;
+    }
 
     @Override
     public boolean hasAccess() {
@@ -14,7 +27,22 @@ public class GetAnnouncementMetadataActionPermissions extends ActionPermissions 
 
     @Override
     public boolean hasAccess(Object obj) {
-        return false;
+        if (!(obj instanceof ActivityMetadata)) {
+            return false;
+        } else if (getResourcePermissions().isUserAnonymous()) {
+            try {
+                // Need to look at the Announcement and see if it is public
+                ActivityMetadata activityMetadata = (ActivityMetadata) obj;
+                AnnouncementDTO announcement = announcementDAO.getById(activityMetadata.getObjectId(), true);
+                return announcement.getIsPublic();
+            } catch (Exception e) {
+                LOGGER.error("There was an error checking permissions.", e);
+                return false;
+            }
+        } else {
+            // User is logged in
+            return true;
+        }
     }
 
 }
