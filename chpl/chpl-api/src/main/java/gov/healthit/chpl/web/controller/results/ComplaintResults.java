e2266a60461aba2df670c2e0335bd41be051a6f5
Katy Ekey <katy.ekey@gmail.com>
2019-07-17 09:30:05 -0400

    feat: change relevant complaint response to ComplaintResults

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/results/ComplaintResults.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/results/ComplaintResults.java
@@ -8,13 +8,13 @@ import gov.healthit.chpl.domain.complaint.Complaint;
 
 public class ComplaintResults implements Serializable{
     private static final long serialVersionUID = 570551290486119131L;
-   
+
     private List<Complaint> results = new ArrayList<Complaint>();
-    
+
     public List<Complaint> getResults() {
         return results;
     }
-    
+
     public void setResults(List<Complaint> results) {
         this.results = results;
     }
