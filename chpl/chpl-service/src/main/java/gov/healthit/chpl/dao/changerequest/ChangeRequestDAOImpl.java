f745eaae8d5869e119abaee083bf4e70299b1d62
tmy1313 <tyoung@ainq.com>
2019-08-27 08:58:04 -0400

    wip - add ChangeRequestDAO
    OCD-2159

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/changerequest/ChangeRequestDAOImpl.java
@@ -0,0 +1,5 @@
+package gov.healthit.chpl.dao.changerequest;
+
+public class ChangeRequestDAOImpl {
+
+}
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/changerequest/ChangeRequestConverter.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/changerequest/ChangeRequestConverter.java
@@ -1,7 +1,11 @@
 package gov.healthit.chpl.domain.changerequest;
 
+import gov.healthit.chpl.domain.Developer;
+import gov.healthit.chpl.dto.DeveloperDTO;
+import gov.healthit.chpl.entity.changerequest.ChangeRequestEntity;
 import gov.healthit.chpl.entity.changerequest.ChangeRequestStatusTypeEntity;
 import gov.healthit.chpl.entity.changerequest.ChangeRequestTypeEntity;
+import gov.healthit.chpl.entity.developer.DeveloperEntity;
 
 public class ChangeRequestConverter {
 
@@ -19,4 +23,27 @@ public class ChangeRequestConverter {
         return status;
     }
 
+    public static ChangeRequestTypeEntity convert(ChangeRequestType type) {
+        ChangeRequestTypeEntity status = new ChangeRequestTypeEntity();
+        status.setId(type.getId());
+        status.setName(type.getName());
+        return status;
+    }
+
+    public static ChangeRequestEntity convert(ChangeRequest changeRequest) {
+        ChangeRequestEntity entity = new ChangeRequestEntity();
+        entity.setId(changeRequest.getId());
+        entity.setChangeRequestType(convert(changeRequest.getChangeRequestType()));
+        entity.setDeveloper(new DeveloperEntity(changeRequest.getDeveloper().getDeveloperId()));
+        return entity;
+    }
+
+    public static ChangeRequest convert(ChangeRequestEntity entity) {
+        ChangeRequest cr = new ChangeRequest();
+        cr.setId(entity.getId());
+        cr.setChangeRequestType(convert(entity.getChangeRequestType()));
+        cr.setDeveloper(new Developer(new DeveloperDTO(entity.getDeveloper())));
+
+        return cr;
+    }
 }
