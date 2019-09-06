c714d2b74858174a454c4461e5cf290d6447f8ed
tmy1313 <tyoung@ainq.com>
2019-09-06 09:33:28 -0400

    add tests for ChangeRequestWebsiteHelper
    OCD-2159

---
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/changerequest/builders/ChangeRequestStatusBuilder.java
@@ -0,0 +1,9 @@
+package gov.healthit.chpl.changerequest.builders;
+
+public class ChangeRequestStatusBuilder {
+
+    public ChangeRequestStatusBuilder() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
