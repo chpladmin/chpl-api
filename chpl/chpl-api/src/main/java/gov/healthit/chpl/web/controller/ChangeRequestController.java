65d6d91124cf7491887d1f4ac01b39b686961703
tmy1313 <tyoung@ainq.com>
2019-09-12 14:03:43 -0400

    add "change-request" feature flag and method descriptions
    OCD-2159

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/ChangeRequestController.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/ChangeRequestController.java
@@ -3,6 +3,7 @@ package gov.healthit.chpl.web.controller;
 import java.util.List;
 
 import org.apache.commons.lang.NotImplementedException;
+import org.ff4j.FF4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import gov.healthit.chpl.changerequest.dao.ChangeRequestStatusTypeDAO;
+import gov.healthit.chpl.FeatureList;
 import gov.healthit.chpl.changerequest.domain.ChangeRequest;
 import gov.healthit.chpl.changerequest.manager.ChangeRequestManager;
 import gov.healthit.chpl.exception.EntityCreationException;
@@ -29,22 +30,26 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/change-requests")
 public class ChangeRequestController {
     
-    private ChangeRequestStatusTypeDAO changeRequestStatusTypeDAO;
     private ChangeRequestManager changeRequestManager;
     private ResourcePermissions resourcePermissions;
+    private FF4j ff4j;
     
     @Autowired
-    public ChangeRequestController(final ChangeRequestStatusTypeDAO changeRequestStatusTypeDAO, 
-            final ChangeRequestManager changeRequestManager, final ResourcePermissions resourcePermissions) {
-        this.changeRequestStatusTypeDAO = changeRequestStatusTypeDAO;
+    public ChangeRequestController(final ChangeRequestManager changeRequestManager, final ResourcePermissions resourcePermissions,
+            final FF4j ff4j) {
         this.changeRequestManager = changeRequestManager;
         this.resourcePermissions = resourcePermissions;
+        this.ff4j = ff4j;
     }
     
     @ApiOperation(value = "Get details about a specific change request.", 
-            notes="")
+            notes="Security Restrictions: ROLE_DEVELOPER can get change requests where they have adminstrative authority based on the developer.")
     @RequestMapping(value = "/{changeRequestId}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     public @ResponseBody ChangeRequest getChangeRequest(@PathVariable final Long changeRequestId) throws EntityRetrievalException {
+        if (!ff4j.check(FeatureList.CHANGE_REQUEST)) {
+            throw new NotImplementedException();
+        }
+
         if (!resourcePermissions.isUserRoleDeveloperAdmin()) {
             throw new NotImplementedException();
         }
@@ -52,9 +57,13 @@ public class ChangeRequestController {
     }
     
     @ApiOperation(value = "Get details about all change requests.", 
-            notes="")
+            notes="Security Restrictions: ROLE_DEVELOPER can get change requests where they have adminstrative authority based on the developer.")
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     public @ResponseBody List<ChangeRequest> getAllChangeRequests() throws EntityRetrievalException {
+        if (!ff4j.check(FeatureList.CHANGE_REQUEST)) {
+            throw new NotImplementedException();
+        }
+
         if (!resourcePermissions.isUserRoleDeveloperAdmin()) {
             throw new NotImplementedException();
         }
@@ -62,19 +71,27 @@ public class ChangeRequestController {
     }
     
     @ApiOperation(value = "Create a new change request.",
-            notes = "Security Restrictions: ROLE_ADMIN or ROLE_ONC to create a new testing lab.")
+            notes = "Security Restrictions: ROLE_DEVELOPER can create change requests where they have adminstrative authority based on the developer.")
     @RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = "application/json; charset=utf-8")
     public ChangeRequest createChangeRequest(@RequestBody final ChangeRequest cr ) throws EntityRetrievalException, ValidationException, JsonProcessingException, EntityCreationException {
+        if (!ff4j.check(FeatureList.CHANGE_REQUEST)) {
+            throw new NotImplementedException();
+        }
+
         return changeRequestManager.createChangeRequest(cr);
     }
     
     
     @ApiOperation(value = "Update an existing request status or request details.",
-            notes = "")
+            notes = "Security Restrictions: ROLE_DEVELOPER can update change requests where they have adminstrative authority based on the developer.")
     @RequestMapping(value = "/{changeRequestId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = "application/json; charset=utf-8")
     public ChangeRequest updateChangeRequest(@RequestBody final ChangeRequest cr) throws EntityRetrievalException, ValidationException, EntityCreationException, JsonProcessingException {
+        if (!ff4j.check(FeatureList.CHANGE_REQUEST)) {
+            throw new NotImplementedException();
+        }
+
         if (!resourcePermissions.isUserRoleDeveloperAdmin()) {
             throw new NotImplementedException();
         }
