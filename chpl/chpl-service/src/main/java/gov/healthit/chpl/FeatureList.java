1a452d9d87fbcc4dba47e0269985f2a216773d8e
Andrew Larned <andrew.larned@gmail.com>
2019-10-29 10:16:10 -0400

    fix: use flag name that works
    [#OCD-3091]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/FeatureList.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/FeatureList.java
@@ -15,7 +15,7 @@ public final class FeatureList {
     /** Features that will be turned on as of the effective rule date. */
     public static final String EFFECTIVE_RULE_DATE = "effective-rule-date";
     /** Features that will be turned on as of one week past the effective rule date. */
-    public static final String EFFECTIVE_RULE_DATE_PLUS_ONE_WEEK = "effective-rule-date+1-week";
+    public static final String EFFECTIVE_RULE_DATE_PLUS_ONE_WEEK = "effective-rule-date-plus-on-week";
     /** Ability to have Developers as User. */
     public static final String ROLE_DEVELOPER = "role-developer";
     /** Function for ONC-ACBs to generate their required quarterly and annual surveillance reporting. */
