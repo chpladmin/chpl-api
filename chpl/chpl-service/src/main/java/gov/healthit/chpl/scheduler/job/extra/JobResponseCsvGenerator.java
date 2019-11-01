f74ef71fd0b6203ef042d97a23b85d6686e18d27
tmy1313 <tyoung@ainq.com>
2019-11-01 13:48:38 -0400

    add CSV outputter for JobResponse objects
    OCD-2175

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/extra/JobResponseCsvGenerator.java
@@ -0,0 +1,56 @@
+package gov.healthit.chpl.scheduler.job.extra;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.logging.log4j.Logger;
+
+public class JobResponseCsvGenerator {
+    private Logger logger;
+
+    public File getCsvFile(final List<JobResponse> responses, final String fileName, final Logger logger)
+            throws IOException {
+        this.logger = logger;
+
+        return writeDataToFile(responses, createFile(fileName));
+    }
+
+    private File createFile(final String fileName) throws IOException {
+        File temp = File.createTempFile(fileName, ".csv");
+        temp.deleteOnExit();
+        return temp;
+    }
+
+    private File writeDataToFile(final List<JobResponse> responses, final File file) throws IOException {
+        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file),
+                Charset.forName("UTF-8").newEncoder());
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.EXCEL)) {
+
+            csvPrinter.printRecord(getHeaderRow());
+            for (JobResponse response : responses) {
+                csvPrinter.printRecord(getDataRow(response));
+            }
+
+            return file;
+        }
+
+    }
+
+    private List<String> getHeaderRow() {
+        return new ArrayList<String>(Arrays.asList("Identifier", "Success", "Message"));
+    }
+
+    private List<String> getDataRow(JobResponse response) {
+        return new ArrayList<String>(Arrays.asList(response.getIdentifier(),
+                String.valueOf(response.isCompletedSuccessfully()), response.getMessage()));
+    }
+
+}
