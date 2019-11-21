f6cdde1fa95aeb15d78ccdba5786899eda27afa0
Katy Ekey <katy.ekey@gmail.com>
2019-11-21 10:59:31 -0500

    add flag check to rule

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/edition2015/RemovedCriteriaComparisonReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/edition2015/RemovedCriteriaComparisonReviewer.java
@@ -1,9 +1,11 @@
 package gov.healthit.chpl.validation.listing.reviewer.edition2015;
 
+import org.ff4j.FF4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import gov.healthit.chpl.FeatureList;
 import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.permissions.ResourcePermissions;
@@ -20,17 +22,23 @@ import gov.healthit.chpl.validation.listing.reviewer.ComparisonReviewer;
 public class RemovedCriteriaComparisonReviewer implements ComparisonReviewer {
     private ResourcePermissions resourcePermissions;
     private ErrorMessageUtil msgUtil;
+    private FF4j ff4j;
 
     @Autowired
     public RemovedCriteriaComparisonReviewer(final ResourcePermissions resourcePermissions,
-            final ErrorMessageUtil msgUtil) {
+            final ErrorMessageUtil msgUtil, final FF4j ff4j) {
         this.resourcePermissions = resourcePermissions;
         this.msgUtil = msgUtil;
+        this.ff4j = ff4j;
     }
 
     @Override
     public void review(final CertifiedProductSearchDetails existingListing,
             final CertifiedProductSearchDetails updatedListing) {
+        if (!ff4j.check(FeatureList.EFFECTIVE_RULE_DATE_PLUS_ONE_WEEK)) {
+            return;
+        }
+
         //checking for the addition of a removed criteria
         //this is only disallowed if the user is not ADMIN/ONC, so first check the permissions
         if (resourcePermissions.isUserRoleAdmin() || resourcePermissions.isUserRoleOnc()) {
