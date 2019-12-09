3d503f22c12d2502155d5fe1257b24729905f09a
Katy Ekey <katy.ekey@gmail.com>
2019-12-09 10:06:12 -0500

    merged

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/FeatureList.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/FeatureList.java
@@ -12,11 +12,7 @@ public final class FeatureList {
     public static final String CHANGE_REQUEST = "change-request";
     /** Features that will be turned on as of the effective rule date. */
     public static final String EFFECTIVE_RULE_DATE = "effective-rule-date";
-<<<<<<< HEAD
-    /** Features that will be turned on as of the effective rule date plus one week. */
-=======
     /** Features that will be turned on as of one week past the effective rule date. */
->>>>>>> upstream/staging
     public static final String EFFECTIVE_RULE_DATE_PLUS_ONE_WEEK = "effective-rule-date-plus-one-week";
     /** Ability to have Developers as User. */
     public static final String ROLE_DEVELOPER = "role-developer";
