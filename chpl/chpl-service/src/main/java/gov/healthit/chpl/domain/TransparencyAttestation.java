e7040e1559eeaac7602b69b44ff732356caa6fae
tmy1313 <tyoung@ainq.com>
2020-01-21 14:40:10 -0500

    add transaparency attestation to the generated XSD file
    OCD-3187

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/TransparencyAttestation.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/TransparencyAttestation.java
@@ -2,23 +2,32 @@ package gov.healthit.chpl.domain;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
 import org.ff4j.FF4j;
 
 import gov.healthit.chpl.FeatureList;
 import gov.healthit.chpl.SpringContext;
 import gov.healthit.chpl.dto.TransparencyAttestationDTO;
 
+@XmlType(namespace = "http://chpl.healthit.gov/listings")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class TransparencyAttestation implements Serializable {
     private static final long serialVersionUID = 4734258569082877872L;
 
     /**
      * Affirmative, Negative, or N/A
      */
+    @XmlElement(required = true)
     private String transparencyAttestation;
 
     /**
      * Indicates whether this data is still part of the CHPL
      */
+    @XmlElement(required = true)
     private Boolean removed;
 
     public TransparencyAttestation() {
