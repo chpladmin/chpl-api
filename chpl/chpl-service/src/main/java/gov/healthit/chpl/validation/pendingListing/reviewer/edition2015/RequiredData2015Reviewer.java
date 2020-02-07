276fbc482c80cfcf0bdce67016357626031f3ce8
Katy Ekey <katy.ekey@gmail.com>
2020-02-07 13:42:24 -0500

    fix: mixup between present and removed criteria caluclations

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/pendingListing/reviewer/edition2015/RequiredData2015Reviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/pendingListing/reviewer/edition2015/RequiredData2015Reviewer.java
@@ -897,11 +897,11 @@ public class RequiredData2015Reviewer extends RequiredDataReviewer {
     private void validateG3(PendingCertifiedProductDTO listing) {
         List<CertificationCriterion> attestedCriteria = getAttestedCriteria(listing);
         List<CertificationCriterion> presentAttestedUcdCriteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.TRUE))
+                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.FALSE))
                 .filter(cert -> certNumberIsInCertList(cert, UCD_RELATED_CERTS))
                 .collect(Collectors.<CertificationCriterion>toList());
         List<CertificationCriterion> removedAttestedUcdCriteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.FALSE))
+                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.TRUE))
                 .filter(cert -> certNumberIsInCertList(cert, UCD_RELATED_CERTS))
                 .collect(Collectors.<CertificationCriterion>toList());
         boolean hasG3 = ValidationUtils.hasCert("170.315 (g)(3)", attestedCriteria);
@@ -920,11 +920,11 @@ public class RequiredData2015Reviewer extends RequiredDataReviewer {
     private void validateG3Inverse(PendingCertifiedProductDTO listing) {
         List<CertificationCriterion> attestedCriteria = getAttestedCriteria(listing);
         List<CertificationCriterion> presentAttestedUcdCriteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.TRUE))
+                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.FALSE))
                 .filter(cert -> certNumberIsInCertList(cert, UCD_RELATED_CERTS))
                 .collect(Collectors.<CertificationCriterion>toList());
         List<CertificationCriterion> removedAttestedUcdCriteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.FALSE))
+                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.TRUE))
                 .filter(cert -> certNumberIsInCertList(cert, UCD_RELATED_CERTS))
                 .collect(Collectors.<CertificationCriterion>toList());
         boolean hasG3 = ValidationUtils.hasCert("170.315 (g)(3)", attestedCriteria);
@@ -945,11 +945,11 @@ public class RequiredData2015Reviewer extends RequiredDataReviewer {
     private void validateG6(PendingCertifiedProductDTO listing) {
         List<CertificationCriterion> attestedCriteria = getAttestedCriteria(listing);
         List<CertificationCriterion> presentAttestedG6Criteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.TRUE))
+                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.FALSE))
                 .filter(cert -> certNumberIsInCertList(cert, CERTS_REQUIRING_G6))
                 .collect(Collectors.<CertificationCriterion>toList());
         List<CertificationCriterion> removedAttestedG6Criteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.FALSE))
+                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.TRUE))
                 .filter(cert -> certNumberIsInCertList(cert, CERTS_REQUIRING_G6))
                 .collect(Collectors.<CertificationCriterion>toList());
         boolean hasG6 = ValidationUtils.hasCert("170.315 (g)(6)", attestedCriteria);
@@ -968,11 +968,11 @@ public class RequiredData2015Reviewer extends RequiredDataReviewer {
     private void validateG6Inverse(PendingCertifiedProductDTO listing) {
         List<CertificationCriterion> attestedCriteria = getAttestedCriteria(listing);
         List<CertificationCriterion> presentAttestedG6Criteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.TRUE))
+                .filter(cert -> cert.getRemoved() == null || cert.getRemoved().equals(Boolean.FALSE))
                 .filter(cert -> certNumberIsInCertList(cert, CERTS_REQUIRING_G6))
                 .collect(Collectors.<CertificationCriterion>toList());
         List<CertificationCriterion> removedAttestedG6Criteria = attestedCriteria.stream()
-                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.FALSE))
+                .filter(cert -> cert.getRemoved() != null && cert.getRemoved().equals(Boolean.TRUE))
                 .filter(cert -> certNumberIsInCertList(cert, CERTS_REQUIRING_G6))
                 .collect(Collectors.<CertificationCriterion>toList());
         boolean hasG6 = ValidationUtils.hasCert("170.315 (g)(6)", attestedCriteria);
