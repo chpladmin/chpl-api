3e4aa3677031dd25cc64a7fd0f4b801f5bec7b02
tmy1313 <tyoung@ainq.com>
2020-02-11 09:43:58 -0500

    update pom to upgrade jUnit and add Surefire

--- chpl/chpl-service/src/test/java/old/gov/healthit/chpl/util/UtilTest.java
+++ chpl/chpl-service/src/test/java/old/gov/healthit/chpl/util/UtilTest.java
@@ -1,4 +1,4 @@
-package gov.healthit.chpl.util;
+package old.gov.healthit.chpl.util;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,12 +10,17 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
 import gov.healthit.chpl.dto.CertificationCriterionDTO;
+import gov.healthit.chpl.util.Util;
 import junit.framework.TestCase;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { gov.healthit.chpl.CHPLTestConfig.class })
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-    DirtiesContextTestExecutionListener.class})
+@ContextConfiguration(classes = {
+        old.gov.healthit.chpl.CHPLTestConfig.class
+})
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class
+})
 public class UtilTest extends TestCase {
 
     private CertificationCriterionDTO c1;
@@ -31,39 +36,39 @@ public class UtilTest extends TestCase {
     public void sortCriteriaBetweenEditions() {
         c1.setNumber("170.302 (h)");
         c2.setNumber("170.314 (a)(1)");
-        assertTrue("2011 should be earlier than 2014", Util.sortCriteria(c1,  c2) < 0);
+        assertTrue("2011 should be earlier than 2014", Util.sortCriteria(c1, c2) < 0);
         c1.setNumber("170.315 (b)(2)");
-        assertTrue("2015 should be later than 2014", Util.sortCriteria(c1,  c2) > 0);
+        assertTrue("2015 should be later than 2014", Util.sortCriteria(c1, c2) > 0);
         c2.setNumber("170.304 (d)");
-        assertTrue("2015 should be later than 2011", Util.sortCriteria(c1,  c2) > 0);
+        assertTrue("2015 should be later than 2011", Util.sortCriteria(c1, c2) > 0);
     }
 
     @Test
     public void sortCriteriaWithOneParagraph() {
         c1.setNumber("170.302 (h)");
         c2.setNumber("170.302 (i)");
-        assertTrue("h should be earlier than i", Util.sortCriteria(c1,  c2) < 0);
+        assertTrue("h should be earlier than i", Util.sortCriteria(c1, c2) < 0);
     }
 
     @Test
     public void sortCriteriaWithTwoParagraphs() {
         c1.setNumber("170.314 (a)(3)");
         c2.setNumber("170.314 (a)(2)");
-        assertTrue("3 should be after 2", Util.sortCriteria(c1,  c2) > 0);
+        assertTrue("3 should be after 2", Util.sortCriteria(c1, c2) > 0);
     }
 
     @Test
     public void sortCriteriaWithThreeParagraphs() {
         c1.setNumber("170.314 (d)(3)(B)");
         c2.setNumber("170.314 (d)(3)(A)");
-        assertTrue("B should be after A", Util.sortCriteria(c1,  c2) > 0);
+        assertTrue("B should be after A", Util.sortCriteria(c1, c2) > 0);
     }
 
     @Test
     public void sortCriteriaWithDifferingParagraphComponentCount() {
         c1.setNumber("170.314 (d)(3)");
         c2.setNumber("170.314 (d)(3)(A)");
-        assertTrue("fewer paragraphs should be before ones with more", Util.sortCriteria(c1,  c2) < 0);
+        assertTrue("fewer paragraphs should be before ones with more", Util.sortCriteria(c1, c2) < 0);
     }
 
     @Test
@@ -72,6 +77,6 @@ public class UtilTest extends TestCase {
         c1.setTitle("This is a title");
         c2.setNumber("170.314 (a)(3)");
         c2.setTitle("This is a title (Cures Update)");
-        assertTrue("should sort by title if paragraphs match", Util.sortCriteria(c1,  c2) < 0);
+        assertTrue("should sort by title if paragraphs match", Util.sortCriteria(c1, c2) < 0);
     }
 }
