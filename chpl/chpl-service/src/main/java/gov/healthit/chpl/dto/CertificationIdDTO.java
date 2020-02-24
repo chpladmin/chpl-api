4c02dfde848d40f6e1f29a0496da05c4a8f415d5
Andrew Larned <andrew.larned@gmail.com>
2020-02-24 12:19:51 -0500

    refactor: use Lombok for DTO class
    [#OCD-3230]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/dto/CertificationIdDTO.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/dto/CertificationIdDTO.java
@@ -5,7 +5,15 @@ import java.util.Date;
 
 import gov.healthit.chpl.entity.CertificationIdEntity;
 import gov.healthit.chpl.util.Util;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CertificationIdDTO implements Serializable {
     private static final long serialVersionUID = 1338639051071192137L;
     private Long id;
@@ -17,9 +25,6 @@ public class CertificationIdDTO implements Serializable {
     private Date creationDate;
     private Date lastModifiedDate;
     private Long lastModifiedUser;
-
-    public CertificationIdDTO() {
-    }
 
     public CertificationIdDTO(CertificationIdEntity entity) {
 
@@ -41,79 +46,11 @@ public class CertificationIdDTO implements Serializable {
         this.creationDate = Util.getNewDate(creationDate);
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public Date getLastModifiedDate() {
         return Util.getNewDate(lastModifiedDate);
     }
 
     public void setLastModifiedDate(final Date lastModifiedDate) {
         this.lastModifiedDate = Util.getNewDate(lastModifiedDate);
-    }
-
-    public Long getLastModifiedUser() {
-        return lastModifiedUser;
-    }
-
-    public void setLastModifiedUser(final Long lastModifiedUser) {
-        this.lastModifiedUser = lastModifiedUser;
-    }
-
-    public void setCertificationId(final String certId) {
-        this.certificationId = certId;
-    }
-
-    public String getCertificationId() {
-        return this.certificationId;
-    }
-
-    public void setYear(final String year) {
-        this.year = year;
-    }
-
-    public String getYear() {
-        return this.year;
-    }
-
-    public void setPracticeTypeId(final Long practiceTypeId) {
-        this.practiceTypeId = practiceTypeId;
-    }
-
-    public Long getPracticeTypeId() {
-        return this.practiceTypeId;
-    }
-
-    public void setKey(final String key) {
-        this.key = key;
-    }
-
-    public String getKey() {
-        return this.key;
-    }
-
-    /**
-     * Provides toString implementation.
-     *
-     * @see java.lang.Object#toString()
-     * @return String representation of this class.
-     */
-    @Override
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-        sb.append("creationDate: " + this.getCreationDate() + ", ");
-        sb.append("id: " + this.getId() + ", ");
-        sb.append("key: " + this.getKey() + ", ");
-        sb.append("lastModifiedDate: " + this.getLastModifiedDate() + ", ");
-        sb.append("lastModifiedUser: " + this.getLastModifiedUser() + ", ");
-        sb.append("certificationId: " + this.getCertificationId() + ", ");
-        sb.append("year: " + this.getYear() + ", ");
-        sb.append("practiceTypeId: " + this.getPracticeTypeId() + ", ");
-        return sb.toString();
     }
 }
