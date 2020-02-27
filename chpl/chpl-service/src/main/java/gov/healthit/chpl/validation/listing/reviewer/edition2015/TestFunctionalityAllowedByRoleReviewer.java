f76258194c430eff60ff4dc4f6f775f227cdc629
tmy1313 <tyoung@ainq.com>
2020-02-27 09:23:41 -0500

    identify test functionlity that cannot be changed based on role
    OCD-3245

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/edition2015/TestFunctionalityAllowedByRoleReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/edition2015/TestFunctionalityAllowedByRoleReviewer.java
@@ -11,10 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
 import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertificationResultTestFunctionality;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.validation.listing.reviewer.ComparisonReviewer;
+import lombok.Data;
+import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 
 @Component("testFunctionalityAllowedByRoleReviewer")
@@ -46,6 +51,8 @@ public class TestFunctionalityAllowedByRoleReviewer implements ComparisonReviewe
 
                 addedCrtfs.stream()
                         .forEach(x -> log.info("Added this CRTF: " + x.toString()));
+                addedCrtfs.stream()
+                        .forEach(x -> isTestFunctionalityChangeAllowedBasedOnRole(updatedCr, x));
 
                 List<CertificationResultTestFunctionality> removedCrtfs = getRemovedCrtfs(listUpdateCrtfs, listExistingCrtfs);
 
@@ -89,4 +96,61 @@ public class TestFunctionalityAllowedByRoleReviewer implements ComparisonReviewe
                 .filter(cr -> cr.getId().equals(certificationResultId))
                 .findFirst();
     }
+
+    private boolean isTestFunctionalityChangeAllowedBasedOnRole(CertificationResult cr,
+            CertificationResultTestFunctionality crtf) {
+        Optional<RestrictedTestFunctionality> restrictedTestFunctionality = findRestrictedTestFunctionality(
+                cr.getCriterion().getId(), crtf.getTestFunctionalityId());
+        if (restrictedTestFunctionality.isPresent()) {
+            log.info("Not Allowed based on Roles: " + restrictedTestFunctionality.get().toString());
+        } else {
+            log.info("No rule based on {" + cr.getCriterion().getId() + ", " + crtf.getTestFunctionalityId() + "}");
+        }
+        return true;
+    }
+
+    private Optional<RestrictedTestFunctionality> findRestrictedTestFunctionality(Long criteriaId, Long testFunctionalityId) {
+        Optional<RestrictedCriteriaTestFunctionality> foundBasedOnCriteriaId = getRestrictedCriteriaTestFunctionality().stream()
+                .filter(x -> x.getCriteriaId().equals(criteriaId))
+                .findAny();
+
+        if (foundBasedOnCriteriaId.isPresent()) {
+            // Is there a match on the test functionality
+            return foundBasedOnCriteriaId.get().getRestrictedTestFunctionalities().stream()
+                    .filter(x -> x.getTestFunctionalityId().equals(testFunctionalityId))
+                    .findAny();
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private List<RestrictedCriteriaTestFunctionality> getRestrictedCriteriaTestFunctionality() {
+        List<RestrictedCriteriaTestFunctionality> restrictedCriteria = new ArrayList<RestrictedCriteriaTestFunctionality>();
+        String json = env.getProperty("testFunctionalities.restrictions");
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            CollectionType javaType = mapper.getTypeFactory().constructCollectionType(List.class,
+                    RestrictedCriteriaTestFunctionality.class);
+            restrictedCriteria = mapper.readValue(json, javaType);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return restrictedCriteria;
+    }
+
+    @Data
+    @ToString
+    static class RestrictedCriteriaTestFunctionality {
+        private Long criteriaId;
+        private List<RestrictedTestFunctionality> restrictedTestFunctionalities;
+    }
+
+    @Data
+    @ToString
+    static class RestrictedTestFunctionality {
+        private Long testFunctionalityId;
+        private List<String> allowedRoleNames;
+    }
+
 }
