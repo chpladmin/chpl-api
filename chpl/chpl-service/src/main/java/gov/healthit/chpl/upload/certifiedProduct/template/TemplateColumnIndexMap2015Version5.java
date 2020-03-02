9441b53dd7458d2e2d81f584ce3306accfa9a630
Katy Ekey <katy.ekey@gmail.com>
2020-03-02 10:17:32 -0500

    wip: adding v19 template

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/certifiedProduct/template/TemplateColumnIndexMap2015Version5.java
@@ -0,0 +1,18 @@
+package gov.healthit.chpl.upload.certifiedProduct.template;
+
+public class TemplateColumnIndexMap2015Version5 extends TemplateColumnIndexMap2015Version4 {
+
+    @Override
+    public int getK2Index() {
+        return -1;
+    }
+
+    public int getSelfDeveloperIndex() {
+        return 27;
+    }
+
+    @Override
+    public int getCriteriaEndIndex() {
+        return 861;
+    }
+}
