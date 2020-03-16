aa515a61585a5fe4e6bd74abec7b4ee75973620a
Katy Ekey <katy.ekey@gmail.com>
2020-03-16 08:43:11 -0400

    feat!: Add d2, d3, and d10 Cures Revised criteria to upload template

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/certifiedProduct/template/TemplateColumnIndexMap2015Version5.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/certifiedProduct/template/TemplateColumnIndexMap2015Version5.java
@@ -64,6 +64,6 @@ public class TemplateColumnIndexMap2015Version5 extends TemplateColumnIndexMap20
 
     @Override
     public int getCriteriaEndIndex() {
-        return 1039;
+        return 1066;
     }
 }
