286e0802a49c3637f84dd7b68bcabd4f4635fa8d
tmy1313 <tyoung@ainq.com>
2020-03-15 22:34:52 -0400

    add basic unit tests for PrivacyAndSecurityCriteriaReviewer class
    OCD-3278

---
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/validation/pendinglisting/reviewer/edition2015/PrivacyAndSecurityCriteriaReviewerTest.java
@@ -0,0 +1,123 @@
+package gov.healthit.chpl.validation.pendinglisting.reviewer.edition2015;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.core.env.Environment;
+
+import gov.healthit.chpl.dao.CertificationCriterionDAO;
+import gov.healthit.chpl.dto.CertificationCriterionDTO;
+import gov.healthit.chpl.dto.listing.pending.PendingCertificationResultDTO;
+import gov.healthit.chpl.dto.listing.pending.PendingCertifiedProductDTO;
+import gov.healthit.chpl.exception.EntityRetrievalException;
+import gov.healthit.chpl.util.ErrorMessageUtil;
+import gov.healthit.chpl.validation.pendingListing.reviewer.edition2015.PrivacyAndSecurityCriteriaReviewer;
+
+public class PrivacyAndSecurityCriteriaReviewerTest {
+
+    private CertificationCriterionDAO certificationCriterionDAO;
+    private Environment env;
+    private ErrorMessageUtil errorMessageUtil;
+
+    @Before
+    public void before() throws EntityRetrievalException {
+        certificationCriterionDAO = Mockito.mock(CertificationCriterionDAO.class);
+        Mockito.when(certificationCriterionDAO.getById(1L)).thenReturn(getCriterionDTO(1l, "170.315 (a)(1)"));
+        Mockito.when(certificationCriterionDAO.getById(2L)).thenReturn(getCriterionDTO(2l, "170.315 (a)(2)"));
+        Mockito.when(certificationCriterionDAO.getById(3L)).thenReturn(getCriterionDTO(3l, "170.315 (a)(3)"));
+        Mockito.when(certificationCriterionDAO.getById(166L)).thenReturn(getCriterionDTO(166l, "170.315 (d)(12)"));
+        Mockito.when(certificationCriterionDAO.getById(167L)).thenReturn(getCriterionDTO(167l, "170.315 (d)(13)"));
+
+        env = Mockito.mock(Environment.class);
+        Mockito.when(env.getProperty("privacyAndSecurityCriteria")).thenReturn("1,2");
+        Mockito.when(env.getProperty("privacyAndSecurityRequiredCriteria")).thenReturn("166,167");
+
+        errorMessageUtil = Mockito.mock(ErrorMessageUtil.class);
+        Mockito.when(errorMessageUtil.getMessage(ArgumentMatchers.anyString(), ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString()))
+                .thenAnswer(i -> String.format("Attesting to Criteria %s requires that Criteria %s must also be attested to.",
+                        i.getArgument(1), i.getArgument(2)));
+
+    }
+
+    @Test
+    public void review_GeneratesErrorMessages_HasErrorMessages() {
+        // This is a trivial test - the real work is done by ValidationUtils.checkSubordinateCriteriaAllRequired
+
+        // Setup
+        PendingCertifiedProductDTO listing = PendingCertifiedProductDTO.builder()
+                .certificationCriterionSingle(PendingCertificationResultDTO.builder()
+                        .meetsCriteria(true)
+                        .criterion(getCriterionDTO(1L, "170.315 (a)(1)"))
+                        .build())
+                .certificationCriterionSingle(PendingCertificationResultDTO.builder()
+                        .meetsCriteria(true)
+                        .criterion(getCriterionDTO(2L, "170.315 (a)(2)"))
+                        .build())
+                .certificationCriterionSingle(PendingCertificationResultDTO.builder()
+                        .meetsCriteria(true)
+                        .criterion(getCriterionDTO(166L, "170.315 (d)(12)"))
+                        .build())
+                .build();
+        listing.setErrorMessages(new HashSet<String>());
+
+        PrivacyAndSecurityCriteriaReviewer reviewer = new PrivacyAndSecurityCriteriaReviewer(certificationCriterionDAO, env,
+                errorMessageUtil);
+        reviewer.postConstruct();
+
+        // Test
+        reviewer.review(listing);
+
+        // Check
+        assertEquals(2, listing.getErrorMessages().size());
+    }
+
+    @Test
+    public void review_DoesNotGeneratesErrorMessages_NoErrorMessages() {
+        // This is a trivial test - the real work is done by ValidationUtils.checkSubordinateCriteriaAllRequired
+
+        // Setup
+        PendingCertifiedProductDTO listing = PendingCertifiedProductDTO.builder()
+                .certificationCriterionSingle(PendingCertificationResultDTO.builder()
+                        .meetsCriteria(true)
+                        .criterion(getCriterionDTO(1L, "170.315 (a)(1)"))
+                        .build())
+                .certificationCriterionSingle(PendingCertificationResultDTO.builder()
+                        .meetsCriteria(true)
+                        .criterion(getCriterionDTO(2L, "170.315 (a)(2)"))
+                        .build())
+                .certificationCriterionSingle(PendingCertificationResultDTO.builder()
+                        .meetsCriteria(true)
+                        .criterion(getCriterionDTO(166L, "170.315 (d)(12)"))
+                        .build())
+                .certificationCriterionSingle(PendingCertificationResultDTO.builder()
+                        .meetsCriteria(true)
+                        .criterion(getCriterionDTO(167L, "170.315 (d)(13)"))
+                        .build())
+                .build();
+        listing.setErrorMessages(new HashSet<String>());
+
+        PrivacyAndSecurityCriteriaReviewer reviewer = new PrivacyAndSecurityCriteriaReviewer(certificationCriterionDAO, env,
+                errorMessageUtil);
+        reviewer.postConstruct();
+
+        // Test
+        reviewer.review(listing);
+
+        // Check
+        assertEquals(0, listing.getErrorMessages().size());
+    }
+
+    private CertificationCriterionDTO getCriterionDTO(Long id, String number) {
+        return CertificationCriterionDTO.builder()
+                .id(id)
+                .number(number)
+                .build();
+    }
+
+}
