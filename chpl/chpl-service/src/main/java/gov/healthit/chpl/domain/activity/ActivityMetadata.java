a195a2342af3e72a5663f56f02f4b12f8eed8d8d
Andrew Larned <andrew.larned@gmail.com>
2020-04-03 13:23:41 -0400

    feat-flag!: Display "Cures Update" everywhere it's appropriate
    [#OCD-3301]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/activity/ActivityMetadata.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/activity/ActivityMetadata.java
@@ -7,34 +7,33 @@ import java.util.Set;
 
 import gov.healthit.chpl.domain.auth.User;
 import gov.healthit.chpl.util.Util;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Singular;
 
-/**
- * Summary data about an activity event. This is intended to be a superclass
- * containing fields that could be applicable to activity on any type of object.
- * @author kekey
- *
- */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ActivityMetadata implements Serializable {
     private static final long serialVersionUID = -3855142961571082535L;
 
     private Long id;
     private ActivityConcept concept;
+    @Singular
     private Set<ActivityCategory> categories = new HashSet<ActivityCategory>();
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
     private Date date;
     private Long objectId;
     private User responsibleUser;
     private String description;
 
-    public ActivityMetadata() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
 
     public Date getDate() {
         return Util.getNewDate(date);
@@ -42,45 +41,5 @@ public class ActivityMetadata implements Serializable {
 
     public void setDate(final Date date) {
         this.date = Util.getNewDate(date);
-    }
-
-    public Long getObjectId() {
-        return objectId;
-    }
-
-    public void setObjectId(final Long objectId) {
-        this.objectId = objectId;
-    }
-
-    public ActivityConcept getConcept() {
-        return concept;
-    }
-
-    public void setConcept(final ActivityConcept concept) {
-        this.concept = concept;
-    }
-
-    public User getResponsibleUser() {
-        return responsibleUser;
-    }
-
-    public void setResponsibleUser(final User responsibleUser) {
-        this.responsibleUser = responsibleUser;
-    }
-
-    public Set<ActivityCategory> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(final Set<ActivityCategory> categories) {
-        this.categories = categories;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
