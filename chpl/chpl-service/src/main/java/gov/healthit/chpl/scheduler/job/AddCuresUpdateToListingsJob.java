65bf00b12d313856cffe8feaf14c00e910b7fbb3
tmy1313 <tyoung@ainq.com>
2020-04-06 14:48:20 -0400

    add activity and  transaction capability
    OCD-3302

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/AddCuresUpdateToListingsJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/AddCuresUpdateToListingsJob.java
@@ -1,4 +1,4 @@
-package gov.healthit.chpl.scheduler;
+package gov.healthit.chpl.scheduler.job;
 
 import java.util.Date;
 import java.util.List;
@@ -6,18 +6,26 @@ import java.util.List;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import gov.healthit.chpl.SpecialProperties;
 import gov.healthit.chpl.dao.CertifiedProductDAO;
 import gov.healthit.chpl.dao.CuresUpdateEventDAO;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.domain.activity.ActivityConcept;
 import gov.healthit.chpl.dto.CertifiedProductDetailsDTO;
 import gov.healthit.chpl.dto.CuresUpdateEventDTO;
 import gov.healthit.chpl.exception.EntityCreationException;
 import gov.healthit.chpl.exception.EntityRetrievalException;
+import gov.healthit.chpl.manager.ActivityManager;
 import gov.healthit.chpl.manager.CertifiedProductDetailsManager;
-import gov.healthit.chpl.scheduler.job.QuartzJob;
 import gov.healthit.chpl.service.CuresUpdateService;
 import lombok.extern.log4j.Log4j2;
 
@@ -39,6 +47,12 @@ public class AddCuresUpdateToListingsJob extends QuartzJob {
     @Autowired
     private CuresUpdateEventDAO curesUpdateDao;
 
+    @Autowired
+    private ActivityManager activityManager;
+
+    @Autowired
+    private JpaTransactionManager txManager;
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
@@ -56,7 +70,8 @@ public class AddCuresUpdateToListingsJob extends QuartzJob {
             LOGGER.info("Retrieving listing information for  (" + cp.getId() + ") " + cp.getChplProductNumber());
             CertifiedProductSearchDetails listing = certifiedProductDetailsManager.getCertifiedProductDetails(cp.getId());
             if (curesUpdateService.isCuresUpdate(listing)) {
-                updateListingAsCuresUpdated(listing.getId());
+                LOGGER.info("************************** Updating " + cp.getId() + " as Cures Updated **************************");
+                updateListingAsCuresUpdated(listing);
             }
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
@@ -64,13 +79,35 @@ public class AddCuresUpdateToListingsJob extends QuartzJob {
 
     }
 
-    private void updateListingAsCuresUpdated(Long certifiedProductId) throws EntityCreationException, EntityRetrievalException {
-        addCuresUpdateEvent(certifiedProductId, true, specialProperties.getEffectiveRuleDate());
-        addCuresUpdateActivity();
+    private void updateListingAsCuresUpdated(CertifiedProductSearchDetails origListing)
+            throws EntityCreationException, EntityRetrievalException, JsonProcessingException {
+
+        TransactionTemplate txTemplate = new TransactionTemplate(txManager);
+        txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        txTemplate.execute(new TransactionCallbackWithoutResult() {
+
+            @Override
+            // can't use the manager method because we don't want to record activity
+            // so wrapping the dao call in a transaction
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                try {
+                    addCuresUpdateEvent(origListing.getId(), true, specialProperties.getEffectiveRuleDate());
+                    addCuresUpdateActivity(origListing,
+                            certifiedProductDetailsManager.getCertifiedProductDetails(origListing.getId()));
+                } catch (Exception e) {
+                    LOGGER.error("Error trying to mark listing " + origListing.getChplProductNumber() + "as Cures updated. "
+                            + e.getMessage(), e);
+                    status.setRollbackOnly();
+                }
+            }
+        });
     }
 
-    private void addCuresUpdateActivity() {
-
+    private void addCuresUpdateActivity(CertifiedProductSearchDetails origListing, CertifiedProductSearchDetails updatedListing)
+            throws JsonProcessingException, EntityCreationException, EntityRetrievalException {
+        activityManager.addActivity(ActivityConcept.CERTIFIED_PRODUCT, origListing.getId(),
+                "Updated certified product " + updatedListing.getChplProductNumber() + ".", origListing,
+                updatedListing, "");
     }
 
     private void addCuresUpdateEvent(Long certifiedProductId, Boolean curesUpdate, Date eventDate)
