faf31bbba8c1352eb524922853299985b11cfea0
tmy1313 <tyoung@ainq.com>
2020-04-16 15:38:46 -0400

    add new listing statistics to the email
    OCD-3307

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/summarystatistics/ListingStatisticsSectionCreator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/summarystatistics/ListingStatisticsSectionCreator.java
@@ -1,6 +1,7 @@
 package gov.healthit.chpl.scheduler.job.summarystatistics;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import gov.healthit.chpl.domain.statistics.CertifiedBodyAltTestStatistics;
 import gov.healthit.chpl.domain.statistics.CertifiedBodyStatistics;
@@ -50,11 +51,32 @@ public class ListingStatisticsSectionCreator extends StatisticsSectionCreator {
         }
         section.append("</ul>");
 
-        section.append(buildItem("Total # of 2014 Listings (Regardless of Status)", stats.getTotal2014Listings()));
+        List<CertifiedBodyStatistics> activeListingCountWithCuresUpdatedByAcb = massager
+                .getStatistics(stats.getActiveListingCountWithCuresUpdatedByAcb());
+        section.append(buildSection(
+                "Total # of Active (Including Suspended by ONC/ONC-ACB 2015-Cures Update Listings)",
+                sumTotalListings(activeListingCountWithCuresUpdatedByAcb),
+                activeListingCountWithCuresUpdatedByAcb));
+
+        List<CertifiedBodyStatistics> listingCountWithCuresUpdatedAndAltTestMethodsByAcb = massager
+                .getStatistics(stats.getListingCountWithCuresUpdatedAndAltTestMethodsByAcb());
+        section.append(buildSection(
+                "Total # of 2015-Cures Update Listings with Alternative Test Methods",
+                sumTotalListings(listingCountWithCuresUpdatedAndAltTestMethodsByAcb),
+                listingCountWithCuresUpdatedAndAltTestMethodsByAcb));
+
+        section.append(buildItem("Total # of 2015-Cures Updated Listings (Regardless of Status)",
+                stats.getAllListingsCountWithCuresUpdated()));
         section.append(buildItem("Total # of 2015 Listings (Regardless of Status)", stats.getTotal2015Listings()));
+        section.append(buildItem("Total # of 2014 Listings (Regardless of Status)", stats.getTotal2014Listings()));
         section.append(buildItem("Total # of 2011 Listings (Regardless of Status)", stats.getTotal2011Listings()));
 
         section.append("</ul>");
         return section.toString();
+    }
+
+    private Long sumTotalListings(List<CertifiedBodyStatistics> stats) {
+        return stats.stream()
+                .collect(Collectors.summingLong(CertifiedBodyStatistics::getTotalListings));
     }
 }
