26483afbd936ccb5b90220d4a48acddb274f215c
tmy1313 <tyoung@ainq.com>
2020-04-19 21:40:26 -0400

    refactor reviewer to work as expected
    OCD-3341

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/edition2015/MacraMeasureComparisonReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/edition2015/MacraMeasureComparisonReviewer.java
@@ -1,23 +1,28 @@
 package gov.healthit.chpl.validation.listing.reviewer.edition2015;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
+import gov.healthit.chpl.domain.CertificationCriterion;
 import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.domain.MacraMeasure;
 import gov.healthit.chpl.permissions.ResourcePermissions;
+import gov.healthit.chpl.service.CertificationCriterionService;
 import gov.healthit.chpl.util.ErrorMessageUtil;
-import gov.healthit.chpl.util.Util;
 import gov.healthit.chpl.validation.listing.reviewer.ComparisonReviewer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
- * This reviewer confirms that an ACB user does not attempt to add a 'removed' macra measure
- * to any certification result.
+ * This reviewer confirms that an ACB user does not attempt to add a 'removed' macra measure to any certification
+ * result.
+ * 
  * @author kekey
  *
  */
@@ -36,76 +41,68 @@ public class MacraMeasureComparisonReviewer implements ComparisonReviewer {
     @Override
     public void review(final CertifiedProductSearchDetails existingListing,
             final CertifiedProductSearchDetails updatedListing) {
-        //checking for the addition of a removed macra measure.
-        //this is only disallowed if the user is not ADMIN/ONC, so first check the permissions
+        // checking for the addition of a removed macra measure.
+        // this is only disallowed if the user is not ADMIN/ONC, so first check the permissions
         if (resourcePermissions.isUserRoleAdmin() || resourcePermissions.isUserRoleOnc()) {
             return;
         }
 
-        for (CertificationResult updatedCert : updatedListing.getCertificationResults()) {
-            for (CertificationResult existingCert : existingListing.getCertificationResults()) {
-                //find matching criteria in existing/updated listings
-                if (!StringUtils.isEmpty(updatedCert.getNumber()) && !StringUtils.isEmpty(existingCert.getNumber())
-                        && updatedCert.getNumber().equals(existingCert.getNumber()) && updatedCert.isSuccess() != null
-                        && updatedCert.isSuccess().equals(Boolean.TRUE)) {
-                    //if the updated listing has attested to the cert, check its g1/g2 macra measures
-                    if (updatedCert.getG1MacraMeasures() != null
-                            && updatedCert.getG1MacraMeasures().size() > 0) {
-                        List<MacraMeasure> addedG1Measures =
-                                getAddedMacraMeasures(existingCert.getG1MacraMeasures(), updatedCert.getG1MacraMeasures());
-                        for (MacraMeasure addedG1Measure : addedG1Measures) {
-                            if (addedG1Measure.getRemoved() != null && addedG1Measure.getRemoved().booleanValue()) {
-                                updatedListing.getErrorMessages().add(msgUtil.getMessage("listing.criteria.removedG1MacraMeasure",
-                                       Util.formatCriteriaNumber(updatedCert.getCriterion()),
-                                       addedG1Measure.getAbbreviation()));
-                            }
-                        }
-                    }
+        List<FlatMacraMeasure> existingListingMacraMeasures = getFlattenedG1MacraMeasures(
+                existingListing.getCertificationResults());
+        List<FlatMacraMeasure> updatedListingMacraMeasures = getFlattenedG1MacraMeasures(
+                updatedListing.getCertificationResults());
 
-                    if (updatedCert.getG2MacraMeasures() != null
-                            && updatedCert.getG2MacraMeasures().size() > 0) {
-                        List<MacraMeasure> addedG2Measures =
-                                getAddedMacraMeasures(existingCert.getG2MacraMeasures(), updatedCert.getG2MacraMeasures());
-                        for (MacraMeasure addedG2Measure : addedG2Measures) {
-                            if (addedG2Measure.getRemoved() != null && addedG2Measure.getRemoved().booleanValue()) {
-                                updatedListing.getErrorMessages().add(msgUtil.getMessage("listing.criteria.removedG2MacraMeasure",
-                                       Util.formatCriteriaNumber(updatedCert.getCriterion()),
-                                       addedG2Measure.getAbbreviation()));
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        // Was a G1 item added?
+        subtractLists(updatedListingMacraMeasures, existingListingMacraMeasures).stream()
+                .forEach(mm -> getErrorMessage("listing.criteria.removedG1MacraMeasure", mm));
+
+        existingListingMacraMeasures = getFlattenedG2MacraMeasures(existingListing.getCertificationResults());
+        updatedListingMacraMeasures = getFlattenedG2MacraMeasures(updatedListing.getCertificationResults());
+
+        // Was a G2 item added?
+        subtractLists(updatedListingMacraMeasures, existingListingMacraMeasures).stream()
+                .forEach(mm -> getErrorMessage("listing.criteria.removedG2MacraMeasure", mm));
     }
 
-    private List<MacraMeasure> getAddedMacraMeasures(
-            final List<MacraMeasure> existingMeasures,
-            final List<MacraMeasure> updatedMeasures) {
-        List<MacraMeasure> addedMeasures = new ArrayList<MacraMeasure>();
+    private List<FlatMacraMeasure> subtractLists(List<FlatMacraMeasure> listA,
+            List<FlatMacraMeasure> listB) {
 
-        // figure out which macra measures to add
-        if (updatedMeasures != null && updatedMeasures.size() > 0) {
-            if (existingMeasures == null || existingMeasures.size() == 0) {
-                // existing criteria has none, add all from the update
-                for (MacraMeasure updatedMeasure : updatedMeasures) {
-                    addedMeasures.add(updatedMeasure);
-                }
-            } else if (existingMeasures.size() > 0) {
-                // existing criteria has some, compare to the update to see if
-                // any are different
-                for (MacraMeasure updatedMeasure : updatedMeasures) {
-                    boolean inExistingCriteria = false;
-                    for (MacraMeasure existingMeasure : existingMeasures) {
-                        inExistingCriteria = !inExistingCriteria ? updatedMeasure.matches(existingMeasure) : inExistingCriteria;
-                    }
+        Predicate<FlatMacraMeasure> notInListB = fromA -> !listB.stream()
+                .anyMatch(fromB -> fromA.getCriterion().getId().equals(fromB.getCriterion().getId())
+                        && fromA.getMacraMeasure().getId().equals(fromB.getMacraMeasure().getId()));
 
-                    if (!inExistingCriteria) {
-                        addedMeasures.add(updatedMeasure);
-                    }
-                }
-            }
-        }
-        return addedMeasures;
+        return listA.stream()
+                .filter(notInListB)
+                .collect(Collectors.toList());
     }
+
+    private List<FlatMacraMeasure> getFlattenedG1MacraMeasures(List<CertificationResult> results) {
+        return results.stream()
+                .filter(result -> Objects.nonNull(result.getG1MacraMeasures()))
+                .flatMap(result -> result.getG1MacraMeasures().stream()
+                        .map(mm -> new FlatMacraMeasure(result.getCriterion(), mm)))
+                .collect(Collectors.toList());
+    }
+
+    private List<FlatMacraMeasure> getFlattenedG2MacraMeasures(List<CertificationResult> results) {
+        return results.stream()
+                .filter(result -> Objects.nonNull(result.getG2MacraMeasures()))
+                .flatMap(result -> result.getG2MacraMeasures().stream()
+                        .map(mm -> new FlatMacraMeasure(result.getCriterion(), mm)))
+                .collect(Collectors.toList());
+    }
+
+    private String getErrorMessage(String messageCode, FlatMacraMeasure flatMacraMeasure) {
+        return msgUtil.getMessage(messageCode,
+                CertificationCriterionService.formatCriteriaNumber(flatMacraMeasure.getCriterion()),
+                flatMacraMeasure.getMacraMeasure().getAbbreviation());
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class FlatMacraMeasure {
+        private CertificationCriterion criterion;
+        private MacraMeasure macraMeasure;
+    }
+
 }
