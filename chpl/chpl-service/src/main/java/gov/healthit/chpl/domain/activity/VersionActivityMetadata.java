853761626f064755e48f58947e614b0f0a7def7b
Andrew Larned <andrew.larned@gmail.com>
2020-05-08 12:54:12 -0400

    refactor: simplify file
    [#OCD-3321]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/activity/VersionActivityMetadata.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/activity/VersionActivityMetadata.java
@@ -1,32 +1,13 @@
 package gov.healthit.chpl.domain.activity;
 
-/**
- * Version specific activity summary fields.
- * @author kekey
- *
- */
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class VersionActivityMetadata extends ActivityMetadata {
     private static final long serialVersionUID = 9069117087924463180L;
 
     private String productName;
     private String version;
-
-    public VersionActivityMetadata() {
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(final String productName) {
-        this.productName = productName;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(final String version) {
-        this.version = version;
-    }
 }
