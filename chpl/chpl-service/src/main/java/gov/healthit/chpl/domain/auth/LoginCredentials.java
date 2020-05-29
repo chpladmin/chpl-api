85d13403b3b5ec01adc1fc21ae86606a6ff69cfc
Andrew Larned <andrew.larned@gmail.com>
2020-05-29 08:57:44 -0400

    fix: add constructor
    [#OCD-3382]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/auth/LoginCredentials.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/auth/LoginCredentials.java
@@ -1,8 +1,10 @@
 package gov.healthit.chpl.domain.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginCredentials {
 
     private String userName;
