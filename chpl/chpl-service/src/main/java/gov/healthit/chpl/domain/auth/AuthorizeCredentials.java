edf74dd9fbc4e48c563d523386ce9810a06a0565
Andrew Larned <andrew.larned@gmail.com>
2020-05-29 11:02:30 -0400

    fix: restore pre-lombok domain objects
    [#OCD-3382]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/auth/AuthorizeCredentials.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/auth/AuthorizeCredentials.java
@@ -2,14 +2,15 @@ package gov.healthit.chpl.domain.auth;
 
 import java.io.Serializable;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.SuperBuilder;
-
-@Data
-@SuperBuilder
-@EqualsAndHashCode(callSuper = false)
 public class AuthorizeCredentials extends LoginCredentials implements Serializable {
     private static final long serialVersionUID = 5419635752541177318L;
     private String hash;
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(final String hash) {
+        this.hash = hash;
+    }
 }
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/auth/LoginCredentials.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/auth/LoginCredentials.java
@@ -1,14 +1,30 @@
 package gov.healthit.chpl.domain.auth;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.experimental.SuperBuilder;
-
-@Data
-@SuperBuilder
-@AllArgsConstructor
 public class LoginCredentials {
-
     private String userName;
     private String password;
+
+    public LoginCredentials() {
+    }
+
+    public LoginCredentials(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
