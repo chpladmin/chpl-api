dd76b2a4064001b13a6b449aff6e3dd3a1f84244
Andrew Larned <andrew.larned@gmail.com>
2020-06-09 08:52:39 -0400

    fix: use property lookup for criteria instead of number/title
    [#OCD-3360]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/pendingListing/reviewer/edition2015/OldCriteriaWithoutIcsReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/pendingListing/reviewer/edition2015/OldCriteriaWithoutIcsReviewer.java
@@ -3,6 +3,8 @@ package gov.healthit.chpl.validation.pendingListing.reviewer.edition2015;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.dto.listing.pending.PendingCertifiedProductDTO;
 import gov.healthit.chpl.service.CertificationCriterionService;
+import gov.healthit.chpl.service.CertificationCriterionService.Criteria2015;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.Util;
 import gov.healthit.chpl.validation.pendingListing.reviewer.Reviewer;
@@ -26,19 +29,35 @@ import lombok.extern.log4j.Log4j2;
 @Component("pendingOldCriteriaWithoutIcsReviewer")
 public class OldCriteriaWithoutIcsReviewer implements Reviewer {
     private static final String B3_CHANGE_DATE = "questionableActivity.b3ChangeDate";
+    private static final String RULE_EFFECTIVE_DATE = "cures.ruleEffectiveDate";
+
     private static final String B3_CRITERIA_NUMBER = "170.315 (b)(3)";
     private static final String D2_CRITERIA_NUMBER = "170.315 (d)(2)";
     private static final String D3_CRITERIA_NUMBER = "170.315 (d)(3)";
     private static final String D10_CRITERIA_NUMBER = "170.315 (d)(10)";
-    private static final String RULE_EFFECTIVE_DATE = "cures.ruleEffectiveDate";
+    private long b3Id;
+    private long d2Id;
+    private long d3Id;
+    private long d10Id;
 
     private Environment env;
     private ErrorMessageUtil msgUtil;
+    private CertificationCriterionService criteriaService;
 
     @Autowired
-    public OldCriteriaWithoutIcsReviewer(Environment env, ErrorMessageUtil msgUtil) {
+    public OldCriteriaWithoutIcsReviewer(Environment env, ErrorMessageUtil msgUtil,
+            CertificationCriterionService criteriaService) {
         this.env = env;
         this.msgUtil = msgUtil;
+        this.criteriaService = criteriaService;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        b3Id = criteriaService.get(Criteria2015.B_3_OLD).getId();
+        d2Id = criteriaService.get(Criteria2015.D_2_OLD).getId();
+        d3Id = criteriaService.get(Criteria2015.D_3_OLD).getId();
+        d10Id = criteriaService.get(Criteria2015.D_10_OLD).getId();
     }
 
     @Override
@@ -56,27 +75,27 @@ public class OldCriteriaWithoutIcsReviewer implements Reviewer {
             return;
         }
 
-        String error = checkForError(listing, B3_CRITERIA_NUMBER, b3ChangeDate);
+        String error = checkForError(listing, B3_CRITERIA_NUMBER, b3Id, b3ChangeDate);
         if (!StringUtils.isEmpty(error)) {
             listing.getErrorMessages().add(error);
         }
-        error = checkForError(listing, D2_CRITERIA_NUMBER, ruleEffectiveDate);
+        error = checkForError(listing, D2_CRITERIA_NUMBER, d2Id, ruleEffectiveDate);
         if (!StringUtils.isEmpty(error)) {
             listing.getErrorMessages().add(error);
         }
-        error = checkForError(listing, D3_CRITERIA_NUMBER, ruleEffectiveDate);
+        error = checkForError(listing, D3_CRITERIA_NUMBER, d3Id, ruleEffectiveDate);
         if (!StringUtils.isEmpty(error)) {
             listing.getErrorMessages().add(error);
         }
-        error = checkForError(listing, D10_CRITERIA_NUMBER, ruleEffectiveDate);
+        error = checkForError(listing, D10_CRITERIA_NUMBER, d10Id, ruleEffectiveDate);
         if (!StringUtils.isEmpty(error)) {
             listing.getErrorMessages().add(error);
         }
     }
 
-    private String checkForError(PendingCertifiedProductDTO listing, String criteria, long relevantDate) {
+    private String checkForError(PendingCertifiedProductDTO listing, String criteria, long criteriaId, long relevantDate) {
         long certificationDate = listing.getCertificationDate().getTime();
-        if (hasRelevantCriteria(listing, criteria) && certificationDate >= relevantDate) {
+        if (hasRelevantCriteria(listing, criteriaId) && certificationDate >= relevantDate) {
             return getErrorMessage("listing.criteria.hasOldVersionOfCriteria", criteria,
                     Util.getDateFormatter().format(relevantDate));
         }
@@ -93,11 +112,10 @@ public class OldCriteriaWithoutIcsReviewer implements Reviewer {
         return sdf.parse(dateAsString).getTime();
     }
 
-    private boolean hasRelevantCriteria(PendingCertifiedProductDTO listing, String criteria) {
+    private boolean hasRelevantCriteria(PendingCertifiedProductDTO listing, long criteriaId) {
         return listing.getCertificationCriterion().stream()
-                .anyMatch(result -> result.getCriterion().getNumber().equals(criteria)
-                        && result.getMeetsCriteria()
-                        && !CertificationCriterionService.hasCuresInTitle(result.getCriterion()));
+                .anyMatch(result -> result.getCriterion().getId().equals(criteriaId)
+                        && result.getMeetsCriteria());
     }
 
     private boolean hasICS(PendingCertifiedProductDTO listing) {
