02a36667f686df34d3b3e85d5085ac0fb74264ed
Katy Ekey <katy.ekey@gmail.com>
2020-06-22 13:46:14 -0400

    fix: remove unneeded checks from dr service

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/service/DirectReviewService.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/service/DirectReviewService.java
@@ -18,9 +18,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gov.healthit.chpl.domain.compliance.DirectReview;
-import gov.healthit.chpl.exception.InvalidArgumentsException;
 import gov.healthit.chpl.util.ChplProductNumberUtil;
-import gov.healthit.chpl.util.ChplProductNumberUtil.ChplProductNumberParts;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 
 @Component("directReviewService")
@@ -39,18 +37,8 @@ public class DirectReviewService {
         this.msgUtil = msgUtil;
     }
 
-    public List<DirectReview> getDirectReviews(String chplProductNumber) throws InvalidArgumentsException {
+    public List<DirectReview> getDirectReviews(String chplProductNumber) {
         LOGGER.info("Fetching direct review data for listing " + chplProductNumber);
-        if (chplNumberUtil.isLegacy(chplProductNumber)) {
-            LOGGER.warn(msgUtil.getMessage("directReview.legacyNotAllowed"));
-            throw new InvalidArgumentsException(msgUtil.getMessage("directReview.legacyNotAllowed"));
-        }
-        ChplProductNumberParts parts = chplNumberUtil.parseChplProductNumber(chplProductNumber);
-        if (parts.getEditionCode().equals(EDITION_2014)) {
-            LOGGER.warn(msgUtil.getMessage("directReview.editionNotAllowed", parts.getEditionCode()));
-            throw new InvalidArgumentsException(msgUtil.getMessage("directReview.editionNotAllowed", parts.getEditionCode()));
-        }
-
         //Code to query jira goes here.
         //Example of how to query by CHPL ID: /search?jql=cf[10213]~15.04.04.2858.Tsys.06.01.1.191223
         //The actual code will replace what is below, which is just selecting a sample JSON file
