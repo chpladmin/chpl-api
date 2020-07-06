58724d5490cb277512820c5ec2223213b29c29f2
Katy Ekey <katy.ekey@gmail.com>
2020-07-06 09:33:07 -0400

    wip: parse chpl product number from file

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/listing/ListingUploadHandler.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/listing/ListingUploadHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.dto.listing.pending.PendingCertifiedProductDTO;
@@ -31,13 +32,40 @@ public class ListingUploadHandler {
         return allCsvRecords.get(getHeadingRecordIndex(allCsvRecords));
     }
 
-    public String parseChplProductNumber(CSVRecord headingRecord, CSVRecord dataRecord) {
+    public String parseChplProductNumber(CSVRecord headingRecord, List<CSVRecord> listingRecords) {
         String chplProductNumber = null;
-        //TODO:
+        int chplProductNumberHeadingIndex = getColumnIndexOfHeading(Headings.UNIQUE_ID, headingRecord);
+        if (chplProductNumberHeadingIndex < 0) {
+            //TODO throw something
+        }
+        for (CSVRecord listingRecord : listingRecords) {
+            if (StringUtils.isEmpty(chplProductNumber)
+                    && !StringUtils.isEmpty(listingRecord.get(chplProductNumberHeadingIndex))) {
+                chplProductNumber = listingRecord.get(chplProductNumberHeadingIndex);
+            }
+        }
         return chplProductNumber;
     }
 
-    public String parseStatus(CSVRecord headingRecord, CSVRecord dataRecord) {
+    public String parseChplProductNumber(CSVRecord headingRecord, CSVRecord listingRecord) {
+        String chplProductNumber = null;
+        int chplProductNumberHeadingIndex = getColumnIndexOfHeading(Headings.UNIQUE_ID, headingRecord);
+        if (chplProductNumberHeadingIndex < 0) {
+            //TODO throw something
+        }
+        if (!StringUtils.isEmpty(listingRecord.get(chplProductNumberHeadingIndex))) {
+            chplProductNumber = listingRecord.get(chplProductNumberHeadingIndex);
+        }
+        return chplProductNumber;
+    }
+
+    public String parseStatus(CSVRecord headingRecord, List<CSVRecord> listingRecords) {
+        String status = null;
+        //TODO:
+        return status;
+    }
+
+    public String parseStatus(CSVRecord headingRecord, CSVRecord listingRecord) {
         String status = null;
         //TODO:
         return status;
@@ -57,5 +85,18 @@ public class ListingUploadHandler {
             }
         }
         return false;
+    }
+
+    private int getColumnIndexOfHeading(Headings heading, CSVRecord headingRecord) {
+        int index = 0;
+        Iterator<String> iter = headingRecord.iterator();
+        while (iter.hasNext()) {
+            String currHeadingValue = iter.next();
+            if (Headings.getHeading(currHeadingValue).equals(heading)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 }
