d81e86185f99f974edb65b68d6aa7fe9c23ceaf0
Katy Ekey <katy.ekey@gmail.com>
2020-07-31 13:07:42 -0400

    fix: Remove code changes used for debugging
    [#OCD-3432]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/CertifiedProductDownloadableResourceCreatorJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/CertifiedProductDownloadableResourceCreatorJob.java
@@ -135,8 +135,7 @@ public class CertifiedProductDownloadableResourceCreatorJob extends Downloadable
     private List<CertifiedProductDetailsDTO> getRelevantListings() throws EntityRetrievalException {
         LOGGER.info("Finding all listings for edition " + edition + ".");
         Date start = new Date();
-        List<CertifiedProductDetailsDTO> listingsForEdition = getCertifiedProductDao().findWithSurveillance().subList(0, 5);
-        //findByEdition(edition);
+        List<CertifiedProductDetailsDTO> listingsForEdition = getCertifiedProductDao().findByEdition(edition);
         Date end = new Date();
         LOGGER.info("Found the " + listingsForEdition.size() + " listings from " + edition + " in "
                 + ((end.getTime() - start.getTime()) / MILLIS_PER_SECOND) + " seconds");
