37af07968b1cc28e36cf1cd9806cbe73a7894d64
Katy Ekey <katy.ekey@gmail.com>
2020-08-11 14:20:26 -0400

    refactor: rename more 'nonconformity' fields to say 'nonConformity'
    [#OCD-3371]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/compliance/DirectReviewNonConformity.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/compliance/DirectReviewNonConformity.java
@@ -28,28 +28,23 @@ public class DirectReviewNonConformity implements Serializable {
     @JsonAlias("customfield_10943")
     private List<String> developerAssociatedListings;
 
-    @JsonProperty(value = "nonconformityType")
+    @JsonProperty(value = "nonConformityType")
     @JsonAlias("customfield_10934")
     @JsonDeserialize(using = SimpleValueDeserializer.class)
-    private String nonconformityType;
+    private String nonConformityType;
 
     @JsonProperty(value = "dateOfDetermination")
     @JsonAlias("customfield_10921")
     @JsonDeserialize(using = DateDeserializer.class)
     private LocalDate dateOfDetermination;
 
-    @JsonProperty(value = "nonconformityStatus")
-    @JsonAlias("customfield_10935")
-    @JsonDeserialize(using = SimpleValueDeserializer.class)
-    private String nonconformityStatus;
-
-    @JsonProperty(value = "nonconformitySummary")
+    @JsonProperty(value = "nonConformitySummary")
     @JsonAlias("customfield_10927")
-    private String nonconformitySummary;
+    private String nonConformitySummary;
 
-    @JsonProperty(value = "nonconformityFindings")
+    @JsonProperty(value = "nonConformityFindings")
     @JsonAlias("customfield_10928")
-    private String nonconformityFindings;
+    private String nonConformityFindings;
 
     @JsonProperty(value = "developerExplanation")
     @JsonAlias("customfield_10929")
