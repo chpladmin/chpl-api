9b7698c130cd5d3a955229da9d92e3cc7aa02165
tmy1313 <tyoung@ainq.com>
2020-08-14 12:41:28 -0400

    send http error 401 when the api key is not valid
    OCD-3246

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/filter/APIKeyAuthenticationFilter.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/filter/APIKeyAuthenticationFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -47,6 +48,8 @@ public class APIKeyAuthenticationFilter extends GenericFilterBean {
     public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest request = null;
+        HttpServletResponse response = (HttpServletResponse) res;
+
         if (req instanceof javax.servlet.http.HttpServletRequest) {
             request = (HttpServletRequest) req;
         } else {
@@ -77,7 +80,8 @@ public class APIKeyAuthenticationFilter extends GenericFilterBean {
                         "API key presented in Header does not match API key presented as URL Parameter.");
                 ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
                 String json = ow.writeValueAsString(errorObj);
-                res.getOutputStream().write(json.getBytes("UTF-8"));
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, json);
+                return;
             }
         }
 
@@ -93,7 +97,8 @@ public class APIKeyAuthenticationFilter extends GenericFilterBean {
             ErrorResponse errorObj = new ErrorResponse("API key must be presented in order to use this API");
             ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
             String json = ow.writeValueAsString(errorObj);
-            res.getOutputStream().write(json.getBytes("UTF-8"));
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, json);
+            return;
         } else {
             try {
                 ApiKeyDTO retrievedKey = apiKeyManager.findKey(key);
@@ -103,7 +108,8 @@ public class APIKeyAuthenticationFilter extends GenericFilterBean {
                     ErrorResponse errorObj = new ErrorResponse("Invalid API Key");
                     ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
                     String json = ow.writeValueAsString(errorObj);
-                    res.getOutputStream().write(json.getBytes("UTF-8"));
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, json);
+                    return;
                 } else {
                     try {
                         apiKeyManager.logApiKeyActivity(key, requestPath);
@@ -114,6 +120,8 @@ public class APIKeyAuthenticationFilter extends GenericFilterBean {
                 }
             } catch (final EntityRetrievalException ex) {
                 LOGGER.error("Cannot find key for HTTP filter: " + key);
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid API Key");
+                return;
             }
         }
     }
