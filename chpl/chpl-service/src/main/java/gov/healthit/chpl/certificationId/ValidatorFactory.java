9be7ad11daddc35fbda9dd837b4a6eaa646a51b6
Katy Ekey <katy.ekey@gmail.com>
2020-09-30 10:58:17 -0400

    refactor: Remove CMS ID validators that can no longer be used

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/certificationId/ValidatorFactory.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/certificationId/ValidatorFactory.java
@@ -1,20 +1,9 @@
 package gov.healthit.chpl.certificationId;
 
-import org.ff4j.FF4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ValidatorFactory {
-
-    private FF4j ff4j;
-
-    @Autowired
-    public ValidatorFactory(
-            final FF4j ff4j) {
-        this.ff4j = ff4j;
-    }
-
     /**
      * Retrieve the validator that should be used for a given year.
      * @param attYear the given year
@@ -23,12 +12,8 @@ public class ValidatorFactory {
     public Validator getValidator(final String attYear) {
         Validator val = null;
 
-        if ("2014".equals(attYear)) {
-            val = new Validator2014();
-        } else if ("2015".equals(attYear)) {
+         if ("2015".equals(attYear)) {
             val = new Validator2015();
-        } else if ("2014/2015".equals(attYear)) {
-            val = new Validator20142015();
         } else {
             val = new ValidatorDefault();
         }
