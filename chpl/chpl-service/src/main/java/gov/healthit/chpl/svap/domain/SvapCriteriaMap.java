21349cae91c9959669187cfeee1db6a9e7152904
tmy1313 <tyoung@ainq.com>
2020-10-05 21:59:28 -0400

    refactor: remove unnecessary variables
    OCD-3508

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/svap/domain/SvapCriteriaMap.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/svap/domain/SvapCriteriaMap.java
@@ -1,7 +1,5 @@
 package gov.healthit.chpl.svap.domain;
 
-import java.util.Objects;
-
 import gov.healthit.chpl.domain.CertificationCriterion;
 import gov.healthit.chpl.dto.CertificationCriterionDTO;
 import gov.healthit.chpl.svap.entity.SvapCriteriaMapEntity;
@@ -15,9 +13,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class SvapCriteriaMap {
-    private static final String INVALID_EDITION_ERROR_KEY = "listing.criteria.f3CannotHaveGap";
-    private static final String INVALID_SVAP_CRITERIA_ERROR_KEY = "listing.criteria.f3CannotHaveGap";
-
     private Long id;
     private CertificationCriterion criterion;
     private Svap svap;
