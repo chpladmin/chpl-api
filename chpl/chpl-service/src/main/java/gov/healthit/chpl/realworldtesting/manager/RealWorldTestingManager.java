f5ec84399b66c5153d93d611cb9813eec7eb41cb
tmy1313 <tyoung@ainq.com>
2020-10-06 15:52:00 -0400

    feat: add permissions to the method RealWorldTestingManager.upload method
    OCD-3434

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/realworldtesting/manager/RealWorldTestingManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/realworldtesting/manager/RealWorldTestingManager.java
@@ -1,13 +1,14 @@
 package gov.healthit.chpl.realworldtesting.manager;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 public class RealWorldTestingManager {
 
     @Transactional
-    //@PreAuthorize("@permissions.hasAccess(T(gov.healthit.chpl.permissions.Permissions).PENDING_SURVEILLANCE, "
-    //        + "T(gov.healthit.chpl.permissions.domains.PendingSurveillanceDomainPermissions).UPLOAD)")
+    @PreAuthorize("@permissions.hasAccess(T(gov.healthit.chpl.permissions.Permissions).REAL_WORLD_TESTING, "
+            + "T(gov.healthit.chpl.permissions.domains.RealWorldTestingDomainPermissions).UPLOAD)")
     public void  uploadPendingSurveillance(MultipartFile file) {
 
     }
