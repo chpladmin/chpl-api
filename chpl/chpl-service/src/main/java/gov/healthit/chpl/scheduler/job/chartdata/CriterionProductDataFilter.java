1b26375711d24649c29a1d06db9f71f5bf090155
tmy1313 <tyoung@ainq.com>
2020-10-21 14:56:00 -0400

    feat: remove the flag effective-rule-date-plus-three-months and references
    OCD-3532

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/chartdata/CriterionProductDataFilter.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/chartdata/CriterionProductDataFilter.java
@@ -5,20 +5,13 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.ff4j.FF4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import gov.healthit.chpl.FeatureList;
 import gov.healthit.chpl.domain.search.CertifiedProductFlatSearchResult;
 
 public class CriterionProductDataFilter {
     private static final Logger LOGGER = LogManager.getLogger("chartDataCreatorJobLogger");
-    private static final String EDITION_2014 = "2014";
     private static final String EDITION_2015 = "2015";
-
-    @Autowired
-    private FF4j ff4j;
 
     public CriterionProductDataFilter() {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
@@ -28,9 +21,7 @@ public class CriterionProductDataFilter {
             final List<CertifiedProductFlatSearchResult> certifiedProducts) {
         List<CertifiedProductFlatSearchResult> results = new ArrayList<CertifiedProductFlatSearchResult>();
         for (CertifiedProductFlatSearchResult result : certifiedProducts) {
-            if ((result.getEdition().equalsIgnoreCase(EDITION_2014)
-                    && !ff4j.check(FeatureList.EFFECTIVE_RULE_DATE_PLUS_THREE_MONTHS))
-                    || result.getEdition().equalsIgnoreCase(EDITION_2015)) {
+            if (result.getEdition().equalsIgnoreCase(EDITION_2015)) {
                 results.add(result);
             }
         }
