10cc6d57ddef568c6476c222ceee1dc2348c2f14
Katy Ekey <katy.ekey@gmail.com>
2020-11-10 13:30:07 -0500

    fix!: Set new objectmapper to ignore unknown json fields
    [#OCD-3402]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/DirectReviewDeserializingObjectMapper.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/DirectReviewDeserializingObjectMapper.java
@@ -3,6 +3,7 @@ package gov.healthit.chpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.cfg.HandlerInstantiator;
 
@@ -11,6 +12,8 @@ public class DirectReviewDeserializingObjectMapper extends ObjectMapper {
     private static final long serialVersionUID = 8512096693958255305L;
 
     public DirectReviewDeserializingObjectMapper() {
+        super();
+        this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
     @Override
