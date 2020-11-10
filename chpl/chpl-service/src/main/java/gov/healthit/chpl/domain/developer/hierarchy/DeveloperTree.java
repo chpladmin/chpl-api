a4f214277d0f90033a4bfd16fcfab12b135a254c
Katy Ekey <katy.ekey@gmail.com>
2020-11-10 13:48:34 -0500

    feat!: Add new endpoint to get developer hierarchy
    OCD-3443

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/developer/hierarchy/DeveloperTree.java
@@ -0,0 +1,44 @@
+package gov.healthit.chpl.domain.developer.hierarchy;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.ObjectUtils;
+
+import gov.healthit.chpl.domain.Developer;
+import gov.healthit.chpl.dto.DeveloperDTO;
+import lombok.Data;
+
+@Data
+public class DeveloperTree extends Developer {
+    private static final long serialVersionUID = -6578977483166240315L;
+
+    private Set<ProductTree> products = new LinkedHashSet<ProductTree>();
+
+    public DeveloperTree() {
+        super();
+    }
+
+    public DeveloperTree(DeveloperDTO dto) {
+        super(dto);
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof DeveloperTree)) {
+            return false;
+        }
+        DeveloperTree otherDev = (DeveloperTree) obj;
+        return ObjectUtils.equals(this.getDeveloperId(), otherDev.getDeveloperId());
+    }
+
+    public int hashCode() {
+        if (this.getDeveloperId() == null) {
+            return -1;
+        }
+
+        return this.getDeveloperId().hashCode();
+    }
+}
---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/developer/hierarchy/ProductTree.java
@@ -0,0 +1,44 @@
+package gov.healthit.chpl.domain.developer.hierarchy;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.ObjectUtils;
+
+import gov.healthit.chpl.domain.Product;
+import gov.healthit.chpl.dto.ProductDTO;
+import lombok.Data;
+
+@Data
+public class ProductTree extends Product {
+    private static final long serialVersionUID = 1317931840652660303L;
+
+    private Set<VersionTree> versions = new LinkedHashSet<VersionTree>();
+
+    public ProductTree() {
+        super();
+    }
+
+    public ProductTree(ProductDTO dto) {
+        super(dto);
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof ProductTree)) {
+            return false;
+        }
+        ProductTree otherProduct = (ProductTree) obj;
+        return ObjectUtils.equals(this.getProductId(), otherProduct.getProductId());
+    }
+
+    public int hashCode() {
+        if (this.getProductId() == null) {
+            return -1;
+        }
+
+        return this.getProductId().hashCode();
+    }
+}
