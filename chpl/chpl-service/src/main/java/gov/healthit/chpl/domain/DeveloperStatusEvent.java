3a39472785211b8dfce37bba82b8dfdf01a65471
Andrew Larned <andrew.larned@gmail.com>
2020-11-10 11:13:15 -0500

    fix: re-enable Developer & Product edit
    [#OCD-3402]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/DeveloperStatusEvent.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/DeveloperStatusEvent.java
@@ -8,6 +8,8 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import gov.healthit.chpl.dto.DeveloperStatusEventDTO;
 import gov.healthit.chpl.util.Util;
 import lombok.AllArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.Builder;
 
 @XmlType(namespace = "http://chpl.healthit.gov/listings")
 @XmlAccessorType(XmlAccessType.FIELD)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 @AllArgsConstructor
 public class DeveloperStatusEvent implements Serializable {
