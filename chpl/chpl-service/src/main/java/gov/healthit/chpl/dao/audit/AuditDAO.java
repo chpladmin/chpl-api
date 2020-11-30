2c3d6025fb236f8aafdef8ed0509b2857e233951
todd.young <tyoung@ainq.com>
2020-11-30 10:54:12 -0500

    feat: add parameter for the audit data file location
    OCD-3538

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/audit/AuditDAO.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/audit/AuditDAO.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import org.hibernate.jdbc.Work;
 import org.postgresql.copy.CopyManager;
 import org.postgresql.jdbc.PgConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.dao.impl.BaseDAOImpl;
@@ -17,6 +19,13 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class AuditDAO extends BaseDAOImpl {
 
+    private String auditDataFilePath;
+
+    @Autowired
+    public AuditDAO(@Value("${auditDataFilePath}") String auditDataFilePath) {
+        this.auditDataFilePath = auditDataFilePath;
+    }
+
     public void doSomething() {
         LOGGER.info("STARTING");
         getSession().doWork(new Work() {
@@ -25,7 +34,7 @@ public class AuditDAO extends BaseDAOImpl {
             public void execute(Connection connection) throws SQLException {
                 CopyManager cm = new CopyManager(connection.getMetaData().getConnection().unwrap(PgConnection.class));
 
-                try (FileWriter fw = new FileWriter(new File("c:\\chpl\\files\\vendor_auto.csv"))) {
+                try (FileWriter fw = new FileWriter(new File(auditDataFilePath + "\\vendor_auto.csv"))) {
                     LOGGER.info("Got this far");
                     cm.copyOut("COPY (SELECT * from openchpl.vendor) TO STDOUT DELIMITER ',' CSV HEADER", fw);
                     LOGGER.info("And even farther");
