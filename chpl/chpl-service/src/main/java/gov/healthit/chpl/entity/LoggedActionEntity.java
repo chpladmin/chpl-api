0f2c97b7bb123dfcb234e264a7a2455db3e98341
todd.young <tyoung@ainq.com>
2020-12-03 08:45:46 -0500

    feat: add a faux id to the entity, though no PK exists on table
    OCD-3538

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/entity/LoggedActionEntity.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/entity/LoggedActionEntity.java
@@ -1,9 +1,11 @@
 package gov.healthit.chpl.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -11,29 +13,38 @@ import lombok.Data;
 @Entity
 @Table(name = "audit.logged_actions")
 @Data
-public class LoggedActionEntity {
+public class LoggedActionEntity implements Serializable {
+    private static final long serialVersionUID = -7980143499756233253L;
 
+    @Id
     @Column(name = "schema_name")
     private String schemaName;
 
+    @Id
     @Column(name = "table_name")
     private String tableName;
 
+    @Id
     @Column(name = "user_name")
     private String userName;
 
+    @Id
     @Column(name = "action_tstamp")
     private Date actionTStamp;
 
+    @Id
     @Column(name = "action")
     private String action;
 
+    @Id
     @Column(name = "original_data")
     private String originalData;
 
+    @Id
     @Column(name = "new_data")
     private String newData;
 
+    @Id
     @Column(name = "query")
     private String query;
 }
