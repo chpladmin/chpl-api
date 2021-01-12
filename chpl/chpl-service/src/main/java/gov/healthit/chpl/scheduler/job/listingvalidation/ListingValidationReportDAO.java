135dff9b8df60a6b32ea324043b2c7df17882eec
todd.young <tyoung@ainq.com>
2021-01-11 21:04:07 -0500

    feat: add required fields for adding a record
    OCD-3563

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/listingvalidation/ListingValidationReportDAO.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/listingvalidation/ListingValidationReportDAO.java
@@ -1,7 +1,10 @@
 package gov.healthit.chpl.scheduler.job.listingvalidation;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Repository;
 
+import gov.healthit.chpl.auth.user.User;
 import gov.healthit.chpl.dao.impl.BaseDAOImpl;
 
 @Repository
@@ -14,6 +17,8 @@ public class ListingValidationReportDAO extends BaseDAOImpl {
                 .certificationStatusName(lvr.getCertificationStatusName())
                 .errorMessage(lvr.getErrorMessage())
                 .reportDate(lvr.getReportDate())
+                .creationDate(new Date())
+                .lastModifiedUser(User.SYSTEM_USER_ID)
                 .build();
 
         entityManager.persist(entity);
