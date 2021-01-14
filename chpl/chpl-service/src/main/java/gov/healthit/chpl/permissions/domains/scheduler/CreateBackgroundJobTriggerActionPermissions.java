0a89fa1652b3737de57a77d82a9b7f7db5d4bb11
Katy Ekey <katy.ekey@gmail.com>
2021-01-14 11:01:29 -0500

    fix: Add merge job as something ACB has permission to do
    [#OCD-3584]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/scheduler/CreateBackgroundJobTriggerActionPermissions.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/scheduler/CreateBackgroundJobTriggerActionPermissions.java
@@ -11,6 +11,7 @@ import gov.healthit.chpl.domain.schedule.ChplOneTimeTrigger;
 import gov.healthit.chpl.permissions.domains.ActionPermissions;
 import gov.healthit.chpl.scheduler.job.RealWorldTestingUploadJob;
 import gov.healthit.chpl.scheduler.job.SplitDeveloperJob;
+import gov.healthit.chpl.scheduler.job.developer.MergeDeveloperJob;
 
 @Component(value = "schedulerCreateBackgroundJobTriggerActionPermissions")
 public class CreateBackgroundJobTriggerActionPermissions extends ActionPermissions {
@@ -20,6 +21,7 @@ public class CreateBackgroundJobTriggerActionPermissions extends ActionPermissio
     @PostConstruct
     public void init() {
         BACKGROUND_JOBS_ACB_CAN_CREATE.add(SplitDeveloperJob.JOB_NAME);
+        BACKGROUND_JOBS_ACB_CAN_CREATE.add(MergeDeveloperJob.JOB_NAME);
         BACKGROUND_JOBS_ACB_CAN_CREATE.add(RealWorldTestingUploadJob.JOB_NAME);
     }
 
