f009c32b5cd7158f0289d641e658051a6983963a
todd.young <tyoung@ainq.com>
2021-01-19 13:07:46 -0500

    feat: add email job to send Listing Validation Report
    OCD-3563

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/listingvalidation/ListingValidationReportCsvCreator.java
@@ -0,0 +1,89 @@
+package gov.healthit.chpl.scheduler.job.listingvalidation;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2(topic = "listingValidationReportEmailJobLogger")
+@Component
+public class ListingValidationReportCsvCreator {
+
+    private Environment env;
+
+    @Autowired
+    public ListingValidationReportCsvCreator(Environment env) {
+        this.env = env;
+    }
+
+    private static final String NEW_LINE_SEPARATOR = "\n";
+
+    public File createCsvFile(List<ListingValidationReport> reports) throws IOException {
+        CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
+
+        File csvFile = getOutputFile();
+        try (FileWriter fileWriter = new FileWriter(csvFile);
+                CSVPrinter csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat)) {
+
+            csvFilePrinter.printRecord(getHeaderRow());
+
+            reports.stream()
+                .sorted(Comparator.comparing(ListingValidationReport::getChplProductNumber))
+                .forEach(report -> printRow(csvFilePrinter, report));
+        }
+        return csvFile;
+    }
+
+
+
+    private File getOutputFile() {
+        File temp = null;
+        try {
+            temp = File.createTempFile(getFilename(), ".csv");
+            temp.deleteOnExit();
+        } catch (IOException ex) {
+            LOGGER.error("Could not create temporary file " + ex.getMessage(), ex);
+        }
+
+        return temp;
+    }
+
+    private List<String> getHeaderRow() {
+        return Arrays.asList(
+                "CHPL Product Number",
+                "Product Name",
+                "Status",
+                "Error Message");
+    }
+
+    private List<String> getRow(ListingValidationReport report) {
+        return Arrays.asList(
+                report.getChplProductNumber(),
+                report.getProductName(),
+                report.getCertificationStatusName(),
+                report.getErrorMessage());
+    }
+
+    private void printRow(CSVPrinter csvFilePrinter, ListingValidationReport report) {
+        try {
+            csvFilePrinter.printRecord(getRow(report));
+        } catch (IOException e) {
+            LOGGER.catching(e);
+        }
+    }
+
+    private String getFilename() {
+        return env.getProperty("listingValidationReport.fileName") + LocalDate.now().toString();
+    }
+}
---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/listingvalidation/ListingValidatorEmailJob.java
@@ -0,0 +1,74 @@
+package gov.healthit.chpl.scheduler.job.listingvalidation;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TimeZone;
+
+import javax.mail.MessagingException;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import gov.healthit.chpl.util.EmailBuilder;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2(topic = "listingValidationReportEmailJobLogger")
+public class ListingValidatorEmailJob  implements Job {
+
+    @Autowired
+    private ListingValidationReportDAO listingValidationReportDAO;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private ListingValidationReportCsvCreator listingValidationReportCsvCreator;
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+        LOGGER.info("********* Starting the Listing Validation Report Email job for " + context.getMergedJobDataMap().getString("email") + " *********");
+        try {
+
+            sendEmail(context, getReportData());
+        } catch (Exception e) {
+            LOGGER.catching(e);
+        } finally {
+            LOGGER.info("********* Completed the Listing Validation Report Email job. *********");
+        }
+    }
+
+    private List<ListingValidationReport> getReportData() {
+        return listingValidationReportDAO.getAll();
+    }
+
+    private void sendEmail(JobExecutionContext context, List<ListingValidationReport> rows) throws MessagingException, IOException {
+        LOGGER.info("Sending email to: " + context.getMergedJobDataMap().getString("email"));
+        EmailBuilder emailBuilder = new EmailBuilder(env);
+        emailBuilder.recipient(context.getMergedJobDataMap().getString("email"))
+                .subject(env.getProperty("listingValidationReport.subject"))
+                .htmlMessage(String.format(env.getProperty("listingValidationReport.body"), getReportDateAsString(rows)))
+                .fileAttachments(Arrays.asList(listingValidationReportCsvCreator.createCsvFile(rows)))
+                .sendEmail();
+        LOGGER.info("Completed Sending email to: " + context.getMergedJobDataMap().getString("email"));
+    }
+
+    private String getReportDateAsString(List<ListingValidationReport> rows) {
+
+
+        if (rows.size() > 0) {
+            SimpleDateFormat etDf = new SimpleDateFormat("MM/dd/yyyy 'at' hh:mma 'ET'");
+            TimeZone etTimeZone = TimeZone.getTimeZone("America/New_York");
+            etDf.setTimeZone(etTimeZone);
+            return etDf.format(rows.get(0).getReportDate().getTime());
+        } else {
+            return "UNKNOWN";
+        }
+    }
+}
