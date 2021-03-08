548770fa215243747529931d89ff4b3ca8e109e2
todd.young <tyoung@ainq.com>
2021-03-07 23:22:58 -0500

    fix : remove unnecessary transaction from the job
    OCD-3580

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/measurefix/MeasureFixJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/measurefix/MeasureFixJob.java
@@ -11,12 +11,7 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import gov.healthit.chpl.auth.permission.GrantedPermission;
@@ -37,9 +32,6 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2(topic = "measureFixJobLogger")
 public class MeasureFixJob implements Job {
-
-    @Autowired
-    private JpaTransactionManager txManager;
 
     @Autowired
     private CertifiedProductDAO certifiedProductDAO;
@@ -111,38 +103,26 @@ public class MeasureFixJob implements Job {
     }
 
     private Boolean resaveListing(CertifiedProductSearchDetails listing) {
-        // We need to manually create a transaction in this case because of how AOP works. When a method is
-        // annotated with @Transactional, the transaction wrapper is only added if the object's proxy is called.
-        // The object's proxy is not called when the method is called from within this class. The object's proxy
-        // is called when the method is public and is called from a different object.
-        // https://stackoverflow.com/questions/3037006/starting-new-transaction-in-spring-bean
-        TransactionTemplate txTemplate = new TransactionTemplate(txManager);
-        txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-        return txTemplate.execute(new TransactionCallback<Boolean>() {
-            @Override
-            public Boolean doInTransaction(TransactionStatus status) {
-                try {
-                    ListingUpdateRequest request = ListingUpdateRequest.builder()
-                            .listing(listing)
-                            .acknowledgeWarnings(true)
-                            .reason(null)
-                            .build();
+        try {
+            ListingUpdateRequest request = ListingUpdateRequest.builder()
+                    .listing(listing)
+                    .acknowledgeWarnings(true)
+                    .reason(null)
+                    .build();
 
-                    certifiedProductManager.update(request, false);
-                    LOGGER.info(String.format("Successfully updated listing %s (%s).", listing.getChplProductNumber(), listing.getId().toString()));
-                    return true;
-                } catch (ValidationException e) {
-                    LOGGER.info(String.format("Could not update listing %s (%s) for the following reasons:", listing.getChplProductNumber(), listing.getId().toString()));
-                    e.getErrorMessages().stream()
-                        .forEach(message -> LOGGER.info(message));
-                    return false;
-                } catch (Exception e) {
-                    LOGGER.info(String.format("Could not update listing %s (%s) for the following reasons:", listing.getChplProductNumber(), listing.getId().toString()));
-                    LOGGER.catching(e);
-                    return false;
-                }
-            }
-        });
+            certifiedProductManager.update(request, false);
+            LOGGER.info(String.format("Successfully updated listing %s (%s).", listing.getChplProductNumber(), listing.getId().toString()));
+            return true;
+        } catch (ValidationException e) {
+            LOGGER.info(String.format("Could not update listing %s (%s) for the following reasons:", listing.getChplProductNumber(), listing.getId().toString()));
+            e.getErrorMessages().stream()
+                .forEach(message -> LOGGER.info(message));
+            return false;
+        } catch (Exception e) {
+            LOGGER.info(String.format("Could not update listing %s (%s) for the following reasons:", listing.getChplProductNumber(), listing.getId().toString()));
+            LOGGER.catching(e);
+            return false;
+        }
     }
 
     private void setSecurityContext() {
