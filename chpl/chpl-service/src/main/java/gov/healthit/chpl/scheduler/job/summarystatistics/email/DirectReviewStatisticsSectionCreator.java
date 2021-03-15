2ac89d47bec3faccd1b0b30da13b1281a6d7b166
todd.young <tyoung@ainq.com>
2021-03-15 18:06:55 -0400

    feat: add new direct review stats to email
    OCD-3597

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/summarystatistics/email/DirectReviewStatisticsSectionCreator.java
@@ -0,0 +1,35 @@
+package gov.healthit.chpl.scheduler.job.summarystatistics.email;
+
+import gov.healthit.chpl.scheduler.job.summarystatistics.data.EmailStatistics;
+
+public class DirectReviewStatisticsSectionCreator extends StatisticsSectionCreator {
+
+    public String build(EmailStatistics stats) {
+        return buildDirectReviewSection(stats);
+    }
+
+    private String buildDirectReviewSection(EmailStatistics stats) {
+        StringBuilder section = new StringBuilder();
+
+        section.append(buildHeader("Total # of Direct Review Activities", handleNullValue(stats.getTotalDirectReviews())));
+        section.append("<ul>");
+        section.append(buildItem("Open Direct Review Activities", handleNullValue(stats.getOpenDirectReviews())));
+        section.append(buildItem("Closed Direct Review Activities", handleNullValue(stats.getOpenDirectReviews())));
+        section.append(buildItem("Average Duration of Closed Direct Review Activities (in days)", handleNullValue(stats.getAverageDaysOpenDirectReviews())));
+        section.append("</ul>");
+
+        section.append(buildHeader("Total # of Direct Review NCs", handleNullValue(stats.getTotalNonConformities())));
+        section.append("<ul>");
+        section.append(buildItem("Open NCs", handleNullValue(stats.getOpenNonConformities())));
+        section.append(buildItem("Closed NCs", handleNullValue(stats.getClosedNonConformities())));
+        section.append(buildItem("Number of Open CAPs", handleNullValue(stats.getOpenCaps())));
+        section.append(buildItem("Number of Closed CAPs", handleNullValue(stats.getClosedCaps())));
+        section.append("</ul>");
+
+        return section.toString();
+    }
+
+    private String handleNullValue(Long value) {
+        return value != null ? value.toString() : "Not Available";
+    }
+}
