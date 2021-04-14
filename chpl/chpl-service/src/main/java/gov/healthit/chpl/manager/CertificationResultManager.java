92abd83c8d70e26b40126b3b74a8b50de31578e5
Katy Ekey <katy.ekey@gmail.com>
2021-04-14 14:10:19 -0400

    fix!: Remove applicable UCD Processes and Test Tasks if a criterion is edited to have SED = false
    [#OCD-3634]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/CertificationResultManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/CertificationResultManager.java
@@ -236,7 +236,8 @@ private SvapDAO svapDao;
                 for (UcdProcess existingUcd : existingListing.getSed().getUcdProcesses()) {
                     boolean ucdMeetsCriteria = false;
                     for (CertificationCriterion ucdCriteria : existingUcd.getCriteria()) {
-                        if (ucdCriteria.getId().equals(updated.getCriterion().getId())) {
+                        if (ucdCriteria.getId().equals(updated.getCriterion().getId())
+                                && orig.isSed() != null && orig.isSed()) {
                             ucdMeetsCriteria = true;
                         }
                     }
@@ -250,7 +251,8 @@ private SvapDAO svapDao;
                 for (UcdProcess updatedUcd : updatedListing.getSed().getUcdProcesses()) {
                     boolean ucdMeetsCriteria = false;
                     for (CertificationCriterion ucdCriteria : updatedUcd.getCriteria()) {
-                        if (ucdCriteria.getId().equals(updated.getCriterion().getId())) {
+                        if (ucdCriteria.getId().equals(updated.getCriterion().getId())
+                                && updated.isSed() != null && updated.isSed()) {
                             ucdMeetsCriteria = true;
                         }
                     }
@@ -268,7 +270,8 @@ private SvapDAO svapDao;
                 for (TestTask existingTask : existingListing.getSed().getTestTasks()) {
                     boolean taskMeetsCriteria = false;
                     for (CertificationCriterion taskCriteria : existingTask.getCriteria()) {
-                        if (taskCriteria.getId().equals(updated.getCriterion().getId())) {
+                        if (taskCriteria.getId().equals(updated.getCriterion().getId())
+                                && orig.isSed() != null && orig.isSed()) {
                             taskMeetsCriteria = true;
                         }
                     }
@@ -316,7 +319,8 @@ private SvapDAO svapDao;
                 for (TestTask updatedTask : updatedListing.getSed().getTestTasks()) {
                     boolean taskMeetsCriteria = false;
                     for (CertificationCriterion taskCriteria : updatedTask.getCriteria()) {
-                        if (taskCriteria.getId().equals(updated.getCriterion().getId())) {
+                        if (taskCriteria.getId().equals(updated.getCriterion().getId())
+                                && updated.isSed() != null && updated.isSed()) {
                             taskMeetsCriteria = true;
                         }
                     }
