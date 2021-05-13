bf29d854c156932714acd3758527fe921b020f9a
Katy Ekey <katy.ekey@gmail.com>
2021-05-13 10:26:22 -0500

    feat!: Remove background jobs, /data/job_types. Surveillance Upload and Meaningful Use Upload move to quartz scheduled jobs
    [#OCD-3594]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/dto/job/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Objects to translate between domain classes and the entity model for jobs.
- */
-package gov.healthit.chpl.dto.job;
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/job/NoJobTypeException.java
+++
@@ -1,7 +0,0 @@
-package gov.healthit.chpl.job;
-
-public class NoJobTypeException extends Exception {
-
-    private static final long serialVersionUID = 8639525372869251406L;
-
-}
