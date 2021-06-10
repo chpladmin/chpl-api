4b07399510faee697005b1e8fed0a733bfdb206b
todd.young <tyoung@ainq.com>
2021-06-09 23:11:48 -0400

    feat: add header indicating prod vs. non-prod environment
    OCD-3656

---
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/filter/EnvironmentHeaderFilter.java
@@ -0,0 +1,36 @@
+package gov.healthit.chpl.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class EnvironmentHeaderFilter extends OncePerRequestFilter {
+
+    private Environment env;
+
+    @Autowired
+    public EnvironmentHeaderFilter(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String serverEnvironment = env.getProperty("server.environment") != null ? env.getProperty("server.environment") : "";
+        if (serverEnvironment.equalsIgnoreCase("production")) {
+            response.addHeader("ENVIRONMENT", "PRODUCTION");
+        } else {
+            response.addHeader("ENVIRONMENT", "NON-PRODUCTION");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
