9a7b3006469728c0058ccc8ce17998391a61c052
Todd Young <tyoung@ainq.com>
2021-06-21 13:48:37 -0400

    feat!: Generate Surveillance Activity Report as Excel spreadsheet
    OCD-3534

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/surveillance/ActivityReportActionPermission.java
@@ -0,0 +1,22 @@
+package gov.healthit.chpl.permissions.domains.surveillance;
+
+import org.springframework.stereotype.Component;
+
+import gov.healthit.chpl.permissions.domains.ActionPermissions;
+
+@Component("surveillanceActivityReportActionPermission")
+public class ActivityReportActionPermission extends ActionPermissions {
+
+    @Override
+    public boolean hasAccess() {
+       return getResourcePermissions().isUserRoleAdmin()
+               || getResourcePermissions().isUserRoleOnc()
+               || getResourcePermissions().isUserRoleOncStaff();
+    }
+
+    @Override
+    public boolean hasAccess(Object obj) {
+        // Not used
+        return false;
+    }
+}
