17517f08b36beaea0ecbfb6a3bc48d2dd97d0324
todd.young <tyoung@ainq.com>
2021-07-02 13:39:58 -0400

    feat: update the text in the Questionable Activity Report
    OCD-3708

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/questionableactivity/ListingQuestionableActivityProvider.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/questionableactivity/ListingQuestionableActivityProvider.java
@@ -472,7 +472,7 @@ public class ListingQuestionableActivityProvider {
         QuestionableActivityListingDTO activity = null;
         if (StringUtils.isEmpty(origListing.getRwtPlansUrl()) && !StringUtils.isEmpty(newListing.getRwtPlansUrl())) {
             activity = new QuestionableActivityListingDTO();
-            activity.setAfter("Added Real World Testing Plans URL for Listing not Real World Testing Eligible: " + origListing.getRwtPlansUrl());
+            activity.setAfter("Added Plans URL " + newListing.getRwtPlansUrl());
         }
         return activity;
     }
@@ -481,7 +481,7 @@ public class ListingQuestionableActivityProvider {
         QuestionableActivityListingDTO activity = null;
         if (StringUtils.isEmpty(origListing.getRwtResultsUrl()) && !StringUtils.isEmpty(newListing.getRwtResultsUrl())) {
             activity = new QuestionableActivityListingDTO();
-            activity.setAfter("Added Real World Testing Results URL for Listing not Real World Testing Eligible: " + origListing.getRwtResultsUrl());
+            activity.setAfter("Added Results URL " + newListing.getRwtResultsUrl());
         }
         return activity;
     }
