bd534fa40fd1a1b76b7e5ded004b8ca912597218
todd.young <tyoung@ainq.com>
2021-08-03 16:28:52 -0400

    feat: basic update of template using static data
    OCD-3727

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/curesStatistics/email/CuresStatisticsEmailJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/curesStatistics/email/CuresStatisticsEmailJob.java
@@ -1,12 +1,20 @@
 package gov.healthit.chpl.scheduler.job.curesStatistics.email;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.mail.MessagingException;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -84,6 +92,18 @@ public class CuresStatisticsEmailJob  extends QuartzJob {
             LOGGER.error("Error sending email!", ex);
         }
 
+        try {
+            updateTemplate();
+        } catch (Exception e) {
+            LOGGER.catching(e);
+        }
+
+        try {
+            curesStatisticsSpreadhseet.getCuresCriterionChartStatistics();
+        } catch (Exception e) {
+            LOGGER.catching(e);
+        }
+
         LOGGER.info("*****Cures Reporting Email Job is complete.*****");
     }
 
@@ -105,4 +125,199 @@ public class CuresStatisticsEmailJob  extends QuartzJob {
                 .sendEmail();
         LOGGER.info("Completed Sending email to: " + emailAddress);
     }
+
+    private void updateTemplate() throws IOException {
+        File template = new File("C:\\CHPL\\files\\Cures_Update.xlsx");
+        FileInputStream fis = new FileInputStream(template);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+        Sheet sheet = wb.getSheet("Data");
+
+        Integer currRow = 1;
+        Cell currCell = null;
+
+        Row row = sheet.getRow(currRow);
+        currCell = row.getCell(1);
+        currCell.setCellValue(2);
+        currCell = row.getCell(2);
+        currCell.setCellValue(100);
+        currCell = row.getCell(3);
+        currCell.setCellValue(544);
+        currCell = row.getCell(4);
+        currCell.setCellValue(10);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(2);
+        currCell = row.getCell(2);
+        currCell.setCellValue(4);
+        currCell = row.getCell(3);
+        currCell.setCellValue(527);
+        currCell = row.getCell(4);
+        currCell.setCellValue(6);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(0);
+        currCell = row.getCell(2);
+        currCell.setCellValue(12);
+        currCell = row.getCell(3);
+        currCell.setCellValue(433);
+        currCell = row.getCell(4);
+        currCell.setCellValue(12);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(0);
+        currCell = row.getCell(2);
+        currCell.setCellValue(2);
+        currCell = row.getCell(3);
+        currCell.setCellValue(66);
+        currCell = row.getCell(4);
+        currCell.setCellValue(2);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(0);
+        currCell = row.getCell(2);
+        currCell.setCellValue(50);
+        currCell = row.getCell(3);
+        currCell.setCellValue(66);
+        currCell = row.getCell(4);
+        currCell.setCellValue(2);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(1);
+        currCell = row.getCell(2);
+        currCell.setCellValue(2);
+        currCell = row.getCell(3);
+        currCell.setCellValue(523);
+        currCell = row.getCell(4);
+        currCell.setCellValue(3);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(6);
+        currCell = row.getCell(2);
+        currCell.setCellValue(11);
+        currCell = row.getCell(3);
+        currCell.setCellValue(416);
+        currCell = row.getCell(4);
+        currCell.setCellValue(17);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(15);
+        currCell = row.getCell(2);
+        currCell.setCellValue(5);
+        currCell = row.getCell(3);
+        currCell.setCellValue(843);
+        currCell = row.getCell(4);
+        currCell.setCellValue(70);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(0);
+        currCell = row.getCell(2);
+        currCell.setCellValue(12);
+        currCell = row.getCell(3);
+        currCell.setCellValue(852);
+        currCell = row.getCell(4);
+        currCell.setCellValue(12);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(1);
+        currCell = row.getCell(2);
+        currCell.setCellValue(6);
+        currCell = row.getCell(3);
+        currCell.setCellValue(106);
+        currCell = row.getCell(4);
+        currCell.setCellValue(7);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue("-");
+        currCell = row.getCell(2);
+        currCell.setCellValue(269);
+        currCell = row.getCell(3);
+        currCell.setCellValue(645);
+        currCell = row.getCell(4);
+        currCell.setCellValue(269);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue("-");
+        currCell = row.getCell(2);
+        currCell.setCellValue(269);
+        currCell = row.getCell(3);
+        currCell.setCellValue(645);
+        currCell = row.getCell(4);
+        currCell.setCellValue(269);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(2);
+        currCell = row.getCell(2);
+        currCell.setCellValue(6);
+        currCell = row.getCell(3);
+        currCell.setCellValue(498);
+        currCell = row.getCell(4);
+        currCell.setCellValue(8);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(0);
+        currCell = row.getCell(2);
+        currCell.setCellValue(1);
+        currCell = row.getCell(3);
+        currCell.setCellValue(81);
+        currCell = row.getCell(4);
+        currCell.setCellValue(3);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(2);
+        currCell = row.getCell(2);
+        currCell.setCellValue(10);
+        currCell = row.getCell(3);
+        currCell.setCellValue(636);
+        currCell = row.getCell(4);
+        currCell.setCellValue(10);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(1);
+        currCell = row.getCell(2);
+        currCell.setCellValue(9);
+        currCell = row.getCell(3);
+        currCell.setCellValue(525);
+        currCell = row.getCell(4);
+        currCell.setCellValue(10);
+
+        row = sheet.getRow(currRow++);
+        currCell = row.getCell(1);
+        currCell.setCellValue(0);
+        currCell = row.getCell(2);
+        currCell.setCellValue(2);
+        currCell = row.getCell(3);
+        currCell.setCellValue(533);
+        currCell = row.getCell(4);
+        currCell.setCellValue(2);
+
+        XSSFFormulaEvaluator.evaluateAllFormulaCells(wb);
+
+        fis.close();
+
+        Date timestamp = new Date();
+        File outputFile = new File("C:\\CHPL\\files\\Cures_Update_" + timestamp.getTime() + ".xlsx");
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        wb.write(outputStream);
+        wb.close();
+        outputStream.close();
+
+    }
+
+
 }
