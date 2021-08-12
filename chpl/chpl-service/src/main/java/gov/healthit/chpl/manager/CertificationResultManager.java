c48045d1a664de72488c632815fee0a2f0458701
Katy Ekey <katy.ekey@gmail.com>
2021-08-12 13:37:57 -0400

    fix: fix bug wrt deleting svap when criteria is unattested
    [#OCD-3709]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/CertificationResultManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/CertificationResultManager.java
@@ -194,7 +194,7 @@ private SvapDAO svapDao;
             numChanges += updateTestData(updated, orig.getTestDataUsed(), null);
             numChanges += updateTestProcedures(updated, orig.getTestProcedures(), null);
             numChanges += updateTestFunctionality(updatedListing, updated, orig.getTestFunctionality(), null);
-            numChanges += updateSvap(updated, orig.getSvaps(), updated.getSvaps());
+            numChanges += updateSvap(updated, orig.getSvaps(), null);
 
             if (existingListing.getSed() != null && existingListing.getSed().getUcdProcesses() != null
                     && existingListing.getSed().getUcdProcesses().size() > 0) {
