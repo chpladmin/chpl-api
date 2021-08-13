eb5d9fd6163d04ca71e3a399b310e034017a6b55
Andrew Larned <andrew.larned@gmail.com>
2021-08-13 06:53:48 -0400

    refactor: extract Test Procedure validation to single reviewer
    [#OCD-3713]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/TestProcedureReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/TestProcedureReviewer.java
@@ -1,5 +1,6 @@
 package gov.healthit.chpl.validation.listing.reviewer;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -7,18 +8,20 @@ import gov.healthit.chpl.dao.TestProcedureDAO;
 import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertificationResultTestProcedure;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.permissions.ResourcePermissions;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.Util;
 
 @Component("testProcedureReviewer")
-public class TestProcedureReviewer implements Reviewer {
-    private ErrorMessageUtil msgUtil;
+public class TestProcedureReviewer extends PermissionBasedReviewer {
     private TestProcedureDAO testProcedureDao;
 
     @Autowired
-    public TestProcedureReviewer(TestProcedureDAO testProcedureDao, ErrorMessageUtil msgUtil) {
+    public TestProcedureReviewer(TestProcedureDAO testProcedureDao, ErrorMessageUtil msgUtil, ResourcePermissions resourcePermissions) {
+        super(msgUtil, resourcePermissions);
         this.msgUtil = msgUtil;
         this.testProcedureDao = testProcedureDao;
+        this.resourcePermissions = resourcePermissions;
     }
 
     @Override
@@ -31,14 +34,40 @@ public class TestProcedureReviewer implements Reviewer {
 
     private void reviewTestProcedure(CertifiedProductSearchDetails listing, CertificationResult certResult,
             CertificationResultTestProcedure testProcedure) {
+        checkIfTestProcedureIsAllowed(listing, certResult, testProcedure);
+        checkIfTestProcedureHasAName(listing, certResult, testProcedure);
+        checkIfTestProcedureHasAVersion(listing, certResult, testProcedure);
+    }
 
+    private void checkIfTestProcedureIsAllowed(CertifiedProductSearchDetails listing, CertificationResult certResult,
+        CertificationResultTestProcedure testProcedure) {
         boolean isAllowed = testProcedureDao.getByCriterionId(certResult.getCriterion().getId()).stream()
                 .anyMatch(tp -> tp.getId() == testProcedure.getTestProcedure().getId());
             if (!isAllowed) {
-                listing.getErrorMessages().add(
-                        msgUtil.getMessage("listing.criteria.badTestProcedureName",
+                addCriterionErrorOrWarningByPermission(listing, certResult,
+                        "listing.criteria.badTestProcedureName",
                         Util.formatCriteriaNumber(certResult.getCriterion()),
-                        testProcedure.getTestProcedure().getName()));
+                        testProcedure.getTestProcedure().getName());
             }
     }
+
+    private void checkIfTestProcedureHasAName(CertifiedProductSearchDetails listing, CertificationResult certResult,
+            CertificationResultTestProcedure testProcedure) {
+                if (testProcedure.getTestProcedure() == null) {
+                    addCriterionErrorOrWarningByPermission(listing, certResult,
+                            "listing.criteria.missingTestProcedureName",
+                            Util.formatCriteriaNumber(certResult.getCriterion()));
+                }
+        }
+
+    private void checkIfTestProcedureHasAVersion(CertifiedProductSearchDetails listing, CertificationResult certResult,
+            CertificationResultTestProcedure testProcedure) {
+                if (testProcedure.getTestProcedure() != null
+                        && !StringUtils.isEmpty(testProcedure.getTestProcedure().getName())
+                        && StringUtils.isEmpty(testProcedure.getTestProcedureVersion())) {
+                    addCriterionErrorOrWarningByPermission(listing, certResult,
+                            "listing.criteria.missingTestProcedureVersion",
+                            Util.formatCriteriaNumber(certResult.getCriterion()));
+                }
+        }
 }
