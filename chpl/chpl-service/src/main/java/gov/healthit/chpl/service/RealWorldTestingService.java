23c730c9470132c3bbbda4fcb06931d5b52f34d2
todd.young <tyoung@ainq.com>
2021-08-12 21:37:14 -0400

    feat: update RWT service to handle listing status and cert date checks
    OCD-3737

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/service/RealWorldTestingService.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/service/RealWorldTestingService.java
@@ -1,7 +1,12 @@
 package gov.healthit.chpl.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +14,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.domain.CertificationCriterion;
+import gov.healthit.chpl.domain.CertificationStatusEvent;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.dto.listing.pending.PendingCertifiedProductDTO;
+import gov.healthit.chpl.entity.CertificationStatusType;
+import gov.healthit.chpl.util.DateUtil;
 import lombok.extern.log4j.Log4j2;
 
 @Component
@@ -22,7 +30,12 @@ public class RealWorldTestingService {
     @Value("${realWorldTestingCriteriaKeys}")
     private String[] eligibleCriteriaKeys;
 
+    @Value("${rwtEligibilityDayOfYear}")
+    private String rwtEligibilityDayOfYear;
+
     private CertificationCriterionService certificationCriterionService;
+
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
     @Autowired
     public RealWorldTestingService(CertificationCriterionService certificationCriterionService) {
@@ -30,21 +43,29 @@ public class RealWorldTestingService {
     }
 
     public boolean doesListingAttestToEligibleCriteria(CertifiedProductSearchDetails listing) {
-        List<CertificationCriterion> eligibleCriteria = getRwtEligibleCriteria();
-        boolean doesExist = listing.getCertificationResults().stream()
-                .filter(result -> result.isSuccess()
-                        && eligibleCriteria.stream()
-                        .filter(crit -> crit.getId().equals(result.getCriterion().getId()))
-                        .findAny()
-                        .isPresent())
-                .findAny()
-                .isPresent();
-
-        if (doesExist) {
-            return true;
-        } else {
-            LOGGER.info("Listing: " + listing.getId() + " - Does not attest to any eligible criteria");
+        if (!isListingStatusActiveAsOfEligibilityDate(listing)) {
+            LOGGER.info("Listing: " + listing.getId() + " - Not active as of " + dateFormatter.format(getMostRecentPastEligibilityDate()));
             return false;
+        } else if (!isCertificationDateBeforeEligibilityDate(listing)) {
+            LOGGER.info("Listing: " + listing.getId() + " - Certification date is not before Eligility Date");
+            return false;
+        } else {
+            List<CertificationCriterion> eligibleCriteria = getRwtEligibleCriteria();
+            boolean doesExist = listing.getCertificationResults().stream()
+                    .filter(result -> result.isSuccess()
+                            && eligibleCriteria.stream()
+                            .filter(crit -> crit.getId().equals(result.getCriterion().getId()))
+                            .findAny()
+                            .isPresent())
+                    .findAny()
+                    .isPresent();
+
+            if (doesExist) {
+                return true;
+            } else {
+                LOGGER.info("Listing: " + listing.getId() + " - Does not attest to any eligible criteria");
+                return false;
+            }
         }
     }
 
@@ -71,5 +92,46 @@ public class RealWorldTestingService {
         return Arrays.asList(eligibleCriteriaKeys).stream()
                 .map(key -> certificationCriterionService.get(key))
                 .collect(Collectors.toList());
+    }
+
+    private boolean isCertificationDateBeforeEligibilityDate(CertifiedProductSearchDetails listing) {
+        if (Objects.isNull(listing) || Objects.isNull(listing.getCertificationDate())) {
+            LOGGER.info("Listing: " + listing.getId() + " - Certification date does not exist");
+            return false;
+        } else {
+            LocalDate certDate = DateUtil.toLocalDate(listing.getCertificationDate());
+            if (certDate.isBefore(getMostRecentPastEligibilityDate())) {
+                return true;
+            } else {
+                LOGGER.info("Listing: " + listing.getId() + " - Certification date is after eligibility start date");
+                return false;
+            }
+        }
+    }
+
+    private boolean isListingStatusActiveAsOfEligibilityDate(CertifiedProductSearchDetails listing) {
+        CertificationStatusEvent event = listing.getStatusOnDate(convertLocalDateToDateUtcAtMidnight(getMostRecentPastEligibilityDate()));
+        if (Objects.nonNull(event)
+                && event.getStatus().getName().equals(CertificationStatusType.Active.getName())) {
+            return true;
+        } else {
+            LOGGER.info("Listing: " + listing.getId() + " - Not Active");
+            return false;
+        }
+    }
+
+    public LocalDate getMostRecentPastEligibilityDate() {
+        LocalDate eligDateWithCurrentYear = LocalDate.from(dateFormatter.parse(rwtEligibilityDayOfYear + "/" + LocalDate.now().getYear()));
+        if (eligDateWithCurrentYear.isBefore(LocalDate.now())) {
+            return eligDateWithCurrentYear;
+        } else {
+            return eligDateWithCurrentYear.minusYears(1L);
+        }
+    }
+
+    private Date convertLocalDateToDateUtcAtMidnight(LocalDate localDate) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(localDate.getYear(), localDate.getMonthValue() - 1, localDate.getDayOfMonth(), 0, 0, 0);
+        return calendar.getTime();
     }
 }
