34e9035ac05cc7c5a43d9c370ae95c9fc1356fb0
Katy Ekey <katy.ekey@gmail.com>
2021-08-16 14:14:00 -0400

    style: Make class final
    [#OCD-3723]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/util/SwaggerSecurityRequirement.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/util/SwaggerSecurityRequirement.java
@@ -1,6 +1,6 @@
 package gov.healthit.chpl.util;
 
-public class SwaggerSecurityRequirement {
+public final class SwaggerSecurityRequirement {
     public static final String API_KEY = "api-key";
     public static final String BEARER = "Bearer-token";
 
