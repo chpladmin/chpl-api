3d7bfefeee2733501c41967e2ff8a16725a3fb2f
Katy Ekey <katy.ekey@gmail.com>
2021-08-16 12:57:14 -0400

    [#OCD-3723]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/util/SwaggerSecurityRequirement.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/util/SwaggerSecurityRequirement.java
@@ -3,4 +3,7 @@ package gov.healthit.chpl.util;
 public class SwaggerSecurityRequirement {
     public static final String API_KEY = "api-key";
     public static final String BEARER = "Bearer-token";
+
+    private SwaggerSecurityRequirement() {
+    }
 }
