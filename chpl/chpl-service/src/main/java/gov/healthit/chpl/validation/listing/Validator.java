544b233e7dc75c6f2840442bc48b76c17e1e0cf6
todd.young <tyoung@ainq.com>
2021-09-20 14:22:08 -0400

    refactor: remoive debug code
    OCD-3742

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/Validator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/Validator.java
@@ -10,31 +10,14 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public abstract class Validator {
 
-    /**
-     * Concrete classes should provide a list of reviewers.
-     * Each reviewer can check a specific part of a listing for errors.
-     * @return the applicable reviewers
-     */
     public abstract List<Reviewer> getReviewers();
 
-    /**
-     * Concrete classes should provide a list of comparison reviewers, if any.
-     * Each comparison reviewer can compare existing listing data against
-     * proposed updated listing data and generate error/warning messages.
-     * @return the applicable reviewers
-     */
     public abstract List<ComparisonReviewer> getComparisonReviewers();
 
-    /**
-     * Validation simply calls each reviewer. The reviewers add
-     * errors and warnings as appropriate.
-     * @param listing the listing to validate
-     */
     public synchronized void validate(final CertifiedProductSearchDetails listing) {
         for (Reviewer reviewer : getReviewers()) {
             try {
                 if (reviewer != null) {
-                    //LOGGER.info("Running Reviewer: " + reviewer.getClass().getName());
                     reviewer.review(listing);
                 } else {
                     LOGGER.info("Cound not run a NULL reviewer.");
@@ -46,14 +29,6 @@ public abstract class Validator {
         }
     }
 
-    /**
-     * Validates the updated listing.
-     * Does additional validation to check whether certain changes were made
-     * between the existing and updated listings that are only allowed
-     * by certain users.
-     * @param existingListing the listing as it currently exists in the system
-     * @param updatedListing the listing with any updates made by the user
-     */
     public void validate(final CertifiedProductSearchDetails existingListing,
             final CertifiedProductSearchDetails updatedListing) {
         validate(updatedListing);
