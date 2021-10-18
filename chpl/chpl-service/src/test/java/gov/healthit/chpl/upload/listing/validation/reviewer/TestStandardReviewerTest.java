59ba28b3abbe79dfbcbaee36d7a9d40bee3af976
Katy Ekey <katy.ekey@gmail.com>
2021-10-18 07:03:49 -0400

    feat: Make the presence of any test standards be an error
    [#OCD-3700]

--- chpl/chpl-service/src/test/java/gov/healthit/chpl/upload/listing/validation/reviewer/TestStandardReviewerTest.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/upload/listing/validation/reviewer/TestStandardReviewerTest.java
@@ -18,39 +18,25 @@ import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertificationResultTestStandard;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.exception.EntityRetrievalException;
-import gov.healthit.chpl.util.CertificationResultRules;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 
 public class TestStandardReviewerTest {
-    private static final String TEST_STANDARDS_NOT_APPLICABLE = "Test Standards are not applicable for the criterion %s.";
-    private static final String TEST_STANDARD_NOT_FOUND = "Criteria %s contains a test standard '%s' which does not exist for edition %s.";
-    private static final String TEST_STANDARD_NAME_MISSING = "There was no test standard name found for certification criteria %s.";
+    private static final String TEST_STANDARDS_NOT_ALLOWED = "Criteria %s contains a test standard '%s' which is not allowed and should be removed.";
 
-    private CertificationResultRules certResultRules;
     private ErrorMessageUtil msgUtil;
     private TestStandardReviewer reviewer;
 
     @Before
     public void before() throws EntityRetrievalException {
         msgUtil = Mockito.mock(ErrorMessageUtil.class);
-        certResultRules = Mockito.mock(CertificationResultRules.class);
-        Mockito.when(msgUtil.getMessage(ArgumentMatchers.eq("listing.criteria.testStandardsNotApplicable"),
-                ArgumentMatchers.anyString()))
-            .thenAnswer(i -> String.format(TEST_STANDARDS_NOT_APPLICABLE, i.getArgument(1), ""));
-        Mockito.when(msgUtil.getMessage(ArgumentMatchers.eq("listing.criteria.testStandardNotFound"),
-                ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
-            .thenAnswer(i -> String.format(TEST_STANDARD_NOT_FOUND, i.getArgument(1), i.getArgument(2), i.getArgument(3)));
-        Mockito.when(msgUtil.getMessage(ArgumentMatchers.eq("listing.criteria.missingTestStandardName"),
-                ArgumentMatchers.anyString()))
-            .thenAnswer(i -> String.format(TEST_STANDARD_NAME_MISSING, i.getArgument(1), ""));
-        reviewer = new TestStandardReviewer(certResultRules, msgUtil);
+        Mockito.when(msgUtil.getMessage(ArgumentMatchers.eq("listing.criteria.testStandardNotAllowed"),
+                ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
+            .thenAnswer(i -> String.format(TEST_STANDARDS_NOT_ALLOWED, i.getArgument(1), i.getArgument(2)));
+        reviewer = new TestStandardReviewer(msgUtil);
     }
 
     @Test
     public void review_nullTestStandards_noError() {
-        Mockito.when(certResultRules.hasCertOption(ArgumentMatchers.anyString(), ArgumentMatchers.eq(CertificationResultRules.STANDARDS_TESTED)))
-            .thenReturn(true);
-
         CertifiedProductSearchDetails listing = CertifiedProductSearchDetails.builder()
                 .certificationResult(CertificationResult.builder()
                         .criterion(CertificationCriterion.builder()
@@ -69,9 +55,6 @@ public class TestStandardReviewerTest {
 
     @Test
     public void review_emptyTestStandards_noError() {
-        Mockito.when(certResultRules.hasCertOption(ArgumentMatchers.anyString(), ArgumentMatchers.eq(CertificationResultRules.STANDARDS_TESTED)))
-            .thenReturn(true);
-
         CertifiedProductSearchDetails listing = CertifiedProductSearchDetails.builder()
                 .certificationResult(CertificationResult.builder()
                         .criterion(CertificationCriterion.builder()
@@ -89,9 +72,7 @@ public class TestStandardReviewerTest {
     }
 
     @Test
-    public void review_testStandardsNotApplicableToCriteria_hasError() {
-        Mockito.when(certResultRules.hasCertOption(ArgumentMatchers.anyString(), ArgumentMatchers.eq(CertificationResultRules.STANDARDS_TESTED)))
-            .thenReturn(false);
+    public void review_testStandardsPresent_hasError() {
         List<CertificationResultTestStandard> testStandards = new ArrayList<CertificationResultTestStandard>();
         testStandards.add(CertificationResultTestStandard.builder()
                 .testStandardName("test std")
@@ -113,57 +94,16 @@ public class TestStandardReviewerTest {
         assertEquals(0, listing.getWarningMessages().size());
         assertEquals(1, listing.getErrorMessages().size());
         assertTrue(listing.getErrorMessages().contains(
-                String.format(TEST_STANDARDS_NOT_APPLICABLE, "170.315 (a)(1)")));
+                String.format(TEST_STANDARDS_NOT_ALLOWED, "170.315 (a)(1)", "test std")));
     }
 
     @Test
-    public void review_testStandardsWithoutId_hasError() {
-        Mockito.when(certResultRules.hasCertOption(ArgumentMatchers.anyString(), ArgumentMatchers.eq(CertificationResultRules.STANDARDS_TESTED)))
-            .thenReturn(true);
-
+    public void review_testStandardsPresentWithoutId_hasError() {
         List<CertificationResultTestStandard> testStandards = new ArrayList<CertificationResultTestStandard>();
         testStandards.add(CertificationResultTestStandard.builder()
-                .testStandardId(1L)
                 .testStandardName("test std")
-                .build());
-        testStandards.add(CertificationResultTestStandard.builder()
-                .testStandardName("bad name")
-                .build());
-
-        CertifiedProductSearchDetails listing = CertifiedProductSearchDetails.builder()
-                .certificationEdition(create2015EditionMap())
-                .certificationResult(CertificationResult.builder()
-                        .criterion(CertificationCriterion.builder()
-                                .id(1L)
-                                .number("170.315 (a)(1)")
-                                .build())
-                        .success(true)
-                        .testStandards(testStandards)
-                        .build())
-                .build();
-        reviewer.review(listing);
-
-        assertEquals(0, listing.getWarningMessages().size());
-        assertEquals(1, listing.getErrorMessages().size());
-        assertTrue(listing.getErrorMessages().contains(
-                String.format(TEST_STANDARD_NOT_FOUND, "170.315 (a)(1)", "bad name", "2015")));
-    }
-
-    @Test
-    public void review_testStandardWithoutNameWithoutId_hasError() {
-        Mockito.when(certResultRules.hasCertOption(ArgumentMatchers.anyString(), ArgumentMatchers.eq(CertificationResultRules.STANDARDS_TESTED)))
-            .thenReturn(true);
-
-        List<CertificationResultTestStandard> testStandards = new ArrayList<CertificationResultTestStandard>();
-        testStandards.add(CertificationResultTestStandard.builder()
-                .testStandardId(1L)
-                .testStandardName("test std")
-                .build());
-        testStandards.add(CertificationResultTestStandard.builder()
                 .testStandardId(null)
-                .testStandardName("")
                 .build());
-
         CertifiedProductSearchDetails listing = CertifiedProductSearchDetails.builder()
                 .certificationEdition(create2015EditionMap())
                 .certificationResult(CertificationResult.builder()
@@ -180,24 +120,16 @@ public class TestStandardReviewerTest {
         assertEquals(0, listing.getWarningMessages().size());
         assertEquals(1, listing.getErrorMessages().size());
         assertTrue(listing.getErrorMessages().contains(
-                String.format(TEST_STANDARD_NAME_MISSING, "170.315 (a)(1)", "")));
+                String.format(TEST_STANDARDS_NOT_ALLOWED, "170.315 (a)(1)", "test std")));
     }
 
     @Test
-    public void review_testStandardsWithoutNameWithId_hasError() {
-        Mockito.when(certResultRules.hasCertOption(ArgumentMatchers.anyString(), ArgumentMatchers.eq(CertificationResultRules.STANDARDS_TESTED)))
-        .thenReturn(true);
-
+    public void review_testStandardsPresentWithoutName_hasError() {
         List<CertificationResultTestStandard> testStandards = new ArrayList<CertificationResultTestStandard>();
         testStandards.add(CertificationResultTestStandard.builder()
-                .testStandardId(1L)
-                .testStandardName("test std")
-                .build());
-        testStandards.add(CertificationResultTestStandard.builder()
-                .testStandardId(2L)
                 .testStandardName("")
+                .testStandardId(1L)
                 .build());
-
         CertifiedProductSearchDetails listing = CertifiedProductSearchDetails.builder()
                 .certificationEdition(create2015EditionMap())
                 .certificationResult(CertificationResult.builder()
@@ -214,39 +146,7 @@ public class TestStandardReviewerTest {
         assertEquals(0, listing.getWarningMessages().size());
         assertEquals(1, listing.getErrorMessages().size());
         assertTrue(listing.getErrorMessages().contains(
-                String.format(TEST_STANDARD_NAME_MISSING, "170.315 (a)(1)", "")));
-    }
-
-    @Test
-    public void review_validTestStandards_noError() {
-        Mockito.when(certResultRules.hasCertOption(ArgumentMatchers.anyString(), ArgumentMatchers.eq(CertificationResultRules.STANDARDS_TESTED)))
-            .thenReturn(true);
-
-        List<CertificationResultTestStandard> testStandards = new ArrayList<CertificationResultTestStandard>();
-        testStandards.add(CertificationResultTestStandard.builder()
-                .testStandardId(1L)
-                .testStandardName("test std")
-                .build());
-        testStandards.add(CertificationResultTestStandard.builder()
-                .testStandardId(2L)
-                .testStandardName("test std 2")
-                .build());
-
-        CertifiedProductSearchDetails listing = CertifiedProductSearchDetails.builder()
-                    .certificationEdition(create2015EditionMap())
-                .certificationResult(CertificationResult.builder()
-                        .criterion(CertificationCriterion.builder()
-                                .id(1L)
-                                .number("170.315 (a)(1)")
-                                .build())
-                        .success(true)
-                        .testStandards(testStandards)
-                        .build())
-                .build();
-        reviewer.review(listing);
-
-        assertEquals(0, listing.getWarningMessages().size());
-        assertEquals(0, listing.getErrorMessages().size());
+                String.format(TEST_STANDARDS_NOT_ALLOWED, "170.315 (a)(1)", "")));
     }
 
     private Map<String, Object> create2015EditionMap() {
