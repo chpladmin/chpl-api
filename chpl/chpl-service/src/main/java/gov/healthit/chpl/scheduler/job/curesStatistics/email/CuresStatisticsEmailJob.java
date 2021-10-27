4ff67ad6fe6b991b83da654a6278a42ca02d5e12
Todd Young <tyoung@ainq.com>
2021-10-27 10:37:47 -0400

    feat!: Add Cures Reporting Phase 1 Charts 4-6
    OCD-3693

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/curesStatistics/email/CuresStatisticsEmailJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/curesStatistics/email/CuresStatisticsEmailJob.java
@@ -12,9 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import gov.healthit.chpl.email.ChplHtmlEmailBuilder;
 import gov.healthit.chpl.email.EmailBuilder;
 import gov.healthit.chpl.exception.EmailNotSentException;
 import gov.healthit.chpl.scheduler.job.QuartzJob;
+import gov.healthit.chpl.scheduler.job.curesStatistics.email.spreadsheet.CuresStatisticsChartSpreadsheet;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2(topic = "curesStatisticsEmailJobLogger")
@@ -42,6 +44,9 @@ public class CuresStatisticsEmailJob  extends QuartzJob {
 
     @Autowired
     private CuresStatisticsChartData curesStatisticsChartData;
+
+    @Autowired
+    private ChplHtmlEmailBuilder chplHtmlEmailBuilder;
 
     @Autowired
     private Environment env;
@@ -86,9 +91,7 @@ public class CuresStatisticsEmailJob  extends QuartzJob {
 
         try {
             LocalDate reportDate = curesStatisticsChartData.getReportDate();
-            attachments.add(
-                curesStatisticsChartSpreadsheet.generateSpreadsheet(
-                        curesStatisticsChartData.getCuresCriterionChartStatistics(reportDate), reportDate));
+            attachments.add(curesStatisticsChartSpreadsheet.generateSpreadsheet(reportDate));
         } catch (IOException ex) {
             LOGGER.error("Error creating charts spreadhseet", ex);
         }
@@ -102,23 +105,24 @@ public class CuresStatisticsEmailJob  extends QuartzJob {
         LOGGER.info("*****Cures Reporting Email Job is complete.*****");
     }
 
-    private String createEmailBody() {
-        String emailBody = "<h2>Cures Upgrade Statistics</h4><br/>";
-        emailBody += listingCuresStatusStatisticsHtmlCreator.createEmailBody();
-        emailBody += privacyAndSecurityListingStatisticsHtmlCreator.createEmailBody();
-        return emailBody;
-    }
-
     private void sendEmail(JobExecutionContext context, List<File> attachments) throws EmailNotSentException {
         String emailAddress = context.getMergedJobDataMap().getString(JOB_DATA_KEY_EMAIL);
         LOGGER.info("Sending email to: " + emailAddress);
         EmailBuilder emailBuilder = new EmailBuilder(env);
         emailBuilder.recipient(emailAddress)
                 .subject(env.getProperty("curesStatisticsReport.subject"))
-                .htmlMessage(createEmailBody())
+                .htmlMessage(createHtmlMessage())
                 .fileAttachments(attachments)
                 .sendEmail();
         LOGGER.info("Completed Sending email to: " + emailAddress);
     }
 
+    private String createHtmlMessage() {
+        return chplHtmlEmailBuilder.initialize()
+                .heading("Cures Upgrade Statistics")
+                .paragraph(listingCuresStatusStatisticsHtmlCreator.getSectionHeader(), listingCuresStatusStatisticsHtmlCreator.getSection())
+                .paragraph(privacyAndSecurityListingStatisticsHtmlCreator.getSectionHeader(), privacyAndSecurityListingStatisticsHtmlCreator.getSection())
+                .footer(true)
+                .build();
+    }
 }
