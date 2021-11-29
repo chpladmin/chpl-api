0bef59173fe41941250b81bc11534b68c82c143d
Andrew Larned <andrew.larned@gmail.com>
2021-11-29 11:56:06 -0500

    feat-flag!: Limit Demographic Change Requests by flag
    [#OCD-3807]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/FeatureList.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/FeatureList.java
@@ -6,6 +6,7 @@ public final class FeatureList {
 
     public static final String CHANGE_REQUEST = "change-request";
     public static final String CONFORMANCE_METHOD = "conformance-method";
+    public static final String DEMOGRAPHIC_CHANGE_REQUEST = "demographic-change-request";
     public static final String ENHANCED_UPLOAD = "enhanced-upload";
     public static final String OPTIONAL_STANDARDS = "optional-standards";
     public static final String OPTIONAL_STANDARDS_ERROR = "optional-standards-error";
