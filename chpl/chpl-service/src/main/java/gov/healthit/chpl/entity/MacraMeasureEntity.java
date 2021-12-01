6b5d4e028443906d45de5a15e8dd8165c637d213
todd.young <tyoung@ainq.com>
2021-12-01 10:55:17 -0500

    feat: add Builder capability to MeasureCriterionMapEntity
    OCD-3799

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/entity/MacraMeasureEntity.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/entity/MacraMeasureEntity.java
@@ -12,10 +12,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
+@Builder
 @Table(name = "macra_criteria_map")
 public class MacraMeasureEntity {
 
