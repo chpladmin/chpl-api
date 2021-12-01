a441ad8649ead6e0fd52390b7b7030338b520614
todd.young <tyoung@ainq.com>
2021-12-01 14:28:52 -0500

    feat: add ability to retrieve MeasureDomainEntity
    OCD-3799

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/listing/measure/MeasureDomainDAO.java
@@ -0,0 +1,41 @@
+package gov.healthit.chpl.listing.measure;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import gov.healthit.chpl.dao.impl.BaseDAOImpl;
+import gov.healthit.chpl.domain.MeasureDomain;
+import gov.healthit.chpl.exception.EntityRetrievalException;
+
+@Repository
+public class MeasureDomainDAO extends BaseDAOImpl {
+    public MeasureDomain findByDomain(String domain) throws EntityRetrievalException {
+        MeasureDomainEntity entity = getByDomain(domain);
+        if (entity != null) {
+            return entity.convert();
+        } else {
+            return null;
+        }
+    }
+
+    private MeasureDomainEntity getByDomain(String domain) throws EntityRetrievalException {
+        Query query = entityManager.createQuery(
+                "SELECT md "
+                + "From MeasureDomainEntity md"
+                + "WHERE md.deleted = false "
+                + "AND dm.domain = :domain ",
+                MeasureEntity.class);
+        query.setParameter("domain", domain);
+        List<MeasureDomainEntity> entities = query.getResultList();
+
+        if (entities != null && entities.size() > 0) {
+            return entities.get(0);
+        } else {
+            throw new EntityRetrievalException(String.format("Could not locate measure domain: %s", domain));
+        }
+
+    }
+}
