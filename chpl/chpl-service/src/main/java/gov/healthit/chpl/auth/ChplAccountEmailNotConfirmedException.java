c3e6931c4472af016a7e7e9509305f8736f84443
todd.young <tyoung@ainq.com>
2021-12-15 22:38:27 -0500

    feat: update exception to require the user's email address
    OCD-3808

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/auth/ChplAccountEmailNotConfirmedException.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/auth/ChplAccountEmailNotConfirmedException.java
@@ -1,9 +1,15 @@
 package gov.healthit.chpl.auth;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class ChplAccountEmailNotConfirmedException extends Exception {
     private static final long serialVersionUID = 8658136990867988527L;
+    private String emailAddress;
 
-    public ChplAccountEmailNotConfirmedException(String msg) {
+    public ChplAccountEmailNotConfirmedException(String msg, String email) {
         super(msg);
     }
 
