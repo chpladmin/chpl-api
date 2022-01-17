d0eb750c68ce20b237862c418aeca746c227ce8a
Katy Ekey <katy.ekey@gmail.com>
2022-01-17 14:17:04 -0500

    fix: Remove Component annotation from classes that use SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext
    [#OCD-3759-take2]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/ChplFF4JProvider.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/ChplFF4JProvider.java
@@ -3,19 +3,22 @@ package gov.healthit.chpl;
 import org.ff4j.FF4j;
 import org.ff4j.web.FF4jProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-@Component
 public class ChplFF4JProvider implements FF4jProvider {
 
     @Autowired
     private FF4j ff4j;
 
+    public ChplFF4JProvider() {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
     @Override
     public FF4j getFF4j() {
-        if (ff4j == null) {
-            ff4j = SpringContext.getBean(FF4j.class);
-        }
+//        if (ff4j == null) {
+//            ff4j = SpringContext.getBean(FF4j.class);
+//        }
         return ff4j;
     }
 }
