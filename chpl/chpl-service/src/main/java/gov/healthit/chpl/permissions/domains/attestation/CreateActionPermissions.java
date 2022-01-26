64bbd3b48c7ab56fd9622092f81d1abf629e6440
todd.young <tyoung@ainq.com>
2022-01-25 21:43:21 -0500

    fix: add unsaved changes to previous commit
    OCD-3717

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/attestation/CreateActionPermissions.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/attestation/CreateActionPermissions.java
@@ -1,5 +1,28 @@
 package gov.healthit.chpl.permissions.domains.attestation;
 
-public class CreateActionPermissions {
+import org.springframework.stereotype.Component;
+
+import gov.healthit.chpl.attestation.domain.DeveloperAttestationSubmission;
+import gov.healthit.chpl.permissions.domains.ActionPermissions;
+
+@Component("attestationCreateActionPermissions")
+public class CreateActionPermissions extends ActionPermissions {
+
+    @Override
+    public boolean hasAccess() {
+        return false;
+    }
+
+    @Override
+    public boolean hasAccess(Object obj) {
+        if (!(obj instanceof DeveloperAttestationSubmission)) {
+            return false;
+        } else if (getResourcePermissions().isUserRoleAcbAdmin()) {
+            DeveloperAttestationSubmission developerAttestationSubmission = (DeveloperAttestationSubmission) obj;
+            return isDeveloperValidForCurrentUser(developerAttestationSubmission.getDeveloper().getDeveloperId());
+        } else {
+            return false;
+        }
+    }
 
 }
