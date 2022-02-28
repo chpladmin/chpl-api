38aad770c64af02717f5c15d9e760ff6105d5480
todd.young <tyoung@ainq.com>
2022-02-28 09:10:17 -0500

    feat: send a "not implemented" email if the attestations flag is off
    OCD-3801

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/developer/attestation/DeveloperAttestationReportJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/developer/attestation/DeveloperAttestationReportJob.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.ff4j.FF4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -18,9 +19,11 @@ import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import gov.healthit.chpl.FeatureList;
 import gov.healthit.chpl.dao.CertificationBodyDAO;
 import gov.healthit.chpl.email.ChplEmailFactory;
 import gov.healthit.chpl.email.ChplHtmlEmailBuilder;
+import gov.healthit.chpl.exception.EmailNotSentException;
 import gov.healthit.chpl.exception.EntityRetrievalException;
 import gov.healthit.chpl.manager.SchedulerManager;
 import lombok.extern.log4j.Log4j2;
@@ -39,6 +42,9 @@ public class DeveloperAttestationReportJob implements Job {
 
     @Autowired
     private CertificationBodyDAO certificationBodyDAO;
+
+    @Autowired
+    private FF4j ff4j;
 
     @Autowired
     private ChplHtmlEmailBuilder chplHtmlEmailBuilder;
@@ -60,6 +66,7 @@ public class DeveloperAttestationReportJob implements Job {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 
         LOGGER.info("********* Starting Developer Attestation Report job. *********");
+
         // We need to manually create a transaction in this case because of how AOP works. When a method is
         // annotated with @Transactional, the transaction wrapper is only added if the object's proxy is called.
         // The object's proxy is not called when the method is called from within this class. The object's proxy
@@ -72,29 +79,44 @@ public class DeveloperAttestationReportJob implements Job {
             @Override
             protected void doInTransactionWithoutResult(TransactionStatus status) {
                 try {
-                    List<DeveloperAttestationReport> reportRows = developerAttestationReportDataCollection.collect(getAcbIds(context));
+                    if (!ff4j.check(FeatureList.ATTESTATIONS)) {
+                        LOGGER.info("Sending report not implemented email to: {}", context.getMergedJobDataMap().getString("email"));
+                        sendNotImplementedEmail(context);
 
-                    File csv = developerAttestationReportCsvWriter.generateFile(reportRows);
-
-                    LOGGER.info("Filename: {} | Size: {}", csv.getName(), csv.length());
-
-                    chplEmailFactory.emailBuilder()
-                            .recipient(context.getMergedJobDataMap().getString("email"))
-                            .subject(emailSubject)
-                            .fileAttachments(Arrays.asList(csv))
-                            .htmlMessage(chplHtmlEmailBuilder.initialize()
-                                    .heading(emailSubject)
-                                    .paragraph(String.format(emailBody), getAcbNamesAsBrSeparatedList(context))
-                                    .footer(true)
-                                    .build())
-                            .sendEmail();
-
+                    } else {
+                        List<DeveloperAttestationReport> reportRows = developerAttestationReportDataCollection.collect(getAcbIds(context));
+                        File csv = developerAttestationReportCsvWriter.generateFile(reportRows);
+                        chplEmailFactory.emailBuilder()
+                                .recipient(context.getMergedJobDataMap().getString("email"))
+                                .subject(emailSubject)
+                                .fileAttachments(Arrays.asList(csv))
+                                .htmlMessage(chplHtmlEmailBuilder.initialize()
+                                        .heading(emailSubject)
+                                        .paragraph(String.format(emailBody), getAcbNamesAsBrSeparatedList(context))
+                                        .footer(true)
+                                        .build())
+                                .sendEmail();
+                    }
                 } catch (Exception e) {
                     LOGGER.catching(e);
                 }
             }
         });
         LOGGER.info("********* Completed Developer Attestation Report job. *********");
+    }
+
+    private void sendNotImplementedEmail(JobExecutionContext context) throws EmailNotSentException {
+        chplEmailFactory.emailBuilder()
+                .recipient(context.getMergedJobDataMap().getString("email"))
+                .subject(emailSubject)
+                .htmlMessage(chplHtmlEmailBuilder.initialize()
+                        .heading(emailSubject)
+                        .paragraph("", "This report is not yet implemented.")
+                        .footer(true)
+                        .build())
+                .sendEmail();
+
+
     }
 
     private List<Long> getAcbIds(JobExecutionContext context) {
