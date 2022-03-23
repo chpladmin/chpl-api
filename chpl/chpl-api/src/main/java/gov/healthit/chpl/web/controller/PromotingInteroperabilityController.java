49a38e6682e25f172d054c2c8e354c9b4e716e08
Katy Ekey <katy.ekey@gmail.com>
2022-03-23 13:46:46 -0400

    feat: Remove /meaningful_use/upload
    [#OCD-3875]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/PromotingInteroperabilityController.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/PromotingInteroperabilityController.java
@@ -52,23 +52,4 @@ public class PromotingInteroperabilityController {
             SchedulerException, IOException, MaxUploadSizeExceededException {
         return piuManager.processUploadAsJob(file, accurateAsOfDate);
     }
-
-    @Deprecated
-    @Operation(summary = "DEPRECATED. Use /promoting-interoperability/upload instead."
-            + "Upload a file to update the number of meaningful use users for each CHPL Product Number",
-            description = "Accepts a CSV file with chpl_product_number and num_meaningful_use_users to update the number of meaningful use users for each CHPL Product Number."
-                    + " The user uploading the file must have ROLE_ADMIN, ROLE_ONC.",
-            deprecated = true,
-            security = {
-                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY),
-                    @SecurityRequirement(name = SwaggerSecurityRequirement.BEARER)
-            })
-    @RequestMapping(value = "/meaningful_use/upload", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
-    public ChplOneTimeTrigger uploadMeaningfulUseUsers(
-            @RequestParam("file") MultipartFile file,
-            @RequestParam("accurate_as_of") Long accurateAsOfDate)
-            throws EntityRetrievalException, EntityCreationException, ValidationException,
-            SchedulerException, IOException, MaxUploadSizeExceededException {
-        return piuManager.processUploadAsJob(file, accurateAsOfDate);
-    }
 }
