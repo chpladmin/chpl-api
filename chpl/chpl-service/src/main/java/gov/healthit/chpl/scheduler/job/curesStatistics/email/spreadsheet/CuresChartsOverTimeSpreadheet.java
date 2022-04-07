ac7d616b177f0f9f5ac93259489c2ff50a2f96ac
todd.young <tyoung@ainq.com>
2022-04-07 09:24:10 -0400

    feat: populate sheets for each uscdi criteria
    OCD-3721

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/curesStatistics/email/spreadsheet/CuresChartsOverTimeSpreadheet.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/curesStatistics/email/spreadsheet/CuresChartsOverTimeSpreadheet.java
@@ -42,6 +42,25 @@ public class CuresChartsOverTimeSpreadheet {
         curesChartsOverTimeSheet.populate(workbook.getSheet("(b)(1)"),
                 new CertificationCriterionDTO(certificationCriteriaService.get(CertificationCriterionService.Criteria2015.B_1_CURES)));
 
+        curesChartsOverTimeSheet.populate(workbook.getSheet("(b)(2)"),
+                new CertificationCriterionDTO(certificationCriteriaService.get(CertificationCriterionService.Criteria2015.B_2_CURES)));
+
+        curesChartsOverTimeSheet.populate(workbook.getSheet("(e)(1)"),
+                new CertificationCriterionDTO(certificationCriteriaService.get(CertificationCriterionService.Criteria2015.E_1_CURES)));
+
+        curesChartsOverTimeSheet.populate(workbook.getSheet("(f)(5)"),
+                new CertificationCriterionDTO(certificationCriteriaService.get(CertificationCriterionService.Criteria2015.F_5_CURES)));
+
+        curesChartsOverTimeSheet.populate(workbook.getSheet("(g)(6)"),
+                new CertificationCriterionDTO(certificationCriteriaService.get(CertificationCriterionService.Criteria2015.G_6_CURES)));
+
+        curesChartsOverTimeSheet.populate(workbook.getSheet("(g)(9)"),
+                new CertificationCriterionDTO(certificationCriteriaService.get(CertificationCriterionService.Criteria2015.G_9_CURES)));
+
+        curesChartsOverTimeSheet.populate(workbook.getSheet("(g)(10)"),
+                new CertificationCriterionDTO(certificationCriteriaService.get(CertificationCriterionService.Criteria2015.G_10)));
+
+
         XSSFFormulaEvaluator.evaluateAllFormulaCells(workbook);
         return writeFileToDisk(workbook, newFile);
     }
