217882cb5b0de215288385d123df7b5e850e4fd6
Todd Young <tyoung@ainq.com>
2022-05-12 13:51:39 -0400

    feat!: Add new job to convert Test Procedures to Conformance Methods
    OCD-3752

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/CertifiedProduct2015Gatherer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/CertifiedProduct2015Gatherer.java
@@ -25,24 +25,39 @@ public class CertifiedProduct2015Gatherer {
 
     public List<CertifiedProductSearchDetails> getAll2015CertifiedProducts(Logger logger, Integer threadPoolSize) throws InterruptedException, ExecutionException {
         ForkJoinPool pool = new ForkJoinPool(threadPoolSize);
+        return pool.submit(() -> getAll2015CertifiedProducts(logger, true)).get();
+    }
+
+    public List<CertifiedProductSearchDetails> getAll2015CertifiedProducts(Logger logger, Integer threadPoolSize, Boolean useListingCache) throws InterruptedException, ExecutionException {
+        ForkJoinPool pool = new ForkJoinPool(threadPoolSize);
         return pool.submit(() -> getAll2015CertifiedProducts(logger)).get();
     }
 
     public List<CertifiedProductSearchDetails> getAll2015CertifiedProducts(Logger logger) {
+        return getAll2015CertifiedProducts(logger, true);
+    }
+
+    public List<CertifiedProductSearchDetails> getAll2015CertifiedProducts(Logger logger, Boolean useListingCache) {
         logger.info("Retrieving all 2015 listings");
         List<CertifiedProductDetailsDTO> listings = certifiedProductDAO.findByEdition(
                 CertificationEditionConcept.CERTIFICATION_EDITION_2015.getYear());
         logger.info("Completed retreiving all 2015 listings");
 
         return listings.parallelStream()
-                .map(dto -> getCertifiedProductSearchDetails(dto.getId(), logger))
+                .map(dto -> getCertifiedProductSearchDetails(dto.getId(), logger, useListingCache))
                 .collect(Collectors.toList());
     }
 
-    private CertifiedProductSearchDetails getCertifiedProductSearchDetails(Long certifiedProductId, Logger logger) {
+    private CertifiedProductSearchDetails getCertifiedProductSearchDetails(Long certifiedProductId, Logger logger, Boolean useListingCache) {
         try {
             long start = (new Date()).getTime();
-            CertifiedProductSearchDetails listing = certifiedProductDetailsManager.getCertifiedProductDetailsUsingCache(certifiedProductId);
+            CertifiedProductSearchDetails listing = null;
+            if (useListingCache) {
+                listing = certifiedProductDetailsManager.getCertifiedProductDetailsUsingCache(certifiedProductId);
+            } else {
+                listing = certifiedProductDetailsManager.getCertifiedProductDetails(certifiedProductId);
+            }
+
             logger.info("Completed details for listing(" + ((new Date()).getTime() - start) + "ms): " + certifiedProductId);
             return listing;
         } catch (EntityRetrievalException e) {
