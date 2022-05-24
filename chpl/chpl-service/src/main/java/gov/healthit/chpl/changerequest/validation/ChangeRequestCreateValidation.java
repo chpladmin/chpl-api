ebd901c9792d1a5d9ca35b75091258ebc41291be
todd.young <tyoung@ainq.com>
2022-05-24 12:02:14 -0400

    fix: update message key
    OCD-3894

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/changerequest/validation/ChangeRequestCreateValidation.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/changerequest/validation/ChangeRequestCreateValidation.java
@@ -1,7 +1,5 @@
 package gov.healthit.chpl.changerequest.validation;
 
-import org.apache.commons.lang3.StringUtils;
-
 import gov.healthit.chpl.changerequest.domain.ChangeRequestDeveloperDemographics;
 import gov.healthit.chpl.manager.rules.ValidationRule;
 
@@ -12,31 +10,27 @@ public class ChangeRequestCreateValidation extends ValidationRule<ChangeRequestV
         if (context.getNewChangeRequest().getChangeRequestType().getId().equals(context.getChangeRequestTypeIds().getDeveloperDemographicsChangeRequestTypeId())) {
             ChangeRequestDeveloperDemographics details = (ChangeRequestDeveloperDemographics) context.getNewChangeRequest().getDetails();
             if (context.getNewChangeRequest().getDetails() == null) {
-                getMessages().add(getErrorMessage("changeRequest.details.invalid"));
+                getMessages().add(getErrorMessage("changeRequest.demographics.invalid"));
                 return false;
             } else {
                 boolean areDetailsValid = true;
                 if (!isChangeRequestSelfDevloperValid(details)) {
-                    getMessages().add(getErrorMessage("changeRequest.details.selfDeveloper.missing"));
+                    getMessages().add(getErrorMessage("changeRequest.demographics.selfDeveloper.missing"));
                     areDetailsValid = false;
                 } else if (!isChangeRequestAddressValid(details)) {
-                    getMessages().add(getErrorMessage("changeRequest.details.address.missing"));
+                    getMessages().add(getErrorMessage("changeRequest.demographics.address.missing"));
                     areDetailsValid = false;
                 } else if (!isChangeRequestContactValid(details)) {
-                    getMessages().add(getErrorMessage("changeRequest.details.contact.missing"));
+                    getMessages().add(getErrorMessage("changeRequest.demographics.contact.missing"));
                     areDetailsValid = false;
                 } else if (!isChangeRequestContactValid(details)) {
-                    getMessages().add(getErrorMessage("changeRequest.details..missing"));
+                    getMessages().add(getErrorMessage("changeRequest.demographics..missing"));
                     areDetailsValid = false;
                 }
                 return areDetailsValid;
             }
         }
         return true;
-    }
-
-    private boolean isChangeRequestWebsiteValid(ChangeRequestDeveloperDemographics details) {
-        return StringUtils.isNotEmpty(details.getWebsite());
     }
 
     private boolean isChangeRequestSelfDevloperValid(ChangeRequestDeveloperDemographics details) {
