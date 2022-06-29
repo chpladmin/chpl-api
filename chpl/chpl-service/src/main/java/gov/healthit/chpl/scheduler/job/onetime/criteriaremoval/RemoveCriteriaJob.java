a561be67b4cb67fa1cb154582a2a16cd547382b9
Andrew Larned <andrew.larned@gmail.com>
2022-06-29 05:26:30 -0400

    feat!: Update removeCriteriaJob for ERD Phase 2 requirements
    [#OCD-3261]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/criteriaremoval/RemoveCriteriaJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/criteriaremoval/RemoveCriteriaJob.java
@@ -1,10 +1,12 @@
 package gov.healthit.chpl.scheduler.job.onetime.criteriaremoval;
 
+import org.ff4j.FF4j;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import gov.healthit.chpl.FeatureList;
 import gov.healthit.chpl.dao.CertificationCriterionDAO;
 import gov.healthit.chpl.dto.CertificationCriterionDTO;
 import gov.healthit.chpl.scheduler.job.QuartzJob;
@@ -17,10 +19,26 @@ public class RemoveCriteriaJob extends QuartzJob {
     @Autowired
     private CertificationCriterionDAO certCriteriaDao;
 
+    @Autowired
+    private FF4j ff4j;
+
     private static final String[] CRITERIA_TO_REMOVE = {
-            "170.315 (a)(10):Drug-Formulary and Preferred Drug List Checks",
-            "170.315 (a)(13):Patient-Specific Education Resources",
-            "170.315 (e)(2):Secure Messaging"};
+            "170.315 (b)(1):Transitions of Care",
+            "170.315 (b)(2):Clinical Information Reconciliation and Incorporation",
+            "170.315 (b)(3):Electronic Prescribing",
+            "170.315 (b)(7):Data Segmentation for Privacy - Send",
+            "170.315 (b)(8):Data Segmentation for Privacy - Receive",
+            "170.315 (b)(9):Care Plan",
+            "170.315 (c)(3):Clinical Quality Measures - Report",
+            "170.315 (d)(2):Auditable Events and Tamper-Resistance",
+            "170.315 (d)(3):Audit Report(s)",
+            "170.315 (d)(10):Auditing Actions on Health Information",
+            "170.315 (e)(1):View, Download, and Transmit to 3rd Party",
+            "170.315 (f)(5):Transmission to Public Health Agencies - Electronic Case Reporting",
+            "170.315 (g)(6):Consolidated CDA Creation",
+            "170.315 (g)(8):Application Access - Data Category Request",
+            "170.315 (g)(9):Application Access - All Data Request"};
+
 
     public RemoveCriteriaJob() throws Exception {
         super();
@@ -30,18 +48,23 @@ public class RemoveCriteriaJob extends QuartzJob {
     public void execute(JobExecutionContext jobContext) throws JobExecutionException {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
         LOGGER.info("********* Starting the Remove Criteria job. *********");
-        for (String criteria : CRITERIA_TO_REMOVE) {
-            try {
-                String[] criterionElements = criteria.split(":");
-                CertificationCriterionDTO certDto = certCriteriaDao.getByNumberAndTitle(criterionElements[0], criterionElements[1]);
-                certDto.setRemoved(true);
-                certCriteriaDao.update(certDto);
-                LOGGER.info("Updated criteria " + criteria);
-            } catch (final Exception ex) {
-                LOGGER.error("Exception updating criteria " + criteria, ex);
+
+        if (ff4j.check(FeatureList.ERD_PHASE_2)) {
+            for (String criteria : CRITERIA_TO_REMOVE) {
+                try {
+                    String[] criterionElements = criteria.split(":");
+                    CertificationCriterionDTO certDto = certCriteriaDao.getByNumberAndTitle(criterionElements[0], criterionElements[1]);
+                    certDto.setRemoved(true);
+                    certCriteriaDao.update(certDto);
+                    LOGGER.info("Updated criteria " + criteria);
+                } catch (final Exception ex) {
+                    LOGGER.error("Exception updating criteria " + criteria, ex);
+                }
             }
+            CacheManager.getInstance().clearAll();
+        } else {
+            LOGGER.info("Could not run job - 'erd-phase-2' flag is not on.");
         }
-        CacheManager.getInstance().clearAll();
         LOGGER.info("********* Completed the Remove Criteria job. *********");
     }
 }
