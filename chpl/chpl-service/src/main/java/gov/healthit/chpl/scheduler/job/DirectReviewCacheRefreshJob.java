b0299ac5cfd062d6335c6d3c8213e5a1a7849c5e
Katy Ekey <katy.ekey@gmail.com>
2022-07-29 11:01:10 -0400

    feat: Add refresh of listing search and collection cache to DR Refresh Job
    [#OCD-3967]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/DirectReviewCacheRefreshJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/DirectReviewCacheRefreshJob.java
@@ -6,8 +6,12 @@ import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import gov.healthit.chpl.caching.CacheNames;
+import gov.healthit.chpl.search.CertifiedProductSearchManager;
+import gov.healthit.chpl.search.ListingSearchManager;
 import gov.healthit.chpl.service.DirectReviewCachingService;
 import lombok.extern.log4j.Log4j2;
+import net.sf.ehcache.CacheManager;
 
 @DisallowConcurrentExecution
 @Log4j2(topic = "directReviewCacheRefreshJobLogger")
@@ -15,6 +19,12 @@ public class DirectReviewCacheRefreshJob extends QuartzJob {
 
     @Autowired
     private DirectReviewCachingService directReviewService;
+
+    @Autowired
+    private CertifiedProductSearchManager certifiedProductSearchManager;
+
+    @Autowired
+    private ListingSearchManager listingSearchManager;
 
     @Override
     public void execute(final JobExecutionContext jobContext) throws JobExecutionException {
@@ -24,8 +34,18 @@ public class DirectReviewCacheRefreshJob extends QuartzJob {
             directReviewService.populateDirectReviewsCache(LOGGER);
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
-        } finally {
-            LOGGER.info("********* Completed the Direct Review Cache Refresh job. *********");
         }
+
+        LOGGER.info("Refreshing searchable listing data");
+        CacheManager.getInstance().getCache(CacheNames.COLLECTIONS_SEARCH).removeAll();
+        listingSearchManager.getAllListings();
+        LOGGER.info("Completed refreshing searchable listing data");
+
+        LOGGER.info("Refreshing searchable listing collection (deprecated)");
+        CacheManager.getInstance().getCache(CacheNames.COLLECTIONS_LISTINGS).removeAll();
+        certifiedProductSearchManager.getFlatListingCollection();
+        LOGGER.info("Completed refreshing searchable listing collection (deprecated)");
+
+        LOGGER.info("********* Completed the Direct Review Cache Refresh job. *********");
     }
 }
