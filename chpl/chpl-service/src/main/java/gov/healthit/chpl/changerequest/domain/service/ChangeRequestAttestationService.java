4f4d93c36ea4d8102d84ea4c14e4b5b7e32b6716
todd.young <tyoung@ainq.com>
2022-08-11 12:41:48 -0400

    feat: update how acb associated with Attestation CR is calculated
    OCD-3940

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/changerequest/domain/service/ChangeRequestAttestationService.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/changerequest/domain/service/ChangeRequestAttestationService.java
@@ -1,15 +1,22 @@
 package gov.healthit.chpl.changerequest.domain.service;
 
 import java.text.DateFormat;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.lang3.StringUtils;
+import org.jfree.data.time.DateRange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -25,20 +32,30 @@ import gov.healthit.chpl.changerequest.dao.ChangeRequestAttestationDAO;
 import gov.healthit.chpl.changerequest.dao.ChangeRequestDAO;
 import gov.healthit.chpl.changerequest.domain.ChangeRequest;
 import gov.healthit.chpl.changerequest.domain.ChangeRequestAttestationSubmission;
+import gov.healthit.chpl.dao.CertificationBodyDAO;
 import gov.healthit.chpl.dao.DeveloperDAO;
 import gov.healthit.chpl.dao.UserDeveloperMapDAO;
 import gov.healthit.chpl.dao.auth.UserDAO;
+import gov.healthit.chpl.domain.CertificationBody;
+import gov.healthit.chpl.domain.CertificationStatus;
+import gov.healthit.chpl.domain.CertificationStatusEvent;
 import gov.healthit.chpl.domain.Developer;
 import gov.healthit.chpl.domain.activity.ActivityConcept;
+import gov.healthit.chpl.domain.concept.CertificationEditionConcept;
 import gov.healthit.chpl.dto.auth.UserDTO;
 import gov.healthit.chpl.email.ChplEmailFactory;
 import gov.healthit.chpl.email.ChplHtmlEmailBuilder;
+import gov.healthit.chpl.entity.CertificationStatusType;
 import gov.healthit.chpl.exception.EmailNotSentException;
 import gov.healthit.chpl.exception.EntityCreationException;
 import gov.healthit.chpl.exception.EntityRetrievalException;
 import gov.healthit.chpl.exception.InvalidArgumentsException;
 import gov.healthit.chpl.exception.UserRetrievalException;
+import gov.healthit.chpl.exception.ValidationException;
 import gov.healthit.chpl.manager.ActivityManager;
+import gov.healthit.chpl.search.ListingSearchService;
+import gov.healthit.chpl.search.domain.ListingSearchResult;
+import gov.healthit.chpl.search.domain.SearchRequest;
 import gov.healthit.chpl.sharedstore.listing.ListingStoreRemove;
 import gov.healthit.chpl.sharedstore.listing.RemoveBy;
 import gov.healthit.chpl.util.AuthUtil;
@@ -48,6 +65,8 @@ import lombok.extern.log4j.Log4j2;
 @Component
 @Log4j2
 public class ChangeRequestAttestationService extends ChangeRequestDetailsService<ChangeRequestAttestationSubmission> {
+    private static final Integer MAX_PAGE_SIZE = 100;
+
     private ChangeRequestDAO crDAO;
     private ChangeRequestAttestationDAO crAttesttionDAO;
     private ChplEmailFactory chplEmailFactory;
@@ -57,6 +76,8 @@ public class ChangeRequestAttestationService extends ChangeRequestDetailsService
     private UserDAO userDAO;
     private DeveloperDAO developerDao;
     private ActivityManager activityManager;
+    private ListingSearchService listingSearchService;
+    private CertificationBodyDAO certificationBodyDAO;
 
     @Value("${changeRequest.attestation.submitted.subject}")
     private String submittedEmailSubject;
@@ -102,7 +123,8 @@ public class ChangeRequestAttestationService extends ChangeRequestDetailsService
             UserDeveloperMapDAO userDeveloperMapDAO, AttestationManager attestationManager,
             AttestationPeriodService attestationPeriodService, ChplEmailFactory chplEmailFactory,
             ChplHtmlEmailBuilder chplHtmlEmailBuilder, UserDAO userDAO, DeveloperDAO developerDao,
-            ActivityManager activityManager) {
+            ActivityManager activityManager, ListingSearchService listingSearchService,
+            CertificationBodyDAO certificationBodyDAO) {
 
         super(userDeveloperMapDAO);
         this.crDAO = crDAO;
@@ -114,7 +136,14 @@ public class ChangeRequestAttestationService extends ChangeRequestDetailsService
         this.userDAO = userDAO;
         this.developerDao = developerDao;
         this.activityManager = activityManager;
+        this.listingSearchService = listingSearchService;
+        this.certificationBodyDAO = certificationBodyDAO;
     }
+
+    private List<String> activeStatuses = Stream.of(CertificationStatusType.Active.getName(),
+            CertificationStatusType.SuspendedByAcb.getName(),
+            CertificationStatusType.SuspendedByOnc.getName())
+            .collect(Collectors.toList());
 
     @Override
     @Transactional
@@ -218,6 +247,88 @@ public class ChangeRequestAttestationService extends ChangeRequestDetailsService
         }
         return cr;
     }
+
+    @Override
+    public List<CertificationBody> getAssociatedCertificationBodies(ChangeRequest cr) {
+        try {
+            return getListingDataForDeveloper(cr.getDeveloper()).stream()
+                .filter(listing -> isListingActiveDuringPeriod(listing, attestationPeriodService.getMostRecentPastAttestationPeriod()))
+                .map(listing -> listing.getCertificationBody())
+                .collect(Collectors.toSet()).stream()
+                .map(pair -> getCertificationBody(pair.getId()))
+                .toList();
+        } catch (ValidationException e) {
+            LOGGER.error("Could not identify Certification Body for Developer with id: {}", cr.getDeveloper().getId());
+            return null;
+        }
+    }
+
+    private CertificationBody getCertificationBody(Long acbId) {
+        try {
+            return new CertificationBody(certificationBodyDAO.getById(acbId));
+        } catch (Exception e) {
+            LOGGER.error("Could not identify Certification Body with id: {}", acbId);
+            return null;
+        }
+    }
+
+    private List<ListingSearchResult> getListingDataForDeveloper(Developer developer) throws ValidationException {
+        SearchRequest request = SearchRequest.builder()
+                .certificationEditions(Stream.of(CertificationEditionConcept.CERTIFICATION_EDITION_2015.getYear()).collect(Collectors.toSet()))
+                .developer(developer.getName())
+                .pageSize(MAX_PAGE_SIZE)
+                .build();
+        return listingSearchService.findListings(request).getResults();
+    }
+
+    private Boolean isListingActiveDuringPeriod(ListingSearchResult listing, AttestationPeriod period) {
+        List<CertificationStatusEvent> statusEvents = listing.getStatusEvents().stream()
+                .map(statusEventSearchResult ->  CertificationStatusEvent.builder()
+                        .status(CertificationStatus.builder()
+                                .name(statusEventSearchResult.getStatus().getName())
+                                .build())
+                        .eventDate(toDate(statusEventSearchResult.getStatusStart()).getTime())
+                        .build())
+                .sorted(Comparator.comparing(CertificationStatusEvent::getEventDate))
+                .toList();
+
+        return isListingActiveDuringAttestationPeriod(statusEvents, period);
+    }
+
+    private boolean isListingActiveDuringAttestationPeriod(List<CertificationStatusEvent> statusEvents, AttestationPeriod period) {
+        List<DateRange> activeDateRanges = getDateRangesWithActiveStatus(statusEvents);
+        return activeDateRanges.stream()
+            .filter(activeDates -> toDate(period.getPeriodStart()).getTime() <= activeDates.getUpperMillis()
+                    && toDate(period.getPeriodEnd()).getTime() >= activeDates.getLowerMillis())
+            .findAny().isPresent();
+    }
+
+    private Date toDate(LocalDate localDate) {
+        return  DateUtil.toDate(localDate);
+    }
+
+    private List<DateRange> getDateRangesWithActiveStatus(List<CertificationStatusEvent> listingStatusEvents) {
+        //Assumes statuses are sorted
+        return IntStream.range(0, listingStatusEvents.size())
+            .filter(i -> listingStatusEvents.get(i) != null && listingStatusEvents.get(i).getStatus() != null
+                && !StringUtils.isEmpty(listingStatusEvents.get(i).getStatus().getName()))
+            .filter(i -> activeStatuses.contains(listingStatusEvents.get(i).getStatus().getName()))
+            .mapToObj(i -> new DateRange(new Date(listingStatusEvents.get(i).getEventDate()),
+                    i < (listingStatusEvents.size() - 1) ? new Date(listingStatusEvents.get(i + 1).getEventDate())
+                            //Math.max here to handle the case where status is a future date
+                            : new Date(Math.max(System.currentTimeMillis(), listingStatusEvents.get(i).getEventDate()))))
+            .collect(Collectors.toList());
+    }
+
+
+
+
+
+
+
+
+
+
 
     private void sendWithdrawnDetailsEmail(ChangeRequest cr) throws EmailNotSentException {
         chplEmailFactory.emailBuilder()
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/CertificationBody.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/CertificationBody.java
@@ -7,7 +7,11 @@ import gov.healthit.chpl.dto.CertificationBodyDTO;
 import gov.healthit.chpl.entity.CertificationBodyEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @Builder
 @AllArgsConstructor
 public class CertificationBody implements Serializable {
@@ -19,12 +23,6 @@ public class CertificationBody implements Serializable {
     private Address address;
     private boolean retired;
     private Date retirementDate;
-
-    /**
-     * No-args constructor.
-     */
-    public CertificationBody() {
-    }
 
     public CertificationBody(CertificationBodyDTO dto) {
         this.id = dto.getId();
@@ -48,66 +46,10 @@ public class CertificationBody implements Serializable {
         }
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(final String website) {
-        this.website = website;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(final Address address) {
-        this.address = address;
-    }
-
-    public String getAcbCode() {
-        return acbCode;
-    }
-
-    public void setAcbCode(final String acbCode) {
-        this.acbCode = acbCode;
-    }
-
-    public boolean isRetired() {
-        return retired;
-    }
-
-    public void setRetired(final boolean retired) {
-        this.retired = retired;
-    }
-
-    public Date getRetirementDate() {
-        return retirementDate;
-    }
-
-    public void setRetirementDate(final Date retirementDate) {
-        this.retirementDate = retirementDate;
-    }
 
     @Override
     public String toString() {
         return "CertificationBody [id=" + id + ", acbCode=" + acbCode + ", name=" + name + ", website=" + website
                 + ", address=" + address + ", retired=" + retired + ", retirementDate=" + retirementDate + "]";
     }
-
 }
