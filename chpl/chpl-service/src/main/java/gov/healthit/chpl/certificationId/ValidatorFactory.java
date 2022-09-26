5c3007b1dab2f83c9ad90ca391a7dcf1906e4272
Katy Ekey <katy.ekey@gmail.com>
2022-09-26 09:28:28 -0400

    feat!: Add (Cures Update) title to relevant criteria in CSV definitions and sort order
    [#OCD-4042]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/certificationId/ValidatorFactory.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/certificationId/ValidatorFactory.java
@@ -5,15 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.FeatureList;
+import gov.healthit.chpl.service.CertificationCriterionService;
 
 @Component
 public class ValidatorFactory {
 
+    private CertificationCriterionService certificationCriterionService;
     private FF4j ff4j;
 
     @Autowired
-    public ValidatorFactory(
-            final FF4j ff4j) {
+    public ValidatorFactory(CertificationCriterionService certificationCriterionService,
+            FF4j ff4j) {
+        this.certificationCriterionService = certificationCriterionService;
         this.ff4j = ff4j;
     }
 
@@ -29,9 +32,9 @@ public class ValidatorFactory {
             val = new Validator2014();
         } else if ("2015".equals(attYear)) {
             if (this.ff4j.check(FeatureList.ERD_PHASE_2)) {
-                val = new Validator2015();
+                val = new Validator2015(certificationCriterionService);
             } else {
-                val = new Validator2015Legacy();
+                val = new Validator2015Legacy(certificationCriterionService);
             }
         } else if ("2014/2015".equals(attYear)) {
             val = new Validator20142015();
