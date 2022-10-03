1f028228a139876ee452a0abf8f9e04b87d6b6dd
todd.young <tyoung@ainq.com>
2022-10-03 14:32:23 -0400

    feat: ensure all err msgs related to surv reqs and ncs have propertly formatted title
    OCD-4029

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/surveillance/reviewer/NewSurveillanceRemovedCriteriaReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/surveillance/reviewer/NewSurveillanceRemovedCriteriaReviewer.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.dao.CertificationCriterionDAO;
-import gov.healthit.chpl.domain.surveillance.NonconformityClassification;
 import gov.healthit.chpl.domain.surveillance.Surveillance;
 import gov.healthit.chpl.domain.surveillance.SurveillanceNonconformity;
 import gov.healthit.chpl.domain.surveillance.SurveillanceRequirement;
-import gov.healthit.chpl.domain.surveillance.SurveillanceRequirementType;
 import gov.healthit.chpl.permissions.ResourcePermissions;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.NullSafeEvaluator;
-import gov.healthit.chpl.util.Util;
 
 @Component
 public class NewSurveillanceRemovedCriteriaReviewer implements Reviewer {
@@ -45,32 +42,15 @@ public class NewSurveillanceRemovedCriteriaReviewer implements Reviewer {
 
     private void checkRequirementForRemovedDetailType(Surveillance surv, SurveillanceRequirement req) {
         if (NullSafeEvaluator.eval(() -> req.getRequirementDetailType().getRemoved(), false)) {
-            if (NullSafeEvaluator.eval(() -> req.getRequirementDetailType().getSurveillanceRequirementType().getId(), -1)
-                    .equals(SurveillanceRequirementType.CERTIFIED_CAPABILITY_ID)) {
-
-                surv.getErrorMessages().add(
-                        msgUtil.getMessage("surveillance.requirementNotAddedForRemovedCriteria", Util.formatCriteriaNumber(req.getRequirementDetailType())));
-            } else if (NullSafeEvaluator.eval(() -> req.getRequirementDetailType().getSurveillanceRequirementType().getId(), -1)
-                    .equals(SurveillanceRequirementType.TRANS_DISCLOSURE_ID)) {
-
-                surv.getErrorMessages().add(
-                        msgUtil.getMessage("surveillance.requirementNotAddedForRemovedRequirement", req.getRequirementDetailType().getTitle()));
-
-            }
+            surv.getErrorMessages().add(
+                    msgUtil.getMessage("surveillance.requirementNotAddedForRemoved", req.getRequirementDetailType().getFormattedTitle()));
         }
     }
 
     private void checkForRemovedNonconformity(Surveillance surv, SurveillanceNonconformity nc) {
         if (NullSafeEvaluator.eval(() -> nc.getType().getRemoved(), false)) {
-            if (NullSafeEvaluator.eval(() -> nc.getType().getClassification(), NonconformityClassification.UNKNOWN).equals(NonconformityClassification.CRITERION)) {
-                surv.getErrorMessages().add(
-                        msgUtil.getMessage("surveillance.nonconformityNotAddedForRemovedCriteria",
-                                Util.formatCriteriaNumber(nc.getType())));
-            } else if (NullSafeEvaluator.eval(() -> nc.getType().getClassification(), NonconformityClassification.UNKNOWN).equals(NonconformityClassification.REQUIREMENT)) {
-                surv.getErrorMessages().add(
-                        msgUtil.getMessage("surveillance.nonconformityNotAddedForRemovedRequirement",
-                        nc.getType().getNumber()));
-            }
+            surv.getErrorMessages().add(
+                        msgUtil.getMessage("surveillance.nonconformityNotAddedForRemoved", nc.getType().getFormattedTitle()));
         }
     }
 }
