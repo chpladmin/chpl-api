22ffcfb7395d20ed93223274ed8361c079a2c9cc
Katy Ekey <katy.ekey@gmail.com>
2022-10-07 07:10:58 -0400

    fix!: Do not allow developers to be created with duplicate names
    [#OCD-3906]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/rules/developer/DeveloperNameValidation.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/rules/developer/DeveloperNameValidation.java
@@ -1,14 +1,30 @@
 package gov.healthit.chpl.manager.rules.developer;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
 
+import gov.healthit.chpl.dao.DeveloperDAO;
+import gov.healthit.chpl.domain.Developer;
 import gov.healthit.chpl.manager.rules.ValidationRule;
 
 public class DeveloperNameValidation extends ValidationRule<DeveloperValidationContext> {
+    private DeveloperDAO developerDao;
+
+    public DeveloperNameValidation(DeveloperDAO developerDao) {
+        this.developerDao = developerDao;
+    }
     @Override
     public boolean isValid(DeveloperValidationContext context) {
         if (ObjectUtils.isEmpty(context.getDeveloper().getName())) {
             getMessages().add(getErrorMessage("developer.nameRequired"));
+            return false;
+        }
+
+        List<Developer> developersWithName = developerDao.getAllByName(context.getDeveloper().getName());
+        if (!CollectionUtils.isEmpty(developersWithName)) {
+            getMessages().add(getErrorMessage("developer.nameNotUnique"));
             return false;
         }
         return true;
