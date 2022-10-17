c951d23a75132e9d9529d271e512ff754f046442
todd.young <tyoung@ainq.com>
2022-10-17 16:03:13 -0400

    refactor: remove Impl class
    OCD-4057

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/ListingValidatorFactory.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/ListingValidatorFactory.java
@@ -1,7 +1,49 @@
 package gov.healthit.chpl.validation.listing;
 
-import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface ListingValidatorFactory {
-    Validator getValidator(CertifiedProductSearchDetails product);
+import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.util.ErrorMessageUtil;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class ListingValidatorFactory  {
+
+
+    private AllowedListingValidator allowedValidator;
+    private Edition2015ListingValidator edition2015Validator;
+    private ErrorMessageUtil msgUtil;
+
+    @Autowired
+    public ListingValidatorFactory(AllowedListingValidator allowedValidator, Edition2015ListingValidator edition2015Validator,
+            ErrorMessageUtil msgUtil) {
+        this.allowedValidator = allowedValidator;
+        this.edition2015Validator = edition2015Validator;
+        this.msgUtil = msgUtil;
+    }
+
+    public Validator getValidator(final CertifiedProductSearchDetails listing) {
+        String edition = listing.getCertificationEdition().get(CertifiedProductSearchDetails.EDITION_NAME_KEY).toString();
+        if (StringUtils.isEmpty(listing.getChplProductNumber())
+                || StringUtils.isEmpty(edition)) {
+            String errMsg = msgUtil.getMessage("listing.validator.editionOrChplNumberNotFound", listing.getId().toString());
+            listing.getErrorMessages().add(errMsg);
+            LOGGER.error(errMsg);
+            return null;
+        }
+
+        if (edition.equals("2011") || edition.equals("2014")) {
+            return allowedValidator;
+        } else if (edition.equals("2015")) {
+            return edition2015Validator;
+        } else {
+            String errMsg = msgUtil.getMessage("listing.validator.certificationEditionNotFound", edition);
+            listing.getErrorMessages().add(errMsg);
+            LOGGER.error(errMsg);
+        }
+        return null;
+    }
 }
