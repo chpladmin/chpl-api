e0f39d4f8b7281ba0bfbc709775551ade9019b73
todd.young <tyoung@ainq.com>
2022-10-19 09:02:01 -0400

    wip: process found activity records
    OCD-4020

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/RwtQuestionableActivityCreatorJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/RwtQuestionableActivityCreatorJob.java
@@ -1,5 +1,6 @@
 package gov.healthit.chpl.scheduler.job.onetime;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -14,26 +15,82 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import gov.healthit.chpl.activity.history.ListingActivityUtil;
 import gov.healthit.chpl.activity.history.explorer.ListingActivityExplorer;
 import gov.healthit.chpl.activity.history.query.ListingActivityQuery;
+import gov.healthit.chpl.certifiedproduct.CertifiedProductDetailsManager;
 import gov.healthit.chpl.dao.ActivityDAO;
+import gov.healthit.chpl.dao.CertifiedProductDAO;
+import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.domain.activity.ActivityConcept;
+import gov.healthit.chpl.domain.concept.CertificationEditionConcept;
 import gov.healthit.chpl.dto.ActivityDTO;
+import gov.healthit.chpl.dto.CertifiedProductDetailsDTO;
+import gov.healthit.chpl.exception.EntityRetrievalException;
+import gov.healthit.chpl.scheduler.job.CertifiedProduct2015Gatherer;
 import gov.healthit.chpl.util.DateUtil;
 import gov.healthit.chpl.util.NullSafeEvaluator;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-public class RwtQuestionableActivityCreatorJob implements Job {
+public class RwtQuestionableActivityCreatorJob extends CertifiedProduct2015Gatherer implements Job {
+    @Autowired
+    private RwtActivityExplorer rwtActivityExplorer;
+
+    @Autowired
+    private CertifiedProductDetailsManager certifiedProductDetailsManager;
+
+    @Autowired
+    private CertifiedProductDAO certifiedProductDAO;
+
+    @Autowired
+    private ListingActivityUtil activityUtil;
+
+    private SimpleDateFormat formatter = new SimpleDateFormat("YYYY MMM dd");
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 
+        LOGGER.info("********* Starting RWT Questionable Activity Creator job. *********");
+        LOGGER.info("Retrieving all 2015 listings");
+        List<CertifiedProductDetailsDTO> listings = certifiedProductDAO.findByEdition(
+                CertificationEditionConcept.CERTIFICATION_EDITION_2015.getYear());
+        LOGGER.info("Completed retreiving all 2015 listings");
+
+        listings.stream()
+                .map(dto -> getCertifiedProductSearchDetails(dto.getId()))
+                //.peek(listing -> LOGGER.info("Checking: {}", listing.getId()))
+                .flatMap(listing -> rwtActivityExplorer.getActivities(new ListingActivityQuery(listing.getId())).stream())
+                .forEach(activity -> LOGGER.info("Listing: {} - On {} - RWT Plan updated {} -> {} ",
+                        activity.getActivityObjectId(),
+                        formatter.format(activity.getActivityDate()),
+                        NullSafeEvaluator.eval(() -> activityUtil.getListing(activity.getOriginalData()).getRwtPlansUrl(), "NULL"),
+                        NullSafeEvaluator.eval(() -> activityUtil.getListing(activity.getNewData()).getRwtPlansUrl(), "NULL")));
+
+        LOGGER.info("********* Completed RWT Questionable Activity Creator job. *********");
     }
 
+    private CertifiedProductSearchDetails getCertifiedProductSearchDetails(Long certifiedProductId) {
+        try {
+            long start = (new Date()).getTime();
+            CertifiedProductSearchDetails listing = null;
+            listing = certifiedProductDetailsManager.getCertifiedProductDetails(certifiedProductId);
 
-    public class RwtActivityExplorer extends ListingActivityExplorer {
+            //LOGGER.info("Completed details for listing(" + ((new Date()).getTime() - start) + "ms): " + certifiedProductId);
+            return listing;
+        } catch (EntityRetrievalException e) {
+            LOGGER.error("Could not retrieve details for listing id: " + certifiedProductId);
+            LOGGER.catching(e);
+            return null;
+        }
+    }
+
+    @Service
+    private static class RwtActivityExplorer extends ListingActivityExplorer {
 
         private ActivityDAO activityDao;
         private ListingActivityUtil activityUtil;
@@ -58,7 +115,7 @@ public class RwtQuestionableActivityCreatorJob implements Job {
         @Transactional
         public List<ActivityDTO> getActivities(ListingActivityQuery query) {
             return getSortedActivities(query).stream()
-                    .filter(activity -> hasRwtPlanInformationBeenUpdated(activity) && wasPlanUpdatedOutsideStandardWindow(activity))
+                    .filter(activity -> hasRwtPlanInformationBeenUpdated(activity) && !wasPlanUpdatedOutsideStandardWindow(activity))
                     .toList();
         }
 
@@ -73,13 +130,13 @@ public class RwtQuestionableActivityCreatorJob implements Job {
                 return null;
             }
 
-            LOGGER.info("Finding all activity for listing ID " + query.getListingId() + ".");
+            //LOGGER.info("Finding all activity for listing ID " + query.getListingId() + ".");
             List<ActivityDTO> listingActivities = activityDao.findByObjectId(query.getListingId(), ActivityConcept.CERTIFIED_PRODUCT, new Date(0), new Date());
             if (CollectionUtils.isEmpty(listingActivities)) {
-                LOGGER.warn("No listing activities were found for listing ID " + query.getListingId() + ". Is the ID valid?");
+                //LOGGER.warn("No listing activities were found for listing ID " + query.getListingId() + ". Is the ID valid?");
                 return Collections.emptyList();
             }
-            LOGGER.info("There are " + listingActivities.size() + " activities for listing ID " + query.getListingId());
+            //LOGGER.info("There are " + listingActivities.size() + " activities for listing ID " + query.getListingId());
 
             sortOldestActivityFirst(listingActivities);
 
@@ -100,17 +157,17 @@ public class RwtQuestionableActivityCreatorJob implements Job {
 
         private boolean wasPlanUpdatedOutsideStandardWindow(ActivityDTO activity) {
             LocalDateTime activityDate = DateUtil.toLocalDateTime(activity.getActivityDate().getTime());
-            return activityDate.isAfter(getPlanStartPeriodBasedOnAcvitityYear(activityDate.getYear()))
+            return activityDate.isAfter(getPlanStartPeriodBasedOnActivityYear(activityDate.getYear()))
                     && activityDate.isBefore(getPlanEndPeriodBasedOnAcvitityYear(activityDate.getYear()));
         }
 
-        private LocalDateTime getPlanStartPeriodBasedOnAcvitityYear(Integer year) {
+        private LocalDateTime getPlanStartPeriodBasedOnActivityYear(Integer year) {
             String[] dateParts = planStartMonthDay.split("/");
             return LocalDateTime.of(year, Integer.valueOf(dateParts[0]), Integer.valueOf(dateParts[1]), 0, 0);
         }
 
         private LocalDateTime getPlanEndPeriodBasedOnAcvitityYear(Integer year) {
-            String[] dateParts = planStartMonthDay.split("/");
+            String[] dateParts = planEndMonthDay.split("/");
             return LocalDateTime.of(year, Integer.valueOf(dateParts[0]), Integer.valueOf(dateParts[1]), 23, 59);
         }
      }
