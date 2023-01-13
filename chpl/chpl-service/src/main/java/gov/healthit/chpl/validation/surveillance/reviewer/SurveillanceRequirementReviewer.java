00fb4c48e3316b0132acce7937f28843947bd98c
todd.young <tyoung@ainq.com>
2023-01-13 12:18:11 -0500

    feat: add error message to surv when nc criteria edition != listing edition
    OCD-4094

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/surveillance/reviewer/SurveillanceRequirementReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/surveillance/reviewer/SurveillanceRequirementReviewer.java
@@ -1,31 +1,41 @@
 package gov.healthit.chpl.validation.surveillance.reviewer;
 
-import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import gov.healthit.chpl.certifiedproduct.CertifiedProductDetailsManager;
 import gov.healthit.chpl.dao.surveillance.SurveillanceDAO;
+import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.domain.surveillance.RequirementGroupType;
 import gov.healthit.chpl.domain.surveillance.RequirementType;
 import gov.healthit.chpl.domain.surveillance.Surveillance;
 import gov.healthit.chpl.domain.surveillance.SurveillanceRequirement;
 import gov.healthit.chpl.domain.surveillance.SurveillanceResultType;
+import gov.healthit.chpl.exception.EntityRetrievalException;
 import gov.healthit.chpl.manager.DimensionalDataManager;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.NullSafeEvaluator;
+import lombok.extern.log4j.Log4j2;
 
 @Component
+@Log4j2
 public class SurveillanceRequirementReviewer implements Reviewer {
+    private static final Long NOT_FOUND = -1L;
     private SurveillanceDAO survDao;
     private ErrorMessageUtil msgUtil;
+    private CertifiedProductDetailsManager certifiedProductDetailsManager;
     private Set<RequirementType> requirementTypes;
 
     @Autowired
-    public SurveillanceRequirementReviewer(SurveillanceDAO survDao, ErrorMessageUtil msgUtil, DimensionalDataManager dimensionalDataManager) {
+    public SurveillanceRequirementReviewer(SurveillanceDAO survDao, ErrorMessageUtil msgUtil, DimensionalDataManager dimensionalDataManager,
+            @Lazy CertifiedProductDetailsManager certifiedProductDetailsManager) {
         this.survDao = survDao;
         this.msgUtil = msgUtil;
         this.requirementTypes = dimensionalDataManager.getRequirementTypes();
+        this.certifiedProductDetailsManager = certifiedProductDetailsManager;
     }
 
     @Override
@@ -35,19 +45,18 @@ public class SurveillanceRequirementReviewer implements Reviewer {
                     surv.getCertifiedProduct().getChplProductNumber()));
             return;
         }
-        for (SurveillanceRequirement req : surv.getRequirements()) {
+        surv.getRequirements().forEach(req -> {
             checkRequirementExists(surv, req);
             checkResultExistsIfSurveillanceClosed(surv, req);
             checkResultTypeValidity(surv, req);
-        }
+            checkCriteriaEditionMatchesListingEdition(surv, req);
+        });
     }
 
     private void checkRequirementExists(Surveillance surv, SurveillanceRequirement req) {
         if (!NullSafeEvaluator.eval(() -> req.getRequirementType().getId(), -1L).equals(-1L)) {
-            Optional<RequirementType> reqDetailTypeFound = requirementTypes.stream()
-                    .filter(rdt -> rdt.getId().equals(req.getRequirementType().getId()))
-                    .findAny();
-            if (reqDetailTypeFound.isEmpty()) {
+            RequirementType reqDetailTypeFound = getRequirementTypeFullyPopulated(req.getRequirementType().getId());
+            if (reqDetailTypeFound == null) {
                 surv.getErrorMessages().add(msgUtil.getMessage("surveillance.requirementIsRequired"));
             }
         }
@@ -80,6 +89,46 @@ public class SurveillanceRequirementReviewer implements Reviewer {
             } else {
                 req.setResult(resType);
             }
+        }
+    }
+
+    private void checkCriteriaEditionMatchesListingEdition(Surveillance surv, SurveillanceRequirement req) {
+            if (isRequirementTypeCertifiedCapability(req.getRequirementType())) {
+                Long requirementCriteriaEdition = NullSafeEvaluator.eval(() ->
+                        getRequirementTypeFullyPopulated(req.getRequirementType().getId())
+                                .getCertificationEdition().getCertificationEditionId(), NOT_FOUND);
+
+                if (!requirementCriteriaEdition.equals(NOT_FOUND)) {
+                    Long listingEdition = NullSafeEvaluator.eval(() -> getCertificationEditionIdFromListing(surv.getCertifiedProduct().getId()), NOT_FOUND);
+                    if (!requirementCriteriaEdition.equals(listingEdition)) {
+                        surv.getErrorMessages().add(msgUtil.getMessage("surveillance.requirementTypeEditionMismatch",
+                                getRequirementTypeFullyPopulated(req.getRequirementType().getId()).getFormattedTitle()));
+                    }
+                }
+            }
+    }
+
+    private boolean isRequirementTypeCertifiedCapability(RequirementType requirementType) {
+        RequirementType requirementTypFromDb = getRequirementTypeFullyPopulated(requirementType.getId());
+        return requirementTypFromDb != null
+                ? requirementTypFromDb.getRequirementGroupType().getId().equals(RequirementGroupType.CERTIFIED_CAPABILITY_ID)
+                : false;
+    }
+
+    private RequirementType getRequirementTypeFullyPopulated(Long id) {
+        return requirementTypes.stream()
+                .filter(rdt -> rdt.getId().equals(id))
+                .findAny()
+                .orElse(null);
+    }
+
+    private Long getCertificationEditionIdFromListing(Long id) {
+        try {
+            return Long.valueOf(certifiedProductDetailsManager.getCertifiedProductDetails(id)
+                    .getCertificationEdition().get(CertifiedProductSearchDetails.EDITION_ID_KEY).toString());
+        } catch (EntityRetrievalException e) {
+            LOGGER.error("Could not retrieve listing", e);
+            return null;
         }
     }
 }
