d0913d8466df3ea75636ed7d85b05331b06bce76
todd.young <tyoung@ainq.com>
2023-01-13 21:21:27 -0500

    feat: update NonconformityType to return CertificationEdition in JSON response
    OCD-4094

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/NonconformityType.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/NonconformityType.java
@@ -12,12 +12,13 @@ import org.apache.commons.lang3.StringUtils;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import gov.healthit.chpl.domain.surveillance.NonconformityClassification;
-import gov.healthit.chpl.util.NullSafeEvaluator;
 import gov.healthit.chpl.util.Util;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -42,59 +43,6 @@ public class NonconformityType implements Serializable {
     @JsonIgnore
     @XmlTransient
     private NonconformityClassification classification;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setCertificationEdition(CertificationEdition certificationEdition) {
-        this.certificationEdition = certificationEdition;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Boolean getRemoved() {
-        return removed;
-    }
-
-    public void setRemoved(Boolean removed) {
-        this.removed = removed;
-    }
-
-    public NonconformityClassification getClassification() {
-        return classification;
-    }
-
-    public void setClassification(NonconformityClassification classification) {
-        this.classification = classification;
-    }
-
-    public void setEdition(String edition) {
-        this.edition = edition;
-    }
-
-    @JsonIgnore
-    public String getEdition() {
-        return NullSafeEvaluator.eval(() -> certificationEdition.getYear(), null);
-    }
 
     @JsonIgnore
     public String getFormattedTitle() {
