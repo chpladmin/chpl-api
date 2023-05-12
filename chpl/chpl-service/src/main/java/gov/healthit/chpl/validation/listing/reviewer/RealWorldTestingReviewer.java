0cae758dc007075e1e8a414e2d1dbacb8ad7274d
Todd Young <todd.young@pointclickcare.com>
2023-05-12 14:46:41 -0400

    feat: switch RWT validation to not use comparison reviewer
    OCD-4112

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/RealWorldTestingReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/RealWorldTestingReviewer.java
@@ -6,30 +6,36 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import gov.healthit.chpl.certifiedproduct.CertifiedProductDetailsManager;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.exception.EntityRetrievalException;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.ValidationUtils;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component("realWorldTestingReviewer")
-public class RealWorldTestingReviewer implements ComparisonReviewer {
+public class RealWorldTestingReviewer implements Reviewer {
 
     private static final String EDITION_2015 = "2015";
 
+    private CertifiedProductDetailsManager certifiedProductDetailsManager;
     private ValidationUtils validationUtils;
     private ErrorMessageUtil errorMessageUtil;
 
     @Autowired
-    public RealWorldTestingReviewer(ValidationUtils validationUtils, ErrorMessageUtil errorMessageUtil) {
+    public RealWorldTestingReviewer(CertifiedProductDetailsManager certifiedProductDetailsManager,
+            ValidationUtils validationUtils, ErrorMessageUtil errorMessageUtil) {
+
+        this.certifiedProductDetailsManager = certifiedProductDetailsManager;
         this.validationUtils = validationUtils;
         this.errorMessageUtil = errorMessageUtil;
     }
 
     @Override
-    public void review(CertifiedProductSearchDetails existingListing, CertifiedProductSearchDetails updatedListing) {
+    public void review(CertifiedProductSearchDetails updatedListing) {
 
-        if (isListingCurrentlyRwtEligible(existingListing)) {
+        if (isListingCurrentlyRwtEligible(updatedListing)) {
             if (isRwtPlansDataSubmitted(updatedListing)) {
                 validateRwtPlansUrl(updatedListing);
                 validateRwtPlansCheckDate(updatedListing);
@@ -48,12 +54,17 @@ public class RealWorldTestingReviewer implements ComparisonReviewer {
         return isListing2015Edition(listing);
     }
 
-    @SuppressWarnings("checkstyle:linelength")
     private boolean isListing2015Edition(CertifiedProductSearchDetails listing) {
-        if (listing.getCertificationEdition().containsKey(CertifiedProductSearchDetails.EDITION_NAME_KEY)) {
-            return listing.getCertificationEdition().get(CertifiedProductSearchDetails.EDITION_NAME_KEY).toString().equals(EDITION_2015);
+        try {
+            CertifiedProductSearchDetails cpsd = certifiedProductDetailsManager.getCertifiedProductDetails(listing.getId());
+            if (cpsd.getCertificationEdition().containsKey(CertifiedProductSearchDetails.EDITION_NAME_KEY)) {
+                return cpsd.getCertificationEdition().get(CertifiedProductSearchDetails.EDITION_NAME_KEY).toString().equals(EDITION_2015);
+            }
+            return false;
+        } catch (EntityRetrievalException e) {
+            LOGGER.error("Could not determine the edition of listing {}", listing.getId(), e);
+            return false;
         }
-        return false;
     }
 
     private void validateRwtPlansUrl(CertifiedProductSearchDetails listing) {
