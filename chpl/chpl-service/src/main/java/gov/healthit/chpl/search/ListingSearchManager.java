f6d0d7358042199e855304b5741365976fbd750c
Todd Young <todd.young@pointclickcare.com>
2023-06-07 14:28:36 -0400

    debug: determine if this parallel stream is preventing redis from working
    OCD-4228

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/search/ListingSearchManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/search/ListingSearchManager.java
@@ -38,7 +38,8 @@ public class ListingSearchManager {
 
     public  List<ListingSearchResult> getAllListingsNoCache() {
         List<ListingSearchResult> results = searchDao.getListingSearchResults();
-        results.parallelStream()
+        //results.parallelStream()
+        results.stream()
             .forEach(searchResult -> populateDirectReviews(searchResult));
         return results;
     }
