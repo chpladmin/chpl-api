e7e8b416899e8ac3d3cfe51eff8ffe492b07f89d
Todd Young <todd.young@pointclickcare.com>
2023-06-08 13:28:21 -0400

    debug: hard code DirectReviewSearchService.areDirectReviewsLoading to return false;
    OCD-4228

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/compliance/directreview/DirectReviewSearchService.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/compliance/directreview/DirectReviewSearchService.java
@@ -51,11 +51,12 @@ public class DirectReviewSearchService {
 
     public boolean areDirectReviewsLoading() {
         //if there are any entries in the cache with "null" http status then it's still loading
-        List<DirectReviewContainer> drContainers = getAll();
-        return CollectionUtils.isEmpty(drContainers)
-                || drContainers.stream()
-                    .filter(drResponse -> drResponse.getHttpStatus() == null)
-                    .findAny().isPresent();
+//        List<DirectReviewContainer> drContainers = getAll();
+//        return CollectionUtils.isEmpty(drContainers)
+//                || drContainers.stream()
+//                    .filter(drResponse -> drResponse.getHttpStatus() == null)
+//                    .findAny().isPresent();
+        return false;
     }
 
     public List<DirectReviewContainer> getAll() {
