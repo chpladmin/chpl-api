ff35d9f652f872f61521f121f5a4ab6fd28d3ef4
Todd Young <todd.young@pointclickcare.com>
2023-06-21 21:56:26 -0400

    fix: update day of week constants
    OCD-4175

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/urluptime/DatadogDayOfWeek.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/urluptime/DatadogDayOfWeek.java
@@ -1,11 +1,11 @@
 package gov.healthit.chpl.scheduler.job.urluptime;
 
 public class DatadogDayOfWeek {
-    public static final Integer SUNDAY = 1;
-    public static final Integer MONDAY = 2;
-    public static final Integer TUESDAY = 3;
-    public static final Integer WEDNESDAY = 4;
-    public static final Integer THURSDAY = 5;
-    public static final Integer FRIDAY = 6;
-    public static final Integer SATURDAY = 7;
+    public static final Integer SUNDAY = 7;
+    public static final Integer MONDAY = 1;
+    public static final Integer TUESDAY = 2;
+    public static final Integer WEDNESDAY = 3;
+    public static final Integer THURSDAY = 4;
+    public static final Integer FRIDAY = 5;
+    public static final Integer SATURDAY = 6;
 }
