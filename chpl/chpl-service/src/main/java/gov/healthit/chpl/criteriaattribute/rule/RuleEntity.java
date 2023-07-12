586fc58a8dfd48325972f41975e94e7f1b72a93e
Todd Young <todd.young@pointclickcare.com>
2023-07-12 15:51:48 -0400

    fix: add required constructors
    OCD-4242

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/criteriaattribute/rule/RuleEntity.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/criteriaattribute/rule/RuleEntity.java
@@ -10,13 +10,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "rule")
 public class RuleEntity {
