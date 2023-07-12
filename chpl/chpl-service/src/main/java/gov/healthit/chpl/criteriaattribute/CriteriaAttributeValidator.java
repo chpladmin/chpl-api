26fef8c67ff2a7546d9a1898549dd9d2b3356d07
Todd Young <todd.young@pointclickcare.com>
2023-07-12 16:05:26 -0400

    fix: formatting of error messages
    OCD-4242

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/criteriaattribute/CriteriaAttributeValidator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/criteriaattribute/CriteriaAttributeValidator.java
@@ -148,8 +148,10 @@ public class CriteriaAttributeValidator {
 
         if (!CollectionUtils.isEmpty(listings)) {
             String message = errorMessageUtil.getMessage("criteriaAttribute.delete.listingsExist",
+                    context.getName(),
                     listings.size(),
-                    listings.size() > 1 ? "s" : "");
+                    listings.size() > 1 ? "s" : "",
+                    "");
             if (listings.size() < MAX_LISTINGS_IN_DELETE_ERROR_MESSAGE) {
                 message = message + " "
                         + listings.stream()
@@ -180,7 +182,8 @@ public class CriteriaAttributeValidator {
                                 context.getName(),
                                 CertificationCriterionService.formatCriteriaNumber(crit),
                                 listings.size(),
-                                listings.size() > 1 ? "s" : "");
+                                listings.size() > 1 ? "s" : "",
+                                "");
                         if (listings.size() < MAX_LISTINGS_IN_DELETE_ERROR_MESSAGE) {
                                 message = message + " "
                                         + listings.stream()
