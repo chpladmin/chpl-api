5c42892381c16918df296481ea7c794bc6fbe902
Todd Young <todd.young@pointclickcare.com>
2023-07-18 11:15:00 -0400

    feat: add /rules endpoint to return all CHPL rules
    OCD-4242

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/criteriaattribute/rule/RuleDAO.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/criteriaattribute/rule/RuleDAO.java
@@ -10,6 +10,13 @@ import gov.healthit.chpl.dao.impl.BaseDAOImpl;
 
 @Component
 public class RuleDAO extends BaseDAOImpl {
+
+    public List<Rule> getAll() {
+        return getAllEntities().stream()
+                .map(entity -> entity.toDomain())
+                .toList();
+    }
+
     public RuleEntity getRuleEntityById(Long id) {
         RuleEntity entity = null;
         Query query = entityManager.createQuery(
@@ -22,5 +29,13 @@ public class RuleDAO extends BaseDAOImpl {
             entity = result.get(0);
         }
         return entity;
+    }
+
+    public List<RuleEntity> getAllEntities() {
+        Query query = entityManager.createQuery(
+                "FROM RuleEntity "
+                + "WHERE (NOT deleted = true) ", RuleEntity.class);
+        List<RuleEntity> result = query.getResultList();
+        return result;
     }
 }
