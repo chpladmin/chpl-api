4b6f48352127a4581368534e973c16ff60b4e6d4
Todd Young <todd.young@pointclickcare.com>
2023-08-03 15:39:43 -0400

    fix: update method name
    OCD-4242

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/TestToolController.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/TestToolController.java
@@ -49,7 +49,7 @@ public class TestToolController {
                     @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
             })
     @RequestMapping(value = "/criteria", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public @ResponseBody List<CertificationCriterion> getCertificationCriteriaForSTestTools() {
+    public @ResponseBody List<CertificationCriterion> getCertificationCriteriaForTestTools() {
         return testToolManager.getCertificationCriteriaForTestTools();
     }
 
