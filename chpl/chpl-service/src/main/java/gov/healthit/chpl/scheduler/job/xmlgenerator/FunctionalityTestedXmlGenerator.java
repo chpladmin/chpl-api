7ae7221798cc9218b8ed040c7eee7530fc12ac96
Todd Young <todd.young@pointclickcare.com>
2023-08-22 23:02:54 -0400

    fix: update order of XML output
    OCD-4288

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/xmlgenerator/FunctionalityTestedXmlGenerator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/xmlgenerator/FunctionalityTestedXmlGenerator.java
@@ -8,8 +8,8 @@ import gov.healthit.chpl.criteriaattribute.functionalitytested.FunctionalityTest
 public class FunctionalityTestedXmlGenerator extends XmlGenerator {
     public static void add(FunctionalityTested functionalityTested, String rootNodeName, XMLStreamWriter sw) throws XMLStreamException {
         if (functionalityTested != null) {
+            //Criteria Attribute values first
             sw.writeStartElement(rootNodeName);
-            createSimpleElement(functionalityTested.getAdditionalInformation(), "additionalInformation", sw);
             createSimpleElement(functionalityTested.getEndDay(), "endDay", sw);
             createSimpleElement(functionalityTested.getId(), "id", sw);
             PracticeTypeXmlGenerator.add(functionalityTested.getPracticeType(), "practiceType", sw);
@@ -18,6 +18,9 @@ public class FunctionalityTestedXmlGenerator extends XmlGenerator {
             RuleXmlGenerator.add(functionalityTested.getRule(), "rule", sw);
             createSimpleElement(functionalityTested.getStartDay(), "startDay", sw);
             createSimpleElement(functionalityTested.getValue(), "value", sw);
+            //The Functionality Tested specific values
+            createSimpleElement(functionalityTested.getAdditionalInformation(), "additionalInformation", sw);
+            PracticeTypeXmlGenerator.add(functionalityTested.getPracticeType(), "practiceType", sw);
             sw.writeEndElement();
         }
     }
