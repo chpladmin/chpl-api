fb8faa23cbc4c7f174887589755b1e2fdb571d16
Todd Young <todd.young@pointclickcare.com>
2023-09-05 10:17:15 -0400

    fix: fix order of xml elements
    OCD-4288

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/xmlgenerator/FunctionalityTestedXmlGenerator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/xmlgenerator/FunctionalityTestedXmlGenerator.java
@@ -10,16 +10,15 @@ public class FunctionalityTestedXmlGenerator extends XmlGenerator {
         if (functionalityTested != null) {
             //Criteria Attribute values first
             sw.writeStartElement(rootNodeName);
+            createSimpleElement(functionalityTested.getAdditionalInformation(), "additionalInformation", sw);
             createSimpleElement(functionalityTested.getEndDay(), "endDay", sw);
             createSimpleElement(functionalityTested.getId(), "id", sw);
+            PracticeTypeXmlGenerator.add(functionalityTested.getPracticeType(), "practiceType", sw);
             createSimpleElement(functionalityTested.getRegulatoryTextCitation(), "regulatoryTextCitation", sw);
             createSimpleElement(functionalityTested.getRequiredDay(), "requiredDay", sw);
             RuleXmlGenerator.add(functionalityTested.getRule(), "rule", sw);
             createSimpleElement(functionalityTested.getStartDay(), "startDay", sw);
             createSimpleElement(functionalityTested.getValue(), "value", sw);
-            //The Functionality Tested specific values
-            createSimpleElement(functionalityTested.getAdditionalInformation(), "additionalInformation", sw);
-            PracticeTypeXmlGenerator.add(functionalityTested.getPracticeType(), "practiceType", sw);
             sw.writeEndElement();
         }
     }
