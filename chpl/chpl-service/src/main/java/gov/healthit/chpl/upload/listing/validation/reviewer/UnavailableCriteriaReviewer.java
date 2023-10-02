5c201bfb223ac3701d13354fed3a5a412921be57
Katy Ekey <katy.ekey@gmail.com>
2023-10-02 16:25:42 -0400

    feat: Allow upload of removed criteria if removed in last year
    [#OCD-4313]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/listing/validation/reviewer/UnavailableCriteriaReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/listing/validation/reviewer/UnavailableCriteriaReviewer.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.permissions.ResourcePermissions;
 import gov.healthit.chpl.util.DateUtil;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.Util;
@@ -15,10 +16,12 @@ import gov.healthit.chpl.validation.listing.reviewer.Reviewer;
 @Component("listingUploadUnavailableCriteriaReviewer")
 public class UnavailableCriteriaReviewer implements Reviewer {
     private ErrorMessageUtil msgUtil;
+    private ResourcePermissions resourcePermissions;
 
     @Autowired
-    public UnavailableCriteriaReviewer(ErrorMessageUtil msgUtil) {
+    public UnavailableCriteriaReviewer(ErrorMessageUtil msgUtil, ResourcePermissions resourcePermissions) {
         this.msgUtil = msgUtil;
+        this.resourcePermissions = resourcePermissions;
     }
 
     @Override
@@ -34,7 +37,8 @@ public class UnavailableCriteriaReviewer implements Reviewer {
         }
 
         if (isCriterionAttested(certResult)
-                && !isCriterionAvailable(listing, certResult)) {
+                && (!doCriterionDatesOverlapCertificationDay(listing, certResult)
+                        || !isCriterionEditableByUser(listing, certResult))) {
             listing.addBusinessErrorMessage(msgUtil.getMessage("listing.unavailableCriteriaAddNotAllowed",
                     Util.formatCriteriaNumber(certResult.getCriterion())));
         }
@@ -44,10 +48,17 @@ public class UnavailableCriteriaReviewer implements Reviewer {
         return BooleanUtils.isTrue(certResult.isSuccess());
     }
 
-    private boolean isCriterionAvailable(CertifiedProductSearchDetails listing, CertificationResult certResult) {
+    private boolean doCriterionDatesOverlapCertificationDay(CertifiedProductSearchDetails listing, CertificationResult certResult) {
 
         return certResult.getCriterion() != null
                 && DateUtil.datesOverlap(Pair.of(listing.getCertificationDay(), listing.getDecertificationDay()),
                         Pair.of(certResult.getCriterion().getStartDay(), certResult.getCriterion().getEndDay()));
+    }
+
+    private boolean isCriterionEditableByUser(CertifiedProductSearchDetails listing, CertificationResult certResult) {
+        if (resourcePermissions.isUserRoleAdmin() || resourcePermissions.isUserRoleOnc()) {
+            return true;
+        }
+        return certResult.getCriterion().isEditable();
     }
 }
--- chpl/chpl-service/src/test/java/gov/healthit/chpl/upload/listing/validation/reviewer/UnavailableCriteriaReviewerTest.java
+++ chpl/chpl-service/src/test/java/gov/healthit/chpl/upload/listing/validation/reviewer/UnavailableCriteriaReviewerTest.java
@@ -14,6 +14,7 @@ import gov.healthit.chpl.certificationCriteria.CertificationCriterion;
 import gov.healthit.chpl.domain.CertificationResult;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.exception.EntityRetrievalException;
+import gov.healthit.chpl.permissions.ResourcePermissions;
 import gov.healthit.chpl.util.DateUtil;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 
@@ -21,15 +22,20 @@ public class UnavailableCriteriaReviewerTest {
     private static final String UNAVAILABLE_CRITERIA_NOT_ALLOWED = "The criterion %s is unavailable for this listing.";
 
     private ErrorMessageUtil msgUtil;
+    private ResourcePermissions resourcePermissions;
     private UnavailableCriteriaReviewer reviewer;
 
     @Before
     public void before() throws EntityRetrievalException {
+        resourcePermissions = Mockito.mock(ResourcePermissions.class);
+        Mockito.when(resourcePermissions.isUserRoleOnc()).thenReturn(false);
+        Mockito.when(resourcePermissions.isUserRoleAdmin()).thenReturn(false);
+
         msgUtil = Mockito.mock(ErrorMessageUtil.class);
         Mockito.when(msgUtil.getMessage(ArgumentMatchers.eq("listing.unavailableCriteriaAddNotAllowed"),
                 ArgumentMatchers.anyString()))
             .thenAnswer(i -> String.format(UNAVAILABLE_CRITERIA_NOT_ALLOWED, i.getArgument(1), ""));
-        reviewer = new UnavailableCriteriaReviewer(msgUtil);
+        reviewer = new UnavailableCriteriaReviewer(msgUtil, resourcePermissions);
     }
 
     @Test
