28b21b324b210ee59ae9286625101fd0a14f727c
Todd Young <todd.young@ainq.com>
2023-10-18 12:37:34 -0400

    feat: validate requirement type is valid based on surv start date
    OCD-4308

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/surveillance/reviewer/SurveillanceRequirementReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/surveillance/reviewer/SurveillanceRequirementReviewer.java
@@ -2,18 +2,17 @@ package gov.healthit.chpl.validation.surveillance.reviewer;
 
 import java.util.Set;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.compliance.surveillance.SurveillanceDAO;
-import gov.healthit.chpl.dao.CertifiedProductDAO;
-import gov.healthit.chpl.domain.surveillance.RequirementGroupType;
 import gov.healthit.chpl.domain.surveillance.RequirementType;
 import gov.healthit.chpl.domain.surveillance.Surveillance;
 import gov.healthit.chpl.domain.surveillance.SurveillanceRequirement;
 import gov.healthit.chpl.domain.surveillance.SurveillanceResultType;
-import gov.healthit.chpl.exception.EntityRetrievalException;
 import gov.healthit.chpl.manager.DimensionalDataManager;
+import gov.healthit.chpl.util.DateUtil;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.NullSafeEvaluator;
 import lombok.extern.log4j.Log4j2;
@@ -24,16 +23,13 @@ public class SurveillanceRequirementReviewer implements Reviewer {
     private static final Long NOT_FOUND = -1L;
     private SurveillanceDAO survDao;
     private ErrorMessageUtil msgUtil;
-    private CertifiedProductDAO certifiedProductDAO;
     private Set<RequirementType> requirementTypes;
 
     @Autowired
-    public SurveillanceRequirementReviewer(SurveillanceDAO survDao, ErrorMessageUtil msgUtil, DimensionalDataManager dimensionalDataManager,
-            CertifiedProductDAO certifiedProductDAO) {
+    public SurveillanceRequirementReviewer(SurveillanceDAO survDao, ErrorMessageUtil msgUtil, DimensionalDataManager dimensionalDataManager) {
         this.survDao = survDao;
         this.msgUtil = msgUtil;
         this.requirementTypes = dimensionalDataManager.getRequirementTypes();
-        this.certifiedProductDAO = certifiedProductDAO;
     }
 
     @Override
@@ -47,7 +43,7 @@ public class SurveillanceRequirementReviewer implements Reviewer {
             checkRequirementExists(surv, req);
             checkResultExistsIfSurveillanceClosed(surv, req);
             checkResultTypeValidity(surv, req);
-            checkCriteriaEditionMatchesListingEdition(surv, req);
+            checkRequirementValidForSurveillanceStartDate(surv, req);
         });
     }
 
@@ -90,31 +86,20 @@ public class SurveillanceRequirementReviewer implements Reviewer {
         }
     }
 
-    private void checkCriteriaEditionMatchesListingEdition(Surveillance surv, SurveillanceRequirement req) {
-        if (!isRequirementTypeOther(req.getRequirementType()) && isRequirementTypeCertifiedCapability(req.getRequirementType())) {
-            Long requirementCriteriaEdition = NullSafeEvaluator.eval(() ->
-                    getRequirementTypeFullyPopulated(req.getRequirementType().getId())
-                            .getCertificationEdition().getId(), NOT_FOUND);
+    private void checkRequirementValidForSurveillanceStartDate(Surveillance surv, SurveillanceRequirement req) {
+        if (isAddingNewSurveillance(surv)
+            && !DateUtil.isDateBetweenInclusive(Pair.of(req.getRequirementType().getStartDay(), req.getRequirementType().getEndDay()), surv.getStartDay())) {
 
-            if (!requirementCriteriaEdition.equals(NOT_FOUND)) {
-                Long listingEdition = NullSafeEvaluator.eval(() -> getCertificationEditionIdFromListing(surv.getCertifiedProduct().getId()), NOT_FOUND);
-                if (!requirementCriteriaEdition.equals(listingEdition)) {
-                    surv.getErrorMessages().add(msgUtil.getMessage("surveillance.requirementTypeEditionMismatch",
-                            getRequirementTypeFullyPopulated(req.getRequirementType().getId()).getFormattedTitle()));
-                }
-            }
+            surv.getErrorMessages().add(msgUtil.getMessage("surveillance.nonConformityType.notValid", req.getRequirementType().getFormattedTitle()));
         }
+    }
+
+    private boolean isAddingNewSurveillance(Surveillance surv) {
+        return surv.getId() == null;
     }
 
     private boolean isRequirementTypeOther(RequirementType requirementType) {
         return NullSafeEvaluator.eval(() -> requirementType.getId(), NOT_FOUND).equals(NOT_FOUND);
-    }
-
-    private boolean isRequirementTypeCertifiedCapability(RequirementType requirementType) {
-        RequirementType requirementTypFromDb = getRequirementTypeFullyPopulated(requirementType.getId());
-        return requirementTypFromDb != null
-                ? requirementTypFromDb.getRequirementGroupType().getId().equals(RequirementGroupType.CERTIFIED_CAPABILITY_ID)
-                : false;
     }
 
     private RequirementType getRequirementTypeFullyPopulated(Long id) {
@@ -124,12 +109,4 @@ public class SurveillanceRequirementReviewer implements Reviewer {
                 .orElse(null);
     }
 
-    private Long getCertificationEditionIdFromListing(Long id) {
-        try {
-            return certifiedProductDAO.getById(id).getCertificationEditionId();
-        } catch (EntityRetrievalException e) {
-            LOGGER.error("Could not retrieve listing", e);
-            return NOT_FOUND;
-        }
-    }
 }
