ec4606735f3fa4adedc324959ca705623c8d8550
Katy Ekey <katy.ekey@gmail.com>
2023-12-14 09:32:41 -0500

    feat!: Include companion guide link in criterion API response
    [#OCD-4417]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/certificationCriteria/CertificationCriterion.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/certificationCriteria/CertificationCriterion.java
@@ -92,6 +92,8 @@ public class CertificationCriterion implements Serializable {
     @XmlElement(required = false, nillable = true)
     private Rule rule;
 
+    private String companionGuideLink;
+
     @Deprecated
     @DeprecatedResponseField(message = "This property will be removed. It can be derived based on the endDay.",
         removalDate = "2024-01-01")
