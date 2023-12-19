817c5266f973fa332f8af0950cb1341c9793d52a
Todd Young <tyoung@ainq.com>
2023-12-19 14:53:36 -0500

    feat!: Update saving of Certification Results for 2014 Listings to handle unattested Criteria
    OCD-4383

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/listing/normalizer/CertificationCriterionNormalizer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/upload/listing/normalizer/CertificationCriterionNormalizer.java
@@ -45,8 +45,8 @@ public class CertificationCriterionNormalizer {
         //criteria can still g1_success and g2_success values.
         return cr.getCriterion().getCertificationEdition() != null
                 && cr.getCriterion().getCertificationEdition().equals(CertificationEditionConcept.CERTIFICATION_EDITION_2014.getYear())
-                && ((cr.isG1Success() != null && cr.isG1Success())
-                        || (cr.isG2Success() != null && cr.isG2Success()));
+                && (certResultRules.hasCertOption(cr.getCriterion().getId(), CertificationResultRules.G1_SUCCESS)
+                        || certResultRules.hasCertOption(cr.getCriterion().getId(), CertificationResultRules.G2_SUCCESS));
     }
 
     private void nullifyNotApplicableFieldsInCertificationResults(CertifiedProductSearchDetails listing) {
