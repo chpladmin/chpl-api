95c5820395ea206279b7b6f1c566aa2217853dec
Katy Ekey <katy.ekey@gmail.com>
2024-01-31 16:37:31 -0500

    feat: Non-ONC/ADMIN user should not be able to use "by ONC" statuses
    [#OCD-4452]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/DeveloperBanComparisonReviewer.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/validation/listing/reviewer/DeveloperBanComparisonReviewer.java
@@ -1,57 +1,87 @@
 package gov.healthit.chpl.validation.listing.reviewer;
 
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import gov.healthit.chpl.domain.CertificationStatusEvent;
 import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
 import gov.healthit.chpl.entity.CertificationStatusType;
 import gov.healthit.chpl.permissions.ResourcePermissions;
 import gov.healthit.chpl.util.AuthUtil;
 import gov.healthit.chpl.util.ErrorMessageUtil;
+import gov.healthit.chpl.util.Util;
 
-/**
- * Makes sure the user has the correct privileges to change the status.
- *
- * @author kekey
- *
- */
 @Component("developerBanComparisonReviewer")
 public class DeveloperBanComparisonReviewer implements ComparisonReviewer {
     private ResourcePermissions resourcePermissions;
     private ErrorMessageUtil msgUtil;
+    private List<String> certStatusesRequiringOncRole;
 
     @Autowired
-    public DeveloperBanComparisonReviewer(final ResourcePermissions resourcePermissions,
-            final ErrorMessageUtil msgUtil) {
+    public DeveloperBanComparisonReviewer(ResourcePermissions resourcePermissions,
+            ErrorMessageUtil msgUtil) {
         this.resourcePermissions = resourcePermissions;
         this.msgUtil = msgUtil;
+        this.certStatusesRequiringOncRole = Stream.of(
+                CertificationStatusType.SuspendedByOnc.getName(),
+                CertificationStatusType.TerminatedByOnc.getName()).collect(Collectors.toList());
     }
 
     @Override
-    public void review(final CertifiedProductSearchDetails existingListing,
-            final CertifiedProductSearchDetails updatedListing) {
-        if (existingListing.getCurrentStatus() != null
-                && updatedListing.getCurrentStatus() != null
-                && !existingListing.getCurrentStatus().getStatus().getId()
-                        .equals(updatedListing.getCurrentStatus().getStatus().getId())) {
-            // if the status is to or from suspended by onc make sure the user
-            // has admin
-            if ((existingListing.getCurrentStatus().getStatus().getName()
-                    .equals(CertificationStatusType.SuspendedByOnc.toString())
-                    || updatedListing.getCurrentStatus().getStatus().getName()
-                            .equals(CertificationStatusType.SuspendedByOnc.toString())
-                    || existingListing.getCurrentStatus().getStatus().getName()
-                            .equals(CertificationStatusType.TerminatedByOnc.toString())
-                    || updatedListing.getCurrentStatus().getStatus().getName()
-                            .equals(CertificationStatusType.TerminatedByOnc.toString()))
-                    && !resourcePermissions.isUserRoleOnc()
-                    && !resourcePermissions.isUserRoleAdmin()) {
-                updatedListing.addBusinessErrorMessage(msgUtil.getMessage("listing.certStatusChange.notAllowed",
-                        AuthUtil.getUsername(),
-                        existingListing.getChplProductNumber(),
-                        existingListing.getCurrentStatus().getStatus().getName(),
-                        updatedListing.getCurrentStatus().getStatus().getName()));
-            }
+    public void review(CertifiedProductSearchDetails existingListing,
+            CertifiedProductSearchDetails updatedListing) {
+        List<CertificationStatusEvent> addedCertificationStatusEvents = getAddedCertificationStatusEvents(existingListing, updatedListing);
+        List<CertificationStatusEvent> removedCertificationStatusEvents = getRemovedCertificationStatusEvents(existingListing, updatedListing);
+        Set<CertificationStatusEvent> modifiedCertificationStatusEvents = Stream.concat(
+                addedCertificationStatusEvents.stream(),
+                removedCertificationStatusEvents.stream())
+                .collect(Collectors.toSet());
+        Set<String> modifiedCertificationStatusesRequiringOncRole = modifiedCertificationStatusEvents.stream()
+                .filter(cse -> certStatusesRequiringOncRole.contains(cse.getStatus().getName()))
+                .map(cse -> cse.getStatus().getName())
+                .collect(Collectors.toSet());
+
+        //A non-ONC/ADMIN user should not be allowed to muck with the 'By ONC' status events
+        if (!resourcePermissions.isUserRoleOnc()
+                && !resourcePermissions.isUserRoleAdmin()
+                && !CollectionUtils.isEmpty(modifiedCertificationStatusesRequiringOncRole)) {
+            updatedListing.addBusinessErrorMessage(msgUtil.getMessage("listing.certStatusChange.notAllowed",
+                    AuthUtil.getUsername(),
+                    modifiedCertificationStatusesRequiringOncRole.size() > 1 ? "es" : "",
+                    Util.joinListGrammatically(modifiedCertificationStatusesRequiringOncRole.stream().toList())));
         }
+    }
+
+    private List<CertificationStatusEvent> getAddedCertificationStatusEvents(CertifiedProductSearchDetails existingListing,
+            CertifiedProductSearchDetails updatedListing) {
+        return subtractLists(updatedListing.getCertificationEvents(), existingListing.getCertificationEvents());
+    }
+
+    private List<CertificationStatusEvent> getRemovedCertificationStatusEvents(CertifiedProductSearchDetails existingListing,
+            CertifiedProductSearchDetails updatedListing) {
+        return subtractLists(existingListing.getCertificationEvents(), updatedListing.getCertificationEvents());
+    }
+
+    private List<CertificationStatusEvent> subtractLists(List<CertificationStatusEvent> listA, List<CertificationStatusEvent> listB) {
+        Predicate<CertificationStatusEvent> notInListB = eventFromA -> !listB.stream()
+                .anyMatch(event -> doValuesMatch(eventFromA, event));
+
+        return listA.stream()
+                .filter(notInListB)
+                .collect(Collectors.toList());
+    }
+
+    private boolean doValuesMatch(CertificationStatusEvent event1, CertificationStatusEvent event2) {
+        return event1.getStatus().getName().equals(event2.getStatus().getName())
+                && event1.getEventDay().equals(event2.getEventDay())
+                && StringUtils.equalsIgnoreCase(event1.getReason(), event2.getReason());
     }
 }
