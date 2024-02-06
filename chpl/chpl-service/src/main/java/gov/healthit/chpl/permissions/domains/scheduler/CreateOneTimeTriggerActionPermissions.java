6e245fe239730472dd4ab27043a320b98b59a978
Todd Young <tyoung@ainq.com>
2024-02-05 19:25:57 -0500

    feat: add some logging for debug on DEV
    OCD-4379

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/scheduler/CreateOneTimeTriggerActionPermissions.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/permissions/domains/scheduler/CreateOneTimeTriggerActionPermissions.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 import gov.healthit.chpl.domain.schedule.ChplOneTimeTrigger;
 import gov.healthit.chpl.permissions.domains.ActionPermissions;
 import gov.healthit.chpl.scheduler.job.DirectReviewCacheRefreshJob;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Component(value = "schedulerCreateOneTimeTriggerActionPermissions")
 public class CreateOneTimeTriggerActionPermissions extends ActionPermissions {
 
@@ -16,17 +18,23 @@ public class CreateOneTimeTriggerActionPermissions extends ActionPermissions {
 
     @Override
     public boolean hasAccess(Object obj) {
+        LOGGER.info("ResourcePermissions type {}", getResourcePermissions().getClass().toString());
         if (getResourcePermissions().isUserRoleAdmin()) {
+            LOGGER.info("User is Admin");
             return true;
         } else if (getResourcePermissions().isUserRoleStartup()
                 && obj instanceof ChplOneTimeTrigger) {
+            LOGGER.info("User is Startup");
             ChplOneTimeTrigger trigger = (ChplOneTimeTrigger) obj;
             if (trigger.getJob() != null
                     && DirectReviewCacheRefreshJob.JOB_NAME.equals(trigger.getJob().getName())
                     && DirectReviewCacheRefreshJob.JOB_GROUP.equals(trigger.getJob().getGroup())) {
+                LOGGER.info("Job information lines up...");
+                LOGGER.info("Return true");
                 return true;
             }
         }
+        LOGGER.info("Return false");
         return false;
     }
 
