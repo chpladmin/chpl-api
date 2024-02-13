63f2b3708dbc3f15db26aa6abdba92823365f2a6
Katy Ekey <katy.ekey@gmail.com>
2024-02-13 11:09:58 -0500

    feat!: Add searchTermRecordCount to search API response
    [#OCD-4339]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/search/domain/ListingSearchResponse.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/search/domain/ListingSearchResponse.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +14,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ListingSearchResponse implements Serializable {
     private static final long serialVersionUID = 5130424471765198329L;
+    @Schema(description = "The total number of records in the system matching all filters in the search request.")
     private Integer recordCount;
+    @Schema(description = "The total number of records in the system matching the 'searchTerm' filter only. "
+            + "If the search request has no 'searchTerm' filter supplied this value is not applicable to the response and will be null.")
+    private Integer searchTermRecordCount;
+    @Schema(description = "The maximum number of records returned for this page of search results.")
     private Integer pageSize;
+    @Schema(description = "The page number for this set of search results, 0-based.")
     private Integer pageNumber;
+    @Schema(description = "Whether or not direct reviews are available. "
+            + "This dictates whether non-conformity filters can be expected to have correct results.")
     private Boolean directReviewsAvailable;
+    @Schema(description = "The set of records matching all filters and paging parameters in the search request.")
     private List<ListingSearchResult> results = new ArrayList<ListingSearchResult>();
 }
