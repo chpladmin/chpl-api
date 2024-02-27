e27d3e48e7ed2bb1f491b1b1ac4d4b829c59895d
Todd Young <tyoung@ainq.com>
2024-02-27 15:07:59 -0500

    feat: update titles in spreadhseet to match criteria
    OCD-4438

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/updatedcriteriastatusreport/UpdatedCriteriaStatusReportSheet.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/updatedcriteriastatusreport/UpdatedCriteriaStatusReportSheet.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellUtil;
+import org.apache.poi.xssf.usermodel.XSSFChart;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.certificationCriteria.CertificationCriterion;
@@ -22,6 +25,8 @@ public class UpdatedCriteriaStatusReportSheet {
     private static final Integer STANDARDS_UP_TP_DATE_ROW_IDX = 5;
     private static final Integer LISTING_COUNT_ROW_IDX = 6;
 
+    private static final Integer DESCRIPTIONS_COL_IDX = 0;
+
     private UpdatedCriteriaStatusReportDAO updatedCriteriaStatusReportDAO;
 
     public UpdatedCriteriaStatusReportSheet(UpdatedCriteriaStatusReportDAO updatedCriteriaStatusReportDAO) {
@@ -31,6 +36,9 @@ public class UpdatedCriteriaStatusReportSheet {
     public void generateSheetForCriteria(CertificationCriterion criterion, Workbook workbook) {
         Sheet sheet = addWorksheetForCriteria(criterion, workbook);
         LocalDate reportDate = updatedCriteriaStatusReportDAO.getMaxReportDate();
+
+        CellUtil.getCell(CellUtil.getRow(DATE_ROW_IDX, sheet), DESCRIPTIONS_COL_IDX).setCellValue(criterion.getNumber() + " Up-to-Date Progress");
+        updateChartTitle(sheet, criterion);
 
         for (int i = TOTAL_NUMBER_OF_MONTHS; i >= 1; --i) {
             UpdatedCriteriaStatusReport report = getDataFromOnOrAroundDate(reportDate, criterion);
@@ -43,6 +51,16 @@ public class UpdatedCriteriaStatusReportSheet {
             CellUtil.getCell(CellUtil.getRow(LISTING_COUNT_ROW_IDX, sheet), i).setCellValue(report.getListingsWithCriterionCount());
 
             reportDate = reportDate.minusMonths(1);
+        }
+    }
+
+    private void updateChartTitle(Sheet sheet, CertificationCriterion criterion) {
+        XSSFDrawing drawing = ((XSSFSheet) sheet).getDrawingPatriarch();
+        if (drawing != null) {
+            List<XSSFChart> charts = drawing.getCharts();
+            if (charts != null && charts.size() > 0) {
+                charts.get(0).setTitleText(criterion.getNumber() + " Up-to-Date Progress");
+            }
         }
     }
 
