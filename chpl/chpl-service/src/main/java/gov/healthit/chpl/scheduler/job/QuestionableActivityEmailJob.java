1d36947cc0a7467f2e1172e6188e1345e5eab274
Todd Young <tyoung@ainq.com>
2024-03-14 14:20:22 -0400

    feat: display the removed code set questionable activity in report
    OCD-4501

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/QuestionableActivityEmailJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/QuestionableActivityEmailJob.java
@@ -546,6 +546,8 @@ public class QuestionableActivityEmailJob extends QuartzJob {
             currRow.set(ACTIVITY_DESCRIPTION_COL, activity.getBefore() + " updated to " + activity.getAfter());
         } else if (activity.getTrigger().getName().equals(QuestionableActivityTriggerConcept.CURES_UPDATE_REMOVED.getName())) {
             currRow.set(ACTIVITY_DESCRIPTION_COL, activity.getBefore() + " updated to " + activity.getAfter());
+        } else if (activity.getTrigger().getName().equals(QuestionableActivityTriggerConcept.REMOVED_CODE_SET.getName())) {
+            currRow.set(ACTIVITY_DESCRIPTION_COL, "Removed " + activity.getBefore());
         }
         currRow.set(ACTIVITY_REASON_COL, activity.getReason());
     }
