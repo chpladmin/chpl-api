c8b5de59db399e3b911e4a2fca41f08d948e6d29
Todd Young <tyoung@ainq.com>
2024-04-19 11:13:30 -0400

    feat: add phone number to the Cognito user object
    OCD-4494

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/user/cognito/CognitoApiWrapper.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/user/cognito/CognitoApiWrapper.java
@@ -291,14 +291,12 @@ public class CognitoApiWrapper {
         user.setFullName(getUserAttribute(userType.attributes(), "name").value());
         user.setEmail(getUserAttribute(userType.attributes(), "email").value());
         user.setTitle(getUserAttribute(userType.attributes(), "custom:title").value());
+        user.setPhoneNumber(getUserAttribute(userType.attributes(), "phone_number").value());
         user.setAccountLocked(!userType.enabled());
         user.setAccountEnabled(userType.enabled());
-        //user.setCredentialsExpired(userType.userStatus() == UserStatusType.);
         user.setStatus(userType.userStatusAsString());
-
-
         user.setPasswordResetRequired(userType.userStatus().equals(UserStatusType.RESET_REQUIRED));
-        //user.setLastLoggedInDate(new Date());
+
         AdminListGroupsForUserRequest groupsRequest = AdminListGroupsForUserRequest.builder()
                 .userPoolId(userPoolId)
                 .username(user.getEmail())
