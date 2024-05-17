c6ae699671bb3cff5a272affe87d60ee337aa7f3
Todd Young <tyoung@ainq.com>
2024-05-17 11:18:12 -0400

    fix: update HttpSecurity to support chpl and ff4j urls
    OCD-4237

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/CHPLHttpSecurityConfig.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/CHPLHttpSecurityConfig.java
@@ -1,16 +1,24 @@
 package gov.healthit.chpl;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import gov.healthit.chpl.api.ApiKeyManager;
 import gov.healthit.chpl.auth.authentication.JWTUserConverterFacade;
@@ -48,208 +56,54 @@ public class CHPLHttpSecurityConfig {
     private JWTUserConverterFacade userConverterFacade;
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain configureFf4j(HttpSecurity http) throws Exception {
         LOGGER.info("Configure CHPL Security");
-        return http.csrf(csrf -> csrf.disable())
+        return http.securityMatcher(new AntPathRequestMatcher(ff4jWebConsoleUrl + "/**"))
+                .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                //.authorizeHttpRequests(request -> request
-                //        .requestMatchers("/favicon.ico").permitAll()
-                //        .requestMatchers("/resources/**").permitAll()
-                //        .requestMatchers("/").permitAll())
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest()
+                        .hasAnyRole(FF4J_ROLE))
+                .httpBasic(withDefaults())
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain configureApi(HttpSecurity http) throws Exception {
+        LOGGER.info("Configure CHPL Security");
+        return http.securityMatcher(new AntPathRequestMatcher("/**"))
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                       .requestMatchers(new AntPathRequestMatcher("/**"))
+                       .permitAll())
                 .addFilterBefore(apiKeyAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new JWTAuthenticationFilter(userConverterFacade), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .headers(header -> header.cacheControl(ctrl -> ctrl.disable()))
                 .build();
     }
 
     @Bean
-    public APIKeyAuthenticationFilter apiKeyAuthenticationFilter() {
-        LOGGER.info("get APIKeyAuthenticationFilter");
-        return new APIKeyAuthenticationFilter(apiKeyManager);
-    }
-}
-
-    /*
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        LOGGER.info("Configure CHPL Security");
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()   --Done
-
-        .exceptionHandling().and().anonymous().and().servletApi().and()
-        // .headers().cacheControl().and()
-        .authorizeRequests().antMatchers("/favicon.ico").permitAll().antMatchers("/resources/**").permitAll()
-
-        // allow anonymous resource requests
-        .antMatchers("/").permitAll().and()
-        .addFilterBefore(apiKeyAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-        // custom Token based authentication based on the header previously given to the client
-        .addFilterBefore(new JWTAuthenticationFilter(userConverterFacade), UsernamePasswordAuthenticationFilter.class)
-        .headers().cacheControl();
-    }
-    */
-    /*
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-        LOGGER.info("configure FF4J Security for urls like " + ff4jWebConsoleUrl);
-        http.authorizeHttpRequests(request -> request.requestMatchers(ff4jWebConsoleUrl + "/**")
-                    .hasAnyRole(FF4J_ROLE))
-            .csrf(AbstractHttpConfigurer::disable)
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .build();
-            //        .hasAnyAuthority(FF4J_ROLE)
-            //        .and()
-            //        .httpBasic()
-            //        .and()
-            //        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-
-
-
-
-            //. (ff4jWebConsoleUrl + "/**")
-            //.authorizeRequests().anyRequest().hasRole(FF4J_ROLE)
-            //.and().httpBasic()
-            //.and().sessionManagement()
-            //    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-
-        // Configure AuthenticationManagerBuilder
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-        // Get AuthenticationManager
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
-        http
-                .cors(withDefaults())
-                .csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((authz) -> authz
-                .requestMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
-                .anyRequest().authenticated())
-
-                .authenticationManager(authenticationManager)
-                .addFilter(authenticationFilter)
-                .addFilter(new AuthorizationFilter(authenticationManager))
-                .sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username(ff4jUsername)
-            .password(ff4jPassword)
-            .roles(FF4J_ROLE)
-            .build();
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username(ff4jUsername)
+                .password("{noop}" + ff4jPassword)
+                .roles(FF4J_ROLE)
+                .build();
         return new InMemoryUserDetailsManager(user);
     }
 
-    protected AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
-        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager);
-        filter.setFilterProcessesUrl("/users/login");
-        return filter;
+    private APIKeyAuthenticationFilter apiKeyAuthenticationFilter() {
+        LOGGER.info("get APIKeyAuthenticationFilter");
+        return new APIKeyAuthenticationFilter(apiKeyManager);
+    }
+
+    private JWTAuthenticationFilter jwtAuthenticationFilter() {
+        LOGGER.info("get JWTAuthenticationFilter");
+        return new JWTAuthenticationFilter(userConverterFacade);
     }
 }
-/*
-public class CHPLHttpSecurityConfig extends WebSecurityConfigurerAdapter {
-    private static final String FF4J_ROLE = "ff4jUser";
-
-    @Configuration
-    @Order(1)
-    public static class FF4JSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Value("${ff4j.webconsole.username}")
-        private String ff4jUsername;
-
-        @Value("${ff4j.webconsole.password}")
-        private String ff4jPassword;
-
-        @Value("${ff4j.webconsole.url}")
-        private String ff4jWebConsoleUrl;
-
-        @Autowired
-        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-            auth.inMemoryAuthentication()
-            .withUser(ff4jUsername).password("{bcrypt}" + passwordEncoder().encode(ff4jPassword)).roles(FF4J_ROLE);
-        }
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            LOGGER.info("configure AuthenticationManagerBuilder");
-            auth.inMemoryAuthentication()
-                .withUser(ff4jUsername).password("{bcrypt}" + passwordEncoder().encode(ff4jPassword)).roles(FF4J_ROLE);
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            LOGGER.info("configure FF4J Security for urls like " + ff4jWebConsoleUrl);
-            http
-                .csrf().disable()
-                .antMatcher(ff4jWebConsoleUrl + "/**")
-                .authorizeRequests().anyRequest().hasRole(FF4J_ROLE)
-                .and().httpBasic()
-                .and().sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        }
-
-        @Bean
-        public BCryptPasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-    }
-
-    @Configuration
-    @Order(2)
-    public static class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        public JwtSecurityConfig() {
-            super(true);
-        }
-
-        @Autowired
-        private JWTUserConverterFacade userConverterFacade;
-
-        @Autowired
-        private ObjectFactory<ApiKeyManager> apiKeyManagerObjectFactory;
-
-        @Bean
-        public APIKeyAuthenticationFilter apiKeyAuthenticationFilter() {
-            LOGGER.info("get APIKeyAuthenticationFilter");
-            ApiKeyManager apiKeyManager = this.apiKeyManagerObjectFactory.getObject();
-            return new APIKeyAuthenticationFilter(apiKeyManager);
-        }
-
-        @Bean
-        @Override
-        public AuthenticationManager authenticationManagerBean() throws Exception {
-            LOGGER.info("Get AuthenticationManager");
-            return super.authenticationManagerBean();
-        }
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            LOGGER.info("Configure AuthenticationManagerBuilder");
-            auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin")
-            .password("password").roles("USER", "ADMIN");
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            LOGGER.info("Configure CHPL Security");
-            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-
-            .exceptionHandling().and().anonymous().and().servletApi().and()
-            // .headers().cacheControl().and()
-            .authorizeRequests().antMatchers("/favicon.ico").permitAll().antMatchers("/resources/**").permitAll()
-
-            // allow anonymous resource requests
-            .antMatchers("/").permitAll().and()
-            .addFilterBefore(apiKeyAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-            // custom Token based authentication based on the header previously given to the client
-            .addFilterBefore(new JWTAuthenticationFilter(userConverterFacade), UsernamePasswordAuthenticationFilter.class)
-            .headers().cacheControl();
-        }
-    }
-}
-*/
