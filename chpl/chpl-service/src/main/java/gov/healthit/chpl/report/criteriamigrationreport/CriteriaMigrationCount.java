e9d550b6f6045a2d14ddc44c9f1d640e86337826
Todd Young <tyoung@ainq.com>
2024-06-05 13:35:15 -0400

    docs: added json schema definitions
    OCD-4597

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/report/criteriamigrationreport/CriteriaMigrationCount.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/report/criteriamigrationreport/CriteriaMigrationCount.java
@@ -8,12 +8,16 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import gov.healthit.chpl.util.LocalDateDeserializer;
 import gov.healthit.chpl.util.LocalDateSerializer;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Schema(description = "Defines the counts for a CriteriaMigrationDefinition on a particular date")
 public class CriteriaMigrationCount {
+
+    @Schema(description = "Internal ID for instance of CriteriaMigrationCount")
     private Long id;
 
     @JsonIgnore
@@ -21,9 +25,15 @@ public class CriteriaMigrationCount {
 
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
+    @Schema(description = "Date which the counts were calculated")
     private LocalDate reportDate;
 
+    @Schema(description = "Number of listings attesting the original criteria as of the reportDate")
     private Integer originalCriterionCount;
+
+    @Schema(description = "Number of listings attesting the original criteria as of the reportDate")
     private Integer updatedCriterionCount;
+
+    @Schema(description = "Number of listings where the original criteria was attested to, then unattested to, and trhe updated criteria has been attested to")
     private Integer originalToUpdatedCriterionCount;
 }
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/report/criteriamigrationreport/CriteriaMigrationReport.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/report/criteriamigrationreport/CriteriaMigrationReport.java
@@ -8,17 +8,28 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import gov.healthit.chpl.util.LocalDateDeserializer;
 import gov.healthit.chpl.util.LocalDateSerializer;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Schema(description = "Defines the report and asssociated criteria migrations for the report")
 public class CriteriaMigrationReport {
+
+    @Schema(description = "Internal ID for instance of CriteriaMigrationReport")
     private Long id;
+
+    @Schema(description = "The name of the report used to group a series to criteria migrations")
     private String reportName;
 
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
+    @Schema(description = "The date used as the maximum date to look back to determine if a criteria was upgraded.  "
+            + "Typically, this date is something like the Cures implementation date or HTI-1 implemenation date")
     private LocalDate startDate;
+
+    @Schema(description = "Collection of CriteriaMigrationDefinition objects that define the criteria migrations "
+            + "associated with a report")
     private List<CriteriaMigrationDefinition> criteriaMigrationDefinitions;
 }
