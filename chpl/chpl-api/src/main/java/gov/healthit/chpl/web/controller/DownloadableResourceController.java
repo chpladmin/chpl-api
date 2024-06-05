a7310e5982c2e8204dc969a0b642955d5386e48a
Katy Ekey <katy.ekey@gmail.com>
2024-06-05 11:00:30 -0400

    feat!: Deprecate download/{listingType} for {listingType}/download
    The listing download endpoint had a URL inconsistent with all other
    download endpoints.
    
    [#OCD-4600]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/DownloadableResourceController.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/DownloadableResourceController.java
@@ -25,6 +25,7 @@ import gov.healthit.chpl.svap.manager.SvapManager;
 import gov.healthit.chpl.util.ErrorMessageUtil;
 import gov.healthit.chpl.util.FileUtils;
 import gov.healthit.chpl.util.SwaggerSecurityRequirement;
+import gov.healthit.chpl.web.controller.annotation.DeprecatedApi;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -71,7 +72,25 @@ public class DownloadableResourceController {
             security = {
                     @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
             })
+    @Deprecated
+    @DeprecatedApi(friendlyUrl = "/download/{listingType}", httpMethod = "GET",
+        message = "This endpoing is deprecated and will be removed. Please use '{listingType}/download'.",
+        removalDate = "2024-12-31")
     @RequestMapping(value = "/download/{listingType:2011|2014|active|inactive}", method = RequestMethod.GET, produces = "text/csv")
+    public void downloadListingsDeprecated(@PathVariable(value = "listingType", required = true) String listingType,
+            @RequestParam(value = "format", defaultValue = "csv", required = false) String formatInput,
+            @RequestParam(value = "definition", defaultValue = "false", required = false) Boolean isDefinition,
+            HttpServletRequest request, HttpServletResponse response) throws IOException, InvalidArgumentsException {
+        downloadListings(listingType, formatInput, isDefinition, request, response);
+    }
+
+    @Operation(summary = "Download all listings of a given type in the specified format.",
+            description = "Valid values for 'listingType' are active, inactive, 2011 and 2014."
+                    + "Valid values for 'format' are csv and json.",
+            security = {
+                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
+            })
+    @RequestMapping(value = "/{listingType:2011|2014|active|inactive}/download", method = RequestMethod.GET, produces = "text/csv")
     public void downloadListings(@PathVariable(value = "listingType", required = true) String listingType,
             @RequestParam(value = "format", defaultValue = "csv", required = false) String formatInput,
             @RequestParam(value = "definition", defaultValue = "false", required = false) Boolean isDefinition,
