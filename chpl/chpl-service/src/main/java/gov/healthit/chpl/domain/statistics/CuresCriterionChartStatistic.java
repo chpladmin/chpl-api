2731af4bef0e4519db7d5e7c9b7e2440a20efac2
Todd Young <tyoung@ainq.com>
2024-06-18 10:08:15 -0400

    feat!: Add endpoint to retrieve (b)(6) to (b)(10) over time report

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/statistics/CuresCriterionChartStatistic.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/statistics/CuresCriterionChartStatistic.java
@@ -1,6 +1,13 @@
 package gov.healthit.chpl.domain.statistics;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import gov.healthit.chpl.certificationCriteria.CertificationCriterion;
+import gov.healthit.chpl.util.LocalDateDeserializer;
+import gov.healthit.chpl.util.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +23,8 @@ public class CuresCriterionChartStatistic {
     private Long newCertificationCount;
     private Long listingCount;
     private Long requiresUpdateCount;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate reportDate;
 }
