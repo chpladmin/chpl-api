1b2228198b80b51f68eb7779a25ed005fb07c89b
Todd Young <tyoung@ainq.com>
2024-07-08 09:42:02 -0400

    refactor: replace magic numbers with variables
    OCD-4602

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/PasswordGenerator.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/PasswordGenerator.java
@@ -10,11 +10,18 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class PasswordGenerator {
+    private static final int MIN_UPPER_CASE_LETTER = 65;
+    private static final int MAX_UPPER_CASE_LETTER = 65;
+    private static final int MIN_LOWER_CASE_LETTER = 97;
+    private static final int MAX_LOWER_CASE_LETTER = 122;
+    private static final int MIN_SPECIAL_CHAR = 33;
+    private static final int MAX_SPECIAL_CHAR = 47;
+
     public final String generate() {
-        String upperCaseLetters = RandomStringUtils.random(2, 65, 90, true, true);
-        String lowerCaseLetters = RandomStringUtils.random(2, 97, 122, true, true);
+        String upperCaseLetters = RandomStringUtils.random(2, MIN_UPPER_CASE_LETTER, MAX_UPPER_CASE_LETTER, true, true);
+        String lowerCaseLetters = RandomStringUtils.random(2, MIN_LOWER_CASE_LETTER, MAX_LOWER_CASE_LETTER, true, true);
         String numbers = RandomStringUtils.randomNumeric(2);
-        String specialChar = RandomStringUtils.random(2, 33, 47, false, false);
+        String specialChar = RandomStringUtils.random(2, MIN_SPECIAL_CHAR, MAX_SPECIAL_CHAR, false, false);
         String totalChars = RandomStringUtils.randomAlphanumeric(2);
         String combinedChars = upperCaseLetters.concat(lowerCaseLetters)
           .concat(numbers)
