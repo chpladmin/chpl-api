1a43cf9febcf08b246644fcde85a95d411d07850
Katy Ekey <katy.ekey@gmail.com>
2024-07-17 11:22:48 -0400

    feat!: Remove Cures Update Report and report-data/cures-update-report
    Breaking Change
    
    [#OCD-4625]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/ReportDataController.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/ReportDataController.java
@@ -1,14 +1,11 @@
 package gov.healthit.chpl.web.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.healthit.chpl.domain.statistics.CuresCriterionChartStatistic;
 import gov.healthit.chpl.report.ReportDataManager;
 import gov.healthit.chpl.report.criteriamigrationreport.CriteriaMigrationReport;
 import gov.healthit.chpl.util.SwaggerSecurityRequirement;
@@ -25,16 +22,6 @@ public class ReportDataController {
     @Autowired
     public ReportDataController(ReportDataManager reportDataManager) {
         this.reportDataManager = reportDataManager;
-    }
-
-    @Operation(summary = "Retrieves the data used to generate the Cures Update Report.",
-            description = "Retrieves the data used to generate the Cures Update Report.",
-            security = {
-                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
-            })
-    @RequestMapping(value = "/cures-update-report", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public @ResponseBody List<CuresCriterionChartStatistic> getCuresUpdateReportData() {
-        return reportDataManager.getCuresUpdateReportData();
     }
 
     @Operation(summary = "Retrieves the data used to generate the HTI-1 Criteria Migration Report.",
