36dd31685e50f3809545087ae51bfc424423da4d
Todd Young <tyoung@ainq.com>
2024-08-28 21:28:21 -0400

    feat: add endpoints for Surveillance Stats report: surveillance and non-conformity
    OCD-4668

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/ReportDataController.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/ReportDataController.java
@@ -1,5 +1,6 @@
 package gov.healthit.chpl.web.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import gov.healthit.chpl.report.ReportDataManager;
 import gov.healthit.chpl.report.criteriamigrationreport.CriteriaMigrationReport;
+import gov.healthit.chpl.report.surveillance.NonconformityCounts;
+import gov.healthit.chpl.report.surveillance.SurveillanceActivityCounts;
+import gov.healthit.chpl.scheduler.job.summarystatistics.data.CertificationBodyStatistic;
+import gov.healthit.chpl.search.domain.ListingSearchResult;
 import gov.healthit.chpl.util.SwaggerSecurityRequirement;
 import gov.healthit.chpl.web.controller.results.ReportUrlResult;
 import io.swagger.v3.oas.annotations.Operation;
@@ -56,6 +61,66 @@ public class ReportDataController {
         return ReportUrlResult.builder()
                 .reportUrl(reportUrl)
                 .build();
+    }
+
+    @Operation(summary = "Retrieves the data used to generate the Surveillance Activity Counts report.",
+            description = "Retrieves the data used to generate the Surveillance Activity Counts report.",
+            security = {
+                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
+            })
+    @RequestMapping(value = "/surveillance-activity-counts", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public @ResponseBody SurveillanceActivityCounts getSurveillanceActivityCounts() {
+        return reportDataManager.getSurveillanceActivityCounts();
+    }
+
+    @Operation(summary = "Retrieves the data used to generate the Open Surveillance Activity Counts by ACB report.",
+            description = "Retrieves the data used to generate the Open Surveillance Activity Counts by ACB report.",
+            security = {
+                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
+            })
+    @RequestMapping(value = "/open-surveillance-activity-counts-by-acb", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public @ResponseBody List<CertificationBodyStatistic> getOpenSurveillanceActivityCountsByAcb() {
+        return reportDataManager.getOpenSurveillanceActivityCountsByAcb();
+    }
+
+    @Operation(summary = "Retrieves the data used to generate the Listings with Open Surveillance report.",
+            description = "Retrieves the data used to generate the Listings with Open Surveillance report.",
+            security = {
+                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
+            })
+    @RequestMapping(value = "/open-surveillance-listings", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public @ResponseBody List<ListingSearchResult> getListingsWithOpenSurveillance() {
+        return reportDataManager.getListingsWithOpenSurveillance();
+    }
+
+    @Operation(summary = "Retrieves the data used to generate the Non-conformity Counts report.",
+            description = "Retrieves the data used to generate the Non-conformity Counts report.",
+            security = {
+                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
+            })
+    @RequestMapping(value = "/non-conformity-counts", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public @ResponseBody NonconformityCounts getNonconformityCounts() {
+        return reportDataManager.getNonconformityCounts();
+    }
+
+    @Operation(summary = "Retrieves the data used to generate the Open Non-conformity Counts by ACB report.",
+            description = "Retrieves the data used to generate the Open Non-conformity Counts by ACB report.",
+            security = {
+                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
+            })
+    @RequestMapping(value = "/open-non-conformity-counts-by-acb", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public @ResponseBody List<CertificationBodyStatistic> getOpenNonconformityCountsByAcb() {
+        return reportDataManager.getOpenNonconformityCountsByAcb();
+    }
+
+    @Operation(summary = "Retrieves the data used to generate the Listings with Open Non-conformity report.",
+            description = "Retrieves the data used to generate the Listings with Open Non-conformity report.",
+            security = {
+                    @SecurityRequirement(name = SwaggerSecurityRequirement.API_KEY)
+            })
+    @RequestMapping(value = "/open-non-conformity-listings", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public @ResponseBody List<ListingSearchResult> getListingsWithOpenNonconformity() {
+        return reportDataManager.getListingsWithOpenNonconformity();
     }
 
 }
--- chpl/chpl-service/src/main/java/gov/healthit/chpl/report/ReportDataManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/report/ReportDataManager.java
@@ -1,10 +1,17 @@
 package gov.healthit.chpl.report;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import gov.healthit.chpl.report.criteriamigrationreport.CriteriaMigrationReport;
 import gov.healthit.chpl.report.criteriamigrationreport.CriteriaMigrationReportService;
+import gov.healthit.chpl.report.surveillance.NonconformityCounts;
+import gov.healthit.chpl.report.surveillance.SurveillanceActivityCounts;
+import gov.healthit.chpl.report.surveillance.SurveillanceReportsService;
+import gov.healthit.chpl.scheduler.job.summarystatistics.data.CertificationBodyStatistic;
+import gov.healthit.chpl.search.domain.ListingSearchResult;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -12,15 +19,40 @@ import lombok.extern.log4j.Log4j2;
 public class ReportDataManager {
 
     private CriteriaMigrationReportService criteriaMigrationReportService;
+    private SurveillanceReportsService surveillanceReportsService;
 
     @Autowired
-    public ReportDataManager(CriteriaMigrationReportService criteriaMigrationReportService) {
+    public ReportDataManager(CriteriaMigrationReportService criteriaMigrationReportService, SurveillanceReportsService surveillanceReportsService) {
         this.criteriaMigrationReportService = criteriaMigrationReportService;
+        this.surveillanceReportsService = surveillanceReportsService;
     }
-
 
     public CriteriaMigrationReport getHti1CriteriaMigrationReport() {
         return criteriaMigrationReportService.getReport(CriteriaMigrationReportService.HTI1_REPORT_ID);
+    }
+
+    public SurveillanceActivityCounts getSurveillanceActivityCounts() {
+        return surveillanceReportsService.getSurveiilanceActivityCounts();
+    }
+
+    public List<CertificationBodyStatistic> getOpenSurveillanceActivityCountsByAcb() {
+        return surveillanceReportsService.getOpenSurveillanceActivityCountsByAcb();
+    }
+
+    public List<ListingSearchResult> getListingsWithOpenSurveillance() {
+        return surveillanceReportsService.getListingsWithOpenSurveillance();
+    }
+
+    public NonconformityCounts getNonconformityCounts() {
+        return surveillanceReportsService.getNonconformityCounts();
+    }
+
+    public List<CertificationBodyStatistic> getOpenNonconformityCountsByAcb() {
+        return surveillanceReportsService.getOpenNonconformityCountsByAcb();
+    }
+
+    public List<ListingSearchResult> getListingsWithOpenNonconformity() {
+        return surveillanceReportsService.getListingsWithOpenNonconformity();
     }
 }
 
