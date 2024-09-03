292f57bc2e223012751df1d3d9df70e4922dbe4d
Katy Ekey <katy.ekey@gmail.com>
2024-09-03 13:25:25 -0400

    feat!: Create one-time job that updates SED friendly IDs
    The IDs should be updated according to what was initially put into the
    upload files.
    
    [#OCD-4662]

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/sed/ReprocessFromUploadedCsvStrategy.java
@@ -0,0 +1,316 @@
+package gov.healthit.chpl.scheduler.job.onetime.sed;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import gov.healthit.chpl.dao.impl.BaseDAOImpl;
+import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.domain.ListingUpload;
+import gov.healthit.chpl.domain.TestParticipant;
+import gov.healthit.chpl.domain.TestTask;
+import gov.healthit.chpl.upload.listing.ListingUploadEntity;
+import gov.healthit.chpl.upload.listing.ListingUploadHandlerUtil;
+import gov.healthit.chpl.upload.listing.handler.ListingDetailsUploadHandler;
+import gov.healthit.chpl.upload.listing.normalizer.ListingDetailsNormalizer;
+import gov.healthit.chpl.validation.listing.Edition2015ListingValidator;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Component
+@Log4j2(topic = "updatedSedFriendlyIdsJobLogger")
+public class ReprocessFromUploadedCsvStrategy {
+    private ProcessedListingUploadManager processedListingUploadManager;
+
+    @Autowired
+    public ReprocessFromUploadedCsvStrategy(ProcessedListingUploadManager processedListingUploadManager) {
+        this.processedListingUploadManager = processedListingUploadManager;
+    }
+
+    public boolean updateTasks(CertifiedProductSearchDetails currentListing) {
+        CertifiedProductSearchDetails uploadedListing =
+                processedListingUploadManager.getUploadedDetailsByConfirmedCertifiedProductId(currentListing.getId());
+        if (hasSedErrors(uploadedListing)) {
+            printListingErrorsAndWarnings(uploadedListing);
+            return false;
+        }
+
+        int numTasksUpdated = 0;
+        Iterator<TestTask> currentTaskIter = currentListing.getSed().getTestTasks().iterator();
+        while (currentTaskIter.hasNext()) {
+            TestTask currentTestTask = currentTaskIter.next();
+            if (StringUtils.isEmpty(currentTestTask.getFriendlyId())) {
+                if (!hasAnyDuplicates(currentTestTask, currentListing.getSed().getTestTasks())) {
+                    Optional<TestTask> uploadedTestTask = findTestTask(currentTestTask, uploadedListing.getSed().getTestTasks());
+                    if (uploadedTestTask.isPresent()) {
+                        //if we are still processing, then this confirmed test task and this uploaded test task have the same values
+                        //TODO: We did not check associated criteria or participants - does that matter???
+                        LOGGER.info("Setting friendly ID to " + uploadedTestTask.get().getFriendlyId() + " for test task " + currentTestTask.getId());
+                        currentTestTask.setFriendlyId(uploadedTestTask.get().getFriendlyId());
+                        numTasksUpdated++;
+                    } else {
+                        LOGGER.warn("Test task with ID " + currentTestTask.getId() + " cannot be re-processed. No matching uploaded test task was found.");
+                    }
+                } else {
+                    LOGGER.warn("Test task with ID " + currentTestTask.getId() + " has the same data as at least one other test task in listing " + currentListing.getId() + ". Skipping.");
+                }
+            } else {
+                LOGGER.info("Test task with ID " + currentTestTask.getId() + " already has a friendly ID. Skipping.");
+            }
+        }
+        return numTasksUpdated > 0;
+    }
+
+    public boolean updateParticipants(CertifiedProductSearchDetails currentListing) {
+        CertifiedProductSearchDetails uploadedListing =
+                processedListingUploadManager.getUploadedDetailsByConfirmedCertifiedProductId(currentListing.getId());
+        if (hasSedErrors(uploadedListing)) {
+            printListingErrorsAndWarnings(uploadedListing);
+            return false;
+        }
+
+        int numParticipantsUpdated = 0;
+        List<TestParticipant> allCurrentParticipants = currentListing.getSed().getTestTasks().stream()
+                .flatMap(tt -> tt.getTestParticipants().stream())
+                .collect(Collectors.toList());
+
+        List<TestParticipant> uploadedTestParticipants = uploadedListing.getSed().getTestTasks().stream()
+                .flatMap(tt -> tt.getTestParticipants().stream())
+                .collect(Collectors.toList());
+
+        Iterator<TestParticipant> currentParticipantsIter = allCurrentParticipants.iterator();
+        while (currentParticipantsIter.hasNext()) {
+            TestParticipant currentParticipant = currentParticipantsIter.next();
+            if (StringUtils.isEmpty(currentParticipant.getFriendlyId())) {
+                if (!hasAnyDuplicates(currentParticipant, allCurrentParticipants)) {
+                    Optional<TestParticipant> uploadedTestParticipant = findTestParticipant(currentParticipant, uploadedTestParticipants);
+                    if (uploadedTestParticipant.isPresent()) {
+                        LOGGER.info("Setting friendly ID to " + uploadedTestParticipant.get().getFriendlyId() + " for test participant " + currentParticipant.getId());
+                        currentParticipant.setFriendlyId(uploadedTestParticipant.get().getFriendlyId());
+                        numParticipantsUpdated++;
+                    } else {
+                        LOGGER.warn("No test participant was found in the upload file matching " + currentParticipant.toString());
+                    }
+                } else {
+                    LOGGER.warn("Test participant with ID " + currentParticipant.getId() + " has the same data as at least one other participant in listing " + currentListing.getId() + ". Skipping.");
+                }
+            } else {
+                LOGGER.info("Test participant with ID " + currentParticipant.getId() + " already has a friendly ID. Skipping.");
+            }
+        }
+        return numParticipantsUpdated > 0;
+    }
+
+    private boolean hasSedErrors(CertifiedProductSearchDetails listing) {
+        //there are probably errors and warnings that weren't here originally -
+        //some criteria have been removed, we changed measure parsing, etc
+        return !CollectionUtils.isEmpty(listing.getErrorMessages().castToCollection())
+             && isAnyMessageAboutSed(listing.getErrorMessages().castToCollection());
+    }
+
+    private boolean isAnyMessageAboutSed(Collection<String> messages) {
+        return messages.stream()
+            .map(msg -> msg.toUpperCase())
+            .filter(upperCaseMsg -> upperCaseMsg.contains("SED")
+                    || upperCaseMsg.contains("TASK")
+                    || upperCaseMsg.contains("PARTICIPANT"))
+            .count() > 0;
+    }
+
+    private void printListingErrorsAndWarnings(CertifiedProductSearchDetails listing) {
+        LOGGER.info("Errors for listing: " + listing.getChplProductNumber() + " (confirmed listing " + listing.getId() + ")");
+        //there might be errors due to measure parsing since all of these files had the old measure columns...
+        if (CollectionUtils.isEmpty(listing.getErrorMessages().castToCollection())) {
+            LOGGER.info("\t0 errors.");
+        } else {
+            listing.getErrorMessages().stream()
+                .forEach(msg -> LOGGER.info("\t" + msg));
+        }
+
+        LOGGER.info("Warnings for listing: " + listing.getChplProductNumber() + " (confirmed listing " + listing.getId() + ")");
+        if (CollectionUtils.isEmpty(listing.getWarningMessages().castToCollection())) {
+            LOGGER.info("\t0 warnings.");
+        } else {
+            listing.getWarningMessages().stream()
+                .forEach(msg -> LOGGER.info("\t" + msg));
+        }
+    }
+
+    private List<TestTask> subtractTestTasks(Collection<TestTask> tasksFromA, Collection<TestTask> tasksFromB) {
+        Predicate<TestTask> notInListB = taskFromA -> !tasksFromB.stream()
+                .anyMatch(taskFromB -> testTaskMatches(taskFromA, taskFromB));
+
+        return tasksFromA.stream()
+                .filter(notInListB)
+                .collect(Collectors.toList());
+    }
+
+    private List<TestParticipant> subtractTestParticipants(Collection<TestParticipant> participantsFromA, Collection<TestParticipant> participantsFromB) {
+        Predicate<TestParticipant> notInListB = participantFromA -> !participantsFromB.stream()
+                .anyMatch(participantFromB -> testParticipantMatches(participantFromA, participantFromB));
+
+        return participantsFromA.stream()
+                .filter(notInListB)
+                .collect(Collectors.toList());
+    }
+
+    private boolean hasAnyDuplicates(TestTask task, List<TestTask> tasksToSearch) {
+        return tasksToSearch.stream()
+                .filter(currTask -> testTaskMatches(task, currTask))
+                .count() > 1;
+    }
+
+    private boolean hasAnyDuplicates(TestParticipant participant, List<TestParticipant> participantsToSearch) {
+        return participantsToSearch.stream()
+                .filter(currParticipant -> testParticipantMatches(participant, currParticipant))
+                .filter(matchedParticipant -> !matchedParticipant.getId().equals(participant.getId()))
+                .count() > 1;
+    }
+
+    private Optional<TestTask> findTestTask(TestTask task, List<TestTask> tasksToSearch) {
+        return tasksToSearch.stream()
+                .filter(currTask -> testTaskMatches(task, currTask))
+                .findAny();
+    }
+
+    private boolean testTaskMatches(TestTask testTask1, TestTask testTask2) {
+        boolean result = false;
+        if (StringUtils.equalsIgnoreCase(StringUtils.normalizeSpace(testTask1.getDescription()),
+                StringUtils.normalizeSpace(testTask2.getDescription()))
+                && Objects.equals(testTask1.getTaskErrors(), testTask2.getTaskErrors())
+                && Objects.equals(testTask1.getTaskErrorsStddev(), testTask2.getTaskErrorsStddev())
+                && Objects.equals(testTask1.getTaskPathDeviationObserved(), testTask2.getTaskPathDeviationObserved())
+                && Objects.equals(testTask1.getTaskPathDeviationOptimal(), testTask2.getTaskPathDeviationOptimal())
+                && Objects.equals(testTask1.getTaskRating(), testTask2.getTaskRating())
+                && StringUtils.equalsIgnoreCase(StringUtils.normalizeSpace(testTask1.getTaskRatingScale()),
+                        StringUtils.normalizeSpace(testTask2.getTaskRatingScale()))
+                && Objects.equals(testTask1.getTaskRatingStddev(), testTask2.getTaskRatingStddev())
+                && Objects.equals(testTask1.getTaskSuccessAverage(), testTask2.getTaskSuccessAverage())
+                && Objects.equals(testTask1.getTaskSuccessStddev(), testTask2.getTaskSuccessStddev())
+                && Objects.equals(testTask1.getTaskTimeAvg(), testTask2.getTaskTimeAvg())
+                && Objects.equals(testTask1.getTaskTimeDeviationObservedAvg(),
+                        testTask2.getTaskTimeDeviationObservedAvg())
+                && Objects.equals(testTask1.getTaskTimeDeviationOptimalAvg(),
+                        testTask2.getTaskTimeDeviationOptimalAvg())
+                && Objects.equals(testTask1.getTaskTimeStddev(), testTask2.getTaskTimeStddev())
+                && testTask1.getTestParticipants().size() == testTask2.getTestParticipants().size()) {
+            result = true;
+        }
+        return result;
+    }
+
+    private Optional<TestParticipant> findTestParticipant(TestParticipant participant, List<TestParticipant> participantsToSearch) {
+        return participantsToSearch.stream()
+                .filter(currParticipant -> testParticipantMatches(participant, currParticipant))
+                .findAny();
+    }
+
+    private boolean testParticipantMatches(TestParticipant testParticipant1, TestParticipant testParticipant2) {
+        boolean result = false;
+        if (StringUtils.equalsIgnoreCase(testParticipant1.getAge().getName(), testParticipant2.getAge().getName())
+                && Objects.equals(testParticipant1.getAge().getId(), testParticipant2.getAge().getId())
+                && StringUtils.equalsIgnoreCase(testParticipant1.getAssistiveTechnologyNeeds(),
+                        testParticipant2.getAssistiveTechnologyNeeds())
+                && Objects.equals(testParticipant1.getComputerExperienceMonths(),
+                        testParticipant2.getComputerExperienceMonths())
+                && StringUtils.equalsIgnoreCase(testParticipant1.getEducationType().getName(), testParticipant2.getEducationType().getName())
+                && Objects.equals(testParticipant1.getEducationType().getId(), testParticipant2.getEducationType().getId())
+                && StringUtils.equalsIgnoreCase(testParticipant1.getGender(), testParticipant2.getGender())
+                && StringUtils.equalsIgnoreCase(testParticipant1.getOccupation(), testParticipant2.getOccupation())
+                && Objects.equals(testParticipant1.getProductExperienceMonths(),
+                        testParticipant2.getProductExperienceMonths())
+                && Objects.equals(testParticipant1.getProfessionalExperienceMonths(),
+                        testParticipant2.getProfessionalExperienceMonths())) {
+            result = true;
+        }
+        return result;
+    }
+
+    @Component
+    @NoArgsConstructor
+    @Log4j2(topic = "updateParticipantsJobLogger")
+    private static class ProcessedListingUploadDao extends BaseDAOImpl {
+        private static final String GET_PROCESSED_ENTITY_HQL_BEGIN = "SELECT ul "
+                + "FROM ListingUploadEntity ul "
+                + "LEFT JOIN FETCH ul.certificationBody acb "
+                + "LEFT JOIN FETCH acb.address "
+                + "WHERE ul.certifiedProductId IS NOT NULL ";
+
+        public ListingUpload getByConfirmedCertifiedProductIdIncludingRecords(Long certifiedProductId) {
+            Query query = entityManager.createQuery(GET_PROCESSED_ENTITY_HQL_BEGIN
+                    + " AND ul.certifiedProductId = :certifiedProductId ", ListingUploadEntity.class);
+            query.setParameter("certifiedProductId", certifiedProductId);
+            List<ListingUploadEntity> entities = query.getResultList();
+            if (entities == null || entities.size() == 0) {
+                LOGGER.error("No processed uploaded listing found with certified product ID " + certifiedProductId);
+                return null;
+            }
+            return entities.get(0).toDomainWithRecords();
+        }
+    }
+
+    @Component
+    @NoArgsConstructor
+    @Log4j2(topic = "updateParticipantsJobLogger")
+    private static class ProcessedListingUploadManager {
+
+        private ListingDetailsNormalizer listingNormalizer;
+        private ListingUploadHandlerUtil uploadUtil;
+        private ProcessedListingUploadDao processedListingUploadDao;
+        //use this validator instead of the ListingUploadValidator because
+        //otherwise we get an error about "existing listing with this CHPL Product Number"
+        private Edition2015ListingValidator listingValidator;
+        private ListingDetailsUploadHandler listingDetailsHandler;
+
+        @Autowired
+        ProcessedListingUploadManager(ListingDetailsNormalizer listingNormalizer,
+                Edition2015ListingValidator listingValidator,
+                ListingDetailsUploadHandler listingDetailsHandler,
+                ProcessedListingUploadDao processedListingUploadDao,
+                ListingUploadHandlerUtil uploadUtil) {
+            this.listingNormalizer = listingNormalizer;
+            this.listingValidator = listingValidator;
+            this.listingDetailsHandler = listingDetailsHandler;
+            this.processedListingUploadDao = processedListingUploadDao;
+            this.uploadUtil = uploadUtil;
+        }
+
+        @Transactional
+        public CertifiedProductSearchDetails getUploadedDetailsByConfirmedCertifiedProductId(Long id) {
+            ListingUpload listingUpload = processedListingUploadDao.getByConfirmedCertifiedProductIdIncludingRecords(id);
+            LOGGER.info("Got listing upload with ID " + id);
+            List<CSVRecord> allCsvRecords = listingUpload.getRecords();
+            if (allCsvRecords == null) {
+                LOGGER.debug("Listing upload with ID " + id + " has no CSV records associated with it.");
+                return null;
+            }
+            LOGGER.trace("Listing upload with ID " + id + " has " + allCsvRecords.size() + " CSV records associated with it.");
+            int headingRowIndex = uploadUtil.getHeadingRecordIndex(allCsvRecords);
+            CSVRecord headingRecord = uploadUtil.getHeadingRecord(allCsvRecords);
+            List<CSVRecord> allListingRecords = allCsvRecords.subList(headingRowIndex + 1, allCsvRecords.size());
+            LOGGER.trace("Converting listing upload with ID " + id + " into CertifiedProductSearchDetails object");
+            CertifiedProductSearchDetails listing =
+                    listingDetailsHandler.parseAsListing(headingRecord, allListingRecords);
+            LOGGER.trace("Converted listing upload with ID " + id + " into CertifiedProductSearchDetails object");
+            listingNormalizer.normalize(listing);
+            listing.setId(id);
+            LOGGER.trace("Normalized listing upload with ID " + id);
+            listingValidator.validate(listing);
+            LOGGER.trace("Validated listing upload with ID " + id);
+            listing.setId(listingUpload.getCertifiedProductId());
+            return listing;
+        }
+    }
+}
---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/sed/UpdateSedFriendlyIdsJob.java
@@ -0,0 +1,196 @@
+package gov.healthit.chpl.scheduler.job.onetime.sed;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import gov.healthit.chpl.certificationCriteria.CertificationCriterion;
+import gov.healthit.chpl.certifiedproduct.CertifiedProductDetailsManager;
+import gov.healthit.chpl.domain.CertifiedProductSearchDetails;
+import gov.healthit.chpl.search.ListingSearchService;
+import gov.healthit.chpl.search.domain.ListingSearchResult;
+import gov.healthit.chpl.search.domain.SearchRequest;
+import gov.healthit.chpl.search.domain.SearchSetOperator;
+import gov.healthit.chpl.service.CertificationCriterionService;
+import gov.healthit.chpl.service.CertificationCriterionService.Criteria2015;
+import gov.healthit.chpl.sharedstore.listing.SharedListingStoreProvider;
+import gov.healthit.chpl.validation.listing.Edition2015ListingValidator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@DisallowConcurrentExecution
+@Log4j2(topic = "updatedSedFriendlyIdsJobLogger")
+public class UpdateSedFriendlyIdsJob implements Job {
+    private static final String FAILURE_TO_UPDATE_MSG = "Listing %s participants could not be updated by any strategy.";
+    private static final long FIRST_LISTING_ID_CONFIRMED_WITH_FLEXIBLE_UPLOAD = 10912;
+
+    @Autowired
+    private ReprocessFromUploadedCsvStrategy reprocessFromUploadStrategy;
+
+    @Autowired
+    private ListingSearchService listingSearchService;
+
+    @Autowired
+    private CertificationCriterionService criteriaService;
+
+    @Autowired
+    private CertifiedProductDetailsManager certifiedProductDetailsManager;
+
+    @Autowired
+    private Edition2015ListingValidator listingValidator;
+
+    @Autowired
+    private SharedListingStoreProvider sharedStoreProvider;
+
+    @Autowired
+    private ParticipantFriendlyIdReplacementDao participantReplacementDao;
+
+    private CertificationCriterion g3 = null;
+
+    @Override
+    public void execute(JobExecutionContext jobContext) throws JobExecutionException {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+        LOGGER.info("********* Starting the Update Participants job. *********");
+        try {
+            g3 = criteriaService.get(Criteria2015.G_3);
+
+            List<ListingSearchResult> activeListingsWithG3ConfirmedWithFlexibleUpload = listingSearchService.getAllPagesOfSearchResults(SearchRequest.builder()
+                    .certificationCriteriaIds(Stream.of(g3.getId()).collect(Collectors.toSet()))
+                    .certificationCriteriaOperator(SearchSetOperator.AND)
+                    .build()).stream()
+                    .filter(listingSearchResult -> listingSearchResult.getId() >= FIRST_LISTING_ID_CONFIRMED_WITH_FLEXIBLE_UPLOAD)
+                    .toList();
+
+            LOGGER.info("Found " + activeListingsWithG3ConfirmedWithFlexibleUpload.size() + " listing uploads attesting to 170.315 (g)(3).");
+
+            //TODO: Create "before" CSV with listing ID, developer, product, version, acb, status, # test tasks, # test participants without friendly ID
+
+            activeListingsWithG3ConfirmedWithFlexibleUpload.stream()
+                .forEach(listing -> attemptToSaveFriendlyIds(listing));
+
+            //TODO: Create "after" CSV with listing ID, developer, product, version, acb, status, # test tasks, # test participants without friendly ID
+
+        } catch (Exception ex) {
+            LOGGER.fatal("Unexpected exception was caught. All listings may not have been processed.", ex);
+        }
+        LOGGER.info("********* Completed the Update Participants job. *********");
+    }
+
+    private void attemptToSaveFriendlyIds(ListingSearchResult listing) {
+        LOGGER.info("Processing listing ID " + listing.getId());
+        CertifiedProductSearchDetails listingDetails = getCertifiedProductSearchDetails(listing.getId());
+        LOGGER.info("Updating test task friendly IDs for listing ID " + listing.getId());
+        ListingUpdateResult listingUpdateResult = updateTaskFriendlyIds(listingDetails);
+        listingDetails = getCertifiedProductSearchDetails(listingDetails.getId());
+        LOGGER.info("Updating test participant friendly IDs for listing ID " + listing.getId());
+        listingUpdateResult = updateParticipantFriendlyIds(listingDetails);
+
+        if (listingUpdateResult.getUpdatedListing() != null) {
+            listingValidator.validate(listingUpdateResult.getOriginalListing(), listingUpdateResult.getUpdatedListing());
+            if (hasSedErrors(listingUpdateResult.getUpdatedListing())) {
+                LOGGER.warn("\tThe updated listing has SED-related errors or warnings so we will not save SED changes: ");
+                printListingErrorsAndWarnings(listingUpdateResult.getUpdatedListing());
+                LOGGER.error(String.format(FAILURE_TO_UPDATE_MSG, listingUpdateResult.getOriginalListing().getId()));
+            } else {
+                //this replacement should execute as a single transaction in case any part of it fails
+                try {
+                    participantReplacementDao.replaceParticipantFriendlyIds(listingUpdateResult.getUpdatedListing());
+                } catch (Exception ex) {
+                    LOGGER.error("Error replacing participants for listing " + listingUpdateResult.getUpdatedListing().getId(), ex);
+                    LOGGER.error(String.format(FAILURE_TO_UPDATE_MSG, listingUpdateResult.getOriginalListing().getId()));
+                } finally {
+                    sharedStoreProvider.remove(listingUpdateResult.getUpdatedListing().getId());
+                }
+            }
+        } else {
+            LOGGER.error(String.format(FAILURE_TO_UPDATE_MSG, listingUpdateResult.getOriginalListing().getId()));
+        }
+    }
+
+    private ListingUpdateResult updateTaskFriendlyIds(CertifiedProductSearchDetails listing) {
+        ListingUpdateResult result = new ListingUpdateResult();
+        result.setOriginalListing(getCertifiedProductSearchDetails(listing.getId()));
+        if (reprocessFromUploadStrategy.updateTasks(listing)) {
+            result.setUpdatedListing(listing);
+            LOGGER.info("\tTasks for listing " + listing.getId() + " were updated.");
+        }
+        return result;
+    }
+
+    private ListingUpdateResult updateParticipantFriendlyIds(CertifiedProductSearchDetails listing) {
+        ListingUpdateResult result = new ListingUpdateResult();
+        //get the details again for the "original" listing because the strategies will change the listing reference
+        result.setOriginalListing(getCertifiedProductSearchDetails(listing.getId()));
+        if (reprocessFromUploadStrategy.updateParticipants(listing)) {
+            result.setUpdatedListing(listing);
+            LOGGER.info("\tParticipants for listing " + listing.getId() + " were updated.");
+        }
+        return result;
+    }
+
+    private boolean hasSedErrors(CertifiedProductSearchDetails listing) {
+        //there are probably errors and warnings that weren't here originally -
+        //some criteria have been removed, we changed measure parsing, etc
+        return !CollectionUtils.isEmpty(listing.getErrorMessages().castToCollection())
+                && isAnyMessageAboutSed(listing.getErrorMessages().castToCollection());
+    }
+
+    private boolean isAnyMessageAboutSed(Collection<String> messages) {
+        return messages.stream()
+            .map(msg -> msg.toUpperCase())
+            .filter(upperCaseMsg -> upperCaseMsg.contains("SED")
+                    || upperCaseMsg.contains("TASK")
+                    || upperCaseMsg.contains("PARTICIPANT"))
+            .count() > 0;
+    }
+
+    private void printListingErrorsAndWarnings(CertifiedProductSearchDetails listing) {
+        LOGGER.info("\tErrors for listing: " + listing.getId());
+        if (CollectionUtils.isEmpty(listing.getErrorMessages().castToCollection())) {
+            LOGGER.info("\t0 errors.");
+        } else {
+            listing.getErrorMessages().stream()
+                .forEach(msg -> LOGGER.info("\t" + msg));
+        }
+
+        LOGGER.info("\tWarnings for listing: " + listing.getId());
+        if (CollectionUtils.isEmpty(listing.getWarningMessages().castToCollection())) {
+            LOGGER.info("\t0 warnings.");
+        } else {
+            listing.getWarningMessages().stream()
+                .forEach(msg -> LOGGER.info("\t" + msg));
+        }
+    }
+
+    private CertifiedProductSearchDetails getCertifiedProductSearchDetails(Long id) {
+        CertifiedProductSearchDetails cp = null;
+        try {
+            cp = certifiedProductDetailsManager.getCertifiedProductDetails(id);
+            LOGGER.debug("Completed retrieval of listing [" + cp.getChplProductNumber() + "]");
+        } catch (Exception e) {
+            LOGGER.error("Could not retrieve listing [" + id + "] - " + e.getMessage(), e);
+        }
+        return cp;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Data
+    private static final class ListingUpdateResult {
+        private CertifiedProductSearchDetails originalListing;
+        private CertifiedProductSearchDetails updatedListing;
+    }
+}
