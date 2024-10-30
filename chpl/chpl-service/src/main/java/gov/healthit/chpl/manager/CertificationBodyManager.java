204359771ba849c2ed5df92053036651bc7740f3
Todd Young <tyoung@ainq.com>
2024-10-30 10:45:33 -0400

    feat!: Update functionality for SSO flag
    OCD-4660

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/CertificationBodyManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/manager/CertificationBodyManager.java
@@ -17,14 +17,18 @@ import gov.healthit.chpl.caching.ListingSearchCacheRefresh;
 import gov.healthit.chpl.dao.CertificationBodyDAO;
 import gov.healthit.chpl.domain.CertificationBody;
 import gov.healthit.chpl.domain.activity.ActivityConcept;
+import gov.healthit.chpl.domain.auth.User;
 import gov.healthit.chpl.exception.ActivityException;
 import gov.healthit.chpl.exception.EmailNotSentException;
 import gov.healthit.chpl.exception.EntityCreationException;
 import gov.healthit.chpl.exception.EntityRetrievalException;
+import gov.healthit.chpl.exception.InvalidArgumentsException;
 import gov.healthit.chpl.exception.ValidationException;
 import gov.healthit.chpl.manager.impl.SecuredManager;
+import gov.healthit.chpl.permissions.ResourcePermissionsFactory;
 import gov.healthit.chpl.sharedstore.listing.ListingStoreRemove;
 import gov.healthit.chpl.sharedstore.listing.RemoveBy;
+import gov.healthit.chpl.util.AuthUtil;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -33,13 +37,16 @@ public class CertificationBodyManager extends SecuredManager {
     private CertificationBodyDAO certificationBodyDao;
     private ActivityManager activityManager;
     private SchedulerManager schedulerManager;
+    private ResourcePermissionsFactory resourcePermissionsFactory;
 
     @Autowired
     public CertificationBodyManager(CertificationBodyDAO certificationBodyDao,
-            ActivityManager activityManager, @Lazy SchedulerManager schedulerManager) {
+            ActivityManager activityManager, @Lazy SchedulerManager schedulerManager,
+            ResourcePermissionsFactory resourcePermissionsFactory) {
         this.certificationBodyDao = certificationBodyDao;
         this.activityManager = activityManager;
         this.schedulerManager = schedulerManager;
+        this.resourcePermissionsFactory = resourcePermissionsFactory;
     }
 
     @Transactional
@@ -89,19 +96,44 @@ public class CertificationBodyManager extends SecuredManager {
     @ListingStoreRemove(removeBy = RemoveBy.ACB_ID, id = "#acb.id")
     @ListingSearchCacheRefresh
     // no other caches have ACB data so we do not need to clear all
-    public CertificationBody update(CertificationBody acb) throws EntityRetrievalException, SchedulerException, ValidationException, ActivityException {
+    public CertificationBody update(CertificationBody acbToUpdate) throws EntityRetrievalException, SchedulerException, ValidationException, ActivityException, InvalidArgumentsException {
+        // Get the ACB as it is currently in the database to find out if
+        // the retired flag was changed.
+        // Retirement and un-retirement is done as a separate manager action
+        // because security is different from normal ACB updates - only admins are
+        // allowed whereas an ACB admin can update other info
 
-        CertificationBody result = null;
-        CertificationBody toUpdate = certificationBodyDao.getById(acb.getId());
-        result = certificationBodyDao.update(acb);
-        if (!StringUtils.equals(acb.getName(), toUpdate.getName())) {
-            schedulerManager.changeAcbName(toUpdate.getName(), acb.getName());
+        CertificationBody existingAcb = resourcePermissionsFactory.get().getAcbIfPermissionById(acbToUpdate.getId());
+        if (acbToUpdate.isRetired()) {
+            // we are retiring this ACB - no other updates can happen
+            existingAcb.setRetirementDay(acbToUpdate.getRetirementDay());
+            existingAcb.setRetired(true);
+            retire(existingAcb);
+        } else {
+            if (existingAcb.isRetired()) {
+                // unretire the ACB
+                unretire(acbToUpdate.getId());
+            }
+
+            if (StringUtils.isEmpty(acbToUpdate.getWebsite())) {
+                throw new InvalidArgumentsException("A website is required to update the certification body");
+            }
+            if (acbToUpdate.getAddress() == null) {
+                throw new InvalidArgumentsException("An address is required to update the certification body");
+            }
+            CertificationBody result = null;
+            CertificationBody toUpdate = certificationBodyDao.getById(acbToUpdate.getId());
+            result = certificationBodyDao.update(acbToUpdate);
+            if (!StringUtils.equals(acbToUpdate.getName(), toUpdate.getName())) {
+                schedulerManager.changeAcbName(toUpdate.getName(), acbToUpdate.getName());
+            }
+
+            String activityMsg = "Updated acb " + acbToUpdate.getName();
+            activityManager.addActivity(ActivityConcept.CERTIFICATION_BODY, result.getId(), activityMsg,
+                    toUpdate, result);
+
         }
-
-        String activityMsg = "Updated acb " + acb.getName();
-        activityManager.addActivity(ActivityConcept.CERTIFICATION_BODY, result.getId(), activityMsg,
-                toUpdate, result);
-        return result;
+        return getById(acbToUpdate.getId());
     }
 
     @Transactional
@@ -157,6 +189,15 @@ public class CertificationBodyManager extends SecuredManager {
     }
 
     @Transactional(readOnly = true)
+    public List<CertificationBody> getAllEditable() {
+        if (AuthUtil.getCurrentUser() != null) {
+            return resourcePermissionsFactory.get().getAllAcbsForCurrentUser();
+        } else {
+            return List.of();
+        }
+    }
+
+    @Transactional(readOnly = true)
     public List<CertificationBody> getAllActive() {
         return certificationBodyDao.findAllActive();
     }
@@ -165,6 +206,19 @@ public class CertificationBodyManager extends SecuredManager {
     public CertificationBody getById(Long id) throws EntityRetrievalException {
         return certificationBodyDao.getById(id);
     }
+
+    @Transactional(readOnly = true)
+    @PreAuthorize("@permissions.hasAccess(T(gov.healthit.chpl.permissions.Permissions).CERTIFICATION_BODY, "
+            + "T(gov.healthit.chpl.permissions.domains.CertificationBodyDomainPermissions).GET_USERS, #acbId)")
+    public List<User> getUsers(Long acbId) throws InvalidArgumentsException, EntityRetrievalException {
+        CertificationBody acb = resourcePermissionsFactory.get().getAcbIfPermissionById(acbId);
+        if (acb == null) {
+            throw new InvalidArgumentsException("Could not find the ACB specified.");
+        }
+
+        return resourcePermissionsFactory.get().getAllUsersOnAcb(acb);
+    }
+
 
     public void setCertificationBodyDAO(final CertificationBodyDAO acbDAO) {
         this.certificationBodyDao = acbDAO;
