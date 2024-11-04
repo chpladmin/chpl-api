bf15439fa678b866e13c7b0ff1462d1fc5747acc
Katy Ekey <katy.ekey@gmail.com>
2024-11-04 14:54:47 -0500

    feat: Allow email address to be switched based on flag
    [#OCD-4730]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/GraphConfiguration.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/GraphConfiguration.java
@@ -1,8 +1,11 @@
 package gov.healthit.chpl;
 
+import org.ff4j.FF4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 
 import com.azure.identity.ClientSecretCredential;
@@ -19,21 +22,58 @@ public class GraphConfiguration {
     @Autowired
     private Environment env;
 
+    @Autowired
+    private FF4j ff4j;
+
+    //When the feature ONC_TO_ASTP_EMAIL is removed the @Scope annotation is no longer needed.
+    //It is here so that each time the GraphServiceClient bean is requested the flag will be checked
+    //to get the correct email configuration. When that no longer needs to be able to be switched between
+    //ONC and ASTP then we can remove the Prototype Scope.
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     @Bean
     public GraphServiceClient getGraphServiceClient() {
         ClientSecretCredential clientSecretCredential = null;
         GraphServiceClient graphServiceClient = null;
 
-        LOGGER.debug("Creating a new ClientSecretCredentialBuilder");
+        LOGGER.info("Creating a new ClientSecretCredentialBuilder");
 
         clientSecretCredential = new ClientSecretCredentialBuilder()
-                .clientId(env.getProperty("azure.clientId"))
-                .tenantId(env.getProperty("azure.tenantId"))
-                .clientSecret(env.getProperty("azure.clientSecret"))
+                .clientId(getClientId())
+                .tenantId(getTenantId())
+                .clientSecret(getClientSecret())
                 .build();
 
         graphServiceClient = new GraphServiceClient(clientSecretCredential, GRAPH_DEFAULT_SCOPE);
-
         return graphServiceClient;
+    }
+
+    private String getClientId() {
+        if (ff4j.check(FeatureList.ONC_TO_ASTP_EMAIL)) {
+            LOGGER.info("Getting ASTP client ID");
+            return env.getProperty("azure.clientId");
+        } else {
+            LOGGER.info("Getting ONC client ID");
+            return env.getProperty("azure.clientId.onc");
+        }
+    }
+
+    private String getTenantId() {
+        if (ff4j.check(FeatureList.ONC_TO_ASTP_EMAIL)) {
+            LOGGER.info("Getting ASTP tenant ID");
+            return env.getProperty("azure.tenantId");
+        } else {
+            LOGGER.info("Getting ONC tenant ID");
+            return env.getProperty("azure.tenantId.onc");
+        }
+    }
+
+    private String getClientSecret() {
+        if (ff4j.check(FeatureList.ONC_TO_ASTP_EMAIL)) {
+            LOGGER.info("Getting ASTP client secret");
+            return env.getProperty("azure.clientSecret");
+        } else {
+            LOGGER.info("Getting ONC client secret");
+            return env.getProperty("azure.clientSecret.onc");
+        }
     }
 }
