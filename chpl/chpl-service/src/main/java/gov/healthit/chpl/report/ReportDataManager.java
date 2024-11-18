1b7f43abb0c0a6c8e1b5a62e2a9168bd5be26532
Todd Young <tyoung@ainq.com>
2024-11-18 10:32:15 -0500

    feat: synchronize calls to the ReportDataManager class
    OCD-4688

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/report/ReportDataManager.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/report/ReportDataManager.java
@@ -19,11 +19,13 @@ import gov.healthit.chpl.report.surveillance.SurveillanceActivityCounts;
 import gov.healthit.chpl.report.surveillance.SurveillanceReportsService;
 import gov.healthit.chpl.scheduler.job.summarystatistics.data.CertificationBodyStatistic;
 import gov.healthit.chpl.search.domain.ListingSearchResult;
+import lombok.Synchronized;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class ReportDataManager {
+    private final Object lock = new Object();
 
     private CriteriaMigrationReportService criteriaMigrationReportService;
     private SurveillanceReportsService surveillanceReportsService;
@@ -39,82 +41,102 @@ public class ReportDataManager {
         this.testToolReportService = testToolReportService;
     }
 
+    @Synchronized("lock")
     public List<CriteriaMigrationReportDenormalized> getHti1CriteriaMigrationReport() {
         return criteriaMigrationReportService.getHtiReportData(CriteriaMigrationReportService.HTI1_REPORT_ID);
     }
 
+    @Synchronized("lock")
     public SurveillanceActivityCounts getSurveillanceActivityCounts() {
         return surveillanceReportsService.getSurveiilanceActivityCounts();
     }
 
+    @Synchronized("lock")
     public List<CertificationBodyStatistic> getOpenSurveillanceActivityCountsByAcb() {
         return surveillanceReportsService.getOpenSurveillanceActivityCountsByAcb();
     }
 
+    @Synchronized("lock")
     public List<ListingSearchResult> getListingsWithOpenSurveillance() {
         return surveillanceReportsService.getListingsWithOpenSurveillance();
     }
 
+    @Synchronized("lock")
     public NonconformityCounts getNonconformityCounts() {
         return surveillanceReportsService.getNonconformityCounts();
     }
 
+    @Synchronized("lock")
     public List<CertificationBodyStatistic> getOpenNonconformityCountsByAcb() {
         return surveillanceReportsService.getOpenNonconformityCountsByAcb();
     }
 
+    @Synchronized("lock")
     public List<ListingSearchResult> getListingsWithOpenNonconformity() {
         return surveillanceReportsService.getListingsWithOpenNonconformity();
     }
 
+    @Synchronized("lock")
     public List<CertificationBodyStatistic> getOpenCapCountsByAcb() {
         return surveillanceReportsService.getOpenCapCountsByAcb();
     }
 
+    @Synchronized("lock")
     public List<CertificationBodyStatistic> getClosedCapCountsByAcb() {
         return surveillanceReportsService.getClosedCapCountsByAcb();
     }
 
+    @Synchronized("lock")
     public List<ListingSearchResult> getListingsWithOpenCap() {
         return surveillanceReportsService.getListingsWithOpenCap();
     }
 
+    @Synchronized("lock")
     public List<ListingSearchResult> getListingsWithClosedCap() {
         return surveillanceReportsService.getListingsWithClosedCap();
     }
 
+    @Synchronized("lock")
     public CapCounts getCapCounts() {
         return surveillanceReportsService.getCapCounts();
     }
 
+    @Synchronized("lock")
     public UniqueDeveloperCount getUniqueDeveloperCount() {
         return developerReportsService.getUniqueDeveloperCount();
     }
 
+    @Synchronized("lock")
     public List<CertificationBodyStatistic> getDeveloperCountsWithActiveListingsByAcb() {
         return developerReportsService.getDeveloperCountsWithActiveListingsByAcb();
     }
 
+    @Synchronized("lock")
     public List<CertificationBodyStatistic> getDeveloperCountsWithWithdrawnListingsByAcb() {
         return developerReportsService.getDeveloperCountsWithWithdrawnListingsByAcb();
     }
 
+    @Synchronized("lock")
     public List<DeveloperSearchResult> getDevelopersWithWithdrawnListingsByAcb() {
         return developerReportsService.getDevelopersWithWithdrawnListingsByAcb();
     }
 
+    @Synchronized("lock")
     public List<CertificationBodyStatistic> getDeveloperCountsWithSuspendedListingsByAcb() {
         return developerReportsService.getDeveloperCountsWithSuspendedListingsByAcb();
     }
 
+    @Synchronized("lock")
     public List<DeveloperSearchResult> getDevelopersWithSuspendedListingsByAcb() {
         return developerReportsService.getDevelopersWithSuspendedListingsByAcb();
     }
 
+    @Synchronized("lock")
     public List<TestToolReport> getTestToolReports() {
         return testToolReportService.getTestToolReports();
     }
 
+    @Synchronized("lock")
     public List<TestToolListingReport> getTestToolListingReports() {
         return testToolReportService.getTestToolListingReports();
     }
