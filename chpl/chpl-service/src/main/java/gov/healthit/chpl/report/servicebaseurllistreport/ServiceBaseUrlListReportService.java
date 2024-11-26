33425b77715b1f2d601fd3719b213e37c5d42863
Todd Young <tyoung@ainq.com>
2024-11-26 14:18:02 -0500

    refactor: use developer search to get associated ACBs
    OCD-4686

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/report/servicebaseurllistreport/ServiceBaseUrlListReportService.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/report/servicebaseurllistreport/ServiceBaseUrlListReportService.java
@@ -2,34 +2,31 @@ package gov.healthit.chpl.report.servicebaseurllistreport;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import gov.healthit.chpl.dao.CertificationBodyDAO;
-import gov.healthit.chpl.dao.DeveloperDAO;
-import gov.healthit.chpl.domain.CertificationBody;
+import gov.healthit.chpl.developer.search.DeveloperSearchRequest;
+import gov.healthit.chpl.developer.search.DeveloperSearchService;
+import gov.healthit.chpl.domain.Developer;
 import gov.healthit.chpl.domain.IdNamePair;
 import gov.healthit.chpl.scheduler.job.urluptime.UrlUptimeMonitorDAO;
 import gov.healthit.chpl.scheduler.job.urluptime.UrlUptimeMonitorTestDAO;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Component
 public class ServiceBaseUrlListReportService {
     private UrlUptimeMonitorDAO urlUptimeMonitorDAO;
     private UrlUptimeMonitorTestDAO urlUptimeMonitorTestDAO;
-    private DeveloperDAO developerDAO;
-    private CertificationBodyDAO certificationBodyDAO;
+    private DeveloperSearchService developerSearchService;
 
     @Autowired
-    public ServiceBaseUrlListReportService(UrlUptimeMonitorDAO urlUptimeMonitorDAO, UrlUptimeMonitorTestDAO urlUptimeMonitorTestDAO, DeveloperDAO developerDAO,
-            CertificationBodyDAO certificationBodyDAO) {
+    public ServiceBaseUrlListReportService(UrlUptimeMonitorDAO urlUptimeMonitorDAO, UrlUptimeMonitorTestDAO urlUptimeMonitorTestDAO, DeveloperSearchService developerSearchService) {
         this.urlUptimeMonitorDAO = urlUptimeMonitorDAO;
         this.urlUptimeMonitorTestDAO = urlUptimeMonitorTestDAO;
-        this.developerDAO = developerDAO;
-        this.certificationBodyDAO = certificationBodyDAO;
+        this.developerSearchService = developerSearchService;
     }
 
     public List<UrlUptimeMonitorEx> getUrlUptimeMonitors() {
@@ -43,33 +40,25 @@ public class ServiceBaseUrlListReportService {
                 .toList();
 
 
-        Map<Long, Set<CertificationBody>> developerAcbMaps = developerDAO.findAllDevelopersWithAcbs().entrySet().stream()
-                .collect(Collectors.toMap(o -> o.getKey().getId(), o -> o.getValue()));
-        List<CertificationBody> activeAcbs = certificationBodyDAO.findAllActive();
-
         urlUptimeMonitors.forEach(monitor -> {
             monitor.setTests(urlUptimeMonitorTestDAO.getChplUptimeMonitorTests(monitor.getId()).stream()
                     .filter(test -> test.getCheckTime().isAfter(LocalDateTime.now().minusYears(1)))
                     .toList());
 
-            if (developerAcbMaps.containsKey(monitor.getDeveloper().getId())) {
-                monitor.setAcbs(developerAcbMaps.get(monitor.getDeveloper().getId()).stream()
-                        .filter(acb -> isAcbInActiveList(acb.getId(), activeAcbs))
-                        .map(acb -> IdNamePair.builder()
-                                .id(acb.getId())
-                                .name(acb.getName())
-                                .build())
-                        .toList());
-            }
+            monitor.setAcbs(List.copyOf(getAssocatedAcbs(monitor.getDeveloper())));
         });
 
         return urlUptimeMonitors;
     }
 
-    private Boolean isAcbInActiveList(Long acbId, List<CertificationBody> activeAcbs) {
-        return activeAcbs.stream()
-                .filter(acb -> acb.getId().equals(acbId))
+    private Set<IdNamePair> getAssocatedAcbs(Developer developer) {
+        DeveloperSearchRequest request = DeveloperSearchRequest.builder()
+                .developerIds(Set.of(developer.getId()))
+                .build();
+
+        return developerSearchService.getAllPagesOfSearchResults(request, LOGGER).stream()
                 .findFirst()
-                .isPresent();
+                .map(res -> res.getAcbsForActiveListings())
+                .orElse(Set.of());
     }
 }
