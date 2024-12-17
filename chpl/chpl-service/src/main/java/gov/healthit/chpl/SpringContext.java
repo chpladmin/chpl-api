33f8e3f587a2bc54d237bbc6ba3917d017994f09
Todd Young <tyoung@ainq.com>
2024-12-17 14:39:12 -0500

    refactor: remove extraneous code
    OCD-4777

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/SpringContext.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/SpringContext.java
@@ -1,21 +1,15 @@
 package gov.healthit.chpl;
 
-import org.quartz.spi.TriggerFiredBundle;
-import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 import org.springframework.stereotype.Component;
 
-import gov.healthit.chpl.scheduler.JobAspect;
-
 @Component
 public class SpringContext extends SpringBeanJobFactory implements ApplicationContextAware {
 
     private static ApplicationContext context;
-    private AutowireCapableBeanFactory beanFactory;
 
     /**
      * Returns the Spring managed bean instance of the given class type if it exists. Returns null otherwise.
@@ -33,16 +27,4 @@ public class SpringContext extends SpringBeanJobFactory implements ApplicationCo
         // store ApplicationContext reference to access required beans later on
         SpringContext.context = appContext;
     }
-
-    @Override
-    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
-        final Object job = super.createJobInstance(bundle);
-        beanFactory.autowireBean(job);
-
-        AspectJProxyFactory pFactory = new AspectJProxyFactory(job);
-        pFactory.addAspect(new JobAspect());
-
-        return pFactory.getProxy();
-    }
-
 }
