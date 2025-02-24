c68bdfd64b94d821c55fb2046b6ebbe1b4c03235
Todd Young <tyoung@ainq.com>
2025-02-24 14:13:31 -0500

    refactor: not require security when updating job using this job
    OCD-4831

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/jobedit/FixJobDataJob.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/scheduler/job/onetime/jobedit/FixJobDataJob.java
@@ -1,23 +1,33 @@
 package gov.healthit.chpl.scheduler.job.onetime.jobedit;
 
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.JobKey.jobKey;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.quartz.Job;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import gov.healthit.chpl.domain.schedule.ChplJob;
 import gov.healthit.chpl.manager.SchedulerManager;
+import gov.healthit.chpl.scheduler.ChplSchedulerReference;
 import gov.healthit.chpl.scheduler.SchedulerSecurityContextService;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2(topic = "fixJobDataJobLogger")
 public class FixJobDataJob implements Job {
+
+    @Autowired
+    private ChplSchedulerReference chplScheduler;
 
     @Autowired
     private SchedulerSecurityContextService securityContextService;
@@ -64,7 +74,7 @@ public class FixJobDataJob implements Job {
                                 LOGGER.info("Added chpl-developer");
                             }
                             job.getJobDataMap().put("authorities", String.join(";", authoritiesList));
-                            schedulerManager.updateJob(job);
+                            updateJob(job);
                         } catch (Exception e) {
                             LOGGER.error("Error occurred while fixing job data for job {}", job.getName(), e);
                         }
@@ -75,7 +85,24 @@ public class FixJobDataJob implements Job {
         } finally {
             LOGGER.info("********* Completed the Fix Job Data job. *********");
         }
-        
+
+    }
+
+    private ChplJob updateJob(ChplJob job) throws SchedulerException {
+        Scheduler scheduler = getScheduler();
+        JobKey jobId = jobKey(job.getName(), job.getGroup());
+        JobDetail oldJob = scheduler.getJobDetail(jobId);
+        JobDetail newJob = newJob(oldJob.getJobClass()).withIdentity(jobId).withDescription(oldJob.getDescription())
+                .usingJobData(job.getJobDataMap()).storeDurably(oldJob.isDurable())
+                .requestRecovery(oldJob.requestsRecovery()).build();
+
+        scheduler.addJob(newJob, true);
+        ChplJob newChplJob = new ChplJob(newJob);
+        return newChplJob;
+    }
+
+    private Scheduler getScheduler() throws SchedulerException {
+        return chplScheduler.getScheduler();
     }
 
 }
