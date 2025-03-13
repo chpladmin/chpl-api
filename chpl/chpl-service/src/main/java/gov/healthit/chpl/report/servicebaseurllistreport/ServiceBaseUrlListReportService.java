24cdd95db590183e4dd5b615cfdf0913a3b18ff2
Todd Young <tyoung@ainq.com>
2025-03-12 20:25:30 -0400

    feat: fix data for the SBUL Power BI report
    OCD-4565

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/report/servicebaseurllistreport/ServiceBaseUrlListReportService.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/report/servicebaseurllistreport/ServiceBaseUrlListReportService.java
@@ -1,19 +1,17 @@
 package gov.healthit.chpl.report.servicebaseurllistreport;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import gov.healthit.chpl.developer.search.DeveloperSearchRequest;
 import gov.healthit.chpl.developer.search.DeveloperSearchService;
-import gov.healthit.chpl.domain.Developer;
 import gov.healthit.chpl.domain.IdNamePair;
 import gov.healthit.chpl.exception.EntityRetrievalException;
 import gov.healthit.chpl.manager.CertificationBodyManager;
+import gov.healthit.chpl.scheduler.job.urluptime.UrlUptimeMonitor;
 import gov.healthit.chpl.scheduler.job.urluptime.UrlUptimeMonitorDAO;
 import gov.healthit.chpl.scheduler.job.urluptime.UrlUptimeMonitorTestDAO;
 import lombok.extern.log4j.Log4j2;
@@ -36,44 +34,32 @@ public class ServiceBaseUrlListReportService {
     }
 
     public List<UrlUptimeMonitorEx> getUrlUptimeMonitors() {
-        List<UrlUptimeMonitorEx> urlUptimeMonitors = (List<UrlUptimeMonitorEx>) urlUptimeMonitorDAO.getAll().stream()
+        return (List<UrlUptimeMonitorEx>) urlUptimeMonitorDAO.getAll().stream()
                 .map(monitor -> UrlUptimeMonitorEx.builder()
                         .id(monitor.getId())
                         .developer(monitor.getDeveloper())
                         .url(monitor.getUrl())
                         .datadogPublicId(monitor.getDatadogPublicId())
+                        .acbs(getAssocatedAcbs(monitor))
+                        .tests(urlUptimeMonitorTestDAO.getChplUptimeMonitorTests(monitor.getId()).stream()
+                                .filter(test -> test.getCheckTime().isAfter(LocalDateTime.now().minusYears(1)))
+                                .toList())
                         .build())
                 .toList();
-
-
-        urlUptimeMonitors.forEach(monitor -> {
-            monitor.setTests(urlUptimeMonitorTestDAO.getChplUptimeMonitorTests(monitor.getId()).stream()
-                    .filter(test -> test.getCheckTime().isAfter(LocalDateTime.now().minusYears(1)))
-                    .toList());
-        });
-
-        return urlUptimeMonitors;
     }
 
-    private Set<IdNamePair> getAssocatedAcbs(Developer developer) {
-        DeveloperSearchRequest request = DeveloperSearchRequest.builder()
-                .developerIds(Set.of(developer.getId()))
-                .build();
-
-        return developerSearchService.getAllPagesOfSearchResults(request, LOGGER).stream()
-                .findFirst()
-                .map(res -> res.getAcbsForActiveListings().stream()
-                        .filter(acb -> isAcbActive(acb.getId()))
-                        .collect(Collectors.toSet()))
-                .orElse(Set.of());
-    }
-
-    private Boolean isAcbActive(Long acbId) {
-        try {
-            return !certificationBodyManager.getById(acbId).isRetired();
-        } catch (EntityRetrievalException e) {
-            LOGGER.error("Could not determine is ACB {} is retired.", acbId);
-            return false;
-        }
+    private List<IdNamePair> getAssocatedAcbs(UrlUptimeMonitor monitor) {
+        return Arrays.asList(monitor.getDelimitedAcbIds().split(",")).stream()
+                .map(acbId -> Long.parseLong(acbId))
+                .map(acbId -> {
+                    try {
+                        return certificationBodyManager.getById(acbId);
+                    } catch (EntityRetrievalException e) {
+                        return null;
+                    }
+                })
+                .filter(acb -> acb != null && !acb.isRetired())
+                .map(acb -> new IdNamePair(acb.getId(), acb.getName()))
+                .toList();
     }
 }
