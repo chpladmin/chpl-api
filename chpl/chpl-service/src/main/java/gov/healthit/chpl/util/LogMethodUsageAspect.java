e53812d770d45c696ab4aa5576f63498baef088f
Katy Ekey <katy.ekey@gmail.com>
2025-04-21 15:00:09 -0400

    feat: Add logging annotation to report-data endpoints
    [#OCD-4807]

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/util/LogMethodUsageAspect.java
@@ -0,0 +1,27 @@
+package gov.healthit.chpl.util;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.log4j.Log4j2;
+
+@Component
+@Aspect
+@Log4j2
+public class LogMethodUsageAspect {
+
+    @Before("@annotation(LogMethodUsage)")
+    public void logMethodStart(JoinPoint joinPoint) {
+        System.out.println(joinPoint.getSignature().getDeclaringTypeName() + " started");
+        LOGGER.info(joinPoint.getSignature().getDeclaringTypeName() + " started");
+    }
+
+    @AfterReturning("@annotation(LogMethodUsage)")
+    public void logMethodCompletion(JoinPoint joinPoint) {
+        System.out.println(joinPoint.getSignature().getDeclaringTypeName() + " completed");
+        LOGGER.info(joinPoint.getSignature().getDeclaringTypeName() + " completed");
+    }
+}
