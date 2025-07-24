e2cfdb37d287f831d92abb940a555e580e827bca
Katy Ekey <katy.ekey@gmail.com>
2025-07-24 13:10:38 -0400

    feat!: Deprecate "gap" field of certification results
    [#OCD-4897]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/CertificationResult.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/domain/CertificationResult.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import gov.healthit.chpl.api.deprecatedUsage.DeprecatedResponseField;
 import gov.healthit.chpl.certificationCriteria.CertificationCriterion;
 import gov.healthit.chpl.codeset.CertificationResultCodeSet;
 import gov.healthit.chpl.codeset.CertificationResultCodeSetComparator;
@@ -59,10 +60,13 @@ public class CertificationResult implements Serializable {
     @JsonIgnore
     private String successStr;
 
+    @Deprecated
+    @DeprecatedResponseField(message = "This field will be removed.", removalDate = "2025-09-01")
     @Schema(description = "This variable indicates if the certification criteria was gap certified. "
             + "It is a binary variable that takes true or false value.")
     private Boolean gap;
 
+    @Deprecated
     @JsonIgnore
     private String gapStr;
 
