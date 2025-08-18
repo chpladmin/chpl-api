9d2c143f4609ca54f9a704dad672c7db13d0ad79
Katy Ekey <katy.ekey@gmail.com>
2025-08-18 12:41:12 -0400

    feat: Remove url type results with "0" count
    [#OCD-4515]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/report/questionableurl/QuestionableUrlReportDao.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/report/questionableurl/QuestionableUrlReportDao.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import gov.healthit.chpl.dao.impl.BaseDAOImpl;
-import gov.healthit.chpl.scheduler.job.urlStatus.data.UrlTypeEntity;
 import jakarta.persistence.Query;
 
 @Repository
@@ -13,36 +12,17 @@ public class QuestionableUrlReportDao extends BaseDAOImpl {
 
     public List<QuestionableUrlReport> getQuestionableUrlReports() {
         //add all url types to the response
-        String hql = "SELECT urlType FROM UrlTypeEntity urlType WHERE deleted = false";
-        Query query = entityManager.createQuery(hql);
-        List<UrlTypeEntity> allUrlTypes = query.getResultList();
-        List<QuestionableUrlReport> response = allUrlTypes.stream()
-                .map(urlType -> QuestionableUrlReport.builder()
-                        .urlType(urlType.getName())
-                        .count(0L)
-                        .build())
-                .toList();
-
-        //update the counts for url types that are nonzero
-        hql = "SELECT urlType, count(*) as urlTypeCount "
+        String hql = "SELECT urlType, count(*) as urlTypeCount "
                 + "FROM QuestionableUrlDetailEntity "
                 + "GROUP BY urlType";
-
-        query = entityManager.createQuery(hql);
+        Query query = entityManager.createQuery(hql);
         List<Object[]> results = query.getResultList();
-        results.stream()
-            .forEach(result -> updateCount(response, (String) result[0], (Long) result[1]));
-        return response;
-    }
-
-    private void updateCount(List<QuestionableUrlReport> reports, String urlTypeName, Long count) {
-        QuestionableUrlReport reportForUrlType = reports.stream()
-            .filter(report -> report.getUrlType().equals(urlTypeName))
-            .findAny()
-            .orElse(null);
-        if (reportForUrlType != null) {
-            reportForUrlType.setCount(count);
-        }
+        return results.stream()
+                .map(result -> QuestionableUrlReport.builder()
+                        .urlType((String) result[0])
+                        .count((Long) result[1])
+                        .build())
+                .toList();
     }
 
     public List<QuestionableUrlDetailReport> getQuestionableUrlDetails() {
