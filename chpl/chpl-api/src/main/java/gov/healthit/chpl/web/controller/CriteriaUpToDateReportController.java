de395cbbd34625f47d121d459fa4dda4164f96c0
Katy Ekey <katy.ekey@gmail.com>
2025-09-09 12:48:05 -0400

    fix: Use all ACBs for updated criteria reporting endpoints
    [#OCD-5017]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/CriteriaUpToDateReportController.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/web/controller/CriteriaUpToDateReportController.java
@@ -1,6 +1,7 @@
 package gov.healthit.chpl.web.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import gov.healthit.chpl.domain.CertificationBody;
+import gov.healthit.chpl.manager.CertificationBodyManager;
 import gov.healthit.chpl.report.ReportDataManager;
 import gov.healthit.chpl.report.criteriauptodate.CriteriaUpToDateReport;
 import gov.healthit.chpl.report.criteriauptodate.ListingNotUpToDateReport;
@@ -24,10 +27,13 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @RequestMapping("/report-data/criteria-up-to-date")
 public class CriteriaUpToDateReportController {
+    private CertificationBodyManager acbManager;
     private ReportDataManager reportDataManager;
 
     @Autowired
-    public CriteriaUpToDateReportController(ReportDataManager reportDataManager) {
+    public CriteriaUpToDateReportController(CertificationBodyManager acbManager,
+            ReportDataManager reportDataManager) {
+        this.acbManager = acbManager;
         this.reportDataManager = reportDataManager;
     }
 
@@ -38,7 +44,11 @@ public class CriteriaUpToDateReportController {
     @LogMethodUsage
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     public @ResponseBody List<CriteriaUpToDateReport> getCriteriaUpToDateReports() {
-        return reportDataManager.getCriteriaAttributeUpToDateService().getAllCriteriaUpToDateReports();
+        List<CertificationBody> allAcbs = acbManager.getAll();
+        List<Long> acbIds = allAcbs.stream()
+                    .map(acb -> acb.getId())
+                    .collect(Collectors.toList());
+        return reportDataManager.getCriteriaAttributeUpToDateService().getAllCriteriaUpToDateReports(acbIds);
     }
 
     @Operation(summary = "Retrieves the data used to generate the Criteria up-to-date counts monthly for the past year",
@@ -47,8 +57,11 @@ public class CriteriaUpToDateReportController {
             })
     @LogMethodUsage
     @RequestMapping(value = "/monthly", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public @ResponseBody List<CriteriaUpToDateReport> getMonthlyCriteriaUpToDateReports() {
-        return reportDataManager.getCriteriaAttributeUpToDateService().getMonthlyCriteriaUpToDateReports();
+    public @ResponseBody List<CriteriaUpToDateReport> getMonthlyCriteriaUpToDateReports() {        List<CertificationBody> allAcbs = acbManager.getAll();
+        List<Long> acbIds = allAcbs.stream()
+                .map(acb -> acb.getId())
+                .collect(Collectors.toList());
+        return reportDataManager.getCriteriaAttributeUpToDateService().getMonthlyCriteriaUpToDateReports(acbIds);
     }
 
     @Operation(summary = "Retrieves all listing and attested criteria combinations that were not up-to-date on the "
