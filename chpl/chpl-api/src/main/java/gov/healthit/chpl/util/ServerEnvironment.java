16e7540e0985f475d756b97899aeb16a94bfdadb
Katy Ekey <katy.ekey@gmail.com>
2026-02-05 12:11:47 -0500

    feat!: Create endpoint to kick off rwt results validation
    [#OCD-4757]

---
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/util/ServerEnvironment.java
@@ -0,0 +1,23 @@
+package gov.healthit.chpl.util;
+
+import java.util.stream.Stream;
+
+import lombok.Getter;
+
+public enum ServerEnvironment {
+    PRODUCITON("production"),
+    NON_PRODUCTION("non-production");
+
+    @Getter
+    private String name;
+    ServerEnvironment(String name) {
+        this.name = name;
+    }
+
+    public static ServerEnvironment getByName(String envName) {
+        return Stream.of(ServerEnvironment.values())
+                .filter(val -> val.getName().equalsIgnoreCase(envName))
+                .findAny()
+                .orElse(null);
+    }
+}
