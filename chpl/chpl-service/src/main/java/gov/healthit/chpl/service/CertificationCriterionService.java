6892a31a6aeaf65764bf5a0ca5c270f1f9f402fc
Todd Young <tyoung@ainq.com>
2026-07-23 11:31:11 -0400

    Revert "test: temporarily break compilation to verify CI gate"
    This reverts commit 1d610f9054f7743e99a0659a925ee289247b1543.

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/service/CertificationCriterionService.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/service/CertificationCriterionService.java
@@ -275,7 +275,7 @@ public class CertificationCriterionService {
     }
 
     public String coerceToCriterionNumberFormat(String input) {
-        String formatRegex = "^\\d{3}\\.\\d{3}\\s{1}\\([a-z]{1}\\)(\\([0-9]{1,2}\\))?$" // TEMP: intentionally broken, testing CI gate
+        String formatRegex = "^\\d{3}\\.\\d{3}\\s{1}\\([a-z]{1}\\)(\\([0-9]{1,2}\\))?$";
         if (input.matches(formatRegex)) {
             LOGGER.debug("\tMatches required format. Not changing input.");
             return input;
