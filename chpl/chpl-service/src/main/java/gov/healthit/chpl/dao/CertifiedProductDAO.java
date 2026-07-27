3dadb9319ae5ef132e7462378c67cd465dfe4aaf
Katy Ekey <katy.ekey@gmail.com>
2026-07-27 13:45:02 -0400

    flag-feat: Do not save RWT Plans on listing create, update
    [#OCD-5385]

--- chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/CertifiedProductDAO.java
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/dao/CertifiedProductDAO.java
@@ -80,8 +80,10 @@ public class CertifiedProductDAO extends BaseDAOImpl {
             entity.setIcs(listing.getIcs() == null || listing.getIcs().getInherits() == null ? Boolean.FALSE : listing.getIcs().getInherits());
             entity.setAccessibilityCertified(listing.getAccessibilityCertified());
             entity.setSvapNoticeUrl(listing.getSvapNoticeUrl());
-            entity.setRwtPlansUrl(listing.getRwtPlansUrl());
-            entity.setRwtPlansCheckDate(listing.getRwtPlansCheckDate());
+            if (!ff4j.check(FeatureList.HTI_5_ERD)) {
+                entity.setRwtPlansUrl(listing.getRwtPlansUrl());
+                entity.setRwtPlansCheckDate(listing.getRwtPlansCheckDate());
+            }
             entity.setRwtResultsUrl(listing.getRwtResultsUrl());
             entity.setRwtResultsCheckDate(listing.getRwtResultsCheckDate());
             entity.setChplProductNumber(null);
@@ -122,8 +124,10 @@ public class CertifiedProductDAO extends BaseDAOImpl {
         entity.setCertificationBodyId(dto.getCertificationBodyId());
         entity.setCertificationEditionId(dto.getCertificationEditionId());
         entity.setProductVersionId(dto.getProductVersionId());
-        entity.setRwtPlansUrl(dto.getRwtPlansUrl());
-        entity.setRwtPlansCheckDate(dto.getRwtPlansCheckDate());
+        if (!ff4j.check(FeatureList.HTI_5_ERD)) {
+            entity.setRwtPlansUrl(dto.getRwtPlansUrl());
+            entity.setRwtPlansCheckDate(dto.getRwtPlansCheckDate());
+        }
         entity.setRwtResultsUrl(dto.getRwtResultsUrl());
         entity.setRwtResultsCheckDate(dto.getRwtResultsCheckDate());
         entity.setSvapNoticeUrl(dto.getSvapNoticeUrl());
